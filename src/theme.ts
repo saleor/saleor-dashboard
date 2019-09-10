d762f29f75cd57a2483eac892187da953d45f3b8
Krzysztof Bialoglowicz <krzysztof@mirumee.com>
2019-09-10 15:47:20 +0200

    Fix snackbars shadows

--- src/theme.ts
+++ src/theme.ts
@@ -276,7 +276,9 @@ export default (colors: IThemeColors): Theme =>
         },
         root: {
           backgroundColor: colors.background.paper,
-          color: colors.font.default
+          color: colors.font.default,
+          boxShadow:
+            "0 6px 10px 0px rgba(0, 0, 0, 0.15), 0 1px 18px 0px rgba(0, 0, 0, 0.12), 0 3px 5px -1px rgba(0, 0, 0, 0.10)"
         }
       },
       MuiSwitch: {
