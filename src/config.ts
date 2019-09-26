5beddf73e2b8507c7e17374c4242517022742fdd
Krzysztof Bialoglowicz <krzysztof@mirumee.com>
2019-09-26 15:49:23 +0200

    Fix config

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import { SearchQueryVariables } from "./containers/BaseSearch";
 import { ListSettings, ListViews } from "./types";
 
 export const APP_MOUNT_URI = process.env.APP_MOUNT_URI || "/";
-export const API_URI = process.env.API_URI || "http://localhost:8008/graphql/";
+export const API_URI = process.env.API_URI || "/graphql/";
 
 export const DEFAULT_INITIAL_SEARCH_DATA: SearchQueryVariables = {
   after: null,
