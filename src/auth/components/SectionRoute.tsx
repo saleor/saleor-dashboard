4da2d8492625c7f8ff2319d69f8b4b800975a90a
dominik-zeglen <flesz3@o2.pl>
2019-10-21 13:44:13 +0200

    Add error boundary

--- src/auth/components/SectionRoute.tsx
+++ src/auth/components/SectionRoute.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import ErrorBoundary from "react-error-boundary";
 import { Route, RouteProps } from "react-router-dom";
 
 import AppLayout from "@saleor/components/AppLayout";
-import { UserContext } from "..";
+import ErrorPage from "@saleor/components/ErrorPage";
+import useNavigator from "@saleor/hooks/useNavigator";
+import useUser from "@saleor/hooks/useUser";
 import NotFound from "../../NotFound";
 import { PermissionEnum } from "../../types/globalTypes";
 import { hasPermission } from "../misc";
@@ -14,21 +17,27 @@ interface SectionRouteProps extends RouteProps {
 export const SectionRoute: React.StatelessComponent<SectionRouteProps> = ({
   permissions,
   ...props
-}) => (
-  <UserContext.Consumer>
-    {({ user }) =>
-      !permissions ||
-      permissions
-        .map(permission => hasPermission(permission, user))
-        .reduce((prev, curr) => prev && curr) ? (
-        <AppLayout>
-          <Route {...props} />
-        </AppLayout>
-      ) : (
-        <NotFound />
-      )
-    }
-  </UserContext.Consumer>
-);
+}) => {
+  const navigate = useNavigator();
+  const { user } = useUser();
+
+  const hasPermissions =
+    !permissions ||
+    permissions
+      .map(permission => hasPermission(permission, user))
+      .reduce((prev, curr) => prev && curr);
+  return hasPermissions ? (
+    <AppLayout>
+      <ErrorBoundary
+        FallbackComponent={() => <ErrorPage onBack={() => navigate("/")} />}
+        key={permissions ? permissions.join(":") : "home"}
+      >
+        <Route {...props} />
+      </ErrorBoundary>
+    </AppLayout>
+  ) : (
+    <NotFound />
+  );
+};
 SectionRoute.displayName = "Route";
 export default SectionRoute;
