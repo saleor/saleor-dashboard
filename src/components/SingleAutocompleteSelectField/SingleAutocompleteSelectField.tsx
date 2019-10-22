afdfab35294c6bd8a8674b26b89d52fa17f78acf
Krzysztof Bialoglowicz <krzysztof@mirumee.com>
2019-10-22 14:43:14 +0200

    Remove unsused declaration

--- src/components/SingleAutocompleteSelectField/SingleAutocompleteSelectField.tsx
+++ src/components/SingleAutocompleteSelectField/SingleAutocompleteSelectField.tsx
@@ -93,7 +93,6 @@ const SingleAutocompleteSelectFieldComponent = withStyles(styles, {
               inputValue,
               selectedItem,
               toggleMenu,
-              openMenu,
               closeMenu,
               highlightedIndex,
               reset
