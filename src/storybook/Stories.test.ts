349c7dbcc01c880f3bfb09e25a8feaebe8fa948c
dominik-zeglen <flesz3@o2.pl>
2019-11-05 11:59:21 +0100

    Update stories

--- src/storybook/Stories.test.ts
+++ src/storybook/Stories.test.ts
@@ -1,22 +1,24 @@
-import { createGenerateClassName } from "@material-ui/core/styles";
+import createGenerateClassName from "@material-ui/styles/createGenerateClassName";
 import initStoryshots from "@storybook/addon-storyshots";
 // tslint:disable no-submodule-imports
 import generateRandomKey from "draft-js/lib/generateRandomKey";
 import { configure, render } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import toJSON from "enzyme-to-json";
+import React from "react";
+
+// Fixes useLayoutEffect warnings
+React.useLayoutEffect = React.useEffect;
 
 configure({ adapter: new Adapter() });
 
-jest.mock("@material-ui/core/styles/createGenerateClassName");
-(createGenerateClassName as any).mockImplementation(
-  () => (rule, stylesheet) => {
-    return [stylesheet.options.meta, rule.key, "id"].join("-");
-  }
-);
-
 jest.mock("draft-js/lib/generateRandomKey");
 (generateRandomKey as any).mockImplementation(() => "testKey");
+
+jest.mock("@material-ui/styles/createGenerateClassName");
+(createGenerateClassName as any).mockImplementation(() => (rule, stylesheet) =>
+  [stylesheet.options.meta, rule.key, "id"].join("-")
+);
 
 initStoryshots({
   configPath: "src/storybook/",
