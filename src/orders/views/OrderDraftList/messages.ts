9d0e5851a1f253157f33801dbcd7d4865d203c80
dominik-zeglen <flesz3@o2.pl>
2020-01-02 17:55:34 +0100

    Add filters to draft order list

---
+++ src/orders/views/OrderDraftList/messages.ts
@@ -0,0 +1,14 @@
+import { defineMessages } from "react-intl";
+
+const messages = defineMessages({
+  customer: {
+    defaultMessage: "Customer",
+    description: "draft order"
+  },
+  created: {
+    defaultMessage: "Created",
+    description: "draft order"
+  }
+});
+
+export default messages;
