7e9659e447563164c9f7854cd1b97ee11fd2827a
dominik-zeglen <flesz3@o2.pl>
2020-01-03 16:17:51 +0100

    Add filters to voucher list

--- src/discounts/types.ts
+++ src/discounts/types.ts
@@ -1,13 +1,21 @@
 import { FilterOpts, MinMax } from "@saleor/types";
 import {
   DiscountStatusEnum,
-  DiscountValueTypeEnum
+  DiscountValueTypeEnum,
+  VoucherDiscountType
 } from "@saleor/types/globalTypes";
 
 export interface SaleListFilterOpts {
   saleType: FilterOpts<DiscountValueTypeEnum>;
   started: FilterOpts<MinMax>;
   status: FilterOpts<DiscountStatusEnum[]>;
+}
+
+export interface VoucherListFilterOpts {
+  saleType: FilterOpts<VoucherDiscountType[]>;
+  started: FilterOpts<MinMax>;
+  status: FilterOpts<DiscountStatusEnum[]>;
+  timesUsed: FilterOpts<MinMax>;
 }
 
 export enum RequirementsPicker {
