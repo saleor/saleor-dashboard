7b4ed6ef277c820f9b2bd3084a7e2be680605baf
dominik-zeglen <flesz3@o2.pl>
2020-01-07 14:34:45 +0100

    Add filtering to collections

---
+++ src/collections/views/CollectionList/messages.ts
@@ -0,0 +1,14 @@
+import { defineMessages } from "react-intl";
+
+const messages = defineMessages({
+  hidden: {
+    defaultMessage: "Hidden",
+    description: "collection"
+  },
+  published: {
+    defaultMessage: "Published",
+    description: "collection"
+  }
+});
+
+export default messages;
