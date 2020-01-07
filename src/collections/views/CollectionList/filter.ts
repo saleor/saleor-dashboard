b4cb44834c3fbcc67acbccefc166dfa3b7b23039
dominik-zeglen <flesz3@o2.pl>
2020-01-07 14:34:45 +0100

    Add filtering to collections

--- src/collections/views/CollectionList/filter.ts
+++ src/collections/views/CollectionList/filter.ts
@@ -1,22 +1,97 @@
-import { CollectionFilterInput } from "@saleor/types/globalTypes";
+import { IntlShape } from "react-intl";
+
 import {
-  createFilterTabUtils,
-  createFilterUtils
-} from "../../../utils/filters";
+  CollectionFilterInput,
+  CollectionPublished
+} from "@saleor/types/globalTypes";
+import { IFilterElement, IFilter } from "@saleor/components/Filter";
+import { maybe, findValueInEnum } from "@saleor/misc";
+import { createOptionsField } from "@saleor/utils/filters/fields";
+import { commonMessages } from "@saleor/intl";
+import { CollectionListFilterOpts } from "../../types";
 import {
   CollectionListUrlFilters,
   CollectionListUrlFiltersEnum,
   CollectionListUrlQueryParams
 } from "../../urls";
+import {
+  createFilterTabUtils,
+  createFilterUtils
+} from "../../../utils/filters";
+import messages from "./messages";
 
 export const COLLECTION_FILTERS_KEY = "collectionFilters";
+
+export enum CollectionFilterKeys {
+  status = "status"
+}
+
+export function getFilterOpts(
+  params: CollectionListUrlFilters
+): CollectionListFilterOpts {
+  return {
+    status: {
+      active: maybe(() => params.status !== undefined, false),
+      value: maybe(() => findValueInEnum(status, CollectionPublished))
+    }
+  };
+}
+
+export function createFilterStructure(
+  intl: IntlShape,
+  opts: CollectionListFilterOpts
+): IFilter<CollectionFilterKeys> {
+  return [
+    {
+      ...createOptionsField(
+        CollectionFilterKeys.status,
+        intl.formatMessage(commonMessages.status),
+        [opts.status.value],
+        false,
+        [
+          {
+            label: intl.formatMessage(messages.published),
+            value: CollectionPublished.PUBLISHED
+          },
+          {
+            label: intl.formatMessage(messages.hidden),
+            value: CollectionPublished.HIDDEN
+          }
+        ]
+      ),
+      active: opts.status.active
+    }
+  ];
+}
 
 export function getFilterVariables(
   params: CollectionListUrlFilters
 ): CollectionFilterInput {
   return {
+    published: params.status
+      ? findValueInEnum(params.status, CollectionPublished)
+      : undefined,
     search: params.query
   };
+}
+
+export function getFilterQueryParam(
+  filter: IFilterElement<CollectionFilterKeys>
+): CollectionListUrlFilters {
+  const { active, name, value } = filter;
+
+  switch (name) {
+    case CollectionFilterKeys.status:
+      if (!active) {
+        return {
+          status: undefined
+        };
+      }
+
+      return {
+        status: value[0]
+      };
+  }
 }
 
 export const {
