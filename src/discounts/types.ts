7c69c81ce638ea8ade9272bef005494b02bbfd52
dominik-zeglen <flesz3@o2.pl>
2020-01-10 14:01:26 +0100

    Separate visual and app layer in vouchers

--- src/discounts/types.ts
+++ src/discounts/types.ts
@@ -1,16 +1,3 @@
-import {
-  VoucherDiscountType,
-  DiscountStatusEnum
-} from "@saleor/types/globalTypes";
-import { MinMax, FilterOpts } from "@saleor/types";
-
-export interface VoucherListFilterOpts {
-  saleType: FilterOpts<VoucherDiscountType[]>;
-  started: FilterOpts<MinMax>;
-  status: FilterOpts<DiscountStatusEnum[]>;
-  timesUsed: FilterOpts<MinMax>;
-}
-
 export enum RequirementsPicker {
   ORDER = "ORDER",
   ITEM = "ITEM",
