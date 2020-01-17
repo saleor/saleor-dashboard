fdd988d1dbcae5040c4ed0499436cef3f794e3a0
dominik-zeglen <flesz3@o2.pl>
2020-01-17 16:00:35 +0100

    Remove default value because it's unused

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ import { SearchVariables } from "./hooks/makeSearch";
 import { ListSettings, ListViews } from "./types";
 
 export const APP_MOUNT_URI = process.env.APP_MOUNT_URI || "/";
-export const API_URI = process.env.API_URI || "/graphql/";
+export const API_URI = process.env.API_URI;
 
 export const DEFAULT_INITIAL_SEARCH_DATA: SearchVariables = {
   after: null,
