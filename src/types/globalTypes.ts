10f2964cd64cffef742a494eac3486104362e605
dominik-zeglen <flesz3@o2.pl>
2020-02-05 15:11:37 +0100

    Use apollo hooks

--- src/types/globalTypes.ts
+++ src/types/globalTypes.ts
@@ -730,6 +730,15 @@ export enum VoucherTypeEnum {
   SPECIFIC_PRODUCT = "SPECIFIC_PRODUCT",
 }
 
+export enum WarehouseErrorCode {
+  ALREADY_EXISTS = "ALREADY_EXISTS",
+  GRAPHQL_ERROR = "GRAPHQL_ERROR",
+  INVALID = "INVALID",
+  NOT_FOUND = "NOT_FOUND",
+  REQUIRED = "REQUIRED",
+  UNIQUE = "UNIQUE",
+}
+
 export enum WarehouseSortField {
   NAME = "NAME",
 }
@@ -1392,11 +1401,12 @@ export interface WarehouseAddressInput {
 }
 
 export interface WarehouseCreateInput {
-  name: string;
+  slug?: string | null;
   companyName?: string | null;
-  shippingZones?: (string | null)[] | null;
   email?: string | null;
+  name: string;
   address: WarehouseAddressInput;
+  shippingZones?: (string | null)[] | null;
 }
 
 export interface WarehouseFilterInput {
@@ -1409,10 +1419,10 @@ export interface WarehouseSortingInput {
 }
 
 export interface WarehouseUpdateInput {
-  name: string;
+  slug?: string | null;
   companyName?: string | null;
-  shippingZones?: (string | null)[] | null;
   email?: string | null;
+  name?: string | null;
   address?: WarehouseAddressInput | null;
 }
 
