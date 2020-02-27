cbc585399cbafca21fe9f2e3552a215362cf5fdf
dominik-zeglen <flesz3@o2.pl>
2020-02-27 11:27:16 +0100

    Compare added and introduced errors

--- .travis/check-strict-null-errors.sh
+++ .travis/check-strict-null-errors.sh
@@ -1,19 +1,54 @@
 #!/bin/sh
 
-snapshot=`cat .travis/check-strict-null-errors.snapshot` 
+snapshotPath=".travis/check-strict-null-errors.snapshot"
+
+snapshot=`cat $snapshotPath` 
 calculated=`tsc --noEmit --strictNullChecks | node scripts/count-strict-null-check-errors.js` 
-master=`curl -s -f https://raw.githubusercontent.com/mirumee/saleor-dashboard/master/.travis/check-strict-null-errors.snapshot`
+master=`curl -s -f https://raw.githubusercontent.com/mirumee/saleor-dashboard/master/$snapshotPath`
 
-if [ $calculated -ne $snapshot ]
+diffCalculatedAndSnapshot=`echo "$calculated" | diff --strip-trailing-cr $snapshotPath - | wc -w`
+
+if [ $diffCalculatedAndSnapshot -gt 0 ]
 then
-    echo ERROR: Outdated snapshot found. Before: $snapshot, now: $calculated errors.
+    echo ERROR: Outdated snapshot found. Run \"npm run check-strict-null-errors -- -u\" to update it.
     exit 1
 fi
 
-if [ $calculated -gt $master ]
+errorsBefore=`echo "$master" | wc -l`
+errorsAfter=`echo "$snapshot" | wc -l`
+
+diff=`echo "$master" | diff $snapshotPath -`
+resolved=`echo "$diff" | grep -e ^\>`
+resolvedNum=`echo -n "$resolved" | wc -l`
+added=`echo "$diff" | grep -e ^\<`
+addedNum=`echo -n "$added" | wc -l`
+
+if [ `echo "$resolved" | wc -w` -eq 0 ]
 then
-    echo ERROR: Number of errors increased from $master to $calculated.
-    exit 1
+    resolvedNum=0
 fi
+
+if [ `echo "$added" | wc -w` -eq 0 ]
+then
+    addedNum=0
+fi
+
+echo Resolved errors: $resolvedNum. Introduced errors: $addedNum.
+
+if [ $addedNum -gt 0 ]
+then
+    echo ERROR: Introduced new $addedNum errors. Fix them before merge.
+    echo "\n$added"
+    exit 1
+else 
+    if [ $errorsBefore -eq $errorsAfter ]
+    then
+        echo Number of errors has not changed.
+    else
+        echo Number of errors decreased from $errorsBefore to $errorsAfter.
+    fi
+fi
+
+
 
 exit 0
