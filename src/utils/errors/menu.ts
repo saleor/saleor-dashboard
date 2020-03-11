61c3eb51fd5ce266394e5b0e771d722f62799cfa
dominik-zeglen <flesz3@o2.pl>
2020-03-11 14:03:31 +0100

    Use error codes in navigation section

---
+++ src/utils/errors/menu.ts
@@ -0,0 +1,28 @@
+import { IntlShape } from "react-intl";
+
+import { MenuErrorCode } from "@saleor/types/globalTypes";
+import { commonMessages } from "@saleor/intl";
+import { MenuErrorFragment } from "@saleor/navigation/types/MenuErrorFragment";
+import commonErrorMessages from "./common";
+
+function getMenuErrorMessage(
+  err: Omit<MenuErrorFragment, "__typename"> | undefined,
+  intl: IntlShape
+): string {
+  if (err) {
+    switch (err.code) {
+      case MenuErrorCode.GRAPHQL_ERROR:
+        return intl.formatMessage(commonErrorMessages.graphqlError);
+      case MenuErrorCode.REQUIRED:
+        return intl.formatMessage(commonMessages.requiredField);
+      case MenuErrorCode.INVALID:
+        return intl.formatMessage(commonErrorMessages.invalid);
+      default:
+        return intl.formatMessage(commonErrorMessages.unknownError);
+    }
+  }
+
+  return undefined;
+}
+
+export default getMenuErrorMessage;
