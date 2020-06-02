4ecbd9a1a1a7c49acc59736781626e0bf70e2c10
AlicjaSzu <alicja.szukuc@gmail.com>
2020-06-02 16:33:44 +0200

    create app activate/deactivate dialogs, create app details stories

--- src/apps/urls.ts
+++ src/apps/urls.ts
@@ -5,10 +5,15 @@ import { ActiveTab, Dialog, Pagination, SingleAction } from "../types";
 
 export type AppListUrlDialog = "remove" | "remove-app" | "remove-custom-app";
 
+export type AppDetailsUrlDialog = "app-activate" | "app-deactivate";
+
 export type AppListUrlQueryParams = ActiveTab &
   Dialog<AppListUrlDialog> &
   SingleAction &
   Pagination;
+
+export type AppDetailsUrlQueryParams = Dialog<AppDetailsUrlDialog> &
+  SingleAction;
 
 export enum AppListUrlSortField {
   name = "name",
@@ -20,7 +25,7 @@ export const appsListPath = appsSection;
 
 export const appPath = (id: string) => urlJoin(appsSection, id);
 
-export const appUrl = (id: string, params?: AppListUrlQueryParams) =>
+export const appUrl = (id: string, params?: AppDetailsUrlQueryParams) =>
   appPath(encodeURIComponent(id)) + "?" + stringifyQs(params);
 
 export const appsListUrl = (params?: AppListUrlQueryParams) =>
