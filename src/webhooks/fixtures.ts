5a5b18b1c0fb89cd2522e18a3fd8cae5ae27606c
AlicjaSzu <alicja.szukuc@gmail.com>
2020-06-04 18:03:20 +0200

    update webhooks fixtures

--- src/webhooks/fixtures.ts
+++ src/webhooks/fixtures.ts
@@ -4,49 +4,49 @@ import { Webhooks_webhooks_edges_node } from "./types/Webhooks";
 export const webhookList: Webhooks_webhooks_edges_node[] = [
   {
     __typename: "Webhook",
-    id: "Jzx123sEt==",
-    isActive: true,
-    name: "Webhook Test",
-    serviceAccount: {
-      __typename: "ServiceAccount",
+    app: {
+      __typename: "App",
       id: "Jzx123sEt==",
-      name: "Test Account"
-    }
-  },
-  {
-    __typename: "Webhook",
+      name: "Test App"
+    },
     id: "Jzx123sEt==",
     isActive: true,
-    name: "Webhook Test 2",
-    serviceAccount: {
-      __typename: "ServiceAccount",
-      id: "Jzx1ss23sEt==",
-      name: "Test Account 2"
-    }
+    name: "Webhook Test"
   },
   {
     __typename: "Webhook",
+    app: {
+      __typename: "App",
+      id: "Jzx1ss23sEt==",
+      name: "Test App 2"
+    },
+    id: "Jzx123sEt==",
+    isActive: true,
+    name: "Webhook Test 2"
+  },
+  {
+    __typename: "Webhook",
+    app: {
+      __typename: "App",
+      id: "Jzx1ss23sEt==",
+      name: "Test App 2"
+    },
     id: "Jzx12123sEt==",
     isActive: false,
-    name: null,
-    serviceAccount: {
-      __typename: "ServiceAccount",
-      id: "Jzx1ss23sEt==",
-      name: "Test Account 2"
-    }
+    name: null
   }
 ];
 export const webhook: WebhookDetails_webhook = {
   __typename: "Webhook",
+  app: {
+    __typename: "App",
+    id: "Jzx1ss23sEt==",
+    name: "Test App 2"
+  },
   events: [],
   id: "Jzx123sEt==",
   isActive: true,
   name: "Webhook Test 2",
   secretKey: "zxczx_asdas",
-  serviceAccount: {
-    __typename: "ServiceAccount",
-    id: "Jzx1ss23sEt==",
-    name: "Test Account 2"
-  },
   targetUrl: "http://www.getsaleor.com"
 };
