a34dbea0aa39c725d3ea7d6f76c44fd4c3bdf6b9
AlicjaSzu <alicja.szukuc@gmail.com>
2020-06-09 17:58:22 +0200

    remove service account section

--- src/services/components/ServiceDeleteDialog/index.ts
+++
@@ -1,2 +0,0 @@
-export { default } from "./ServiceDeleteDialog";
-export * from "./ServiceDeleteDialog";
--- src/services/components/ServiceTokenCreateDialog/index.ts
+++
@@ -1,2 +0,0 @@
-export { default } from "./ServiceTokenCreateDialog";
-export * from "./ServiceTokenCreateDialog";
