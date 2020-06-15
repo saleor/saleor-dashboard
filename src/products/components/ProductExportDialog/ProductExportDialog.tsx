60ff69a9a98f572e32725d64c35d841b8ff17c47
dominik-zeglen <flesz3@o2.pl>
2020-06-15 15:09:20 +0200

    Add component backbone

---
+++ src/products/components/ProductExportDialog/ProductExportDialog.tsx
@@ -0,0 +1,67 @@
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import ConfirmButton, {
+  ConfirmButtonTransitionState
+} from "@saleor/components/ConfirmButton";
+import { buttonMessages } from "@saleor/intl";
+import { DialogProps } from "@saleor/types";
+import React from "react";
+import { FormattedMessage } from "react-intl";
+
+import ProductExportDialogSettings from "../ProductExportDialogSettings";
+
+export interface ProductExportSubmitData {}
+
+export enum ProductExportStep {
+  SETTINGS
+}
+
+export interface ProductExportDialogProps extends DialogProps {
+  confirmButtonState: ConfirmButtonTransitionState;
+  onSubmit: (data: ProductExportSubmitData) => void;
+}
+
+const ProductExportDialog: React.FC<ProductExportDialogProps> = ({
+  confirmButtonState,
+  onClose,
+  onSubmit,
+  open
+}) => {
+  const [step, setStep] = React.useState(ProductExportStep.SETTINGS);
+
+  return (
+    <Dialog onClose={onClose} open={open} maxWidth="sm" fullWidth>
+      <DialogTitle>
+        <FormattedMessage
+          defaultMessage="Export Information"
+          description="export products to csv file, dialog header"
+        />
+      </DialogTitle>
+      <DialogContent>
+        {step === ProductExportStep.SETTINGS && <ProductExportDialogSettings />}
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose}>
+          <FormattedMessage {...buttonMessages.back} />
+        </Button>
+        <ConfirmButton
+          onClick={onSubmit}
+          transitionState={confirmButtonState}
+          variant="contained"
+          type="submit"
+        >
+          <FormattedMessage
+            defaultMessage="export products"
+            description="export products to csv file, button"
+          />
+        </ConfirmButton>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+ProductExportDialog.displayName = "ProductExportDialog";
+export default ProductExportDialog;
---
+++ src/products/components/ProductExportDialogSettings/ProductExportDialogSettings.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export interface ProductExportDialogSettingsProps {}
+
+const ProductExportDialogSettings: React.FC<ProductExportDialogSettingsProps> = () =>
+  null;
+
+ProductExportDialogSettings.displayName = "ProductExportDialogSettings";
+export default ProductExportDialogSettings;
