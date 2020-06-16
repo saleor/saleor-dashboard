3c3fa7b077eb64c0c0f22f83d920dd13a1cb1587
dominik-zeglen <flesz3@o2.pl>
2020-06-16 10:30:23 +0200

    Move types to separate file

---
+++ src/products/components/ProductExportDialog/types.ts
@@ -0,0 +1,15 @@
+export interface ProductExportSubmitData {}
+
+export enum ProductExportStep {
+  INFO,
+  SETTINGS
+}
+export enum ProductsToExport {
+  ALL,
+  SEARCH,
+  SELECTED
+}
+export enum ProductExportType {
+  CSV,
+  SPREADSHEET
+}
