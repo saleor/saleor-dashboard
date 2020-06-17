0be0aab66be251392bce74f9bc6905baa4bd5fa9
dominik-zeglen <flesz3@o2.pl>
2020-06-17 02:37:30 +0200

    Add error handling

--- src/products/components/ProductExportDialog/ProductExportDialog.stories.tsx
+++ src/products/components/ProductExportDialog/ProductExportDialog.stories.tsx
@@ -1,4 +1,5 @@
 import Decorator from "@saleor/storybook/Decorator";
+import { CsvErrorCode, ExportProductsInput } from "@saleor/types/globalTypes";
 import { storiesOf } from "@storybook/react";
 import React from "react";
 
@@ -8,6 +9,7 @@ import ProductExportDialog, {
 
 const props: ProductExportDialogProps = {
   confirmButtonState: "default",
+  errors: [],
   onClose: () => undefined,
   onSubmit: () => undefined,
   open: true
@@ -15,4 +17,16 @@ const props: ProductExportDialogProps = {
 
 storiesOf("Views / Products / Export / Export settings", module)
   .addDecorator(Decorator)
-  .add("interactive", () => <ProductExportDialog {...props} />);
+  .add("interactive", () => <ProductExportDialog {...props} />)
+  .add("errors", () => (
+    <ProductExportDialog
+      {...props}
+      errors={(["fileType", "scope"] as Array<keyof ExportProductsInput>).map(
+        field => ({
+          __typename: "CsvError",
+          code: CsvErrorCode.INVALID,
+          field
+        })
+      )}
+    />
+  ));
