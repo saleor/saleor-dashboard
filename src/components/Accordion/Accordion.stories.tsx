48e6a2e0c5fe9bcb32e92aad3f813f823a3aabe8
dominik-zeglen <flesz3@o2.pl>
2020-06-24 14:13:43 +0200

    Add accordion component

---
+++ src/components/Accordion/Accordion.stories.tsx
@@ -0,0 +1,16 @@
+import CardDecorator from "@saleor/storybook/CardDecorator";
+import Decorator from "@saleor/storybook/Decorator";
+import { storiesOf } from "@storybook/react";
+import React from "react";
+
+import Accordion from "./Accordion";
+
+storiesOf("Generics / Accordion", module)
+  .addDecorator(Decorator)
+  .addDecorator(CardDecorator)
+  .add("default", () => <Accordion title="Title">Content</Accordion>)
+  .add("opened", () => (
+    <Accordion title="Title" initialExpand={true}>
+      Content
+    </Accordion>
+  ));
