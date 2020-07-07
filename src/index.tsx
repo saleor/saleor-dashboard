96a6e9817c73f6cb51fa5792b9f8e0714f06c014
AlicjaSzu <alicja.szukuc@gmail.com>
2020-07-07 12:43:27 +0200

    fix ChannelForm props

--- src/index.tsx
+++ src/index.tsx
@@ -285,7 +285,7 @@ const Routes: React.FC = () => {
                     component={WarehouseSection}
                   />
                   <SectionRoute
-                    permissions={[PermissionEnum.MANAGE_PRODUCTS]}
+                    permissions={[PermissionEnum.MANAGE_CHANNELS]}
                     path={channelsSection}
                     component={ChannelsSection}
                   />
