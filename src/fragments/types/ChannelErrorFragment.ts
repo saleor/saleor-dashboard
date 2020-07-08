52464573b5a5c373e0d6a91cdb802f92ea46f000
AlicjaSzu <alicja.szukuc@gmail.com>
2020-07-08 13:06:04 +0200

    update Channels fragments

---
+++ src/fragments/types/ChannelErrorFragment.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+import { ChannelErrorCode } from "./../../types/globalTypes";
+
+// ====================================================
+// GraphQL fragment: ChannelErrorFragment
+// ====================================================
+
+export interface ChannelErrorFragment {
+  __typename: "ChannelError";
+  code: ChannelErrorCode;
+  field: string | null;
+  message: string | null;
+}
