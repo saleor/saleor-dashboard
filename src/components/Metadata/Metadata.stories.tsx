eea09783228d9fe585eda79399b7c9a10f8e1711
dominik-zeglen <flesz3@o2.pl>
2020-08-20 17:21:33 +0200

    Add tests

--- src/components/Metadata/Metadata.stories.tsx
+++ src/components/Metadata/Metadata.stories.tsx
@@ -3,28 +3,8 @@ import Decorator from "@saleor/storybook/Decorator";
 import { storiesOf } from "@storybook/react";
 import React from "react";
 
-import Metadata, { MetadataProps } from "./Metadata";
-
-const props: MetadataProps = {
-  data: {
-    metadata: [
-      {
-        key: "key",
-        value: "value"
-      },
-      {
-        key: "key2",
-        value: '{\n  "jsonValue": "some-value"\n}'
-      },
-      {
-        key: "key3",
-        value: "some-value"
-      }
-    ],
-    privateMetadata: []
-  },
-  onChange: () => undefined
-};
+import { props } from "./fixtures";
+import Metadata from "./Metadata";
 
 const InteractiveStory: React.FC = () => {
   const { change, data } = useForm(props.data, () => undefined);
