8f79225652006d05415c55c2e1c3dd6958a9c8e5
Michalina Graczyk <michalina.graczyk@mirumee.com>
2020-08-25 12:03:20 +0200

    WIP-add new product

---
+++ cypress/elements/account/left-menu/left-menu-selectors.js
@@ -0,0 +1,16 @@
+/* eslint-disable sort-keys */
+export const LEFT_MENU_SELECTORS = {
+  catalog: "[data-testid='catalog']",
+
+  PRODUCTS_SELECTORS: {
+    products: "[data-testid='catalogue']",
+    createProductBtn: "[data-test='add-product']",
+    productNameInput: "[name='name']",
+    productTypeInput: "data-test='product-type'",
+    categoryInput: "[data-test='category']",
+    categoryDropdown: "[data-test='singleautocomplete-select-option']",
+    visibleRadioBtn: "[name='isPublished']",
+    saveBtn: "[data-test='button-bar-confirm']",
+    confirmationMsg: "[data-test='notification']"
+  }
+};
