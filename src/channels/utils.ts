a7344590fce50b0a8d72ad2d18a9b0324e5dae13
AlicjaSzu <alicja.szukuc@gmail.com>
2020-08-27 21:47:47 +0200

    channels shipping views (#662)
    * update ChannelsAvailability component
    
    * updates after review
    
    * create PriceRate views, update types
    
    * create weight rates views
    
    * update shipping views, stories, messages
    
    * update snapshots
    
    * update snapshots
    
    * update useChannels hook

--- src/channels/utils.ts
+++ src/channels/utils.ts
@@ -1,5 +1,6 @@
 import { Channels_channels } from "@saleor/channels/types/Channels";
 import { ProductDetails_product_channelListing } from "@saleor/products/types/ProductDetails";
+import { ShippingZone_shippingZone_shippingMethods_channels } from "@saleor/shipping/types/ShippingZone";
 
 export interface ChannelData {
   id: string;
@@ -17,6 +18,35 @@ export const createChannelsData = (
     name: channel.name,
     publicationDate: null
   })) || [];
+
+export const createShippingChannels = (
+  data?: Channels_channels[]
+): ChannelShippingData[] | [] =>
+  data?.map(channel => ({
+    id: channel.id,
+    maxValue: "",
+    minValue: "",
+    name: channel.name,
+    price: ""
+  })) || [];
+
+export const createShippingChannelsFromRate = (
+  data?: ShippingZone_shippingZone_shippingMethods_channels[]
+): ChannelShippingData[] | [] =>
+  data?.map(channelData => ({
+    id: channelData.channel.id,
+    maxValue: channelData.maxValue ? channelData.maxValue.toString() : "",
+    minValue: channelData.minValue ? channelData.minValue.toString() : "",
+    name: channelData.channel.name,
+    price: channelData.price ? channelData.price.toString() : ""
+  })) || [];
+export interface ChannelShippingData {
+  id: string;
+  minValue: string;
+  name: string;
+  maxValue: string;
+  price: string;
+}
 
 export const createChannelsDataFromProduct = (
   productData?: ProductDetails_product_channelListing[]
