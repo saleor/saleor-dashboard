989f66f7de3270119ec49744f08250462cbf9e06
dominik-zeglen <flesz3@o2.pl>
2020-09-11 12:43:55 +0200

    Add SquareButton component

--- src/components/SideBar/ExpandButton.tsx
+++ src/components/SideBar/ExpandButton.tsx
@@ -1,25 +1,15 @@
 import { ButtonProps } from "@material-ui/core/Button";
-import ButtonBase from "@material-ui/core/ButtonBase";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import ArrowIcon from "@material-ui/icons/ArrowBack";
 import classNames from "classnames";
 import React from "react";
 
+import SquareButton from "../SquareButton";
+
 const useStyles = makeStyles(
   theme => ({
     arrow: {
       transition: theme.transitions.duration.shortest + "ms"
-    },
-    root: {
-      "&:hover, &:focus": {
-        background: "#daedeb"
-      },
-      background: theme.palette.background.paper,
-      borderRadius: 16,
-      color: theme.palette.primary.main,
-      height: 48,
-      transition: theme.transitions.duration.shortest + "ms",
-      width: 48
     },
     shrunk: {
       transform: "scaleX(-1)"
@@ -34,21 +24,17 @@ export interface ExpandButtonProps extends ButtonProps {
   isShrunk: boolean;
 }
 
-const ExpandButton: React.FC<ExpandButtonProps> = ({
-  className,
-  isShrunk,
-  ...rest
-}) => {
+const ExpandButton: React.FC<ExpandButtonProps> = ({ isShrunk, ...rest }) => {
   const classes = useStyles({});
 
   return (
-    <ButtonBase className={classNames(classes.root, className)} {...rest}>
+    <SquareButton {...rest}>
       <ArrowIcon
         className={classNames(classes.arrow, {
           [classes.shrunk]: isShrunk
         })}
       />
-    </ButtonBase>
+    </SquareButton>
   );
 };
 
