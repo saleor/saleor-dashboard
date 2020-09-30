ba3d31b78acf7aa2f6f48de69a2de3975b6acaf4
AlicjaSzu <alicja.szukuc@gmail.com>
2020-09-30 11:39:51 +0200

    Product List - remove publish/unpublish buttons (#727)
    * ProductList - remove publish and unpublish buttons
    
    * update messages
    
    * update snapshots, messages
    
    * revert changes in ChannelsAvailabilityDropdown

--- src/components/ChannelsAvailabilityDropdown/ChannelsAvailabilityDropdown.tsx
+++ src/components/ChannelsAvailabilityDropdown/ChannelsAvailabilityDropdown.tsx
@@ -29,7 +29,6 @@ export const ChannelsAvailabilityDropdown: React.FC<ChannelsAvailabilityDropdown
   const handleClick = event => setAnchorEl(event.currentTarget);
 
   const handleClose = () => setAnchorEl(null);
-
   return (
     <div onClick={e => e.stopPropagation()}>
       <div
