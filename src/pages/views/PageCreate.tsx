93b6560da4808429e2a79157d00efcae9423ebb4
dominik-zeglen <flesz3@o2.pl>
2020-10-20 13:20:55 +0200

    Add metadata editor to page views

--- src/pages/views/PageCreate.tsx
+++ src/pages/views/PageCreate.tsx
@@ -1,10 +1,15 @@
 import { WindowTitle } from "@saleor/components/WindowTitle";
 import useNavigator from "@saleor/hooks/useNavigator";
 import useNotifier from "@saleor/hooks/useNotifier";
+import createMetadataCreateHandler from "@saleor/utils/handlers/metadataCreateHandler";
+import {
+  useMetadataUpdate,
+  usePrivateMetadataUpdate
+} from "@saleor/utils/metadata/updateMetadata";
 import React from "react";
 import { useIntl } from "react-intl";
 
-import PageDetailsPage from "../components/PageDetailsPage";
+import PageDetailsPage, { FormData } from "../components/PageDetailsPage";
 import { TypedPageCreate } from "../mutations";
 import { PageCreate as PageCreateData } from "../types/PageCreate";
 import { pageListUrl, pageUrl } from "../urls";
@@ -17,6 +22,8 @@ export const PageCreate: React.FC<PageCreateProps> = () => {
   const navigate = useNavigator();
   const notify = useNotifier();
   const intl = useIntl();
+  const [updateMetadata] = useMetadataUpdate({});
+  const [updatePrivateMetadata] = usePrivateMetadataUpdate({});
 
   const handlePageCreate = (data: PageCreateData) => {
     if (data.pageCreate.errors.length === 0) {
@@ -32,41 +39,52 @@ export const PageCreate: React.FC<PageCreateProps> = () => {
 
   return (
     <TypedPageCreate onCompleted={handlePageCreate}>
-      {(pageCreate, pageCreateOpts) => (
-        <>
-          <WindowTitle
-            title={intl.formatMessage({
-              defaultMessage: "Create Page",
-              description: "header"
-            })}
-          />
-          <PageDetailsPage
-            disabled={pageCreateOpts.loading}
-            errors={pageCreateOpts.data?.pageCreate.errors || []}
-            saveButtonBarState={pageCreateOpts.status}
-            page={null}
-            onBack={() => navigate(pageListUrl())}
-            onRemove={() => undefined}
-            onSubmit={formData =>
-              pageCreate({
-                variables: {
-                  input: {
-                    contentJson: JSON.stringify(formData.content),
-                    isPublished: formData.isPublished,
-                    publicationDate: formData.publicationDate,
-                    seo: {
-                      description: formData.seoDescription,
-                      title: formData.seoTitle
-                    },
-                    slug: formData.slug === "" ? null : formData.slug,
-                    title: formData.title
-                  }
-                }
-              })
+      {(pageCreate, pageCreateOpts) => {
+        const handleCreate = async (formData: FormData) => {
+          const result = await pageCreate({
+            variables: {
+              input: {
+                contentJson: JSON.stringify(formData.content),
+                isPublished: formData.isPublished,
+                publicationDate: formData.publicationDate,
+                seo: {
+                  description: formData.seoDescription,
+                  title: formData.seoTitle
+                },
+                slug: formData.slug === "" ? null : formData.slug,
+                title: formData.title
+              }
             }
-          />
-        </>
-      )}
+          });
+
+          return result.data.pageCreate.page?.id || null;
+        };
+        const handleSubmit = createMetadataCreateHandler(
+          handleCreate,
+          updateMetadata,
+          updatePrivateMetadata
+        );
+
+        return (
+          <>
+            <WindowTitle
+              title={intl.formatMessage({
+                defaultMessage: "Create Page",
+                description: "header"
+              })}
+            />
+            <PageDetailsPage
+              disabled={pageCreateOpts.loading}
+              errors={pageCreateOpts.data?.pageCreate.errors || []}
+              saveButtonBarState={pageCreateOpts.status}
+              page={null}
+              onBack={() => navigate(pageListUrl())}
+              onRemove={() => undefined}
+              onSubmit={handleSubmit}
+            />
+          </>
+        );
+      }}
     </TypedPageCreate>
   );
 };
