72b358b37702221491b8c129c8956115f5dbd607
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2020-10-23 12:28:12 +0200

    Updatte page type list style

--- src/pageTypes/views/PageTypeList/fliters.ts
+++ src/pageTypes/views/PageTypeList/fliters.ts
@@ -1,14 +1,8 @@
-import { IFilterElement } from "@saleor/components/Filter";
-import {
-  PageTypeFilterKeys,
-  PageTypeListFilterOpts
-} from "@saleor/pageTypes/components/PageTypeListPage";
 import { PageTypeFilterInput } from "@saleor/types/globalTypes";
 
 import {
   createFilterTabUtils,
-  createFilterUtils,
-  getSingleValueQueryParam
+  createFilterUtils
 } from "../../../utils/filters";
 import {
   PageTypeListUrlFilters,
@@ -18,29 +12,12 @@ import {
 
 export const PAGE_TYPE_FILTERS_KEY = "pageTypeFilters";
 
-export function getFilterOpts(
-  _params: PageTypeListUrlFilters
-): PageTypeListFilterOpts {
-  return {};
-}
-
 export function getFilterVariables(
   params: PageTypeListUrlFilters
 ): PageTypeFilterInput {
   return {
     search: params.query
   };
-}
-
-export function getFilterQueryParam(
-  filter: IFilterElement<PageTypeFilterKeys>
-): PageTypeListUrlFilters {
-  const { name } = filter;
-
-  switch (name) {
-    case PageTypeFilterKeys.type:
-      return getSingleValueQueryParam(filter, PageTypeListUrlFiltersEnum.type);
-  }
 }
 
 export const {
