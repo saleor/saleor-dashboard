eeb121e45b5812f7fde56530bd31e41b2d24f9a4
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2020-10-26 11:38:56 +0100

    Add page types attribute assignment handling

--- src/fragments/attributes.ts
+++ src/fragments/attributes.ts
@@ -32,3 +32,11 @@ export const attributeDetailsFragment = gql`
     }
   }
 `;
+
+export const availableAttributeFragment = gql`
+  fragment AvailableAttributeFragment on Attribute {
+    id
+    name
+    slug
+  }
+`;
