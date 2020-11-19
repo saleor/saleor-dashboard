552625a6a96690c0289c6b7f06c8d72c2a034d3d
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2020-11-19 18:57:38 +0100

    Add order setting query

---
+++ src/orders/types/OrderSettings.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: OrderSettings
+// ====================================================
+
+export interface OrderSettings_orderSettings {
+  __typename: "OrderSettings";
+  automaticallyConfirmAllNewOrders: boolean;
+}
+
+export interface OrderSettings {
+  orderSettings: OrderSettings_orderSettings | null;
+}
