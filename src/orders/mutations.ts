953a0906c1a0e3ed2ad67135aa97a48988a9a43c
Magdalena Markusik <magdalena.markusik@mirumee.com>
2020-11-20 10:29:33 +0100

    Add order confirm mutation and types

--- src/orders/mutations.ts
+++ src/orders/mutations.ts
@@ -25,6 +25,7 @@ import {
 import { OrderAddNote, OrderAddNoteVariables } from "./types/OrderAddNote";
 import { OrderCancel, OrderCancelVariables } from "./types/OrderCancel";
 import { OrderCapture, OrderCaptureVariables } from "./types/OrderCapture";
+import { OrderConfirm, OrderConfirmVariables } from "./types/OrderConfirm";
 import {
   OrderDraftBulkCancel,
   OrderDraftBulkCancelVariables
@@ -126,6 +127,27 @@ const orderDraftBulkCancelMutation = gql`
     }
   }
 `;
+
+export const orderConfirmMutation = gql`
+  ${fragmentOrderDetails}
+  ${orderErrorFragment}
+  mutation OrderConfirm($id: ID!) {
+    orderConfirm(id: $id) {
+      errors: orderErrors {
+        ...OrderErrorFragment
+      }
+      order {
+        ...OrderDetailsFragment
+      }
+    }
+  }
+`;
+
+export const useOrderConfirmMutation = makeMutation<
+  OrderConfirm,
+  OrderConfirmVariables
+>(orderConfirmMutation);
+
 export const TypedOrderDraftBulkCancelMutation = TypedMutation<
   OrderDraftBulkCancel,
   OrderDraftBulkCancelVariables
