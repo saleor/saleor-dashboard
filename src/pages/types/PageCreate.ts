fd7d2bafa5a745911cc582b41b0b31d5c278e08e
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2020-11-20 18:22:52 +0100

    Update order settings story

--- src/pages/types/PageCreate.ts
+++ src/pages/types/PageCreate.ts
@@ -67,6 +67,7 @@ export interface PageCreate_pageCreate_page_pageType {
   id: string;
   name: string;
   attributes: (PageCreate_pageCreate_page_pageType_attributes | null)[] | null;
+  message: string | null;
 }
 
 export interface PageCreate_pageCreate_page_metadata {
