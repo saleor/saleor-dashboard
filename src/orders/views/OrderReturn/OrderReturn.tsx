92389e6e50a269833a12928c986db241287bd69f
Magdalena Markusik <magdalena.markusik@mirumee.com>
2020-12-10 13:30:18 +0100

    wip

--- src/orders/views/OrderReturn/OrderReturn.tsx
+++ src/orders/views/OrderReturn/OrderReturn.tsx
@@ -1,169 +1,39 @@
 import makeQuery from "@saleor/hooks/makeQuery";
 import useNavigator from "@saleor/hooks/useNavigator";
-import useNotifier from "@saleor/hooks/useNotifier";
-// import {
-//   OrderRefundAmountCalculationMode,
-//   OrderRefundSubmitData,
-//   OrderRefundType
-// } from "@saleor/orders/components/OrderRefundPage/form";
 import OrderReturnPage from "@saleor/orders/components/OrderReturnPage";
-import { orderDetailsQuery, useOrderRefundData } from "@saleor/orders/queries";
-import { OrderDetails_order } from "@saleor/orders/types/OrderDetails";
-// import {
-//   useOrderFulfillmentRefundProductsMutation,
-//   useOrderRefundMutation
-// } from "@saleor/orders/mutations";
-// import { useOrderRefundData } from "@saleor/orders/queries";
+import { orderDetailsQuery } from "@saleor/orders/queries";
 import { orderUrl } from "@saleor/orders/urls";
 import React from "react";
-import { useIntl } from "react-intl";
 
-// const getAutomaticallyCalculatedProductsRefundInput = (
-//   formData: OrderRefundSubmitData
-// ) => ({
-//   fulfillmentLines: formData.refundedFulfilledProductQuantities
-//     .filter(line => line.value !== "0")
-//     .map(line => ({
-//       fulfillmentLineId: line.id,
-//       quantity: Number(line.value)
-//     })),
-//   includeShippingCosts: formData.refundShipmentCosts,
-//   orderLines: formData.refundedProductQuantities
-//     .filter(line => line.value !== "0")
-//     .map(line => ({
-//       orderLineId: line.id,
-//       quantity: Number(line.value)
-//     }))
-// });
-// const getManuallySetProductsRefundInput = (
-//   formData: OrderRefundSubmitData
-// ) => ({
-//   amountToRefund: formData.amount,
-//   fulfillmentLines: formData.refundedFulfilledProductQuantities
-//     .filter(line => line.value !== "0")
-//     .map(line => ({
-//       fulfillmentLineId: line.id,
-//       quantity: Number(line.value)
-//     })),
-//   includeShippingCosts: formData.refundShipmentCosts,
-//   orderLines: formData.refundedProductQuantities
-//     .filter(line => line.value !== "0")
-//     .map(line => ({
-//       orderLineId: line.id,
-//       quantity: Number(line.value)
-//     }))
-// });
-
-interface OrderRefundProps {
+interface OrderReturnProps {
   orderId: string;
 }
 
-const OrderRefund: React.FC<OrderRefundProps> = ({ match: { params } }) => {
+const OrderReturn: React.FC<OrderReturnProps> = ({ orderId }) => {
   const navigate = useNavigator();
-  const notify = useNotifier();
-  const intl = useIntl();
-  const { id } = params;
-
-  // const { data, loading } = useOrderRefundData({
-  //   displayLoader: true,
-  //   variables: {
-  //     orderId
-  //   }
-  // });
-
-  //   const [refundOrder, refundOrderOpts] = useOrderRefundMutation({
-  //     onCompleted: data => {
-  //       if (data.orderRefund.errors.length === 0) {
-  //         navigate(orderUrl(orderId), true);
-  //         notify({
-  //           status: "success",
-  //           text: intl.formatMessage({
-  //             defaultMessage: "Refunded Items",
-  //             description: "order refunded success message"
-  //           })
-  //         });
-  //       }
-  //     }
-  //   });
-  //   const [
-  //     refundOrderFulfillmentProducts,
-  //     refundOrderFulfillmentProductsOpts
-  //   ] = useOrderFulfillmentRefundProductsMutation({
-  //     onCompleted: data => {
-  //       if (data.orderFulfillmentRefundProducts.errors.length === 0) {
-  //         navigate(orderUrl(orderId), true);
-  //         notify({
-  //           status: "success",
-  //           text: intl.formatMessage({
-  //             defaultMessage: "Refunded Items",
-  //             description: "order refunded success message"
-  //           })
-  //         });
-  //       }
-  //     }
-  //   });
-
-  //   const handleSubmitMiscellaneousRefund = async (
-  // formData: OrderRefundSubmitData
-  //   ) => {
-  //     const response = await refundOrder({
-  //       variables: {
-  //         amount: formData.amount,
-  //         id: orderId
-  //       }
-  //     });
-
-  //     return response.errors || [];
-  //   };
-
-  //   const handleSubmitProductsRefund = async (
-  //     formData: OrderRefundSubmitData
-  //   ) => {
-  //     const input =
-  //       formData.amountCalculationMode ===
-  //       OrderRefundAmountCalculationMode.AUTOMATIC
-  //         ? getAutomaticallyCalculatedProductsRefundInput(formData)
-  //         : getManuallySetProductsRefundInput(formData);
-
-  //     const response = await refundOrderFulfillmentProducts({
-  //       variables: {
-  //         input,
-  //         order: orderId
-  //       }
-  //     });
-
-  //     return response.errors || [];
-  //   };
-
-  //   const handleSubmit = async (formData: OrderRefundSubmitData) =>
-  //     formData.type === OrderRefundType.MISCELLANEOUS
-  //       ? handleSubmitMiscellaneousRefund(formData)
-  //       : handleSubmitProductsRefund(formData);
-  const handleSubmit = () => {};
 
   const useOrderQuery = makeQuery(orderDetailsQuery);
 
   const { data, loading } = useOrderQuery({
     displayLoader: true,
     variables: {
-      id
+      id: orderId
     }
   });
 
   return (
     <OrderReturnPage
+      // fixes in next pr, with connecting form
+      // @ts-ignore
       order={data?.order}
-      loading={
-        loading
-        // loading ||
-        // refundOrderOpts.loading ||
-        // refundOrderFulfillmentProductsOpts.loading
-      }
-      //   errors={[}
-      onSubmit={handleSubmit}
-      onBack={() => navigate(orderUrl(id))}
+      loading={loading}
+      // @ts-ignore
+      // eslint-disable-next-line
+      onSubmit={() => {}}
+      onBack={() => navigate(orderUrl(orderId))}
     />
   );
 };
-OrderRefund.displayName = "OrderRefund";
-export default OrderRefund;
+
+OrderReturn.displayName = "OrderReturn";
+export default OrderReturn;
