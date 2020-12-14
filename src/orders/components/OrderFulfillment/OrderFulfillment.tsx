ed0e962fe1bec904c9239216f42e143d65631075
Magdalena Markusik <magdalena.markusik@mirumee.com>
2020-12-14 19:54:24 +0100

    Add handling of new statuses to order details cards. Also refactor, and devide order fulfillment card into couple of smaller components

--- src/orders/components/OrderFulfillment/OrderFulfillment.tsx
+++ src/orders/components/OrderFulfillment/OrderFulfillment.tsx
@@ -1,21 +1,11 @@
-import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
 import { makeStyles } from "@material-ui/core/styles";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
-import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Typography from "@material-ui/core/Typography";
 import CardMenu from "@saleor/components/CardMenu";
-import CardTitle from "@saleor/components/CardTitle";
-import Money from "@saleor/components/Money";
 import ResponsiveTable from "@saleor/components/ResponsiveTable";
-import Skeleton from "@saleor/components/Skeleton";
-import StatusLabel from "@saleor/components/StatusLabel";
-import TableCellAvatar, {
-  AVATAR_MARGIN
-} from "@saleor/components/TableCellAvatar";
 import { mergeRepeatedOrderLines } from "@saleor/orders/utils/data";
 import classNames from "classnames";
 import React from "react";
@@ -24,35 +14,13 @@ import { FormattedMessage, useIntl } from "react-intl";
 import { getStringOrPlaceholder, maybe, renderCollection } from "../../../misc";
 import { FulfillmentStatus } from "../../../types/globalTypes";
 import { OrderDetails_order_fulfillments } from "../../types/OrderDetails";
+import CardTitle from "../OrderReturnPage/OrderReturnRefundItemsCard/CardTitle";
+import FulfilledActionButtons from "./FulfilledActionButtons";
+import TableHeader from "./TableHeader";
+import TableLine from "./TableLine";
 
 const useStyles = makeStyles(
   theme => ({
-    clickableRow: {
-      cursor: "pointer"
-    },
-    colName: {
-      width: "auto"
-    },
-    colNameLabel: {
-      marginLeft: AVATAR_MARGIN
-    },
-    colPrice: {
-      textAlign: "right",
-      width: 120
-    },
-    colQuantity: {
-      textAlign: "center",
-      width: 120
-    },
-    colSku: {
-      textAlign: "right",
-      textOverflow: "ellipsis",
-      width: 120
-    },
-    colTotal: {
-      textAlign: "right",
-      width: 120
-    },
     infoLabel: {
       display: "inline-block"
     },
@@ -61,13 +29,6 @@ const useStyles = makeStyles(
     },
     infoRow: {
       padding: theme.spacing(2, 3)
-    },
-    orderNumber: {
-      display: "inline",
-      marginLeft: theme.spacing(1)
-    },
-    statusBar: {
-      paddingTop: 0
     },
     table: {
       tableLayout: "fixed"
@@ -96,71 +57,31 @@ const OrderFulfillment: React.FC<OrderFulfillmentProps> = props => {
 
   const intl = useIntl();
 
-  const lines =
-    fulfillment?.status === FulfillmentStatus.REFUNDED
-      ? mergeRepeatedOrderLines(fulfillment?.lines)
-      : fulfillment?.lines;
-  const status = maybe(() => fulfillment.status);
-  const quantity = lines
-    ? lines.map(line => line.quantity).reduce((prev, curr) => prev + curr, 0)
-    : "...";
+  if (!fulfillment) {
+    return null;
+  }
+
+  const getLines = () => {
+    const statusesToMergeLines = [
+      FulfillmentStatus.REFUNDED,
+      FulfillmentStatus.REFUNDED_AND_RETURNED,
+      FulfillmentStatus.RETURNED
+    ];
+
+    if (statusesToMergeLines.includes(fulfillment?.status)) {
+      return mergeRepeatedOrderLines(fulfillment.lines);
+    }
+
+    return fulfillment?.lines || [];
+  };
 
   return (
     <Card>
       <CardTitle
-        title={
-          !!lines ? (
-            <StatusLabel
-              label={
-                <>
-                  {status === FulfillmentStatus.FULFILLED
-                    ? intl.formatMessage(
-                        {
-                          defaultMessage: "Fulfilled ({quantity})",
-                          description: "section header"
-                        },
-                        {
-                          quantity
-                        }
-                      )
-                    : status === FulfillmentStatus.REFUNDED
-                    ? intl.formatMessage(
-                        {
-                          defaultMessage: "Refunded ({quantity})",
-                          description: "refunded fulfillment, section header"
-                        },
-                        {
-                          quantity
-                        }
-                      )
-                    : intl.formatMessage(
-                        {
-                          defaultMessage: "Cancelled ({quantity})",
-                          description: "cancelled fulfillment, section header"
-                        },
-                        {
-                          quantity
-                        }
-                      )}
-                  <Typography className={classes.orderNumber} variant="body1">
-                    {maybe(
-                      () => `#${orderNumber}-${fulfillment.fulfillmentOrder}`
-                    )}
-                  </Typography>
-                </>
-              }
-              status={
-                status === FulfillmentStatus.FULFILLED
-                  ? "success"
-                  : status === FulfillmentStatus.REFUNDED
-                  ? "unspecified"
-                  : "error"
-              }
-            />
-          ) : (
-            <Skeleton />
-          )
-        }
+        withStatus
+        lines={fulfillment?.lines}
+        fulfillment={fulfillment}
+        orderNumber={orderNumber}
         toolbar={
           maybe(() => fulfillment.status) === FulfillmentStatus.FULFILLED && (
             <CardMenu
@@ -178,84 +99,10 @@ const OrderFulfillment: React.FC<OrderFulfillmentProps> = props => {
         }
       />
       <ResponsiveTable className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <TableCell className={classes.colName}>
-              <span className={classes.colNameLabel}>
-                <FormattedMessage
-                  defaultMessage="Product"
-                  description="product name"
-                />
-              </span>
-            </TableCell>
-            <TableCell className={classes.colSku}>
-              <FormattedMessage
-                defaultMessage="SKU"
-                description="ordered product sku"
-              />
-            </TableCell>
-            <TableCell className={classes.colQuantity}>
-              <FormattedMessage
-                defaultMessage="Quantity"
-                description="ordered product quantity"
-              />
-            </TableCell>
-            <TableCell className={classes.colPrice}>
-              <FormattedMessage
-                defaultMessage="Price"
-                description="product price"
-              />
-            </TableCell>
-            <TableCell className={classes.colTotal}>
-              <FormattedMessage
-                defaultMessage="Total"
-                description="order line total price"
-              />
-            </TableCell>
-          </TableRow>
-        </TableHead>
+        <TableHeader />
         <TableBody>
-          {renderCollection(lines, line => (
-            <TableRow
-              className={!!line ? classes.clickableRow : undefined}
-              hover={!!line}
-              key={maybe(() => line.id)}
-            >
-              <TableCellAvatar
-                className={classes.colName}
-                thumbnail={maybe(() => line.orderLine.thumbnail.url)}
-              >
-                {maybe(() => line.orderLine.productName) || <Skeleton />}
-              </TableCellAvatar>
-              <TableCell className={classes.colSku}>
-                {line?.orderLine.productSku || <Skeleton />}
-              </TableCell>
-              <TableCell className={classes.colQuantity}>
-                {line?.quantity || <Skeleton />}
-              </TableCell>
-              <TableCell className={classes.colPrice}>
-                {maybe(() => line.orderLine.unitPrice.gross) ? (
-                  <Money money={line.orderLine.unitPrice.gross} />
-                ) : (
-                  <Skeleton />
-                )}
-              </TableCell>
-              <TableCell className={classes.colTotal}>
-                {maybe(
-                  () => line.quantity * line.orderLine.unitPrice.gross.amount
-                ) ? (
-                  <Money
-                    money={{
-                      amount:
-                        line.quantity * line.orderLine.unitPrice.gross.amount,
-                      currency: line.orderLine.unitPrice.gross.currency
-                    }}
-                  />
-                ) : (
-                  <Skeleton />
-                )}
-              </TableCell>
-            </TableRow>
+          {renderCollection(getLines(), line => (
+            <TableLine line={line} />
           ))}
           {(fulfillment?.warehouse || fulfillment?.trackingNumber) && (
             <TableRow>
@@ -306,28 +153,13 @@ const OrderFulfillment: React.FC<OrderFulfillmentProps> = props => {
           )}
         </TableBody>
       </ResponsiveTable>
-      {status === FulfillmentStatus.FULFILLED && !fulfillment.trackingNumber && (
-        <CardActions>
-          <Button color="primary" onClick={onTrackingCodeAdd}>
-            <FormattedMessage
-              defaultMessage="Add tracking"
-              description="fulfillment group tracking number"
-            />
-          </Button>
-        </CardActions>
-      )}
-      {status === FulfillmentStatus.FULFILLED && fulfillment.trackingNumber && (
-        <CardActions>
-          <Button color="primary" onClick={onTrackingCodeAdd}>
-            <FormattedMessage
-              defaultMessage="Edit tracking"
-              description="fulfillment group tracking number"
-            />
-          </Button>
-        </CardActions>
-      )}
+      <FulfilledActionButtons
+        status={fulfillment?.status}
+        trackingNumber={fulfillment?.trackingNumber}
+        onTrackingCodeAdd={onTrackingCodeAdd}
+      />
     </Card>
   );
 };
-OrderFulfillment.displayName = "OrderFulfillment";
+
 export default OrderFulfillment;
---
+++ src/orders/components/OrderFulfillment/TableLine.tsx
@@ -0,0 +1,114 @@
+import { makeStyles, TableCell, TableRow } from "@material-ui/core";
+import Money from "@saleor/components/Money";
+import Skeleton from "@saleor/components/Skeleton";
+import TableCellAvatar, {
+  AVATAR_MARGIN
+} from "@saleor/components/TableCellAvatar";
+import { maybe } from "@saleor/misc";
+import { OrderDetails_order_fulfillments_lines } from "@saleor/orders/types/OrderDetails";
+import React from "react";
+
+const useStyles = makeStyles(
+  theme => ({
+    clickableRow: {
+      cursor: "pointer"
+    },
+    colName: {
+      width: "auto"
+    },
+    colNameLabel: {
+      marginLeft: AVATAR_MARGIN
+    },
+    colPrice: {
+      textAlign: "right",
+      width: 120
+    },
+    colQuantity: {
+      textAlign: "center",
+      width: 120
+    },
+    colSku: {
+      textAlign: "right",
+      textOverflow: "ellipsis",
+      width: 120
+    },
+    colTotal: {
+      textAlign: "right",
+      width: 120
+    },
+    infoLabel: {
+      display: "inline-block"
+    },
+    infoLabelWithMargin: {
+      marginBottom: theme.spacing()
+    },
+    infoRow: {
+      padding: theme.spacing(2, 3)
+    },
+    orderNumber: {
+      display: "inline",
+      marginLeft: theme.spacing(1)
+    },
+    statusBar: {
+      paddingTop: 0
+    },
+    table: {
+      tableLayout: "fixed"
+    }
+  }),
+  { name: "TableLine" }
+);
+
+interface TableLineProps {
+  line: OrderDetails_order_fulfillments_lines;
+}
+
+const TableLine = ({ line }: TableLineProps) => {
+  const classes = useStyles({});
+
+  if (!line || !line.orderLine) {
+    return <Skeleton />;
+  }
+
+  return (
+    <TableRow
+      className={!!line ? classes.clickableRow : undefined}
+      hover={!!line}
+      key={line.id}
+    >
+      <TableCellAvatar
+        className={classes.colName}
+        thumbnail={maybe(() => line.orderLine.thumbnail.url)}
+      >
+        {maybe(() => line.orderLine.productName) || <Skeleton />}
+      </TableCellAvatar>
+      <TableCell className={classes.colSku}>
+        {line?.orderLine.productSku || <Skeleton />}
+      </TableCell>
+      <TableCell className={classes.colQuantity}>
+        {line?.quantity || <Skeleton />}
+      </TableCell>
+      <TableCell className={classes.colPrice}>
+        {maybe(() => line.orderLine.unitPrice.gross) ? (
+          <Money money={line.orderLine.unitPrice.gross} />
+        ) : (
+          <Skeleton />
+        )}
+      </TableCell>
+      <TableCell className={classes.colTotal}>
+        {maybe(() => line.quantity * line.orderLine.unitPrice.gross.amount) ? (
+          <Money
+            money={{
+              amount: line.quantity * line.orderLine.unitPrice.gross.amount,
+              currency: line.orderLine.unitPrice.gross.currency
+            }}
+          />
+        ) : (
+          <Skeleton />
+        )}
+      </TableCell>
+    </TableRow>
+  );
+};
+
+export default TableLine;
