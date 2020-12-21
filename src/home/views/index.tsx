5edd3439801d7e5c5b724e9c0e8a7fe5d8db7d2d
Marek Choiński <marekchoinski17@gmail.com>
2020-12-21 10:20:11 +0100

    Use query hook on home page

--- src/home/views/index.tsx
+++ src/home/views/index.tsx
@@ -8,57 +8,58 @@ import { orderListUrl } from "../../orders/urls";
 import { productListUrl, productVariantEditUrl } from "../../products/urls";
 import { OrderStatusFilter, StockAvailability } from "../../types/globalTypes";
 import HomePage from "../components/HomePage";
-import { HomePageQuery } from "../queries";
+import { useHomePage } from "../queries";
 
 const HomeSection = () => {
   const navigate = useNavigator();
   const { user } = useUser();
   const { channel } = useAppChannel();
 
+  const { data } = useHomePage({
+    displayLoader: true,
+    variables: { channel: channel.slug }
+  });
+
   return (
-    <HomePageQuery displayLoader variables={{ channel: channel.slug }}>
-      {({ data }) => (
-        <HomePage
-          activities={maybe(() =>
-            data.activities.edges.map(edge => edge.node).reverse()
-          )}
-          orders={maybe(() => data.ordersToday.totalCount)}
-          sales={maybe(() => data.salesToday.gross)}
-          topProducts={maybe(() =>
-            data.productTopToday.edges.map(edge => edge.node)
-          )}
-          onProductClick={(productId, variantId) =>
-            navigate(productVariantEditUrl(productId, variantId))
-          }
-          onOrdersToCaptureClick={() =>
-            navigate(
-              orderListUrl({
-                status: [OrderStatusFilter.READY_TO_CAPTURE]
-              })
-            )
-          }
-          onOrdersToFulfillClick={() =>
-            navigate(
-              orderListUrl({
-                status: [OrderStatusFilter.READY_TO_FULFILL]
-              })
-            )
-          }
-          onProductsOutOfStockClick={() =>
-            navigate(
-              productListUrl({
-                stockStatus: StockAvailability.OUT_OF_STOCK
-              })
-            )
-          }
-          ordersToCapture={maybe(() => data.ordersToCapture.totalCount)}
-          ordersToFulfill={maybe(() => data.ordersToFulfill.totalCount)}
-          productsOutOfStock={maybe(() => data.productsOutOfStock.totalCount)}
-          userName={getUserName(user, true)}
-          userPermissions={user?.userPermissions || []}
-        />
+    <HomePage
+      activities={maybe(() =>
+        data.activities.edges.map(edge => edge.node).reverse()
       )}
-    </HomePageQuery>
+      orders={maybe(() => data.ordersToday.totalCount)}
+      sales={maybe(() => data.salesToday.gross)}
+      topProducts={maybe(() =>
+        data.productTopToday.edges.map(edge => edge.node)
+      )}
+      onProductClick={(productId, variantId) =>
+        navigate(productVariantEditUrl(productId, variantId))
+      }
+      onOrdersToCaptureClick={() =>
+        navigate(
+          orderListUrl({
+            status: [OrderStatusFilter.READY_TO_CAPTURE]
+          })
+        )
+      }
+      onOrdersToFulfillClick={() =>
+        navigate(
+          orderListUrl({
+            status: [OrderStatusFilter.READY_TO_FULFILL]
+          })
+        )
+      }
+      onProductsOutOfStockClick={() =>
+        navigate(
+          productListUrl({
+            stockStatus: StockAvailability.OUT_OF_STOCK
+          })
+        )
+      }
+      ordersToCapture={maybe(() => data.ordersToCapture.totalCount)}
+      ordersToFulfill={maybe(() => data.ordersToFulfill.totalCount)}
+      productsOutOfStock={maybe(() => data.productsOutOfStock.totalCount)}
+      userName={getUserName(user, true)}
+      userPermissions={user?.userPermissions || []}
+    />
   );
 };
 
