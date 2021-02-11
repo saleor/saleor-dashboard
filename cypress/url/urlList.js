686ecb688814bd82cf4c855ba40e9caf3f70a412
Krzysztof Wolski <krzysztof.k.wolski@gmail.com>
2021-02-11 11:36:04 +0100

    Proper usage of baseUrl

--- cypress/url/urlList.js
+++ cypress/url/urlList.js
@@ -1,9 +1,9 @@
 export const urlList = {
   apiUri: Cypress.env("API_URI"),
-  channels: `${Cypress.env("APP_MOUNT_URI")}channels/`,
-  configuration: `${Cypress.env("APP_MOUNT_URI")}configuration/`,
-  homePage: Cypress.env("APP_MOUNT_URI"),
-  orders: `${Cypress.env("APP_MOUNT_URI")}orders/`,
-  products: `${Cypress.env("APP_MOUNT_URI")}products/`,
-  warehouses: `${Cypress.env("APP_MOUNT_URI")}warehouses/`
+  channels: "channels/",
+  configuration: "configuration/",
+  homePage: "/",
+  orders: "orders/",
+  products: "products/",
+  warehouses: "warehouses/"
 };
