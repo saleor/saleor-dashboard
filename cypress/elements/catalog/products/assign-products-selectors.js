cf0b3b1bf661c8370669101f88c14a15f3626943
Karolina Rakoczy <rakoczy.karolina@gmail.com>
2021-03-02 10:01:58 +0100

    tests for collections

--- cypress/elements/catalog/products/assign-products-selectors.js
+++ cypress/elements/catalog/products/assign-products-selectors.js
@@ -2,6 +2,6 @@
 export const ASSIGN_PRODUCTS_SELECTORS = {
   searchInput: "[name='query']",
   tableRow: "[class*='MuiTableRow']",
-  checkbox: "[class*='checkboxCell']",
+  checkbox: "[type='checkbox']",
   submitButton: "[type='submit']"
 };
