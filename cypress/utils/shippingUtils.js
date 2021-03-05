1434a9166ac4d7b7b63afc643f2f14353a685cdc
Karolina Rakoczy <rakoczy.karolina@gmail.com>
2021-03-05 14:08:34 +0100

    tests for draft orders

--- cypress/utils/shippingUtils.js
+++ cypress/utils/shippingUtils.js
@@ -4,13 +4,17 @@ import Warehouse from "../apiRequests/Warehouse";
 const shippingMethodRequest = new ShippingMethod();
 const warehouseRequest = new Warehouse();
 
+let shippingZone;
+let warehouse;
+let shippingMethod;
+
 export function createShipping({ channelId, name, address, price = 1 }) {
-  return this.createShippingZone(name, address.country)
-    .then(() => this.createWarehouse(name, this.shippingZone.id, address))
-    .then(() => this.createShippingRate(name, this.shippingZone.id))
+  return createShippingZone(name, address.country)
+    .then(() => createWarehouse(name, shippingZone.id, address))
+    .then(() => createShippingRate(name, shippingZone.id))
     .then(() =>
       shippingMethodRequest.addChannelToShippingMethod(
-        this.shippingMethod.id,
+        shippingMethod.id,
         channelId,
         price
       )
@@ -19,14 +23,14 @@ export function createShipping({ channelId, name, address, price = 1 }) {
 
 export function createShippingZone(name, country) {
   return shippingMethodRequest.createShippingZone(name, country).then(resp => {
-    this.shippingZone = resp.body.data.shippingZoneCreate.shippingZone;
+    shippingZone = resp.body.data.shippingZoneCreate.shippingZone;
   });
 }
 export function createWarehouse(name, shippingZoneId, address) {
   return warehouseRequest
     .createWarehouse(name, shippingZoneId, address)
     .then(resp => {
-      this.warehouse = resp.body.data.createWarehouse.warehouse;
+      warehouse = resp.body.data.createWarehouse.warehouse;
     });
 }
 export function createShippingRate(name, shippingZoneId) {
@@ -34,21 +38,20 @@ export function createShippingRate(name, shippingZoneId) {
     .createShippingRate(name, shippingZoneId)
     .then(
       resp =>
-        (this.shippingMethod =
-          resp.body.data.shippingPriceCreate.shippingMethod)
+        (shippingMethod = resp.body.data.shippingPriceCreate.shippingMethod)
     );
 }
 
 export function getShippingMethod() {
-  return this.shippingMethod;
+  return shippingMethod;
 }
 
 export function getShippingZone() {
-  return this.shippingZone;
+  return shippingZone;
 }
 
 export function getWarehouse() {
-  return this.warehouse;
+  return warehouse;
 }
 
 export function deleteShipping(startsWith) {
