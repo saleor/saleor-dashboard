97962b2b2eca1ae3c8cf27722ade146b74b2b38f
Magdalena Markusik <magdalena.markusik@mirumee.com>
2021-03-17 11:15:21 +0100

    Add naked input option to SingleAutocompleteSelectField and update it's stories

--- src/components/SingleAutocompleteSelectField/SingleAutocompleteSelectField.stories.tsx
+++ src/components/SingleAutocompleteSelectField/SingleAutocompleteSelectField.stories.tsx
@@ -31,7 +31,7 @@ const Story: React.FC<Partial<
   SingleAutocompleteSelectFieldProps & {
     enableLoadMore: boolean;
   }
->> = ({ allowCustomValues, emptyOption, enableLoadMore }) => {
+>> = ({ allowCustomValues, emptyOption, enableLoadMore, useNakedInput }) => {
   const [displayValue, setDisplayValue] = React.useState(suggestions[0].label);
 
   return (
@@ -59,6 +59,7 @@ const Story: React.FC<Partial<
                 onFetchMore={enableLoadMore ? onFetchMore : undefined}
                 allowCustomValues={allowCustomValues}
                 emptyOption={emptyOption}
+                useNakedInput={useNakedInput}
               />
             );
           }}
@@ -104,6 +105,7 @@ storiesOf("Generics / Select with autocomplete", module)
   .add("no data", () => (
     <SingleAutocompleteSelectFieldContent {...contentProps} choices={[]} />
   ))
+  .add("naked", () => <Story useNakedInput />)
   .add("interactive", () => <Story />)
   .add("interactive with custom option", () => (
     <Story allowCustomValues={true} />
