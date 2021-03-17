9ee1ee27b726dae1382acd25ae44bb54131fe60e
Magdalena Markusik <magdalena.markusik@mirumee.com>
2021-03-17 12:45:40 +0100

    Move getDefaultNotifierSuccessErrorData function to useNotifier utils, update dir etc., also make order discount provider use it from the new dir

---
+++ src/hooks/useNotifier/useNotifier.ts
@@ -0,0 +1,16 @@
+import { IMessage } from "@saleor/components/messages";
+import { IMessageContext, MessageContext } from "@saleor/components/messages";
+import { useContext } from "react";
+
+export type UseNotifierResult = IMessageContext;
+
+function useNotifier(): UseNotifierResult {
+  const notificationContext = useContext(MessageContext);
+
+  const notify = (options: IMessage) => {
+    const timeout = options.status === "error" ? null : options.autohide;
+    notificationContext.show(options, timeout);
+  };
+  return notify;
+}
+export default useNotifier;
