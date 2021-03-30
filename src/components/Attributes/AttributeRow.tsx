a6dc62bb0e320663b01c2b084c54190c89413ce9
Piotr Grundas <piotrgrundas@gmail.com>
2021-03-30 15:09:17 +0200

    Improvements

--- src/components/Attributes/AttributeRow.tsx
+++ src/components/Attributes/AttributeRow.tsx
@@ -3,141 +3,28 @@ import makeStyles from "@material-ui/core/styles/makeStyles";
 import { AttributeInput } from "@saleor/components/Attributes/Attributes";
 import BasicAttributeRow from "@saleor/components/Attributes/BasicAttributeRow";
 import ExtendedAttributeRow from "@saleor/components/Attributes/ExtendedAttributeRow";
-import FileUploadField, {
-  FileChoiceType
-} from "@saleor/components/FileUploadField";
-import MultiAutocompleteSelectField, {
-  MultiAutocompleteChoiceType
-} from "@saleor/components/MultiAutocompleteSelectField";
-import SingleAutocompleteSelectField, {
-  SingleAutocompleteChoiceType
-} from "@saleor/components/SingleAutocompleteSelectField";
-import SortableChipsField, {
-  SortableChipsFieldValueType
-} from "@saleor/components/SortableChipsField";
-import { AttributeValueFragment } from "@saleor/fragments/types/AttributeValueFragment";
+import {
+  getErrorMessage,
+  getFileChoice,
+  getMultiChoices,
+  getMultiDisplayValue,
+  getReferenceDisplayValue,
+  getSingleChoices
+} from "@saleor/components/Attributes/utils";
+import FileUploadField from "@saleor/components/FileUploadField";
+import MultiAutocompleteSelectField from "@saleor/components/MultiAutocompleteSelectField";
+import SingleAutocompleteSelectField from "@saleor/components/SingleAutocompleteSelectField";
+import SortableChipsField from "@saleor/components/SortableChipsField";
 import { PageErrorWithAttributesFragment } from "@saleor/fragments/types/PageErrorWithAttributesFragment";
 import { ProductErrorWithAttributesFragment } from "@saleor/fragments/types/ProductErrorWithAttributesFragment";
 import { FormsetChange } from "@saleor/hooks/useFormset";
 import { ReorderEvent } from "@saleor/types";
 import { AttributeInputTypeEnum } from "@saleor/types/globalTypes";
-import { getProductErrorMessage } from "@saleor/utils/errors";
-import getPageErrorMessage from "@saleor/utils/errors/page";
 import React from "react";
-import { defineMessages, IntlShape, useIntl } from "react-intl";
-
-function getSingleChoices(
-  values: AttributeValueFragment[]
-): SingleAutocompleteChoiceType[] {
-  return values.map(value => ({
-    label: value.name,
-    value: value.slug
-  }));
-}
-
-function getFileChoice(attribute: AttributeInput): FileChoiceType {
-  const attributeValue = attribute.value?.length > 0 && attribute.value[0];
-
-  const definedAttributeValue = attribute.data.values.find(
-    definedValue => definedValue.slug === attributeValue
-  );
-
-  if (definedAttributeValue) {
-    return {
-      file: definedAttributeValue.file,
-      label: definedAttributeValue.name,
-      value: definedAttributeValue.slug
-    };
-  }
-
-  return {
-    label: attributeValue,
-    value: attributeValue
-  };
-}
-
-function getReferenceDisplayValue(
-  attribute: AttributeInput
-): SortableChipsFieldValueType[] {
-  if (!attribute.value) {
-    return [];
-  }
-
-  return attribute.value.map(attributeValue => {
-    const definedAttributeValue = attribute.data.values.find(
-      definedValue => definedValue.reference === attributeValue
-    );
-    // If value has been previously assigned, use it's data
-    if (!!definedAttributeValue) {
-      return {
-        label: definedAttributeValue.name,
-        value: definedAttributeValue.reference
-      };
-    }
-
-    const definedAttributeReference = attribute.data.references?.find(
-      reference => reference.value === attributeValue
-    );
-    // If value has not been yet assigned, use data of reference
-    if (!!definedAttributeReference) {
-      return definedAttributeReference;
-    }
-
-    return {
-      label: attributeValue,
-      value: attributeValue
-    };
-  });
-}
-
-function getMultiChoices(
-  values: AttributeValueFragment[]
-): MultiAutocompleteChoiceType[] {
-  return values.map(value => ({
-    label: value.name,
-    value: value.slug
-  }));
-}
-
-function getMultiDisplayValue(
-  attribute: AttributeInput
-): MultiAutocompleteChoiceType[] {
-  if (!attribute.value) {
-    return [];
-  }
-
-  return attribute.value.map(attributeValue => {
-    const definedAttributeValue = attribute.data.values.find(
-      definedValue => definedValue.slug === attributeValue
-    );
-    if (!!definedAttributeValue) {
-      return {
-        label: definedAttributeValue.name,
-        value: definedAttributeValue.slug
-      };
-    }
-
-    return {
-      label: attributeValue,
-      value: attributeValue
-    };
-  });
-}
-
-function getErrorMessage(
-  err: ProductErrorWithAttributesFragment | PageErrorWithAttributesFragment,
-  intl: IntlShape
-): string {
-  switch (err?.__typename) {
-    case "ProductError":
-      return getProductErrorMessage(err, intl);
-    case "PageError":
-      return getPageErrorMessage(err, intl);
-  }
-}
+import { defineMessages, useIntl } from "react-intl";
 
 const messages = defineMessages({
-  multipleValueLable: {
+  multipleValueLabel: {
     defaultMessage: "Values",
     description: "attribute values"
   },
@@ -280,7 +167,7 @@ const AttributeRow: React.FC<AttributeRowProps> = ({
             disabled={disabled}
             error={!!error}
             helperText={getErrorMessage(error, intl)}
-            label={intl.formatMessage(messages.multipleValueLable)}
+            label={intl.formatMessage(messages.multipleValueLabel)}
             name={`attribute:${attribute.label}`}
             value={attribute.value}
             onChange={event => onMultiChange(attribute.id, event.target.value)}
