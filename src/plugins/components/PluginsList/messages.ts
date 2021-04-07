f935e47c69f9fa2de09db50ca07fc07c354476c4
Magdalena Markusik <magdalena.markusik@mirumee.com>
2021-04-07 13:01:12 +0200

    Update statusLabel component and add channel status label component for plugins list to use

---
+++ src/plugins/components/PluginsList/messages.ts
@@ -0,0 +1,12 @@
+import { defineMessages } from "react-intl";
+
+export const channelStatusLabelMessages = defineMessages({
+  globalTitle: {
+    defaultMessage: "Global plugin",
+    description: "channel status label global title"
+  },
+  channelTitle: {
+    defaultMessage: "Assigned to {activeChannelsCount}",
+    description: "channel status label channel title"
+  }
+});
