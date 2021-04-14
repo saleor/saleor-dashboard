07bc522af103100664fb18018d953a07c166cb27
Dominik Żegleń <flesz3@o2.pl>
2021-04-14 13:09:13 +0200

    Prevent endless limit refetching (#1059)
    * Fix endless limit refresh loop
    
    * Improve naming

--- src/components/Shop/query.ts
+++ src/components/Shop/query.ts
@@ -1,5 +1,5 @@
 import { limitFragment } from "@saleor/fragments/shop";
-import makeQuery from "@saleor/hooks/makeQuery";
+import makeQuery, { UseQueryOpts } from "@saleor/hooks/makeQuery";
 import gql from "graphql-tag";
 
 import { TypedQuery } from "../../queries";
@@ -39,21 +39,37 @@ const shopInfo = gql`
 `;
 export const TypedShopInfoQuery = TypedQuery<ShopInfo, {}>(shopInfo);
 
+const limitVariables: Record<keyof RefreshLimitsVariables, boolean> = {
+  channels: false,
+  orders: false,
+  productVariants: false,
+  staffUsers: false,
+  warehouses: false
+};
 const limitInfo = gql`
   ${limitFragment}
   query RefreshLimits(
-    $channels: Boolean = false
-    $orders: Boolean = false
-    $productVariants: Boolean = false
-    $staffUsers: Boolean = false
-    $warehouses: Boolean = false
+    $channels: Boolean!
+    $orders: Boolean!
+    $productVariants: Boolean!
+    $staffUsers: Boolean!
+    $warehouses: Boolean!
   ) {
     shop {
       ...ShopLimitFragment
     }
   }
 `;
-export const useShopLimitsQuery = makeQuery<
-  RefreshLimits,
-  RefreshLimitsVariables
->(limitInfo);
+const useBaseShopLimitsQuery = makeQuery<RefreshLimits, RefreshLimitsVariables>(
+  limitInfo
+);
+export const useShopLimitsQuery = (
+  opts: UseQueryOpts<Partial<RefreshLimitsVariables>>
+) =>
+  useBaseShopLimitsQuery({
+    ...opts,
+    variables: {
+      ...limitVariables,
+      ...opts.variables
+    }
+  });
