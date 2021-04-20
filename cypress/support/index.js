9956d88f98b1cdc4f3207a5134637c7cbb132bbe
Karolina Rakoczy <rakoczy.karolina@gmail.com>
2021-04-20 11:08:05 +0200

    add html report

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -2,8 +2,31 @@ import "./user";
 import "./softAssertions";
 import "./deleteElement/index.js";
 import "./elements/index";
+import "cypress-mochawesome-reporter/register";
+
+import addContext from "mochawesome/addContext";
 
 import { urlList } from "../url/urlList";
+
+Cypress.on("test:after:run", (test, runnable) => {
+  if (test.state === "failed") {
+    let item = runnable;
+    const nameParts = [runnable.title];
+
+    // Iterate through all parents and grab the titles
+    while (item.parent) {
+      nameParts.unshift(item.parent.title);
+      item = item.parent;
+    }
+
+    const fullTestName = nameParts.filter(Boolean).join(" -- "); // this is how cypress joins the test title fragments
+
+    const imageUrl = `screenshots/${Cypress.spec.name}/${fullTestName} `;
+    const imageUrlBeforeAll = `-- before all hook (failed).png`;
+
+    addContext({ test }, imageUrlBeforeAll);
+  }
+});
 
 Cypress.Commands.add("clearSessionData", () => {
   // Because of known cypress bug, not all local storage data are cleared.
