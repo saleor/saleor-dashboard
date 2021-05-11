38312983a62fd80dc23dc8a0707465c6d8d1dc91
Karolina Rakoczy <rakoczy.karolina@gmail.com>
2021-05-11 13:35:41 +0200

    fix failing tests

---
+++ cypress/elements/shared/addressForm.js
@@ -0,0 +1,13 @@
+export const ADDRESS_SELECTORS = {
+  firstName: '[name="firstName"]',
+  lastName: '[name="lastName"]',
+  companyName: '[name="companyName"]',
+  phone: '[name="phone"]',
+  streetAddress1: '[name="streetAddress1"]',
+  streetAddress2: '[name="streetAddress2"]',
+  city: '[name="city"]',
+  postalCode: '[name="postalCode"]',
+  country:
+    '[class*="AddressEdit"] > * > [class*="SingleAutocompleteSelectField"]',
+  countryArea: '[name="countryArea"]'
+};
--- src/orders/components/OrderCustomer/OrderCustomer.tsx
+++ src/orders/components/OrderCustomer/OrderCustomer.tsx
@@ -175,7 +175,12 @@ const OrderCustomer: React.FC<OrderCustomerProps> = props => {
           )
         ) : (
           <>
-            <Typography className={classes.userEmail}>{user.email}</Typography>
+            <Typography
+              className={classes.userEmail}
+              data-test-id="customer-email"
+            >
+              {user.email}
+            </Typography>
             <RequirePermissions
               userPermissions={userPermissions}
               requiredPermissions={[PermissionEnum.MANAGE_USERS]}
@@ -248,6 +253,7 @@ const OrderCustomer: React.FC<OrderCustomerProps> = props => {
           {canEditAddresses && (
             <div className={classes.sectionHeaderToolbar}>
               <Button
+                data-test-id="edit-shipping-address"
                 color="primary"
                 variant="text"
                 onClick={onShippingAddressEdit}
@@ -305,6 +311,7 @@ const OrderCustomer: React.FC<OrderCustomerProps> = props => {
           {canEditAddresses && (
             <div className={classes.sectionHeaderToolbar}>
               <Button
+                data-test-id="edit-billing-address"
                 color="primary"
                 variant="text"
                 onClick={onBillingAddressEdit}
