0b1d0a3a54d1620e6f1f4565aca2de79fa6ffc56
Karolina Rakoczy <rakoczy.karolina@gmail.com>
2021-05-31 09:50:31 +0200

    Saleor 3357 tests for orders (#1119)
    * add tests for puchase by type
    
    * add expect for isShippingRequired
    
    * tests for adyen
    
    * tests for adyen
    
    * tests for adyen
    
    * tests for adyen
    
    * adyen
    
    * adyen
    
    * adyen
    
    * adyen
    
    * adyen
    
    * warehouses in chcekout
    
    * rm warehouses in chcekout
    
    * adyen payments
    
    * adyen
    
    * tests for orders
    
    * fix shipping
    
    * unskipp
    
    * orders
    
    * add empty lines
    
    * add empty lines and reduce
    
    * add empty lines

--- cypress/elements/orders/orders-selectors.js
+++ cypress/elements/orders/orders-selectors.js
@@ -2,5 +2,9 @@ export const ORDERS_SELECTORS = {
   orders: "[data-test='submenu-item-label'][data-test-id='orders']",
   createOrder: "[data-test-id='create-order-button']",
   orderRow: "[data-test-id='order-table-row']",
-  salesChannel: "[data-test-id='order-sales-channel']"
+  salesChannel: "[data-test-id='order-sales-channel']",
+  cancelFulfillment: "[data-test-id='cancelFulfillmentButton']",
+  cancelFulfillmentSelectField: "[data-test-id='cancelFulfillmentSelectField']",
+  orderFulfillmentFrame: "[data-test-id='orderFulfillment']",
+  refundButton: '[data-test-id="refund-button"]'
 };
