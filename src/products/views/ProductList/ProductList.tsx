120a80b9179f88120477db16f828e0856f5fdf9b
Dominik Żegleń <flesz3@o2.pl>
2021-06-24 14:26:57 +0200

    Fix product filters

--- src/products/views/ProductList/ProductList.tsx
+++ src/products/views/ProductList/ProductList.tsx
@@ -347,7 +347,7 @@ export const ProductList: React.FC<ProductListProps> = ({ params }) => {
 
   const filterOpts = getFilterOpts(
     params,
-    mapEdgesToItems(initialFilterAttributes?.attributes),
+    mapEdgesToItems(initialFilterAttributes?.attributes) || [],
     searchAttributeValues,
     {
       initial: mapEdgesToItems(initialFilterCategories?.categories) || [],
