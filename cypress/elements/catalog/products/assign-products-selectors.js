f5c5a8770ce5f322e8ae63e29ba31e659c1e16a4
Karolina Rakoczy <rakoczy.karolina@gmail.com>
2021-07-05 12:21:35 +0200

    tests for product types (#1199)

--- cypress/elements/catalog/products/assign-products-selectors.js
+++
@@ -1,7 +0,0 @@
-export const ASSIGN_PRODUCTS_SELECTORS = {
-  searchInput: "[name='query']",
-  tableRow: "[class*='MuiTableRow']",
-  checkbox: "[type='checkbox']",
-  submitButton: "[type='submit']",
-  dialogContent: '[data-test-id="searchQuery"]'
-};
