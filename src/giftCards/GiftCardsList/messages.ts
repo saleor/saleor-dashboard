22801efdb4e61c9cc7f225f1a5985f620dd10ca9
Magdalena Markusik <magdalena.markusik@mirumee.com>
2021-07-16 12:47:48 +0200

    Add gift cards list table wip

---
+++ src/giftCards/GiftCardsList/messages.ts
@@ -0,0 +1,43 @@
+import { defineMessages } from "react-intl";
+
+export const giftCardsListHeaderMenuItemsMessages = defineMessages({
+  settings: {
+    defaultMessage: "Settings",
+    description: "GiftCardsListHeader menu item settings"
+  },
+  bulkIssue: {
+    defaultMessage: "Bulk Issue",
+    description: "GiftCardsListHeader menu item settings"
+  },
+  exportCodes: {
+    defaultMessage: "Export card codes",
+    description: "GiftCardsListHeader menu item settings"
+  }
+});
+
+export const giftCardsListTableMessages = defineMessages({
+  giftCardsTableColumnGiftCardTitle: {
+    defaultMessage: "Gift Card",
+    description: "GiftCardsListTable column title gift card"
+  },
+  giftCardsTableColumnTagTitle: {
+    defaultMessage: "Tag",
+    description: "GiftCardsListTable column title tag"
+  },
+  giftCardsTableColumnProductTitle: {
+    defaultMessage: "Product",
+    description: "GiftCardsListTable column title product"
+  },
+  giftCardsTableColumnCustomerTitle: {
+    defaultMessage: "Customer",
+    description: "GiftCardsListTable column title customer"
+  },
+  giftCardsTableColumnBalanceTitle: {
+    defaultMessage: "Balance",
+    description: "GiftCardsListTable column title balance"
+  },
+  codeEndingWithLabel: {
+    defaultMessage: "Code ending with ",
+    description: "GiftCardsListTable code ending with label"
+  }
+});
