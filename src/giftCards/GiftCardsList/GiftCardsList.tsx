6ceb2504f4a15230f0e2942ad42352b4639e7213
Magdalena Markusik <magdalena.markusik@mirumee.com>
2021-07-19 16:14:50 +0200

    Add / update gift cards list components

--- src/giftCards/GiftCardsList/GiftCardsList.tsx
+++ src/giftCards/GiftCardsList/GiftCardsList.tsx
@@ -1,14 +1,22 @@
 import Container from "@saleor/components/Container";
+import useBulkActions from "@saleor/hooks/useBulkActions";
+import { BulkActionsProvider } from "@saleor/hooks/useBulkActions/BulkActionsProvider";
 import React from "react";
 
 import GiftCardsListHeader from "./GiftCardsListHeader";
 import GiftCardsListTable from "./GiftCardsListTable";
 
-const GiftCardsList: React.FC = () => (
-  <Container>
-    <GiftCardsListHeader />
-    <GiftCardsListTable />
-  </Container>
-);
+const GiftCardsList: React.FC = () => {
+  const bulkActions = useBulkActions();
+
+  return (
+    // <BulkActionsProvider>
+    <Container>
+      <GiftCardsListHeader />
+      <GiftCardsListTable bulkActions={bulkActions} />
+    </Container>
+    // </BulkActionsProvider>
+  );
+};
 
 export default GiftCardsList;
