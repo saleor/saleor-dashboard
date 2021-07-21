a524f37c1017997ecaa64778a91e9e8a80cfee00
Magdalena Markusik <magdalena.markusik@mirumee.com>
2021-07-21 12:33:23 +0200

    Add gift card tag input component along with necessary queries

---
+++ src/giftCards/components/GiftCardTagInput/useGiftCardTagsSearch.ts
@@ -0,0 +1,43 @@
+import { pageInfoFragment } from "@saleor/fragments/pageInfo";
+import makeTopLevelSearch from "@saleor/hooks/makeTopLevelSearch";
+import gql from "graphql-tag";
+
+import {
+  SearchGiftCardTags,
+  SearchGiftCardTagsVariables
+} from "./types/SearchGiftCardTags";
+
+const searchGiftCardTags = gql`
+  ${pageInfoFragment}
+  query SearchGiftCardTags(
+    $query: String!
+    $first: Int!
+    $after: String
+    $last: Int
+    $before: String
+  ) {
+    search: giftCards(
+      filter: { tag: $query }
+      first: $first
+      after: $after
+      last: $last
+      before: $before
+    ) {
+      totalCount
+      edges {
+        node {
+          id
+          tag
+        }
+      }
+      pageInfo {
+        ...PageInfoFragment
+      }
+    }
+  }
+`;
+
+export default makeTopLevelSearch<
+  SearchGiftCardTags,
+  SearchGiftCardTagsVariables
+>(searchGiftCardTags);
