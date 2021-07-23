e4ba7bfc08a7f0f0925ec8adee398dadcb3ac092
Magdalena Markusik <magdalena.markusik@mirumee.com>
2021-07-23 16:26:45 +0200

    Add gift card update info card and refactor

--- src/giftCards/GiftCardUpdatePage/GiftCardDetailsProvider.tsx
+++ src/giftCards/GiftCardUpdatePage/GiftCardDetailsProvider.tsx
@@ -1,4 +1,7 @@
-import { GiftCardExpiryType, TimePeriodType } from "@saleor/types/globalTypes";
+import {
+  GiftCardExpiryTypeEnum,
+  TimePeriodTypeEnum
+} from "@saleor/types/globalTypes";
 import React, { createContext } from "react";
 
 import { GiftCardDetails_giftCard } from "./types/GiftCardDetails";
@@ -23,9 +26,27 @@ const GiftCardDetailsProvider: React.FC<GiftCardDetailsProviderProps> = ({
     id: "1234",
     code: "8361",
     isActive: false,
-    expiryType: GiftCardExpiryType.EXPIRY_PERIOD,
+    expiryType: GiftCardExpiryTypeEnum.EXPIRY_PERIOD,
+    events: [],
+    createdBy: {
+      id: "1222",
+      firstName: "Janusz",
+      lastName: "Kowalski"
+    },
+    product: {
+      id: "1234",
+      name: "Greatest product"
+    },
+    created: "2020-04-01",
+    createdByEmail: "foobar@gmail.com",
+    usedBy: {
+      id: "1234",
+      firstName: "Krzesimir",
+      lastName: "Majsterkowicz"
+    },
+    usedByEmail: "elfo.mail@gmail.com",
     expiryPeriod: {
-      type: TimePeriodType.MONTH,
+      type: TimePeriodTypeEnum.MONTH,
       amount: 12
     },
     currentBalance: {
---
+++ src/giftCards/GiftCardUpdatePage/GiftCardUpdateDetailsCard/TimePeriodField.tsx
@@ -0,0 +1,53 @@
+import { FormChange } from "@saleor/hooks/useForm";
+import { TimePeriodTypeEnum } from "@saleor/types/globalTypes";
+import React from "react";
+import { useIntl } from "react-intl";
+
+import { timePeriodTextWithSelectFieldMessages as messages } from "./messages";
+import TextWithSelectField from "./TextWithSelectField";
+
+interface TimePeriodFieldProps<T> {
+  change: FormChange;
+  periodAmount: number;
+  periodType: TimePeriodTypeEnum;
+  amountFieldName: keyof T;
+  typeFieldName: keyof T;
+}
+
+function TimePeriodField<T>({
+  change,
+  periodAmount,
+  periodType,
+  amountFieldName,
+  typeFieldName
+}: TimePeriodFieldProps<T>) {
+  const intl = useIntl();
+
+  const options = [
+    {
+      label: intl.formatMessage(messages.yearLabel),
+      value: TimePeriodTypeEnum.YEAR
+    },
+    {
+      label: intl.formatMessage(messages.monthLabel),
+      value: TimePeriodTypeEnum.MONTH
+    },
+    {
+      label: intl.formatMessage(messages.dayLabel),
+      value: TimePeriodTypeEnum.DAY
+    }
+  ];
+
+  return (
+    <TextWithSelectField
+      choices={options}
+      change={change}
+      textFieldName={amountFieldName as string}
+      selectFieldName={typeFieldName as string}
+      textFieldValue={periodAmount}
+      selectFieldValue={periodType}
+    />
+  );
+}
+
+export default TimePeriodField;
