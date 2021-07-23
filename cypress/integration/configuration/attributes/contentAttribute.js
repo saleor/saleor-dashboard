1f01a09e8750cc2e23566d1988e4a31463379575
Karolina Rakoczy <rakoczy.karolina@gmail.com>
2021-07-23 12:46:44 +0300

    add tags (#1251)
    * add tags
    
    * edit e2e.yaml
    
    * fix test for customer registration
    
    * add empty lines

---
+++ cypress/integration/configuration/attributes/contentAttribute.js
@@ -0,0 +1,123 @@
+import faker from "faker";
+
+import { getAttribute } from "../../../apiRequests/Attribute";
+import { ATTRIBUTES_DETAILS } from "../../../elements/attribute/attributes_details";
+import { ATTRIBUTES_LIST } from "../../../elements/attribute/attributes_list";
+import { createAttributeWithInputType } from "../../../steps/attributesSteps";
+import filterTests from "../../../support/filterTests";
+import { urlList } from "../../../url/urlList";
+import { deleteAttributesStartsWith } from "../../../utils/attributes/attributeUtils";
+import { expectCorrectDataInAttribute } from "../../../utils/attributes/checkAttributeData";
+
+filterTests(["all"], () => {
+  describe("Create content attribute", () => {
+    const startsWith = "AttrCont";
+    const attributesTypes = [
+      "DROPDOWN",
+      "MULTISELECT",
+      "FILE",
+      "RICH_TEXT",
+      "BOOLEAN"
+    ];
+    const attributeReferenceType = ["PRODUCT", "PAGE"];
+    const attributeNumericType = [
+      { unitSystem: "IMPERIAL", unitsOf: "DISTANCE", unit: "FT" },
+      { unitSystem: "METRIC", unitsOf: "VOLUME", unit: "CUBIC_CENTIMETER" },
+      { unitSystem: "without selecting unit" }
+    ];
+
+    before(() => {
+      cy.clearSessionData().loginUserViaRequest();
+      deleteAttributesStartsWith(startsWith);
+    });
+
+    beforeEach(() => {
+      cy.clearSessionData()
+        .loginUserViaRequest()
+        .visit(urlList.attributes)
+        .get(ATTRIBUTES_LIST.createAttributeButton)
+        .click()
+        .get(ATTRIBUTES_DETAILS.pageTypeAttributeCheckbox)
+        .click();
+    });
+    attributesTypes.forEach(attributeType => {
+      it(`should create ${attributeType} attribute`, () => {
+        const attributeName = `${startsWith}${faker.datatype.number()}`;
+        createAttributeWithInputType({ name: attributeName, attributeType })
+          .then(({ attribute }) => {
+            getAttribute(attribute.id);
+          })
+          .then(attribute => {
+            expectCorrectDataInAttribute(attribute, {
+              attributeName,
+              attributeType
+            });
+          });
+      });
+    });
+
+    attributeReferenceType.forEach(entityType => {
+      it(`should create reference ${entityType} attribute`, () => {
+        const attributeType = "REFERENCE";
+        const attributeName = `${startsWith}${faker.datatype.number()}`;
+        createAttributeWithInputType({
+          name: attributeName,
+          attributeType,
+          entityType
+        })
+          .then(({ attribute }) => {
+            getAttribute(attribute.id);
+          })
+          .then(attribute => {
+            expectCorrectDataInAttribute(attribute, {
+              attributeName,
+              attributeType,
+              entityType
+            });
+          });
+      });
+    });
+
+    attributeNumericType.forEach(numericSystemType => {
+      it(`should create numeric attribute - ${numericSystemType.unitSystem}`, () => {
+        const attributeType = "NUMERIC";
+        const attributeName = `${startsWith}${faker.datatype.number()}`;
+        createAttributeWithInputType({
+          name: attributeName,
+          attributeType,
+          numericSystemType
+        })
+          .then(({ attribute }) => {
+            getAttribute(attribute.id);
+          })
+          .then(attribute => {
+            expectCorrectDataInAttribute(attribute, {
+              attributeName,
+              attributeType,
+              unit: numericSystemType.unit
+            });
+          });
+      });
+    });
+
+    it("should create attribute without required value", () => {
+      const attributeType = "BOOLEAN";
+      const attributeName = `${startsWith}${faker.datatype.number()}`;
+      createAttributeWithInputType({
+        name: attributeName,
+        attributeType,
+        valueRequired: false
+      })
+        .then(({ attribute }) => {
+          getAttribute(attribute.id);
+        })
+        .then(attribute => {
+          expectCorrectDataInAttribute(attribute, {
+            attributeName,
+            attributeType,
+            valueRequired: false
+          });
+        });
+    });
+  });
+});
---
+++ cypress/integration/configuration/shippingMethods/postalCodes.js
@@ -0,0 +1,137 @@
+// <reference types="cypress" />
+import faker from "faker";
+
+import { createCheckout } from "../../../apiRequests/Checkout";
+import { createShippingZone } from "../../../apiRequests/ShippingMethod";
+import { createWarehouse } from "../../../apiRequests/Warehouse";
+import { ONE_PERMISSION_USERS } from "../../../Data/users";
+import {
+  createRateWithPostalCode,
+  postalCodesOptions
+} from "../../../steps/shippingMethodSteps";
+import filterTests from "../../../support/filterTests";
+import { shippingZoneDetailsUrl } from "../../../url/urlList";
+import { getDefaultChannel } from "../../../utils/channelsUtils";
+import {
+  createProductInChannel,
+  createTypeAttributeAndCategoryForProduct,
+  deleteProductsStartsWith
+} from "../../../utils/products/productsUtils";
+import { deleteShippingStartsWith } from "../../../utils/shippingUtils";
+import { isShippingAvailableInCheckout } from "../../../utils/storeFront/checkoutUtils";
+
+filterTests(["all"], () => {
+  describe("Postal codes in shipping", () => {
+    const startsWith = "CyShippingMethods-";
+    const name = `${startsWith}${faker.datatype.number()}`;
+
+    const price = 10;
+
+    let defaultChannel;
+    let usAddress;
+    let secondUsAddress;
+    let shippingZone;
+    let warehouse;
+    let variantsList;
+
+    before(() => {
+      cy.clearSessionData().loginUserViaRequest();
+      deleteShippingStartsWith(startsWith);
+      deleteProductsStartsWith(startsWith);
+
+      getDefaultChannel()
+        .then(channel => {
+          defaultChannel = channel;
+          cy.fixture("addresses");
+        })
+        .then(
+          ({
+            usAddress: usAddressResp,
+            secondUsAddress: secondUsAddressResp
+          }) => {
+            usAddress = usAddressResp;
+            secondUsAddress = secondUsAddressResp;
+            createShippingZone(name, "US", defaultChannel.id);
+          }
+        )
+        .then(shippingZoneResp => {
+          shippingZone = shippingZoneResp;
+          createWarehouse({
+            name,
+            shippingZone: shippingZone.id,
+            address: usAddress
+          });
+        })
+        .then(warehouseResp => {
+          warehouse = warehouseResp;
+          createTypeAttributeAndCategoryForProduct(name);
+        })
+        .then(({ attribute, productType, category }) => {
+          createProductInChannel({
+            name,
+            channelId: defaultChannel.id,
+            warehouseId: warehouse.id,
+            attributeId: attribute.id,
+            categoryId: category.id,
+            productTypeId: productType.id
+          });
+        })
+        .then(({ variantsList: variantsListResp }) => {
+          variantsList = variantsListResp;
+        });
+    });
+
+    beforeEach(() => {
+      cy.clearSessionData()
+        .loginUserViaRequest("auth", ONE_PERMISSION_USERS.shipping)
+        .visit(shippingZoneDetailsUrl(shippingZone.id));
+    });
+
+    it("Create shipping method with included postal codes", () => {
+      const rateName = `${startsWith}${faker.datatype.number()}`;
+
+      createRateWithPostalCode({
+        rateName,
+        price,
+        postalCodeOption: postalCodesOptions.INCLUDE_OPTION,
+        maxPostalCode: usAddress.postalCode,
+        minPostalCode: usAddress.postalCode
+      });
+      isShippingAvailableForAddress(usAddress, rateName).then(
+        isAvailable => expect(isAvailable).to.be.true
+      );
+      isShippingAvailableForAddress(secondUsAddress, rateName).then(
+        isAvailable => expect(isAvailable).to.be.false
+      );
+    });
+
+    it("Create shipping method with excluded postal codes", () => {
+      const rateName = `${startsWith}${faker.datatype.number()}`;
+
+      createRateWithPostalCode({
+        rateName,
+        price,
+        postalCodeOption: postalCodesOptions.EXCLUDE_OPTION,
+        maxPostalCode: usAddress.postalCode,
+        minPostalCode: usAddress.postalCode
+      });
+      isShippingAvailableForAddress(usAddress, rateName).then(
+        isAvailable => expect(isAvailable).to.be.false
+      );
+      isShippingAvailableForAddress(secondUsAddress, rateName).then(
+        isAvailable => expect(isAvailable).to.be.true
+      );
+    });
+
+    function isShippingAvailableForAddress(address, rateName) {
+      return createCheckout({
+        address,
+        channelSlug: defaultChannel.slug,
+        email: "example@example.com",
+        variantsList
+      }).then(({ checkout }) =>
+        isShippingAvailableInCheckout(checkout, rateName)
+      );
+    }
+  });
+});
