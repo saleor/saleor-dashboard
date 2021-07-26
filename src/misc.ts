a0f3fb1526f34676c2941f78c29b570fdc267982
Magdalena Markusik <magdalena.markusik@mirumee.com>
2021-07-26 11:58:22 +0200

    Refactor

--- src/misc.ts
+++ src/misc.ts
@@ -429,8 +429,11 @@ export function transformFormToAddressInput<T>(
   };
 }
 
-export function getStringOrPlaceholder(s: string | undefined): string {
-  return s || "...";
+export function getStringOrPlaceholder(
+  s: string | undefined,
+  placeholder?: string
+): string {
+  return s || placeholder || "...";
 }
 
 export const getDatePeriod = (days: number): DateRangeInput => {
@@ -467,7 +470,7 @@ export const transformAddressToAddressInput = (data?: AddressType) => ({
 export function getFullName<T extends { firstName: string; lastName: string }>(
   data: T
 ) {
-  if (!data) {
+  if (!data || !data.firstName || !data.lastName) {
     return "";
   }
 
