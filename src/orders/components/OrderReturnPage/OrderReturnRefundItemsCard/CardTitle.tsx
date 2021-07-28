591a33a4f8d0c08ba6a7234efd454da1164eb36c
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2021-07-28 09:43:28 +0200

    Display warehouse name for each fulfillment (#1259)

--- src/orders/components/OrderReturnPage/OrderReturnRefundItemsCard/CardTitle.tsx
+++ src/orders/components/OrderReturnPage/OrderReturnRefundItemsCard/CardTitle.tsx
@@ -6,14 +6,24 @@ import { makeStyles } from "@saleor/macaw-ui";
 import { FulfillmentStatus } from "@saleor/types/globalTypes";
 import camelCase from "lodash/camelCase";
 import React from "react";
+import { FormattedMessage } from "react-intl";
 import { defineMessages } from "react-intl";
 import { useIntl } from "react-intl";
 
 const useStyles = makeStyles(
   theme => ({
+    title: {
+      width: "100%",
+      display: "flex"
+    },
     orderNumber: {
       display: "inline",
       marginLeft: theme.spacing(1)
+    },
+    warehouseName: {
+      float: "right",
+      color: theme.palette.text.secondary,
+      margin: `auto ${theme.spacing(1)}px auto auto`
     }
   }),
   { name: "CardTitle" }
@@ -47,6 +57,10 @@ const messages = defineMessages({
   unfulfilled: {
     defaultMessage: "Unfulfilled",
     description: "section header"
+  },
+  fulfilledFrom: {
+    defaultMessage: "Fulfilled from {warehouseName}",
+    description: "fulfilled fulfillment, section header"
   }
 });
 
@@ -62,6 +76,7 @@ interface CardTitleProps {
   status: CardTitleStatus;
   toolbar?: React.ReactNode;
   orderNumber?: string;
+  warehouseName?: string;
   withStatus?: boolean;
 }
 
@@ -89,6 +104,7 @@ const CardTitle: React.FC<CardTitleProps> = ({
   fulfillmentOrder,
   status,
   orderNumber = "",
+  warehouseName,
   withStatus = false,
   toolbar
 }) => {
@@ -108,7 +124,7 @@ const CardTitle: React.FC<CardTitleProps> = ({
   );
 
   const title = (
-    <>
+    <div className={classes.title}>
       {intl.formatMessage(messageForStatus, {
         fulfillmentName,
         quantity: totalQuantity
@@ -116,7 +132,17 @@ const CardTitle: React.FC<CardTitleProps> = ({
       <Typography className={classes.orderNumber} variant="body1">
         {fulfillmentName}
       </Typography>
-    </>
+      {!!warehouseName && (
+        <Typography className={classes.warehouseName} variant="caption">
+          <FormattedMessage
+            {...messages.fulfilledFrom}
+            values={{
+              warehouseName
+            }}
+          />
+        </Typography>
+      )}
+    </div>
   );
 
   return (
