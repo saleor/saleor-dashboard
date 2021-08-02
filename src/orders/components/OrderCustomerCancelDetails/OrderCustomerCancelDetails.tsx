7c9b2fdb28981e9403f710ee6b767b9e1c49d409
Albert Miclat <albertjmiclat@gmail.com>
2021-08-02 23:23:51 +0800

    [fix] build fixes

--- src/orders/components/OrderCustomerCancelDetails/OrderCustomerCancelDetails.tsx
+++ src/orders/components/OrderCustomerCancelDetails/OrderCustomerCancelDetails.tsx
@@ -1,10 +1,11 @@
-import React, { Fragment } from "react";
-import { FormattedMessage, useIntl } from "react-intl";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
-import CardTitle from "@saleor/components/CardTitle";
 import { CardSpacer } from "@saleor/components/CardSpacer";
+import CardTitle from "@saleor/components/CardTitle";
+import React, { Fragment } from "react";
+import { FormattedMessage, useIntl } from "react-intl";
+
 import { OrderDetails_order_metadata } from "../../types/OrderDetails";
 
 interface Props {
@@ -12,20 +13,24 @@ interface Props {
 }
 
 const CancelKeys = {
-  REASON: 'cancellation_reason',
-  CUSTOM_REASON: 'cancellation_choice_custom_value',
+  CUSTOM_REASON: "cancellation_choice_custom_value",
+  REASON: "cancellation_reason"
 };
 
 const OrderCustomerCancelDetails: React.FC<Props> = ({ metadata }) => {
   const intl = useIntl();
 
-  const cancelReasonsMetadata = metadata.filter(({ key }) => {
-    return [CancelKeys.REASON, CancelKeys.CUSTOM_REASON].includes(key);
-  }).sort(({ key }) => {
-    if (key === CancelKeys.REASON) return -1;
+  const cancelReasonsMetadata = metadata
+    .filter(({ key }) =>
+      [CancelKeys.REASON, CancelKeys.CUSTOM_REASON].includes(key)
+    )
+    .sort(({ key }) => {
+      if (key === CancelKeys.REASON) {
+        return -1;
+      }
 
-    return 0;
-  });
+      return 0;
+    });
 
   const LABELS = {
     [CancelKeys.REASON]: intl.formatMessage({
@@ -35,7 +40,7 @@ const OrderCustomerCancelDetails: React.FC<Props> = ({ metadata }) => {
     [CancelKeys.CUSTOM_REASON]: intl.formatMessage({
       defaultMessage: "Custom Response",
       description: "order cancellation custom reason"
-    }),
+    })
   };
 
   return (
@@ -50,17 +55,15 @@ const OrderCustomerCancelDetails: React.FC<Props> = ({ metadata }) => {
         />
         <CardContent>
           {cancelReasonsMetadata.length ? (
-            cancelReasonsMetadata.map(({ key, value }) => {
-              return (
-                <Fragment key={key}>
-                  <Typography style={{ fontWeight: 600 }}>
-                    {LABELS[key].toUpperCase()}
-                  </Typography>
-                  <Typography>{value || '-'}</Typography>
-                  <CardSpacer />
-                </Fragment>
-              );
-            })
+            cancelReasonsMetadata.map(({ key, value }) => (
+              <Fragment key={key}>
+                <Typography style={{ fontWeight: 600 }}>
+                  {LABELS[key].toUpperCase()}
+                </Typography>
+                <Typography>{value || "-"}</Typography>
+                <CardSpacer />
+              </Fragment>
+            ))
           ) : (
             <Typography color="textSecondary">
               <FormattedMessage defaultMessage="No details available" />
@@ -70,6 +73,6 @@ const OrderCustomerCancelDetails: React.FC<Props> = ({ metadata }) => {
       </Card>
     </Fragment>
   );
-}
+};
 
 export default OrderCustomerCancelDetails;
