5c009e468b5d654e3f14709bc34aa322e8fb7385
Magdalena Markusik <magdalena.markusik@mirumee.com>
2021-08-03 13:33:13 +0200

    Add gift card update info card

--- src/giftCards/GiftCardUpdatePage/GiftCardUpdateDetailsCard/GiftCardUpdateDetailsBalanceSection.tsx
+++ src/giftCards/GiftCardUpdatePage/GiftCardUpdateDetailsCard/GiftCardUpdateDetailsBalanceSection.tsx
@@ -2,18 +2,15 @@ import { Typography } from "@material-ui/core";
 import HorizontalSpacer from "@saleor/apps/components/HorizontalSpacer";
 import CardSpacer from "@saleor/components/CardSpacer";
 import Money from "@saleor/components/Money";
-import { useTheme } from "@saleor/macaw-ui";
-import { isDarkTheme } from "@saleor/misc";
 import classNames from "classnames";
 import React, { useContext } from "react";
 import { useIntl } from "react-intl";
 
-import { GiftCardDetailsContext } from "../GiftCardDetailsProvider";
+import { GiftCardDetailsContext } from "../providers/GiftCardDetailsProvider";
 import { giftCardUpdateDetailsCardMessages as messages } from "./messages";
 import { useGiftCardDetailsBalanceStyles as useStyles } from "./styles";
 
 const GiftCardUpdateDetailsBalanceSection: React.FC = () => {
-  const { themeType } = useTheme();
   const classes = useStyles({});
   const intl = useIntl();
 
@@ -43,9 +40,7 @@ const GiftCardUpdateDetailsBalanceSection: React.FC = () => {
       <div className={classes.balanceBar}>
         <div
           style={{ width: `${progressBarWidth}%` }}
-          className={classNames(classes.balanceBarProgress, {
-            [classes.balanceBarProgressDark]: isDarkTheme(themeType)
-          })}
+          className={classes.balanceBarProgress}
         />
       </div>
     </>
