d1f1fbf2941518e8c3d8e85db0884e64160433b4
Magdalena Markusik <magdalena.markusik@mirumee.com>
2021-08-03 13:27:36 +0200

    Add content or skeleton component

---
+++ src/components/ContentOrSkeleton.tsx
@@ -0,0 +1,14 @@
+import Skeleton from "@saleor/components/Skeleton";
+import React from "react";
+
+interface ContentOrSkeletonProps {
+  condition: any;
+  children: React.ReactNode;
+}
+
+const ContentOrSkeleton: React.FC<ContentOrSkeletonProps> = ({
+  condition,
+  children
+}) => (!!condition ? <>{children}</> : <Skeleton />);
+
+export default ContentOrSkeleton;
