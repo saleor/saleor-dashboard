6b05a07a156b6b4a06757b87ed761e7f78cfcdd0
UL CORP <40263018+johndejohn@users.noreply.github.com>
2021-08-09 23:28:14 +0300

    api https update

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ const htmlWebpackPlugin = new HtmlWebpackPlugin({
   template: "./src/index.html"
 });
 const environmentPlugin = new webpack.EnvironmentPlugin({
-  API_URI: "http://api.uyghur.store:8000/graphql/",
+  API_URI: "https://api.uyghur.store/graphql/",
   APP_MOUNT_URI: "/",
   DEMO_MODE: false,
   ENVIRONMENT: "",
