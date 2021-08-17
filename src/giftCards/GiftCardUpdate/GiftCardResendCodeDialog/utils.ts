145366dcf1e358520f51b652cac0c9ac2ac38d24
Magdalena Markusik <magdalena.markusik@mirumee.com>
2021-08-17 19:32:44 +0200

    Add use dialog form reset hook and make update balance dialog use it, to fix it displaying form errors after modal close and open

---
+++ src/giftCards/GiftCardUpdate/GiftCardResendCodeDialog/utils.ts
@@ -0,0 +1,34 @@
+import { UserError } from "@saleor/types";
+import { FormErrors, getFormErrors } from "@saleor/utils/errors";
+import { useEffect, useState } from "react";
+
+export function useDialogFormReset<
+  TError extends UserError,
+  TKey extends string
+>({
+  reset,
+  apiErrors,
+  keys,
+  open
+}: {
+  reset: () => void;
+  apiErrors: TError[];
+  keys: TKey[];
+  open: boolean;
+}) {
+  const [formErrors, setFormErrors] = useState<FormErrors<TKey, TError>>(null);
+
+  useEffect(() => {
+    if (!open) {
+      setFormErrors(null);
+      reset();
+    }
+  }, [open]);
+
+  useEffect(() => {
+    const errors = getFormErrors(keys, apiErrors);
+    setFormErrors(errors);
+  }, [apiErrors]);
+
+  return { formErrors };
+}
