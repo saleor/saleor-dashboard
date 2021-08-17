8354c20bb3fcae7b67ac6d3b56c71de2dcfc55ec
Magdalena Markusik <magdalena.markusik@mirumee.com>
2021-08-17 19:32:44 +0200

    Add use dialog form reset hook and make update balance dialog use it, to fix it displaying form errors after modal close and open

--- src/giftCards/GiftCardUpdate/GiftCardUpdateBalanceDialog/GiftCardUpdateBalanceDialog.tsx
+++ src/giftCards/GiftCardUpdate/GiftCardUpdateBalanceDialog/GiftCardUpdateBalanceDialog.tsx
@@ -1,16 +1,16 @@
 import { TextField, Typography } from "@material-ui/core";
 import ActionDialog from "@saleor/components/ActionDialog";
 import CardSpacer from "@saleor/components/CardSpacer";
-import Form from "@saleor/components/Form";
 import { IMessage } from "@saleor/components/messages";
+import useForm from "@saleor/hooks/useForm";
 import useNotifier from "@saleor/hooks/useNotifier";
-import { getFormErrors } from "@saleor/utils/errors";
 import commonErrorMessages from "@saleor/utils/errors/common";
 import { DialogActionHandlersProps } from "@saleor/utils/handlers/dialogActionHandlers";
 import React from "react";
 import { useIntl } from "react-intl";
 
 import { giftCardsListTableMessages as tableMessages } from "../../GiftCardsList/messages";
+import { useDialogFormReset } from "../GiftCardResendCodeDialog/utils";
 import { getGiftCardErrorMessage } from "../messages";
 import { useGiftCardUpdateMutation } from "../mutations";
 import useGiftCardDetails from "../providers/GiftCardDetailsProvider/hooks/useGiftCardDetails";
@@ -83,54 +83,57 @@ const GiftCardUpdateBalanceDialog: React.FC<DialogActionHandlersProps> = ({
     return result?.data?.giftCardUpdate?.errors;
   };
 
-  const { loading, status, data } = updateGiftCardBalanceOpts;
-
-  const formErrors = getFormErrors(
-    ["initialBalanceAmount"],
-    data?.giftCardUpdate?.errors
+  const { data, change, submit, reset, hasChanged } = useForm(
+    initialFormData,
+    handleSubmit
   );
 
+  const { loading, status, data: submitData } = updateGiftCardBalanceOpts;
+
+  const { formErrors } = useDialogFormReset({
+    open,
+    reset,
+    keys: ["initialBalanceAmount"],
+    apiErrors: submitData?.giftCardUpdate?.errors
+  });
+
   return (
-    <Form initial={initialFormData} onSubmit={handleSubmit}>
-      {({ data, change, submit, hasChanged }) => (
-        <ActionDialog
-          maxWidth="sm"
-          open={open}
-          onConfirm={submit}
-          confirmButtonLabel={intl.formatMessage(messages.changeButtonLabel)}
-          onClose={closeDialog}
-          title={intl.formatMessage(messages.title)}
-          confirmButtonState={status}
-          disabled={loading || !hasChanged}
-        >
-          <Typography>{intl.formatMessage(messages.subtitle)}</Typography>
-          <CardSpacer />
-          <TextField
-            inputProps={{ min: 0 }}
-            error={!!formErrors?.initialBalanceAmount}
-            helperText={getGiftCardErrorMessage(
-              formErrors?.initialBalanceAmount,
-              intl
-            )}
-            name="balanceAmount"
-            value={data.balanceAmount}
-            onChange={change}
-            className={classes.inputContainer}
-            label={intl.formatMessage(
-              tableMessages.giftCardsTableColumnBalanceTitle
-            )}
-            type="number"
-            InputProps={{
-              startAdornment: (
-                <div className={classes.currencyCodeContainer}>
-                  <Typography variant="caption">{currency}</Typography>
-                </div>
-              )
-            }}
-          />
-        </ActionDialog>
-      )}
-    </Form>
+    <ActionDialog
+      maxWidth="sm"
+      open={open}
+      onConfirm={submit}
+      confirmButtonLabel={intl.formatMessage(messages.changeButtonLabel)}
+      onClose={closeDialog}
+      title={intl.formatMessage(messages.title)}
+      confirmButtonState={status}
+      disabled={loading || !hasChanged}
+    >
+      <Typography>{intl.formatMessage(messages.subtitle)}</Typography>
+      <CardSpacer />
+      <TextField
+        inputProps={{ min: 0 }}
+        error={!!formErrors?.initialBalanceAmount}
+        helperText={getGiftCardErrorMessage(
+          formErrors?.initialBalanceAmount,
+          intl
+        )}
+        name="balanceAmount"
+        value={data.balanceAmount}
+        onChange={change}
+        className={classes.inputContainer}
+        label={intl.formatMessage(
+          tableMessages.giftCardsTableColumnBalanceTitle
+        )}
+        type="number"
+        InputProps={{
+          startAdornment: (
+            <div className={classes.currencyCodeContainer}>
+              <Typography variant="caption">{currency}</Typography>
+            </div>
+          )
+        }}
+      />
+    </ActionDialog>
   );
 };
 
