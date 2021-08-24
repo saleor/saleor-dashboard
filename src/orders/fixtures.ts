17ec25082d7f442fdd73f61e286539b0c705c6c2
Magdalena Markusik <magdalena.markusik@mirumee.com>
2021-08-24 14:51:02 +0200

    Update fixtures and messages

--- src/orders/fixtures.ts
+++ src/orders/fixtures.ts
@@ -772,6 +772,7 @@ export const orders: OrderList_orders_edges_node[] = [
 ];
 export const order = (placeholder: string): OrderDetails_order => ({
   __typename: "Order",
+  giftCards: [],
   actions: [
     OrderAction.CAPTURE,
     OrderAction.MARK_AS_PAID,
@@ -1407,6 +1408,7 @@ export const order = (placeholder: string): OrderDetails_order => ({
 });
 export const draftOrder = (placeholder: string): OrderDetails_order => ({
   __typename: "Order" as "Order",
+  giftCards: [],
   actions: [OrderAction.CAPTURE],
   availableShippingMethods: null,
   billingAddress: null,
