e84afc27cd272887fea9aae1ad4921d61a819051
Magdalena Markusik <magdalena.markusik@mirumee.com>
2021-08-24 15:39:21 +0200

    Add auto fulfill non shippable gift cards to order settings

--- src/fragments/orders.ts
+++ src/fragments/orders.ts
@@ -296,6 +296,7 @@ export const fragmentOrderDetails = gql`
 export const fragmentOrderSettings = gql`
   fragment OrderSettingsFragment on OrderSettings {
     automaticallyConfirmAllNewOrders
+    automaticallyFulfillNonShippableGiftCard
   }
 `;
 
