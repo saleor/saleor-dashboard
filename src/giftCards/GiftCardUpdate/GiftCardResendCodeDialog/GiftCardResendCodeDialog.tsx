ae425d066a99d0c19b8c6fc562c4d24658c4a78d
Magdalena Markusik <magdalena.markusik@mirumee.com>
2021-08-27 13:22:11 +0200

    Refactor

--- src/giftCards/GiftCardUpdate/GiftCardResendCodeDialog/GiftCardResendCodeDialog.tsx
+++ src/giftCards/GiftCardUpdate/GiftCardResendCodeDialog/GiftCardResendCodeDialog.tsx
@@ -16,7 +16,7 @@ import useGiftCardDetails from "../providers/GiftCardDetailsProvider/hooks/useGi
 import { giftCardResendCodeDialogMessages as messages } from "./messages";
 import { useGiftCardResendCodeMutation } from "./mutations";
 import { GiftCardResend } from "./types/GiftCardResend";
-import { handleCheckboxChange, useDialogFormReset } from "./utils";
+import { useDialogFormReset } from "./utils";
 
 export interface GiftCardResendCodeFormData {
   email: string;
@@ -111,7 +111,7 @@ const GiftCardResendCodeDialog: React.FC<DialogActionHandlersProps> = ({
         name="differentMailConsent"
         label={intl.formatMessage(messages.consentCheckboxLabel)}
         checked={consentSelected}
-        onChange={handleCheckboxChange(setConsentSelected)}
+        onChange={event => setConsentSelected(event.target.value)}
       />
       <VerticalSpacer />
       <TextField
