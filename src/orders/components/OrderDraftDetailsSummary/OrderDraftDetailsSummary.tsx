f818514bb1200b0e77ff10d9135e27afe60e00c4
Michał Droń <dron.official@yahoo.com>
2021-09-10 10:59:24 +0200

    move messages to a seperate file

--- src/orders/components/OrderDraftDetailsSummary/OrderDraftDetailsSummary.tsx
+++ src/orders/components/OrderDraftDetailsSummary/OrderDraftDetailsSummary.tsx
@@ -8,11 +8,11 @@ import { OrderDiscountData } from "@saleor/products/components/OrderDiscountProv
 import { DiscountValueTypeEnum } from "@saleor/types/globalTypes";
 import React, { useRef } from "react";
 import { useIntl } from "react-intl";
-import { defineMessages } from "react-intl";
 
 import { OrderDetails_order } from "../../types/OrderDetails";
 import OrderDiscountCommonModal from "../OrderDiscountCommonModal";
 import { ORDER_DISCOUNT } from "../OrderDiscountCommonModal/types";
+import messages from "./messages";
 
 const useStyles = makeStyles(
   theme => ({
@@ -45,41 +45,6 @@ const useStyles = makeStyles(
   }),
   { name: "OrderDraftDetailsSummary" }
 );
-
-const messages = defineMessages({
-  addShippingAddressInfo: {
-    defaultMessage: "add shipping address first",
-    description: "add shipping address first label"
-  },
-  subtotal: {
-    defaultMessage: "Subtotal",
-    description: "subtotal price"
-  },
-  addDiscount: {
-    defaultMessage: "Add Discount",
-    description: "add discount button"
-  },
-  discount: {
-    defaultMessage: "Discount",
-    description: "discount button"
-  },
-  addShippingCarrier: {
-    defaultMessage: "Add shipping carrier",
-    description: "add shipping carrier button"
-  },
-  noShippingCarriers: {
-    defaultMessage: "No applicable shipping carriers",
-    description: "no shipping carriers title"
-  },
-  total: {
-    defaultMessage: "Total",
-    description: "total price"
-  },
-  taxes: {
-    defaultMessage: "Taxes (VAT included)",
-    description: "taxes title"
-  }
-});
 
 const PRICE_PLACEHOLDER = "---";
 
