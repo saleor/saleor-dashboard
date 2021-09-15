461c084f138244ece0573bf2abd87379d4e07258
Michał Droń <dron.official@yahoo.com>
2021-09-15 08:01:32 +0200

    disable fulfill button when order consists of at least one deleted product

--- src/orders/components/OrderUnfulfilledProductsCard/OrderUnfulfilledProductsCard.tsx
+++ src/orders/components/OrderUnfulfilledProductsCard/OrderUnfulfilledProductsCard.tsx
@@ -1,15 +1,16 @@
 import { Button, Card, CardActions, TableBody } from "@material-ui/core";
 import CardSpacer from "@saleor/components/CardSpacer";
 import ResponsiveTable from "@saleor/components/ResponsiveTable";
-import { makeStyles } from "@saleor/macaw-ui";
+import { makeStyles, Tooltip } from "@saleor/macaw-ui";
 import { renderCollection } from "@saleor/misc";
 import React from "react";
-import { FormattedMessage } from "react-intl";
+import { FormattedMessage, useIntl } from "react-intl";
 
 import { OrderDetails_order_lines } from "../../types/OrderDetails";
 import TableHeader from "../OrderProductsCardElements/OrderProductsCardHeader";
 import TableLine from "../OrderProductsCardElements/OrderProductsTableRow";
 import CardTitle from "../OrderReturnPage/OrderReturnRefundItemsCard/CardTitle";
+import { messages } from "./messages";
 
 const useStyles = makeStyles(
   () => ({
@@ -29,9 +30,18 @@ interface OrderUnfulfilledProductsCardProps {
 const OrderUnfulfilledProductsCard: React.FC<OrderUnfulfilledProductsCardProps> = props => {
   const { canFulfill, lines, onFulfill } = props;
   const classes = useStyles({});
+  const intl = useIntl();
 
   if (!lines.length) {
     return null;
+  }
+
+  let isDeleted = false;
+  for (const line of lines) {
+    if (!line.variant) {
+      isDeleted = true;
+      break;
+    }
   }
 
   return (
@@ -48,9 +58,25 @@ const OrderUnfulfilledProductsCard: React.FC<OrderUnfulfilledProductsCardProps> 
         </ResponsiveTable>
         {canFulfill && (
           <CardActions>
-            <Button variant="text" color="primary" onClick={onFulfill}>
-              <FormattedMessage defaultMessage="Fulfill" description="button" />
-            </Button>
+            <Tooltip
+              title={intl.formatMessage(messages.deletedVariantDetected)}
+              variant={"info"}
+              placement={"left"}
+            >
+              <div>
+                <Button
+                  disabled={isDeleted}
+                  variant="text"
+                  color="primary"
+                  onClick={onFulfill}
+                >
+                  <FormattedMessage
+                    defaultMessage="Fulfill"
+                    description="button"
+                  />
+                </Button>
+              </div>
+            </Tooltip>
           </CardActions>
         )}
       </Card>
---
+++ src/orders/components/OrderUnfulfilledProductsCard/messages.ts
@@ -0,0 +1,8 @@
+import { defineMessages } from "react-intl";
+
+export const messages = defineMessages({
+  deletedVariantDetected: {
+    defaultMessage: "You cannot fulfill products that are deleted.",
+    description: "tooltip content when disabled variants are detected"
+  }
+});
