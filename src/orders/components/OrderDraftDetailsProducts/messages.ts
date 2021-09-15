399a7bd56d766d713d9b10de028bc4f5dbfc45ff
Michał Droń <dron.official@yahoo.com>
2021-09-15 10:09:06 +0200

    add deleted product badge on unconfirmed orders

---
+++ src/orders/components/OrderDraftDetailsProducts/messages.ts
@@ -0,0 +1,8 @@
+import { defineMessages } from "react-intl";
+
+export const messages = defineMessages({
+  productDeleted: {
+    defaultMessage: "This product variant no longer exists",
+    description: "deleted product info in an unconfirmed order"
+  }
+});
