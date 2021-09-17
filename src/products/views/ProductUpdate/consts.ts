ffe44be733c975aee17488ee8f2947b00496532b
Dominik Żegleń <flesz3@o2.pl>
2021-09-17 18:02:44 +0200

    Fix back button on manage channels at product details (#1380) (#1417)
    * Simplify "changed" logic
    
    * Improve code composition
    
    * Test base state and setters
    
    * Add more tests
    
    * Fix changed logic
    
    * Rename hasChanged output
    
    * Move channel data outside hook
    
    * Move some logic to utils
    
    * Save data in dialog and pass to view
    
    * Split hooks
    
    * Fix react warnings
    
    * Fix story
    
    * Alias type
    
    * Fix stories
    
    * Remove rebase artifact
    
    * Reset state after closing modal
    
    * Capitalize type name

---
+++ src/products/views/ProductUpdate/consts.ts
@@ -0,0 +1,9 @@
+import { ChannelWithVariantData } from "./types";
+
+export const CHANNELS_AVAILIABILITY_MODAL_SELECTOR = "open-channels-picker";
+
+export const initialChannelWithVariantData: ChannelWithVariantData = {
+  variantsIdsToRemove: [],
+  variantsIdsToAdd: [],
+  selectedVariantsIds: []
+};
