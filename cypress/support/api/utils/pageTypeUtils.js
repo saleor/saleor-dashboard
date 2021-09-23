7ecf38becf0d783b55e8891e2101ee25160bd4c8
Karolina Rakoczy <rakoczy.karolina@gmail.com>
2021-09-23 10:55:18 +0200

    update jest

--- cypress/support/api/utils/pageTypeUtils.js
+++ cypress/support/api/utils/pageTypeUtils.js
@@ -1,4 +1,4 @@
-import { deletePageType, getPageTypes } from "../requests/PageTypes";
+import { deletePageType, getPageTypes } from "../requests/PageType";
 
 export function deletePageTypesStartsWith(startsWith) {
   cy.deleteElementsStartsWith(deletePageType, getPageTypes, startsWith);
