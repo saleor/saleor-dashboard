6d8661fe85fb61bf2cb1760c286c99fca07635fd
Wojciech Mista <wojciech.mista@saleor.io>
2021-09-28 12:34:29 +0200

    Change values in create sale page when percentage/fixed amount is switched (#1431) (#1445)
    * WIP
    
    * Fix get sale channel bug
    
    * Add sale value field text
    
    * Extract logic to useEffect
    
    * Code cleanup

--- src/discounts/components/SaleValue/SaleValue.tsx
+++ src/discounts/components/SaleValue/SaleValue.tsx
@@ -4,7 +4,6 @@ import {
   TableBody,
   TableCell,
   TableRow,
-  TextField,
   Typography
 } from "@material-ui/core";
 import CardTitle from "@saleor/components/CardTitle";
@@ -13,20 +12,21 @@ import Skeleton from "@saleor/components/Skeleton";
 import TableHead from "@saleor/components/TableHead";
 import { DiscountErrorFragment } from "@saleor/fragments/types/DiscountErrorFragment";
 import { renderCollection } from "@saleor/misc";
-import { SaleType } from "@saleor/types/globalTypes";
 import { getFormErrors } from "@saleor/utils/errors";
 import getDiscountErrorMessage from "@saleor/utils/errors/discounts";
-import React from "react";
+import * as React from "react";
 import { FormattedMessage, useIntl } from "react-intl";
 
 import { SaleDetailsPageFormData } from "../SaleDetailsPage";
+import SaleValueTextField from "./SaleValueTextField";
 import { useStyles } from "./styles";
+import { SaleValueInputChangeType } from "./types";
 
 export interface SaleValueProps {
   data: SaleDetailsPageFormData;
   disabled: boolean;
   errors: DiscountErrorFragment[];
-  onChange: (channelId: string, discountValue: string) => void;
+  onChange: SaleValueInputChangeType;
 }
 
 const numberOfColumns = 2;
@@ -37,6 +37,7 @@ const SaleValue: React.FC<SaleValueProps> = ({
   errors,
   onChange
 }) => {
+  const { type } = data;
   const intl = useIntl();
   const classes = useStyles({});
   const formErrors = getFormErrors(["value"], errors);
@@ -93,8 +94,8 @@ const SaleValue: React.FC<SaleValueProps> = ({
                       </TableCell>
                       <TableCell>
                         {listing ? (
-                          <TextField
-                            disabled={disabled}
+                          <SaleValueTextField
+                            dataType={type}
                             helperText={
                               error
                                 ? getDiscountErrorMessage(
@@ -103,24 +104,9 @@ const SaleValue: React.FC<SaleValueProps> = ({
                                   )
                                 : ""
                             }
-                            name="value"
-                            onChange={e => onChange(listing.id, e.target.value)}
-                            label={intl.formatMessage({
-                              defaultMessage: "Discount Value",
-                              description: "sale discount"
-                            })}
-                            value={listing.discountValue || ""}
-                            type="number"
-                            fullWidth
-                            inputProps={{
-                              min: 0
-                            }}
-                            InputProps={{
-                              endAdornment:
-                                data.type === SaleType.FIXED
-                                  ? listing.currency
-                                  : "%"
-                            }}
+                            disabled={disabled}
+                            listing={listing}
+                            onChange={onChange}
                           />
                         ) : (
                           <Skeleton />
