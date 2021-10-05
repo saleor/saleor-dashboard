ae1d464b6d3e412b3dcc9dea02bf5d6c80d1ddb3
Wojciech Miśta <wojciech.mista@saleor.io>
2021-10-05 11:47:53 +0200

    WIP

---
+++ src/customers/providers/CustomerDetailsProvider.tsx
@@ -0,0 +1,44 @@
+import React, { createContext } from "react";
+
+import { useCustomerDetails } from "../queries";
+import { CustomerDetails } from "../types/CustomerDetails";
+
+export interface CustomerDetailsProviderProps {
+  id: string;
+}
+
+interface CustomerDetailsConsumerProps {
+  customer: CustomerDetails | null;
+  loading: boolean | null;
+}
+
+export const CustomerDetailsContext = createContext<
+  CustomerDetailsConsumerProps
+>(null);
+
+export const CustomerDetailsProvider: React.FC<CustomerDetailsProviderProps> = ({
+  children,
+  id
+}) => {
+  const { data, loading } = useCustomerDetails({
+    displayLoader: true,
+    variables: {
+      id
+    }
+  });
+
+  console.info(data, loading);
+
+  const providerValues: CustomerDetailsConsumerProps = {
+    customer: data,
+    loading
+  };
+
+  console.info(children);
+
+  return (
+    <CustomerDetailsContext.Provider value={providerValues}>
+      {children}
+    </CustomerDetailsContext.Provider>
+  );
+};
