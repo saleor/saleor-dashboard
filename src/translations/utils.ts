2ffa281279a7d3c2e2df2422929bf729493fe9a2
Kamil Pastuszka <33246308+kamilpastuszka@users.noreply.github.com>
2021-10-06 09:19:11 +0000

    Refactor Translations Page (#1451)
    * refactoring
    
    * rename parameter
    
    * update messages
    
    * changes after review
    
    * change imports

--- src/translations/utils.ts
+++ src/translations/utils.ts
@@ -1,6 +1,12 @@
 import { OutputData } from "@editorjs/editorjs";
+import { AttributeTranslationDetailsFragment_attribute_choices } from "@saleor/fragments/types/AttributeTranslationDetailsFragment";
+import { TranslationField } from "@saleor/translations/types";
+import { mapEdgesToItems } from "@saleor/utils/maps";
 import { getParsedDataForJsonStringField } from "@saleor/utils/richText/misc";
+import { IntlShape } from "react-intl";
 
+import { fieldNames } from "./components/TranslationsAttributesPage";
+import { transtionsAttributesPageFieldsMessages as messages } from "./components/TranslationsAttributesPage/messages";
 import {
   PageTranslationInputFieldName,
   TranslationInputFieldName
@@ -26,3 +32,23 @@ export const getParsedTranslationInputData = ({
 
   return { [fieldName]: data as string };
 };
+
+export const getTranslationFields = (
+  fields: AttributeTranslationDetailsFragment_attribute_choices,
+  intl: IntlShape
+) =>
+  mapEdgesToItems(fields).map(
+    ({ id, name, translation }, attributeValueIndex) => {
+      const displayName = intl.formatMessage(messages.valueNumber, {
+        number: attributeValueIndex + 1
+      });
+
+      return {
+        displayName,
+        name: `${fieldNames.value}:${id}`,
+        translation: translation?.name || null,
+        type: "short" as TranslationField["type"],
+        value: name
+      };
+    }
+  ) || [];
