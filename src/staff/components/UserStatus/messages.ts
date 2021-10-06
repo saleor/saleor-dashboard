8820ed01fe0607ef6d960893ffbd662f1d94859c
Jakub Majorek <majorek.jakub@gmail.com>
2021-10-06 11:45:29 +0200

    Fix wrong words and ordering for staff member form (#1424) (#1471)
    * fix wrong words and ordering for staff member form
    
    * add messages
    
    * remove line
    
    * apply changes from review
    
    * change to local messages
    
    Co-authored-by: Kamil Pastuszka <33246308+kamilpastuszka@users.noreply.github.com>

---
+++ src/staff/components/UserStatus/messages.ts
@@ -0,0 +1,12 @@
+import { defineMessages } from "react-intl";
+
+export const userStatusMessages = defineMessages({
+  userStatus: {
+    defaultMessage: "User Status",
+    description: "section header"
+  },
+  userDisableInstruction: {
+    defaultMessage:
+      "If you want to disable this User please uncheck the box below."
+  }
+});
