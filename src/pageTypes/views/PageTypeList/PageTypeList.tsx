837f27cfc85db284999d825255bd71e8ed5bf5e1
Michał Droń <dron.official@yahoo.com>
2021-10-14 10:15:18 +0300

    remove unused imports

--- src/pageTypes/views/PageTypeList/PageTypeList.tsx
+++ src/pageTypes/views/PageTypeList/PageTypeList.tsx
@@ -5,7 +5,6 @@ import SaveFilterTabDialog, {
   SaveFilterTabDialogFormData
 } from "@saleor/components/SaveFilterTabDialog";
 import TypeDeleteWarningDialog from "@saleor/components/TypeDeleteWarningDialog";
-import { DEFAULT_INITIAL_PAGINATION_DATA } from "@saleor/config";
 import useBulkActions from "@saleor/hooks/useBulkActions";
 import useListSettings from "@saleor/hooks/useListSettings";
 import useNavigator from "@saleor/hooks/useNavigator";
