bfa6b07b12d87e34cb3f5a05998a532c2d96c7da
Michał Droń <dron.official@yahoo.com>
2021-10-15 09:12:26 +0300

    delete unused import

--- src/permissionGroups/components/PermissionGroupList/PermissionGroupList.tsx
+++ src/permissionGroups/components/PermissionGroupList/PermissionGroupList.tsx
@@ -7,7 +7,6 @@ import {
   TableRow
 } from "@material-ui/core";
 import DeleteIcon from "@material-ui/icons/Delete";
-import EditIcon from "@material-ui/icons/Edit";
 import ResponsiveTable from "@saleor/components/ResponsiveTable";
 import Skeleton from "@saleor/components/Skeleton";
 import TableCellHeader from "@saleor/components/TableCellHeader";
