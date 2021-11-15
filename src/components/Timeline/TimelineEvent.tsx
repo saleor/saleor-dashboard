495642a0c3bc77ea5cb665e31c20bc59545a5600
Wojciech Miśta <wojciech.mista@saleor.io>
2021-11-15 10:16:16 +0100

    Allow title to be a react node

--- src/components/Timeline/TimelineEvent.tsx
+++ src/components/Timeline/TimelineEvent.tsx
@@ -80,7 +80,7 @@ export interface TimelineEventProps {
   children?: React.ReactNode;
   date: string;
   secondaryTitle?: string;
-  title?: string;
+  title?: string | React.ReactNode;
   titleElements?: TitleElement[];
 }
 
