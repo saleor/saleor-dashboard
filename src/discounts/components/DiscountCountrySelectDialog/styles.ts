b8055022f51e5b0ed5c6170823d8bfccb71dc7c5
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2021-11-17 23:48:39 +0100

    Fix assign countries dialogs

---
+++ src/discounts/components/DiscountCountrySelectDialog/styles.ts
@@ -0,0 +1,13 @@
+import { makeStyles } from "@saleor/macaw-ui";
+
+export const useStyles = makeStyles(
+  {
+    checkboxCell: {
+      paddingLeft: 0
+    },
+    wideCell: {
+      width: "100%"
+    }
+  },
+  { name: "DiscountCountrySelectDialog" }
+);
