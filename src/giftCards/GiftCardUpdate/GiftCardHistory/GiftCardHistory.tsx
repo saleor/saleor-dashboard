20e1ecad88c42a36fe23403cfe2f2cc0af1e73e0
Wojciech Miśta <wojciech.mista@saleor.io>
2021-11-17 10:43:57 +0100

    Change messages location

--- src/giftCards/GiftCardUpdate/GiftCardHistory/GiftCardHistory.tsx
+++ src/giftCards/GiftCardUpdate/GiftCardHistory/GiftCardHistory.tsx
@@ -20,7 +20,10 @@ import { GIFT_CARD_DETAILS_QUERY } from "../queries";
 import { GiftCardAddNote } from "../types/GiftCardAddNote";
 import { GiftCardDetails_giftCard_events } from "../types/GiftCardDetails";
 import useGiftCardHistoryEvents from "./hooks/useGiftCardHistoryEvents";
-import messages from "./messages";
+import {
+  giftCardHistoryMessages as messages,
+  giftCardHistoryTimelineMessages as timelineMessages
+} from "./messages";
 import useStyles from "./styles";
 
 interface FormData {
@@ -43,119 +46,65 @@ const getEventMessage = (
 
   switch (event.type) {
     case GiftCardEventsEnum.ACTIVATED:
-      return intl.formatMessage(
-        {
-          defaultMessage: "Gift card was activated by {activatedBy}",
-          description: "gift card history message"
-        },
-        {
-          activatedBy: (
-            <Link href={staffMemberDetailsPath(event.user.id)}>
-              {getUserOrApp()}
-            </Link>
-          )
-        }
-      );
+      return intl.formatMessage(timelineMessages.giftCardActivated, {
+        activatedBy: (
+          <Link href={staffMemberDetailsPath(event.user.id)}>
+            {getUserOrApp()}
+          </Link>
+        )
+      });
     case GiftCardEventsEnum.BALANCE_RESET:
-      return intl.formatMessage(
-        {
-          defaultMessage: "Gift card balance was reset by {resetBy}",
-          description: "gift card history message"
-        },
-        {
-          resetBy: (
-            <Link href={staffMemberDetailsPath(event.user.id)}>
-              {getUserOrApp()}
-            </Link>
-          )
-        }
-      );
+      return intl.formatMessage(timelineMessages.giftCardBalanceReset, {
+        resetBy: (
+          <Link href={staffMemberDetailsPath(event.user.id)}>
+            {getUserOrApp()}
+          </Link>
+        )
+      });
     case GiftCardEventsEnum.BOUGHT:
-      return intl.formatMessage(
-        {
-          defaultMessage: "Gift card was bought in order #{orderNumber}",
-          description: "gift card history message"
-        },
-        {
-          orderNumber: event.orderId
-        }
-      );
+      return intl.formatMessage(timelineMessages.giftCardBought, {
+        orderNumber: (
+          <Link href={orderPath(event.orderId)}>#{event.orderNumber}</Link>
+        )
+      });
     case GiftCardEventsEnum.DEACTIVATED:
-      return intl.formatMessage(
-        {
-          defaultMessage: "Gift card was deactivated by {deactivatedBy}",
-          description: "gift card history message"
-        },
-        {
-          deactivatedBy: (
-            <Link href={staffMemberDetailsPath(event.user.id)}>
-              {getUserOrApp()}
-            </Link>
-          )
-        }
-      );
+      return intl.formatMessage(timelineMessages.giftCardDeactivated, {
+        deactivatedBy: (
+          <Link href={staffMemberDetailsPath(event.user.id)}>
+            {getUserOrApp()}
+          </Link>
+        )
+      });
     case GiftCardEventsEnum.EXPIRY_DATE_UPDATED:
-      return intl.formatMessage(
-        {
-          defaultMessage:
-            "Gift card expiry date was updated by {expiryUpdatedBy}",
-          description: "gift card history message"
-        },
-        {
-          expiryUpdatedBy: (
-            <Link href={staffMemberDetailsPath(event.user.id)}>
-              {getUserOrApp()}
-            </Link>
-          )
-        }
-      );
+      return intl.formatMessage(timelineMessages.giftCardExpiryDateUpdate, {
+        expiryUpdatedBy: (
+          <Link href={staffMemberDetailsPath(event.user.id)}>
+            {getUserOrApp()}
+          </Link>
+        )
+      });
     case GiftCardEventsEnum.ISSUED:
-      return intl.formatMessage(
-        {
-          defaultMessage: "Gift card was issued by {issuedBy}",
-          description: "dsc"
-        },
-        {
-          issuedBy: (
-            <Link href={staffMemberDetailsPath(event.user.id)}>
-              {getUserOrApp()}
-            </Link>
-          )
-        }
-      );
+      return intl.formatMessage(timelineMessages.giftCardIssued, {
+        issuedBy: (
+          <Link href={staffMemberDetailsPath(event.user.id)}>
+            {getUserOrApp()}
+          </Link>
+        )
+      });
     case GiftCardEventsEnum.RESENT:
-      return intl.formatMessage({
-        defaultMessage: "Gift card was resent",
-        description: "gift card history message"
-      });
+      return intl.formatMessage(timelineMessages.giftCardResent);
     case GiftCardEventsEnum.SENT_TO_CUSTOMER:
-      return intl.formatMessage({
-        defaultMessage: "Gift card was sent to customer",
-        description: "gift card history message"
-      });
+      return intl.formatMessage(timelineMessages.giftCardSentToCustomer);
     case GiftCardEventsEnum.TAG_UPDATED:
-      return intl.formatMessage({
-        defaultMessage: "Gift card tag was updated",
-        description: "gift card history message"
-      });
+      return intl.formatMessage(timelineMessages.giftCardTagUpdated);
     case GiftCardEventsEnum.UPDATED:
-      return intl.formatMessage({
-        defaultMessage: "Gift card was updated",
-        description: "gift card history message"
-      });
+      return intl.formatMessage(timelineMessages.giftCardTagUpdated);
     case GiftCardEventsEnum.USED_IN_ORDER:
-      return intl.formatMessage(
-        {
-          defaultMessage:
-            "Gift card was used as a payment method on order {orderLink}",
-          description: "gift card history message"
-        },
-        {
-          orderLink: (
-            <Link href={orderPath(event.orderId)}>#{event.orderNumber}</Link>
-          )
-        }
-      );
+      return intl.formatMessage(timelineMessages.giftCardUsedInOrder, {
+        orderLink: (
+          <Link href={orderPath(event.orderId)}>#{event.orderNumber}</Link>
+        )
+      });
   }
 };
 
--- src/giftCards/GiftCardUpdate/GiftCardHistory/messages.ts
+++ src/giftCards/GiftCardUpdate/GiftCardHistory/messages.ts
@@ -15,4 +15,48 @@ const giftCardHistoryMessages = defineMessages({
   }
 });
 
-export default giftCardHistoryMessages;
+const giftCardHistoryTimelineMessages = defineMessages({
+  giftCardActivated: {
+    defaultMessage: "Gift card was activated by {activatedBy}",
+    description: "gift card history message"
+  },
+  giftCardBalanceReset: {
+    defaultMessage: "Gift card balance was reset by {resetBy}",
+    description: "gift card history message"
+  },
+  giftCardBought: {
+    defaultMessage: "Gift card was bought in order {orderNumber}",
+    description: "gift card history message"
+  },
+  giftCardDeactivated: {
+    defaultMessage: "Gift card was deactivated by {deactivatedBy}",
+    description: "gift card history message"
+  },
+  giftCardExpiryDateUpdate: {
+    defaultMessage: "Gift card expiry date was updated by {expiryUpdatedBy}",
+    description: "gift card history message"
+  },
+  giftCardIssued: {
+    defaultMessage: "Gift card was issued by {issuedBy}",
+    description: "dsc"
+  },
+  giftCardResent: {
+    defaultMessage: "Gift card was resent",
+    description: "gift card history message"
+  },
+  giftCardSentToCustomer: {
+    defaultMessage: "Gift card was sent to customer",
+    description: "gift card history message"
+  },
+  giftCardTagUpdated: {
+    defaultMessage: "Gift card tag was updated",
+    description: "gift card history message"
+  },
+  giftCardUsedInOrder: {
+    defaultMessage:
+      "Gift card was used as a payment method on order {orderLink}",
+    description: "gift card history message"
+  }
+});
+
+export { giftCardHistoryMessages, giftCardHistoryTimelineMessages };
