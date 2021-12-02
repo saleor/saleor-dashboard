e14377a3a96fad3a176839115bac2ac7e32971d2
Jakub Majorek <majorek.jakub@gmail.com>
2021-12-02 14:40:30 +0100

    Update type import

--- testUtils/intl.ts
+++ testUtils/intl.ts
@@ -1,6 +1,6 @@
-import { OptionalIntlConfig } from "react-intl/src/components/provider";
+import { IntlConfig } from "react-intl";
 
-export const config: OptionalIntlConfig = {
+export const config: IntlConfig = {
   defaultLocale: "en",
   locale: "en"
 };
