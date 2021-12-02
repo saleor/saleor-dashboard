93b9c92d763ea094a288cef0deb0b439d7504aa4
Wojciech <wojciech.mista@hotmail.com>
2021-12-02 15:06:15 +0100

    Update descriptions for gift card messages to be more descriptive

--- src/giftCards/GiftCardsList/messages.ts
+++ src/giftCards/GiftCardsList/messages.ts
@@ -3,81 +3,81 @@ import { defineMessages } from "react-intl";
 export const giftCardsListHeaderMenuItemsMessages = defineMessages({
   settings: {
     defaultMessage: "Settings",
-    description: "GiftCardsListHeader menu item settings"
+    description: "settings menu item"
   },
   bulkIssue: {
     defaultMessage: "Bulk Issue",
-    description: "GiftCardsListHeader menu item settings"
+    description: "bulk issue menu item"
   },
   exportCodes: {
     defaultMessage: "Export card codes",
-    description: "GiftCardsListHeader menu item settings"
+    description: "export card codes menu item"
   },
   issueButtonLabel: {
     defaultMessage: "Issue card",
-    description: "GiftCardsListHeader issue button label"
+    description: "issue card button label"
   },
   noGiftCardsAlertTitle: {
     defaultMessage: "You haven’t defined a gift card product!",
-    description: "GiftCardsListHeader alert"
+    description: "no card defuned alert message"
   },
   noGiftCardsProductsAndProductTypes: {
     defaultMessage:
       "{createGiftCardProductType} and {giftCardProduct} to start selling gift cards in your store.",
-    description: "GiftCardsListHeader alert"
+    description: "no gift card products and types alert message"
   },
   noGiftCardsProductTypes: {
     defaultMessage:
       "{createGiftCardProductType} to start selling gift cards in your store.",
-    description: "GiftCardsListHeader alert"
+    description: "no gift card product types alert message"
   },
   noGiftCardsProducts: {
     defaultMessage:
       "{createGiftCardProduct} to start selling gift cards in your store.",
-    description: "GiftCardsListHeader alert"
+    description: "no gift card products alert message"
   },
   createGiftCardProductType: {
     defaultMessage: "Create a gift card product type",
-    description: "GiftCardsListHeader alert"
+    description: "create gift card product type alert message"
   },
   createGiftCardProduct: {
     defaultMessage: "Create a gift card product",
-    description: "GiftCardsListHeader alert"
+    description: "create gift card product alert message"
   },
   giftCardProduct: {
     defaultMessage: "gift card product",
-    description: "GiftCardsListHeader alert"
+    description: "gift card product message"
   }
 });
 
 export const giftCardsListTableMessages = defineMessages({
   giftCardsTableColumnGiftCardTitle: {
     defaultMessage: "Gift Card",
-    description: "GiftCardsListTable column title gift card"
+    description: "column title gift card"
   },
   giftCardsTableColumnTagTitle: {
     defaultMessage: "Tag",
-    description: "GiftCardsListTable column title tag"
+    description: "column title tag"
   },
   giftCardsTableColumnProductTitle: {
     defaultMessage: "Product",
-    description: "GiftCardsListTable column title product"
+    description: "column title product"
   },
   giftCardsTableColumnCustomerTitle: {
     defaultMessage: "Used by",
-    description: "GiftCardsListTable column title customer"
+    description: "column title used by/customer"
   },
   giftCardsTableColumnBalanceTitle: {
     defaultMessage: "Balance",
-    description: "GiftCardsListTable column title balance"
+    description: "column title balance"
   },
   codeEndingWithLabel: {
     defaultMessage: "Code ending with {last4CodeChars}",
-    description: "GiftCardsListTable code ending with label"
+    description: "code ending with label"
   },
   noGiftCardsFound: {
     defaultMessage: "No gift cards found",
-    description: "GiftCardsListTable no cards found title"
+    description: "no cards found title message"
   }
 });
 
--- src/giftCards/components/GiftCardCustomerCard/messages.ts
+++ src/giftCards/components/GiftCardCustomerCard/messages.ts
@@ -3,22 +3,22 @@ import { defineMessages } from "react-intl";
 export const giftCardCustomerCardMessages = defineMessages({
   customerGiftCardsPresentSubtitle: {
     defaultMessage: "Only five newest gift cards are shown here",
-    description: "subtitle"
+    description: "customer gift cards card subtitle"
   },
   customerGiftCardsAbsentSubtitle: {
     defaultMessage: "There are no gift cards assigned to this customer",
-    description: "subtitle"
+    description: "customer gift cards card no cards subtitle"
   },
   customerGiftCardsViewAllButton: {
     defaultMessage: "View All",
-    description: "button"
+    description: "customer gift cards card view all button"
   },
   customerGiftCardsIssueNewCardButton: {
     defaultMessage: "Issue new card",
-    description: "button"
+    description: "customer gift cards card issue button"
   },
   customerGiftCardsCardTitle: {
     defaultMessage: "Gift Cards",
-    description: "title"
+    description: "customer gift cards card title"
   }
 });
