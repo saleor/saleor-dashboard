c3651576d9530c092748cf1f8d846ea8e8b49e57
Bartłomiej Wiaduch <tukan2can@gmail.com>
2021-12-13 14:02:45 +0100

    Refactor createSvgIcon import for icons
    It fixes ts declarations resolution

--- src/icons/Trash.tsx
+++ src/icons/Trash.tsx
@@ -1,4 +1,4 @@
-import createSvgIcon from "@material-ui/icons/utils/createSvgIcon";
+import { createSvgIcon } from "@material-ui/core/utils";
 import { useTheme } from "@saleor/macaw-ui";
 import React from "react";
 
--- src/icons/Warehouses.tsx
+++ src/icons/Warehouses.tsx
@@ -1,4 +1,4 @@
-import createSvgIcon from "@material-ui/icons/utils/createSvgIcon";
+import { createSvgIcon } from "@material-ui/core/utils";
 import React from "react";
 
 const Warehouses = createSvgIcon(
