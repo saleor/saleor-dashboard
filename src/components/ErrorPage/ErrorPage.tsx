7e83839a89ccaa1f0c560bb14eb462d1a7e6bf0e
Dominik Żegleń <flesz3@o2.pl>
2021-12-15 10:17:04 +0100

    Rework error page (#1670)
    * Remake error page
    
    * Fix types
    
    * Update error id styles

--- src/components/ErrorPage/ErrorPage.tsx
+++ src/components/ErrorPage/ErrorPage.tsx
@@ -1,108 +1,58 @@
 import notFoundImage from "@assets/images/what.svg";
 import { Typography } from "@material-ui/core";
-import { Button, makeStyles } from "@saleor/macaw-ui";
+import { Button } from "@saleor/macaw-ui";
 import React from "react";
 import SVG from "react-inlinesvg";
 import { FormattedMessage } from "react-intl";
 
+import messages from "./messages";
+import useStyles from "./styles";
+
 export interface ErrorPageProps {
   id?: string | null;
   onBack: () => void;
+  onRefresh: () => void;
 }
 
-const useStyles = makeStyles(
-  theme => ({
-    bottomHeader: {
-      fontWeight: 600 as 600,
-      textTransform: "uppercase"
-    },
-    button: {
-      marginTop: theme.spacing(2),
-      padding: 20
-    },
-    container: {
-      [theme.breakpoints.down("sm")]: {
-        gridTemplateColumns: "1fr",
-        padding: theme.spacing(3),
-        width: "100%"
-      },
-      display: "grid",
-      gridTemplateColumns: "1fr 487px",
-      margin: "0 auto",
-      width: 830
-    },
-    errorId: {
-      marginTop: theme.spacing(3)
-    },
-    innerContainer: {
-      [theme.breakpoints.down("sm")]: {
-        order: 1,
-        textAlign: "center"
-      },
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center"
-    },
-    notFoundImage: {
-      "& svg": {
-        width: "100%"
-      }
-    },
-    root: {
-      alignItems: "center",
-      display: "flex",
-      height: "calc(100vh - 180px)"
-    },
-    upperHeader: {
-      fontWeight: 600 as 600
-    }
-  }),
-  { name: "ErrorPage" }
-);
-
-const ErrorPage: React.FC<ErrorPageProps> = props => {
-  const { onBack, id } = props;
-
-  const classes = useStyles(props);
+const ErrorPage: React.FC<ErrorPageProps> = ({ onBack, onRefresh, id }) => {
+  const classes = useStyles();
 
   return (
     <div className={classes.root}>
       <div className={classes.container}>
+        <SVG className={classes.notFoundImage} src={notFoundImage} />
         <div className={classes.innerContainer}>
           <div>
-            <Typography className={classes.upperHeader} variant="h4">
-              <FormattedMessage defaultMessage="Ooops!..." />
-            </Typography>
-            <Typography className={classes.bottomHeader} variant="h3">
-              <FormattedMessage defaultMessage="Error" />
+            <Typography className={classes.header} variant="h1">
+              <FormattedMessage {...messages.header} />
             </Typography>
             <Typography>
-              <FormattedMessage defaultMessage="We've encountered a problem..." />
-            </Typography>
-            <Typography>
-              <FormattedMessage defaultMessage="Don't worry, everything is gonna be fine" />
+              <FormattedMessage {...messages.content} />
             </Typography>
             {!!id && (
-              <Typography variant="subtitle2" className={classes.errorId}>
-                Error ID: {id}
-              </Typography>
+              <div>
+                <Typography
+                  variant="caption"
+                  color="textSecondary"
+                  className={classes.errorId}
+                >
+                  Error ID
+                </Typography>
+                <Typography variant="body1">{id}</Typography>
+              </div>
             )}
           </div>
-          <div>
-            <Button
-              className={classes.button}
-              variant="primary"
-              onClick={onBack}
-            >
-              <FormattedMessage
-                defaultMessage="Back to home"
-                description="button"
-              />
+          <div className={classes.buttonContainer}>
+            <Button variant="primary" onClick={onBack}>
+              <FormattedMessage {...messages.btnDashboard} />
+            </Button>
+            <div className={classes.conjunction}>
+              <FormattedMessage {...messages.or} />
+            </div>
+            <Button variant="secondary" onClick={onRefresh}>
+              <FormattedMessage {...messages.btnRefresh} />
             </Button>
           </div>
-        </div>
-        <div>
-          <SVG className={classes.notFoundImage} src={notFoundImage} />
         </div>
       </div>
     </div>
