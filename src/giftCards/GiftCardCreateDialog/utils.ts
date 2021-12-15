d317c3ec280710bfc5f788554eab8259ff30ac01
Wojciech Mista <wojciech.mista@saleor.io>
2021-12-15 12:16:50 +0100

    Add expiry error handling for issuing gift cards (#1634)
    * Add expiry error handling for issuing gift cards
    
    * Add expiry error message to gift card bulk issue

--- src/giftCards/GiftCardCreateDialog/utils.ts
+++ src/giftCards/GiftCardCreateDialog/utils.ts
@@ -1,7 +1,13 @@
+import { IMessage } from "@saleor/components/messages";
 import { TimePeriodTypeEnum } from "@saleor/types/globalTypes";
+import commonErrorMessages from "@saleor/utils/errors/common";
 import moment from "moment-timezone";
+import { IntlShape } from "react-intl";
 
 import { GiftCardCreateCommonFormData } from "../GiftCardBulkCreateDialog/types";
+import { giftCardUpdateFormMessages } from "../GiftCardsList/messages";
+import { giftCardCreateMessages as messages } from "./messages";
+import { GiftCardCreate_giftCardCreate_errors } from "./types/GiftCardCreate";
 
 const addToCurrentDate = (
   currentDate: number,
@@ -26,6 +32,39 @@ export const getExpiryPeriodTerminationDate = (
     default:
       return null;
   }
+};
+
+export const getGiftCardExpiryError = (intl: IntlShape): IMessage => ({
+  title: intl.formatMessage(
+    giftCardUpdateFormMessages.giftCardInvalidExpiryDateHeader
+  ),
+  text: intl.formatMessage(
+    giftCardUpdateFormMessages.giftCardInvalidExpiryDateContent
+  ),
+  status: "error"
+});
+
+export const getGiftCardCreateOnCompletedMessage = (
+  errors: GiftCardCreate_giftCardCreate_errors[],
+  intl: IntlShape,
+  successMessage?: IMessage
+): IMessage => {
+  const hasExpiryError = errors.some(error => error.field === "expiryDate");
+  const successGiftCardMessage = successMessage || {
+    status: "success",
+    text: intl.formatMessage(messages.createdSuccessAlertTitle)
+  };
+
+  if (hasExpiryError) {
+    return getGiftCardExpiryError(intl);
+  }
+
+  return !!errors?.length
+    ? {
+        status: "error",
+        text: intl.formatMessage(commonErrorMessages.unknownError)
+      }
+    : successGiftCardMessage;
 };
 
 export const getGiftCardExpiryInputData = (
