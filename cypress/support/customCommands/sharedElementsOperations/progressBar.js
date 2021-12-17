760f951fd8bccc0ba808a14bb7ccfb6d88d4da26
Karolina Rakoczy <rakoczy.karolina@gmail.com>
2021-12-17 11:36:39 +0100

    Add tests for updating & deleting collections and categories (#1662)
    * tests for updating & deleting collections and categories
    
    * add empty spaces
    
    * fix create category

--- cypress/support/customCommands/sharedElementsOperations/progressBar.js
+++ cypress/support/customCommands/sharedElementsOperations/progressBar.js
@@ -1,18 +1,5 @@
 import { SHARED_ELEMENTS } from "../../../elements/shared/sharedElements";
 
-// export function visitAndWaitForProgressBarToDisappear(url) {
-//   cy.visit(url);
-//   return waitForProgressBarToNotBeVisible();
-// }
-
-// export function waitForProgressBarToNotBeVisible() {
-//   return cy.get(SHARED_ELEMENTS.progressBar).should("be.not.visible");
-// }
-
-// export function waitForProgressBarToNotExist() {
-//   return cy.get(SHARED_ELEMENTS.progressBar).should("not.exist");
-// }
-
 Cypress.Commands.add("visitAndWaitForProgressBarToDisappear", url => {
   cy.visit(url).waitForProgressBarToNotBeVisible();
 });
