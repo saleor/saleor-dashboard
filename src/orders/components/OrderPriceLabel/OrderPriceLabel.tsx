5769fe334986c9595d337eed367690f9b7f84235
Wojciech <wojciech.mista@hotmail.com>
2021-12-17 13:13:31 +0100

    Lint fix

--- src/orders/components/OrderPriceLabel/OrderPriceLabel.tsx
+++ src/orders/components/OrderPriceLabel/OrderPriceLabel.tsx
@@ -1,26 +1,29 @@
 import { Typography } from "@material-ui/core";
-import { makeStyles } from "@saleor/macaw-ui";
 import Money from "@saleor/components/Money";
-import * as React from "react";
+import { makeStyles } from "@saleor/macaw-ui";
 import { SearchOrderVariant_search_edges_node_variants_pricing } from "@saleor/orders/types/SearchOrderVariant";
+import * as React from "react";
 
 interface OrderPriceLabelProps {
   pricing: SearchOrderVariant_search_edges_node_variants_pricing;
 }
 
-const useStyles = makeStyles(theme => ({
-  percentDiscountLabelContainer: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "flex-end",
-    justifyContent: "flex-end"
-  },
-  strike: {
-    textDecoration: "line-through",
-    color: theme.palette.grey[400],
-    fontSize: "smaller"
-  }
-}));
+const useStyles = makeStyles(
+  theme => ({
+    percentDiscountLabelContainer: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "flex-end",
+      justifyContent: "flex-end"
+    },
+    strike: {
+      textDecoration: "line-through",
+      color: theme.palette.grey[400],
+      fontSize: "smaller"
+    }
+  }),
+  { name: "OrderPriceLabel" }
+);
 
 const OrderPriceLabel: React.FC<OrderPriceLabelProps> = ({ pricing }) => {
   const classes = useStyles();
