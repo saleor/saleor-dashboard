9fc6fcf1502613c366d3b05420d4ad73e3134648
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2022-01-10 13:34:25 +0100

    Fix EditorJS read-mode toggle race condition (#1712)
    * Fix EditorJS read-mode toggle race condition
    
    Fixes rare edge case when toggling read mode is peerformed at the same
    time.
    
    * Make PromiseQueue function

--- src/misc.ts
+++ src/misc.ts
@@ -445,3 +445,18 @@ export const flatten = (obj: unknown) => {
 
   return result;
 };
+
+export function PromiseQueue() {
+  let queue = Promise.resolve();
+
+  function add<T>(operation: (value: T | void) => PromiseLike<T>) {
+    return new Promise((resolve, reject) => {
+      queue = queue
+        .then(operation)
+        .then(resolve)
+        .catch(reject);
+    });
+  }
+
+  return { queue, add };
+}
