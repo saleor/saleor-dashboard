f3eab43d7bc62a3c124c03f3e94689f6ea695b5b
Wojciech <wojciech.mista@hotmail.com>
2022-01-18 12:25:34 +0100

    Pass combined choices to autocomplete handler

--- src/products/components/ProductUpdatePage/form.tsx
+++ src/products/components/ProductUpdatePage/form.tsx
@@ -21,6 +21,7 @@ import useFormset, {
   FormsetChange,
   FormsetData
 } from "@saleor/hooks/useFormset";
+import { combinedMultiAutocompleteChoices } from "@saleor/misc";
 import { ProductDetails_product } from "@saleor/products/types/ProductDetails";
 import {
   getAttributeInputFromProduct,
@@ -232,7 +233,7 @@ function useProductUpdateForm(
     event => form.toggleValue(event, triggerChange),
     opts.setSelectedCollections,
     opts.selectedCollections,
-    opts.collections
+    combinedMultiAutocompleteChoices(opts.selectedCollections, opts.collections)
   );
   const handleCategorySelect = createSingleAutocompleteSelectHandler(
     handleChange,
