624ae06dc9ccd8ee0ce38839dad82bc976e10caa
Kjaroszew <97674352+Kjaroszew@users.noreply.github.com>
2022-01-24 14:54:01 +0100

    adding changes to transitions form branch 3.0 to 3.1

--- cypress/support/customCommands/sharedElementsOperations/tables.js
+++ cypress/support/customCommands/sharedElementsOperations/tables.js
@@ -2,6 +2,7 @@ import { BUTTON_SELECTORS } from "../../../elements/shared/button-selectors";
 import { SHARED_ELEMENTS } from "../../../elements/shared/sharedElements";
 
 Cypress.Commands.add("findElementOnTable", (elementName, alias) => {
+  cy.get(SHARED_ELEMENTS.skeleton).should("not.exist");
   cy.getTextFromElement(SHARED_ELEMENTS.table).then(tableText => {
     if (tableText.includes(elementName)) {
       cy.contains(SHARED_ELEMENTS.tableRow, elementName).click({ force: true });
