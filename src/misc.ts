1e3f3eae8d60edd953ca3a7593c30185479a364a
Wojciech Mista <wojciech.mista@saleor.io>
2022-01-24 14:58:27 +0100

    Removing collection fix (#1764)
    * Pass combined choices to autocomplete handler
    
    * Refactor autocomplete handler to use combined choices

--- src/misc.ts
+++ src/misc.ts
@@ -1,9 +1,11 @@
 import { ThemeType } from "@saleor/macaw-ui";
+import uniqBy from "lodash/uniqBy";
 import moment from "moment-timezone";
 import { MutationFunction, MutationResult } from "react-apollo";
 import { IntlShape } from "react-intl";
 
 import { ConfirmButtonTransitionState } from "./components/ConfirmButton";
+import { MultiAutocompleteChoiceType } from "./components/MultiAutocompleteSelectField";
 import { StatusType } from "./components/StatusChip/types";
 import { StatusLabelProps } from "./components/StatusLabel";
 import { AddressType, AddressTypeInput } from "./customers/types";
@@ -469,3 +471,8 @@ export function PromiseQueue() {
 
   return { queue, add };
 }
+
+export const combinedMultiAutocompleteChoices = (
+  selected: MultiAutocompleteChoiceType[],
+  choices: MultiAutocompleteChoiceType[]
+) => uniqBy([...selected, ...choices], "value");
