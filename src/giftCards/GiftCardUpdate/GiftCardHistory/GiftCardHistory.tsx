881d27165fa2f174e291f516150cd441c3f6a556
Dominik Żegleń <flesz3@o2.pl>
2022-01-27 16:43:02 +0100

    Fix messages

--- src/giftCards/GiftCardUpdate/GiftCardHistory/GiftCardHistory.tsx
+++ src/giftCards/GiftCardUpdate/GiftCardHistory/GiftCardHistory.tsx
@@ -1,139 +1,27 @@
 import { Typography } from "@material-ui/core";
-import { appPath } from "@saleor/apps/urls";
 import Form from "@saleor/components/Form";
 import Hr from "@saleor/components/Hr";
-import Link from "@saleor/components/Link";
 import Skeleton from "@saleor/components/Skeleton";
 import Timeline, {
   TimelineAddNote,
-  TimelineEvent,
   TimelineNote
 } from "@saleor/components/Timeline";
-import { customerPath } from "@saleor/customers/urls";
 import useNotifier from "@saleor/hooks/useNotifier";
-import { orderPath } from "@saleor/orders/urls";
-import { staffMemberDetailsPath } from "@saleor/staff/urls";
 import { GiftCardEventsEnum } from "@saleor/types/globalTypes";
 import React from "react";
 import { FormattedMessage, useIntl } from "react-intl";
-import { IntlShape } from "react-intl";
 
 import { useGiftCardTimelineNoteAddMutation } from "../mutations";
 import { GIFT_CARD_DETAILS_QUERY } from "../queries";
 import { GiftCardAddNote } from "../types/GiftCardAddNote";
-import { GiftCardDetails_giftCard_events } from "../types/GiftCardDetails";
+import GiftCardTimelineEvent from "./GiftCardTimelineEvent";
 import useGiftCardHistoryEvents from "./hooks/useGiftCardHistoryEvents";
-import {
-  giftCardHistoryMessages as messages,
-  giftCardHistoryTimelineMessages as timelineMessages
-} from "./messages";
+import { giftCardHistoryMessages as messages } from "./messages";
 import useStyles from "./styles";
 
 interface FormData {
   message: string;
 }
-
-const getEventMessage = (
-  event: GiftCardDetails_giftCard_events,
-  intl: IntlShape
-) => {
-  const getUserOrApp = () => {
-    if (event.user) {
-      const { firstName, lastName, email } = event.user;
-
-      if (lastName === "" || firstName === "") {
-        return email;
-      }
-
-      return `${firstName} ${lastName}`;
-    }
-
-    if (event.app) {
-      return event.app.name;
-    }
-  };
-
-  const getUserOrAppLink = () => {
-    if (event.user) {
-      return customerPath(event.user.id);
-    }
-
-    if (event.app) {
-      return appPath(event.app.id);
-    }
-  };
-
-  return "";
-
-  switch (event.type) {
-    case GiftCardEventsEnum.ACTIVATED:
-      return intl.formatMessage(timelineMessages.giftCardActivated, {
-        activatedBy: (
-          <Link href={staffMemberDetailsPath(event.user.id)}>
-            {getUserOrApp()}
-          </Link>
-        )
-      });
-    case GiftCardEventsEnum.BALANCE_RESET:
-      return intl.formatMessage(timelineMessages.giftCardBalanceReset, {
-        resetBy: (
-          <Link href={staffMemberDetailsPath(event.user.id)}>
-            {getUserOrApp()}
-          </Link>
-        )
-      });
-    case GiftCardEventsEnum.BOUGHT:
-      return intl.formatMessage(timelineMessages.giftCardBought, {
-        orderNumber: (
-          <Link href={orderPath(event.orderId)}>#{event.orderNumber}</Link>
-        )
-      });
-    case GiftCardEventsEnum.DEACTIVATED:
-      return intl.formatMessage(timelineMessages.giftCardDeactivated, {
-        deactivatedBy: (
-          <Link href={staffMemberDetailsPath(event.user.id)}>
-            {getUserOrApp()}
-          </Link>
-        )
-      });
-    case GiftCardEventsEnum.EXPIRY_DATE_UPDATED:
-      return intl.formatMessage(timelineMessages.giftCardExpiryDateUpdate, {
-        expiryUpdatedBy: (
-          <Link href={staffMemberDetailsPath(event.user.id)}>
-            {getUserOrApp()}
-          </Link>
-        )
-      });
-    case GiftCardEventsEnum.ISSUED:
-      return intl.formatMessage(timelineMessages.giftCardIssued, {
-        issuedBy: (
-          <Link href={staffMemberDetailsPath(event.user.id)}>
-            {getUserOrApp()}
-          </Link>
-        )
-      });
-    case GiftCardEventsEnum.RESENT:
-      return intl.formatMessage(timelineMessages.giftCardResent);
-    case GiftCardEventsEnum.SENT_TO_CUSTOMER:
-      return intl.formatMessage(timelineMessages.giftCardSentToCustomer);
-    case GiftCardEventsEnum.TAGS_UPDATED:
-      return intl.formatMessage(timelineMessages.giftCardTagsUpdated);
-    case GiftCardEventsEnum.UPDATED:
-      return intl.formatMessage(timelineMessages.giftCardTagsUpdated);
-    case GiftCardEventsEnum.USED_IN_ORDER:
-      return intl.formatMessage(timelineMessages.giftCardUsedInOrder, {
-        orderLink: (
-          <Link href={orderPath(event.orderId)}>#{event.orderNumber}</Link>
-        ),
-        buyer: content =>
-          getUserOrApp() ? (
-            <Link
-              href={getUserOrAppLink()}
-            >{`${content} ${getUserOrApp()}`}</Link>
-          ) : null
-      });
-  }
-};
 
 const GiftCardHistory: React.FC = () => {
   const intl = useIntl();
@@ -205,11 +93,7 @@ const GiftCardHistory: React.FC = () => {
                 }
 
                 return (
-                  <TimelineEvent
-                    key={id}
-                    date={date}
-                    title={getEventMessage(event, intl)}
-                  />
+                  <GiftCardTimelineEvent key={id} date={date} event={event} />
                 );
               })}
           </>
