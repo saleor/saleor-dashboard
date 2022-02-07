10fb086d6ec9c62a4da5b3cb991dc92608acd362
Karolina Rakoczy <rakoczy.karolina@gmail.com>
2022-02-07 10:30:52 +0100

    Add tests for product types for 3.1 (#1806)
    * tests for product types for 3.1
    
    * updated stories
    
    * chage test cases names for product types
    
    * add test for setting attribute as variant selection in product type
    
    * add test for setting attribute as variant selection in product type
    
    * split product types spec file
    
    * uncomment purchase with product types test
    
    * Update cypress/integration/configuration/productTypes/attributesInProduductTypes.js
    
    Co-authored-by: Ewa Czerniak <ewa.czerniak@saleor.io>
    
    Co-authored-by: Ewa Czerniak <ewa.czerniak@saleor.io>

--- src/productTypes/components/ProductTypeAttributes/ProductTypeAttributes.tsx
+++ src/productTypes/components/ProductTypeAttributes/ProductTypeAttributes.tsx
@@ -177,6 +177,7 @@ const ProductTypeAttributes: React.FC<ProductTypeAttributesProps> = props => {
                   </TableCell>
                   <TableCell className={classes.colAction}>
                     <IconButton
+                      data-test-id="delete-icon"
                       disabled={disabled}
                       variant="secondary"
                       onClick={stopPropagation(() =>
