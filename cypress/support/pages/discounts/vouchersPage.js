da0c49228f3588a2660040d429d4d2ebbee70564
Karolina Rakoczy <rakoczy.karolina@gmail.com>
2022-02-08 17:39:40 +0100

    Add tests for updating vouchers

--- cypress/support/pages/discounts/vouchersPage.js
+++ cypress/support/pages/discounts/vouchersPage.js
@@ -1,5 +1,9 @@
 import { VOUCHERS_SELECTORS } from "../../../elements/discounts/vouchers";
 import { BUTTON_SELECTORS } from "../../../elements/shared/button-selectors";
+import { voucherDetailsUrl } from "../../../fixtures/urlList";
+import { urlList } from "../../../fixtures/urlList";
+import { ONE_PERMISSION_USERS } from "../../../fixtures/users";
+import { createCheckoutWithVoucher } from "../../api/utils/ordersUtils";
 import { selectChannelInDetailsPages } from "../channelsPage";
 
 export const discountOptions = {
@@ -26,4 +30,61 @@ export function createVoucher({
   cy.get(BUTTON_SELECTORS.confirm)
     .click()
     .confirmationMessageShouldDisappear();
+}
+
+export function setVoucherDate({
+  voucherId,
+  startDate,
+  endDate,
+  endTime,
+  hasEndDate = false
+}) {
+  cy.visit(voucherDetailsUrl(voucherId)).waitForProgressBarToNotBeVisible();
+  if (startDate) {
+    cy.get(VOUCHERS_SELECTORS.startDateInput).type(startDate);
+  }
+  if (endDate) {
+    if (hasEndDate) {
+      cy.get(VOUCHERS_SELECTORS.hasEndDateCheckbox).click();
+    }
+    cy.get(VOUCHERS_SELECTORS.endDateInput)
+      .type(endDate)
+      .get(VOUCHERS_SELECTORS.endTimeInput)
+      .type(endTime);
+  }
+  cy.addAliasToGraphRequest("VoucherUpdate")
+    .get(BUTTON_SELECTORS.confirm)
+    .click()
+    .wait("@VoucherUpdate");
+}
+
+export function loginAndCreateCheckoutForVoucherWithDiscount({
+  discount,
+  voucherValue,
+  voucherCode,
+  channelName,
+  dataForCheckout,
+  usageLimit,
+  applyOnePerCustomer,
+  onlyStaff,
+  minOrderValue,
+  minAmountOfItems
+}) {
+  cy.clearSessionData()
+    .loginUserViaRequest("auth", ONE_PERMISSION_USERS.discount)
+    .visit(urlList.vouchers);
+  cy.softExpectSkeletonIsVisible();
+  createVoucher({
+    voucherCode,
+    voucherValue,
+    discountOption: discount,
+    channelName,
+    usageLimit,
+    applyOnePerCustomer,
+    onlyStaff,
+    minOrderValue,
+    minAmountOfItems
+  });
+  dataForCheckout.voucherCode = voucherCode;
+  return createCheckoutWithVoucher(dataForCheckout);
 }
