1e77665b4a2a2f2e779bee37f4b8ea3eb27b1100
Dominik Żegleń <flesz3@o2.pl>
2022-03-25 13:37:01 +0100

    Minor visual fixes (#1927)
    * Fix label
    
    * Improve product organization widget labels
    
    * Fix misaligned label
    
    * Fix arrow behavior
    
    * Add clickable rows to country picker
    
    * Update snapshots
    
    * Update messages
    
    * Fix misaligned label
    
    * Update snapshots

--- src/products/components/ProductVariantPrice/ProductVariantPrice.tsx
+++ src/products/components/ProductVariantPrice/ProductVariantPrice.tsx
@@ -28,16 +28,7 @@ import React from "react";
 import { FormattedMessage, MessageDescriptor, useIntl } from "react-intl";
 
 const useStyles = makeStyles(
-  theme => ({
-    caption: {
-      fontSize: 14,
-      padding: theme.spacing(0, 3, 2, 3)
-    },
-    colName: {
-      fontSize: 14,
-      paddingLeft: 0,
-      width: "auto"
-    },
+  () => ({
     colPrice: {
       textAlign: "right",
       verticalAlign: "top",
@@ -124,126 +115,125 @@ const ProductVariantPrice: React.FC<ProductVariantPriceProps> = props => {
           description: "product pricing, section header"
         })}
       />
-      <CardContent className={classes.pricingContent}>
-        <Typography variant="caption" className={classes.caption}>
+      <CardContent>
+        <Typography variant="body2">
           {intl.formatMessage({
             defaultMessage:
               "Channels that don’t have assigned prices will use their parent channel to define the price. Price will be converted to channel’s currency",
             description: "info text"
           })}
         </Typography>
-        <ResponsiveTable className={classes.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell>
-                <FormattedMessage
-                  defaultMessage="Channel Name"
-                  description="tabel column header"
-                />
-              </TableCell>
-              <TableCell className={classes.colType}>
-                <FormattedMessage
-                  defaultMessage="Selling Price"
-                  description="tabel column header"
-                />
-              </TableCell>
-              <TableCell className={classes.colType}>
-                <FormattedMessage
-                  defaultMessage="Cost price"
-                  description="tabel column header"
-                />
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {renderCollection(
-              ProductVariantChannelListings,
-              (listing, index) => {
-                const priceError = getFormChannelError(
-                  formErrors.price,
-                  listing.id
-                );
-                const costPriceError = getFormChannelError(
-                  formErrors.costPrice,
-                  listing.id
-                );
+      </CardContent>
+      <ResponsiveTable className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>
+              <FormattedMessage
+                defaultMessage="Channel Name"
+                description="tabel column header"
+              />
+            </TableCell>
+            <TableCell className={classes.colType}>
+              <FormattedMessage
+                defaultMessage="Selling Price"
+                description="tabel column header"
+              />
+            </TableCell>
+            <TableCell className={classes.colType}>
+              <FormattedMessage
+                defaultMessage="Cost price"
+                description="tabel column header"
+              />
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {renderCollection(
+            ProductVariantChannelListings,
+            (listing, index) => {
+              const priceError = getFormChannelError(
+                formErrors.price,
+                listing.id
+              );
+              const costPriceError = getFormChannelError(
+                formErrors.costPrice,
+                listing.id
+              );
 
-                return (
-                  <TableRow key={listing?.id || `skeleton-${index}`}>
-                    <TableCell>{listing?.name || <Skeleton />}</TableCell>
-                    <TableCell className={classes.colPrice}>
-                      {listing ? (
-                        <PriceField
-                          className={classes.input}
-                          error={!!priceError}
-                          label={intl.formatMessage({
-                            defaultMessage: "Price"
-                          })}
-                          name={`${listing.id}-channel-price`}
-                          value={listing.price || ""}
-                          currencySymbol={listing.currency}
-                          onChange={e =>
-                            onChange(listing.id, {
-                              costPrice: listing.costPrice,
-                              price: e.target.value,
-                              preorderThreshold: listing.preorderThreshold
-                            })
-                          }
-                          disabled={loading}
-                          required
-                          hint={
-                            priceError &&
-                            getProductErrorMessage(priceError, intl)
-                          }
-                        />
-                      ) : (
-                        <Skeleton />
-                      )}
-                    </TableCell>
-                    <TableCell className={classes.colPrice}>
-                      {listing ? (
-                        <PriceField
-                          className={classes.input}
-                          error={!!costPriceError}
-                          label={intl.formatMessage({
-                            defaultMessage: "Cost",
-                            description: "tabel column header"
-                          })}
-                          name={`${listing.id}-channel-costPrice`}
-                          value={listing.costPrice || ""}
-                          currencySymbol={listing.currency}
-                          onChange={e =>
-                            onChange(listing.id, {
-                              costPrice: e.target.value,
-                              price: listing.price,
-                              preorderThreshold: listing.preorderThreshold
-                            })
-                          }
-                          disabled={loading}
-                          hint={
-                            costPriceError
-                              ? getProductErrorMessage(costPriceError, intl)
-                              : ""
-                          }
-                        />
-                      ) : (
-                        <Skeleton />
-                      )}
-                    </TableCell>
-                  </TableRow>
-                );
-              },
-              () => (
-                <TableRow>
-                  <TableCell colSpan={numberOfColumns}>
-                    <FormattedMessage defaultMessage="No channels found" />
+              return (
+                <TableRow key={listing?.id || `skeleton-${index}`}>
+                  <TableCell>{listing?.name || <Skeleton />}</TableCell>
+                  <TableCell className={classes.colPrice}>
+                    {listing ? (
+                      <PriceField
+                        className={classes.input}
+                        error={!!priceError}
+                        label={intl.formatMessage({
+                          defaultMessage: "Price"
+                        })}
+                        name={`${listing.id}-channel-price`}
+                        value={listing.price || ""}
+                        currencySymbol={listing.currency}
+                        onChange={e =>
+                          onChange(listing.id, {
+                            costPrice: listing.costPrice,
+                            price: e.target.value,
+                            preorderThreshold: listing.preorderThreshold
+                          })
+                        }
+                        disabled={loading}
+                        required
+                        hint={
+                          priceError && getProductErrorMessage(priceError, intl)
+                        }
+                      />
+                    ) : (
+                      <Skeleton />
+                    )}
+                  </TableCell>
+                  <TableCell className={classes.colPrice}>
+                    {listing ? (
+                      <PriceField
+                        className={classes.input}
+                        error={!!costPriceError}
+                        label={intl.formatMessage({
+                          defaultMessage: "Cost",
+                          description: "tabel column header"
+                        })}
+                        name={`${listing.id}-channel-costPrice`}
+                        value={listing.costPrice || ""}
+                        currencySymbol={listing.currency}
+                        onChange={e =>
+                          onChange(listing.id, {
+                            costPrice: e.target.value,
+                            price: listing.price,
+                            preorderThreshold: listing.preorderThreshold
+                          })
+                        }
+                        disabled={loading}
+                        hint={
+                          costPriceError
+                            ? getProductErrorMessage(costPriceError, intl)
+                            : ""
+                        }
+                      />
+                    ) : (
+                      <Skeleton />
+                    )}
                   </TableCell>
                 </TableRow>
-              )
-            )}
-          </TableBody>
-        </ResponsiveTable>
-      </CardContent>
+              );
+            },
+            () => (
+              <TableRow>
+                <TableCell colSpan={numberOfColumns}>
+                  <FormattedMessage defaultMessage="No channels found" />
+                </TableCell>
+              </TableRow>
+            )
+          )}
+        </TableBody>
+      </ResponsiveTable>
     </Card>
   );
 };
