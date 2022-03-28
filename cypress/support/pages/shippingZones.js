8bc29316794a143a57a1a618fe7249482fc11a9d
Karolina Rakoczy <rakoczy.karolina@gmail.com>
2022-03-28 13:52:06 +0400

    Fix flaky tests for gift cards (#1938)
    * fix gift cards
    
    * add new command to search elements on table
    
    * remove unused imports

--- cypress/support/pages/shippingZones.js
+++ cypress/support/pages/shippingZones.js
@@ -1,5 +1,3 @@
-import { BUTTON_SELECTORS } from "../../elements/shared/button-selectors";
-import { SHARED_ELEMENTS } from "../../elements/shared/sharedElements";
 import {
   SHIPPING_ZONE_CHECKBOX,
   SHIPPING_ZONE_NAME
@@ -10,47 +8,13 @@ export function enterAndSelectShippings(
   shippingIds,
   actionFunction = selectShippingZone
 ) {
-  cy.addAliasToGraphRequest("ShippingZones").visit(urlList.shippingMethods);
-  selectShippingsOnTable(shippingIds, actionFunction);
-}
-
-export function selectShippingsOnTable(
-  shippingIds,
-  actionFunction = selectShippingZone,
-  counter = 0
-) {
-  cy.get(SHARED_ELEMENTS.skeleton)
-    .should("not.exist")
-    .wait("@ShippingZones")
-    .its("response.body")
-    .then(body => {
-      const shippingResults = body.find(element => {
-        if (element.data.shippingZones) {
-          return element;
-        }
-      });
-      const shippingList = shippingResults.data.shippingZones.edges;
-      const notSelectedShippings = [];
-      shippingIds = Array.isArray(shippingIds) ? shippingIds : [shippingIds];
-      shippingIds.forEach(id => {
-        const isShippingOnList = shippingList.find(
-          element => element.node.id === id
-        );
-        if (isShippingOnList) {
-          actionFunction(id);
-          counter += 1;
-        } else {
-          notSelectedShippings.push(id);
-        }
-        if (counter === shippingIds.length) {
-          return;
-        }
-      });
-      if (counter === shippingIds.length) {
-        return;
-      }
-      cy.get(BUTTON_SELECTORS.nextPaginationButton).click();
-      selectShippingsOnTable(notSelectedShippings, actionFunction, counter);
+  cy.addAliasToGraphRequest("ShippingZones")
+    .visit(urlList.shippingMethods)
+    .findElementsAndMakeActionOnTable({
+      elementsGraphqlAlias: "ShippingZones",
+      elementsName: "shippingZones",
+      elementsIds: shippingIds,
+      actionFunction
     });
 }
 
