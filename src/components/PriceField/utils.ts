5abf0180ce460d11d293f29404d2636159bd06dc
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2022-03-30 09:42:16 +0200

    Add fallback when no currency symbol is provided

--- src/components/PriceField/utils.ts
+++ src/components/PriceField/utils.ts
@@ -1,10 +1,31 @@
 import { SEPARATOR_CHARACTERS } from "./consts";
 
-export const getCurrencyDecimalPoints = (currency: string = "USD") => {
-  const options = new Intl.NumberFormat("en-GB", {
-    style: "currency",
-    currency
-  }).resolvedOptions();
+const getNumberFormatting = (currency: string = "USD") => {
+  try {
+    return new Intl.NumberFormat("en-GB", {
+      style: "currency",
+      currency
+    });
+  } catch (e) {
+    try {
+      // fallback to "USD" if currency wasn't recognised
+      return new Intl.NumberFormat("en-GB", {
+        style: "currency",
+        currency: "USD"
+      });
+    } catch {
+      // everything is broken - try to return something that makes sense
+      return {
+        resolvedOptions: () => ({
+          maximumFractionDigits: 2
+        })
+      };
+    }
+  }
+};
+
+export const getCurrencyDecimalPoints = (currency?: string) => {
+  const options = getNumberFormatting(currency).resolvedOptions();
   return options.maximumFractionDigits;
 };
 
