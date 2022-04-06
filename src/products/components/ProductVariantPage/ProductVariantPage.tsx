fa01ec9cb7a98909650a9e7c14a6857d7b90d639
Deven Vyas <vyas.deven@gmail.com>
2022-04-07 01:46:12 +0530

    Create copy of media array before sorting

--- src/products/components/ProductVariantPage/ProductVariantPage.tsx
+++ src/products/components/ProductVariantPage/ProductVariantPage.tsx
@@ -28,6 +28,7 @@ import { Backlink, ConfirmButtonTransitionState } from "@saleor/macaw-ui";
 import { FetchMoreProps, RelayToFlat, ReorderAction } from "@saleor/types";
 import React from "react";
 import { defineMessages, useIntl } from "react-intl";
+import clone from "lodash/clone";
 
 import ProductShipping from "../ProductShipping/ProductShipping";
 import ProductStocks, { ProductStockInput } from "../ProductStocks";
@@ -165,9 +166,11 @@ const ProductVariantPage: React.FC<ProductVariantPageProps> = ({
   ] = React.useState(false);
 
   const variantMedia = variant?.media?.map(image => image.id);
-  const productMedia = variant?.product?.media?.sort((prev, next) =>
+  const unsortedProductMedia = clone(variant?.product?.media)
+  const productMedia = unsortedProductMedia?.sort((prev, next) =>
     prev.sortOrder > next.sortOrder ? 1 : -1
   );
+
   const media = productMedia
     ?.filter(image => variantMedia.indexOf(image.id) !== -1)
     .sort((prev, next) => (prev.sortOrder > next.sortOrder ? 1 : -1));
