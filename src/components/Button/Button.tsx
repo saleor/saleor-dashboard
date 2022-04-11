df5c033abac2d11d42ae157ca0731e32c92a9ffc
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2022-04-11 17:06:24 +0200

    Rename _Button to Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,13 +4,13 @@ import { isExternalURL } from "@saleor/utils/urls";
 import React from "react";
 import { Link } from "react-router-dom";
 
-const _Button: React.FC<any> = React.forwardRef(({ href, ...props }) => {
+const Button: React.FC<any> = React.forwardRef(({ href, ...props }) => {
   if (href && !isExternalURL(href)) {
     return <MacawButton {...props} to={href} component={Link} />;
   }
 
   return <MacawButton href={href} {...props} />;
 });
-_Button.displayName = "Button";
+Button.displayName = "Button";
 
-export default _Button as OverridableComponent<ButtonTypeMap>;
+export default Button as OverridableComponent<ButtonTypeMap>;
