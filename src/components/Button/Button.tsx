1b32dff272209f649500eb8d1746e3d028e25e84
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2022-04-26 13:14:34 +0200

    Fix refs not passed to Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,12 +4,12 @@ import { isExternalURL } from "@saleor/utils/urls";
 import React from "react";
 import { Link } from "react-router-dom";
 
-const _Button: React.FC<any> = React.forwardRef(({ href, ...props }) => {
+const _Button: React.FC<any> = React.forwardRef(({ href, ...props }, ref) => {
   if (href && !isExternalURL(href)) {
-    return <MacawButton {...props} to={href} component={Link} />;
+    return <MacawButton {...props} to={href} component={Link} ref={ref} />;
   }
 
-  return <MacawButton href={href} {...props} />;
+  return <MacawButton href={href} {...props} ref={ref} />;
 });
 _Button.displayName = "Button";
 
