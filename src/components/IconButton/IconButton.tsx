d9b6f12e129793cd54c2e416d1ec00a146da47b9
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2022-04-26 16:16:58 +0200

    Add IconButton in dashboard for mapping internal links to Link component

---
+++ src/components/IconButton/IconButton.tsx
@@ -0,0 +1,25 @@
+import {
+  IconButton as MacawIconButton,
+  IconButtonProps
+} from "@saleor/macaw-ui";
+import { isExternalURL } from "@saleor/utils/urls";
+import React from "react";
+import { Link } from "react-router-dom";
+
+const _IconButton: React.FC<any> = React.forwardRef(
+  ({ href, ...props }, ref) => {
+    if (href && !isExternalURL(href)) {
+      return (
+        <MacawIconButton {...props} to={href} component={Link} ref={ref} />
+      );
+    }
+
+    return <MacawIconButton href={href} {...props} ref={ref} />;
+  }
+);
+
+export const IconButton = _IconButton as <
+  T extends React.ElementType = "button"
+>(
+  props: IconButtonProps<T>
+) => ReturnType<typeof _IconButton>;
