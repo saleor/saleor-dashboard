f8f9a490245b24add4f2956d0b1ac54f1e898ec1
Dominik Żegleń <flesz3@o2.pl>
2022-05-05 10:36:32 +0200

    Autocomplete in channel selection (#2020)
    * Add autocomplete to channel picker
    
    * Update macaw
    
    * Update macaw
    
    * Use filter from fuzzaldrin

---
+++ src/hooks/useChoiceSearch.ts
@@ -0,0 +1,16 @@
+import { Choice } from "@saleor/components/SingleSelectField";
+import { filter } from "fuzzaldrin";
+import { useMemo, useState } from "react";
+
+function useChoiceSearch(choices: Array<Choice<string, string>>) {
+  const [query, setQuery] = useState("");
+
+  const sortedChoices = useMemo(
+    () => filter(choices, query, { key: "label" }) || [],
+    [choices, query]
+  );
+
+  return { search: setQuery, result: sortedChoices };
+}
+
+export default useChoiceSearch;
