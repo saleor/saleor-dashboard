82e8d157e8f087afaba11664262f72e5af8c89f2
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2022-05-13 11:52:31 +0200

    Add comments about reasoning behind TableButtonWrapper

--- src/components/TableButtonWrapper/TableButtonWrapper.tsx
+++ src/components/TableButtonWrapper/TableButtonWrapper.tsx
@@ -7,13 +7,28 @@ interface TableButtonWrapper {
   }>;
 }
 
+/**
+ * Wraps any `Button` or `Link` used inside `TableRowLink` to handle events properly
+ *
+ * @example
+ * ```jsx
+ * <TableButtonWrapper>
+ *   <Button href="/some-page" />
+ * </TableButtonWrapper>
+ * ```
+ */
 export const TableButtonWrapper = <T extends HTMLElement>({
   children
 }: TableButtonWrapper) => {
   const onClick = (e: React.MouseEvent<T>) => {
     if (!children.props.href) {
+      // <TableRowLink> is a <a> that wraps each <tr>
+      // This causes buttons to act like links rather than buttons
+      // The events isn't boubled up to react-router's link, rather
+      // browser makes full page navigation directly, so we need to stop that
       e.preventDefault();
     }
+    // Stop propagation of clicks to the <Link> component, to prevent navigation
     e.stopPropagation();
 
     if (children.props.onClick) {
