bf173d9089e351625754b92349f608c9634eeb15
Michał Droń <dron.official@yahoo.com>
2022-05-16 21:03:36 +0200

    Add countries list view

---
+++ src/taxes/views/CountriesList.tsx
@@ -0,0 +1,31 @@
+import useNavigator from "@saleor/hooks/useNavigator";
+import React from "react";
+
+import { countriesListUrl, taxTabSectionUrl } from "../urls";
+
+interface CountriesListProps {
+  id: string;
+}
+
+export const CountriesList: React.FC<CountriesListProps> = ({ id }) => {
+  const navigate = useNavigator();
+
+  const handleTabChange = (tab: string) => {
+    navigate(taxTabSectionUrl(tab));
+  };
+
+  const exampleData = [
+    { id: "129837", name: "Brazil" },
+    { id: "9182739", name: "Andora" }
+  ];
+
+  React.useEffect(() => {
+    if (id === "undefined" && exampleData) {
+      navigate(countriesListUrl(exampleData?.[0].id));
+    }
+  }, [id, exampleData]);
+
+  return null;
+};
+
+export default CountriesList;
