7a785eca77d80a5d9e406241f7301c2f9529425d
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2022-05-26 17:24:13 +0200

    Refactor ShippingZonesList to use new usePaginator hook

--- src/storybook/stories/shipping/ShippingZonesListPage.tsx
+++ src/storybook/stories/shipping/ShippingZonesListPage.tsx
@@ -1,4 +1,5 @@
 import { WeightUnitsEnum } from "@saleor/graphql";
+import { PaginatorContextDecorator } from "@saleor/storybook/PaginatorContextDecorator";
 import { storiesOf } from "@storybook/react";
 import React from "react";
 
@@ -25,6 +26,7 @@ const props: ShippingZonesListPageProps = {
 
 storiesOf("Views / Shipping / Shipping zones list", module)
   .addDecorator(Decorator)
+  .addDecorator(PaginatorContextDecorator)
   .add("default", () => <ShippingZonesListPage {...props} />)
   .add("loading", () => (
     <ShippingZonesListPage
