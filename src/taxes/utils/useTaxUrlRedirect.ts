afcb1b19bd5c9a2f4398edb305d68a270686957d
Michał Droń <dron.official@yahoo.com>
2022-05-30 11:40:06 +0200

    Tax configuration - implement channels view (#2048)
    * Add channels view
    
    * Fix channels view import
    
    * Remove legacy stories references
    
    * Fix link in configuration
    
    * Update snapshots
    
    * Remove sample checkboxes props
    
    * Disable hover in country exceptions
    
    * Update snapshots
    
    * Extract country exception rows to seperate component
    
    * Extract components to seperate files
    
    * Remove duplicated section name
    
    * Remove backlink
    
    * Add translations to section names
    
    * Extract messages
    
    * Add ListItemLink component
    
    * Replace navigator with link in TaxChannelsMenu
    
    * Fix horizontal scroll in TaxChannelsMenu
    
    * Change codegen to build from custom schema
    
    * Build types
    
    * Update fragments
    
    * Add fixtures
    
    * Change any to proper types
    
    * Add story for tax channels page
    
    * Replace MUI Skeleton with Saleor Skeleton
    
    * Change clsx import to classnames
    
    * Fix checkboxes shadows in settings card
    
    * Update IDs in fixtures
    
    * Fix offset in TaxChannelsMenu
    
    * Update snapshots
    
    * Remove any from TaxSettings
    
    * Fix todos
    
    * Change relative marginLeft to before pseudoelement
    
    * Extract styles to seperate files
    
    * Change folder structure
    
    * Extract redirect logic to custom hook
    
    * Update snapshots
    
    * Fix comment
    
    * Add early return in channels view

---
+++ src/taxes/utils/useTaxUrlRedirect.ts
@@ -0,0 +1,34 @@
+/**
+ * This is a url to the tax page: /taxes/channels/{taxConfigurationId}
+ * However, when we load the page for the first time we don't know
+ * the first taxConfigurationId, we need to query it first. So this hook
+ * redirects from generic url (/taxes/channels/) to the one with id.
+ * Its use is similar in countries and tax classes views.
+ */
+
+import { UseNavigatorResult } from "@saleor/hooks/useNavigator";
+import React from "react";
+
+interface TaxEntity {
+  id: string;
+}
+
+interface UseTaxUrlRedirectOpts {
+  id: string;
+  data: TaxEntity[];
+  navigate: UseNavigatorResult;
+  urlFunction: (id: string) => string;
+}
+
+export function useTaxUrlRedirect({
+  id,
+  data,
+  navigate,
+  urlFunction
+}: UseTaxUrlRedirectOpts): void {
+  React.useEffect(() => {
+    if (id === "undefined" && data) {
+      navigate(urlFunction(data[0].id));
+    }
+  }, [id, data]);
+}
