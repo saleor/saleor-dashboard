a525e8625328bbbfab47f8f581c471d6ff4d6502
Karolina Rakoczy <rakoczy.karolina@gmail.com>
2022-06-02 11:47:12 +0200

    fix navigate through shop

--- cypress/integration/navigation.js
+++ cypress/integration/navigation.js
@@ -1,17 +1,52 @@
 /// <reference types="cypress"/>
 /// <reference types="../support"/>
 
+import {
+  APP_MENU_SELECTORS,
+  LEFT_MENU_SELECTORS
+} from "../elements/account/left-menu/left-menu-selectors";
+import { PERMISSIONS } from "../fixtures/permissions";
 import { PERMISSIONS_OPTIONS } from "../fixtures/permissionsUsers";
+import { urlList } from "../fixtures/urlList";
 import filterTests from "../support/filterTests";
 import * as permissionsSteps from "../support/pages/permissionsPage";
 
 describe("As a staff user I want to navigate through shop using different permissions", () => {
-  Object.keys(PERMISSIONS_OPTIONS).forEach(key => {
+  const permissionsOptions = PERMISSIONS_OPTIONS;
+
+  before(() => {
+    cy.loginUserViaRequest()
+      .visit("/")
+      .get("body")
+      .then(body => {
+        // This will be deleted when Marketplace is released
+        // Consider this solution as temporary
+        if (body.find(LEFT_MENU_SELECTORS.appSection)) {
+          const appPermissions = {
+            parent: {
+              parentMenuSelector: LEFT_MENU_SELECTORS.appSection,
+              parentSelectors: [APP_MENU_SELECTORS]
+            },
+            permissionSelectors: [APP_MENU_SELECTORS.app]
+          };
+          permissionsOptions.all.permissions.push(appPermissions);
+          permissionsOptions.app.permissions = [appPermissions];
+        } else {
+          const appPermissions = {
+            permissionSelectors: [menuSelectors.LEFT_MENU_SELECTORS.app]
+          };
+          permissionsOptions.all.permissions.push(appPermissions);
+          permissionsOptions.app.permissions = [appPermissions];
+        }
+      });
+  });
+
+  Object.keys(permissionsOptions).forEach(key => {
     const tags =
       key === "all" ? ["critical", "all", "refactored"] : ["all", "refactored"];
     filterTests({ definedTags: tags }, () => {
-      it(`should be able to navigate through shop as a staff member using ${key} permission. ${PERMISSIONS_OPTIONS[key].testCase}`, () => {
-        const permissionOption = PERMISSIONS_OPTIONS[key];
+      it(`should be able to navigate through shop as a staff member using ${key} permission. ${permissionsOptions[key].testCase}`, () => {
+        const permissionOption = permissionsOptions[key];
         const permissions = permissionOption.permissions;
         cy.clearSessionData();
         permissionsSteps.navigateToAllAvailablePageAndCheckIfDisplayed(
