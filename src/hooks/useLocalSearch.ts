4eae8cc3e8528e514104d10dbd39b26ca6da1c22
Michał Droń <dron.official@yahoo.com>
2022-06-24 14:23:11 +0200

    Add hook description

--- src/hooks/useLocalSearch.ts
+++ src/hooks/useLocalSearch.ts
@@ -1,8 +1,23 @@
 import React from "react";
 
+/**
+ * This function removes characters which break
+ * regexps but are rarely useful in searches
+ */
 const parseQuery = (query: string) =>
   query.replace(/([.?*+\-=:^$\\[\]<>(){}|])/g, "\\$&");
 
+/**
+ * useLocalSearch is a hook that is useful when client-side
+ * search is used on particular array of items.
+ *
+ * @param array Array to search through
+ * @param getStringToSearch Function which specifies object
+ * property (possibly nested) on which search is used, eg:
+ * (element) => element.name where name is a string
+ * @returns query, setQuery - useState result
+ * searchResult - filtered array
+ */
 export function useLocalSearch<T>(
   array: T[] | undefined,
   getStringToSearch: (element: T) => string
