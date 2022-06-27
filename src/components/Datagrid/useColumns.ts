2f51dbac8254d56e67bedde04afc2f9edabfda62
Dominik Żegleń <flesz3@o2.pl>
2022-06-27 11:55:18 +0200

    Memoize callbacks

--- src/components/Datagrid/useColumns.ts
+++ src/components/Datagrid/useColumns.ts
@@ -1,7 +1,7 @@
 import { GridColumn } from "@glideapps/glide-data-grid";
 import useStateFromProps from "@saleor/hooks/useStateFromProps";
 import { addAtIndex, removeAtIndex } from "@saleor/utils/lists";
-import { useMemo, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 
 import { AvailableColumn } from "./types";
 
@@ -12,28 +12,35 @@ function useColumns(availableColumns: readonly AvailableColumn[]) {
   );
   const [columnState, setColumnState] = useStateFromProps(availableColumns);
 
-  const onColumnMoved = (startIndex: number, endIndex: number): void => {
-    setDisplayedColumns(old =>
-      addAtIndex(old[startIndex], removeAtIndex(old, startIndex), endIndex)
-    );
-  };
-
-  const onColumnResize = (column: GridColumn, newSize: number) =>
-    setColumnState(prevColumns =>
-      prevColumns.map(prevColumn =>
-        prevColumn.id === column.id
-          ? { ...prevColumn, width: newSize }
-          : prevColumn
-      )
-    );
-
-  const onColumnsChange = (picked: string[]) =>
-    setDisplayedColumns(prevColumns => [
-      ...prevColumns.filter(column => picked.includes(column)),
-      ...picked
-        .filter(column => !prevColumns.find(c => c === column))
-        .map(column => availableColumns.find(ac => ac.id === column).id)
-    ]);
+  const onColumnMoved = useCallback(
+    (startIndex: number, endIndex: number): void => {
+      setDisplayedColumns(old =>
+        addAtIndex(old[startIndex], removeAtIndex(old, startIndex), endIndex)
+      );
+    },
+    []
+  );
+  const onColumnResize = useCallback(
+    (column: GridColumn, newSize: number) =>
+      setColumnState(prevColumns =>
+        prevColumns.map(prevColumn =>
+          prevColumn.id === column.id
+            ? { ...prevColumn, width: newSize }
+            : prevColumn
+        )
+      ),
+    []
+  );
+  const onColumnsChange = useCallback(
+    (picked: string[]) =>
+      setDisplayedColumns(prevColumns => [
+        ...prevColumns.filter(column => picked.includes(column)),
+        ...picked
+          .filter(column => !prevColumns.find(c => c === column))
+          .map(column => availableColumns.find(ac => ac.id === column).id)
+      ]),
+    [availableColumns]
+  );
 
   const columns = useMemo(
     () => displayedColumns.map(id => columnState.find(ac => ac.id === id)),
