07d47c1754f152f51c85f112d86d50db97fb07bd
Timur <timuric@gmail.com>
2022-07-01 16:45:38 +0200

    Test fira sans

--- src/themeOverrides.ts
+++ src/themeOverrides.ts
@@ -1,6 +1,22 @@
 import { Theme } from "@material-ui/core/styles";
 
+const fontFamily = "Fira Sans, sans-serif";
 const themeOverrides: Partial<Theme> = {
+  // @ts-ignore
+  typography: {
+    fontFamily,
+    h1: { fontFamily },
+    h2: { fontFamily },
+    h3: { fontFamily },
+    h4: { fontFamily },
+    body1: { fontFamily },
+    body2: { fontFamily },
+    caption: { fontFamily },
+    button: { fontFamily },
+    h6: { fontFamily },
+    h5: { fontFamily },
+    subtitle2: { fontFamily },
+  },
   overrides: {
     MuiTableCell: {
       body: {
