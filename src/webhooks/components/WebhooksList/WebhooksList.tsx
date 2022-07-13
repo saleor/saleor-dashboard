18ef3a5c4430a3cade9f4163e9e0a81b9034f294
Dawid <tarasiukdawid@gmail.com>
2022-07-13 13:12:58 +0200

    Add status pills to webhooks list (#2153)

--- src/webhooks/components/WebhooksList/WebhooksList.tsx
+++ src/webhooks/components/WebhooksList/WebhooksList.tsx
@@ -13,7 +13,8 @@ import { TableButtonWrapper } from "@saleor/components/TableButtonWrapper/TableB
 import TableCellHeader from "@saleor/components/TableCellHeader";
 import TableRowLink from "@saleor/components/TableRowLink";
 import { AppQuery } from "@saleor/graphql";
-import { DeleteIcon, IconButton } from "@saleor/macaw-ui";
+import { commonMessages, sectionNames } from "@saleor/intl";
+import { DeleteIcon, IconButton, Pill } from "@saleor/macaw-ui";
 import { renderCollection, stopPropagation } from "@saleor/misc";
 import { webhookPath } from "@saleor/webhooks/urls";
 import { isUnnamed } from "@saleor/webhooks/utils";
@@ -21,6 +22,7 @@ import classNames from "classnames";
 import React from "react";
 import { FormattedMessage, useIntl } from "react-intl";
 
+import { messages } from "./messages";
 import { useStyles } from "./styles";
 
 export interface WebhooksListProps {
@@ -35,17 +37,13 @@ const WebhooksList: React.FC<WebhooksListProps> = ({
   onRemove,
 }) => {
   const intl = useIntl();
-  const classes = useStyles({});
+  const classes = useStyles();
   const numberOfColumns = webhooks?.length === 0 ? 2 : 3;
 
   return (
     <Card>
       <CardTitle
-        title={intl.formatMessage({
-          id: "jqnwW9",
-          defaultMessage: "Webhooks",
-          description: "header",
-        })}
+        title={intl.formatMessage(sectionNames.webhooks)}
         toolbar={
           !!createHref && (
             <Button
@@ -53,11 +51,7 @@ const WebhooksList: React.FC<WebhooksListProps> = ({
               href={createHref}
               data-test-id="create-webhook"
             >
-              <FormattedMessage
-                id="wlr0Si"
-                defaultMessage="Create Webhook"
-                description="button"
-              />
+              <FormattedMessage {...messages.createWebhook} />
             </Button>
           )
         }
@@ -66,20 +60,15 @@ const WebhooksList: React.FC<WebhooksListProps> = ({
         <TableHead>
           <TableRow>
             <TableCellHeader>
-              {intl.formatMessage({
-                id: "OTpV1t",
-                defaultMessage: "Name",
-                description: "webhook name",
-              })}
+              {intl.formatMessage(commonMessages.name)}
+            </TableCellHeader>
+            <TableCellHeader>
+              {intl.formatMessage(commonMessages.status)}
             </TableCellHeader>
             <TableCell
               className={classNames(classes.colAction, classes.colRight)}
             >
-              {intl.formatMessage({
-                id: "a/QJBx",
-                defaultMessage: "Action",
-                description: "user action bar",
-              })}
+              <FormattedMessage {...messages.action} />
             </TableCell>
           </TableRow>
         </TableHead>
@@ -99,12 +88,23 @@ const WebhooksList: React.FC<WebhooksListProps> = ({
                   })}
                 >
                   {isUnnamed(webhook) ? (
-                    <FormattedMessage
-                      id="1eCau/"
-                      defaultMessage="Unnamed webhook"
-                    />
+                    <FormattedMessage {...messages.unnamedWebhook} />
                   ) : (
                     webhook?.name || <Skeleton />
+                  )}
+                </TableCell>
+                <TableCell>
+                  {webhook ? (
+                    <Pill
+                      label={
+                        webhook.isActive
+                          ? intl.formatMessage(commonMessages.active)
+                          : intl.formatMessage(commonMessages.notActive)
+                      }
+                      color={webhook.isActive ? "success" : "error"}
+                    />
+                  ) : (
+                    <Skeleton />
                   )}
                 </TableCell>
                 <TableCell
@@ -129,10 +129,7 @@ const WebhooksList: React.FC<WebhooksListProps> = ({
             () => (
               <TableRow>
                 <TableCell colSpan={numberOfColumns}>
-                  {intl.formatMessage({
-                    id: "wbjuR4",
-                    defaultMessage: "No webhooks found",
-                  })}
+                  {intl.formatMessage(messages.noWebhooks)}
                 </TableCell>
               </TableRow>
             ),
