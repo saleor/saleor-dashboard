83988f08c50c946c4fb22377b41305c6b2e414f7
Ewa Czerniak <ewa.czerniak@saleor.io>
2022-07-15 11:50:24 +0200

    Add test for sorting by price, unification of productList folder (#2170)
    * Add test for sorting by price, unification of productList folder
    
    * Correction after review
    
    * correction of function name

--- cypress/elements/catalog/products/products-list.js
+++ cypress/elements/catalog/products/products-list.js
@@ -10,13 +10,13 @@ export const PRODUCTS_LIST = {
     name: '[data-test-id="name"]',
     type: '[data-test-id="product-type"]',
     availability: '[data-test-id="availability"]',
-    price: '[data-test-id="price"]'
+    price: '[data-test-id="price"]',
   },
   tableHeaders: {
     name: "[data-test-id='col-name-header']",
     type: "[data-test-id='col-type-header']",
     availability: "[data-test-id='col-availability-header']",
-    price: "[data-test-id='col-price-header']"
+    price: "[data-test-id='col-price-header']",
   },
   showFiltersButton: '[data-test-id="show-filters-button"]',
   filters: {
@@ -27,7 +27,7 @@ export const PRODUCTS_LIST = {
       collection: '[data-test-id="filter-group-active-collections"]',
       productType: '[data-test-id="filter-group-active-productType"]',
       stock: '[data-test-id="filter-group-active-stock"]',
-      channel: '[data-test-id="filter-group-active-channel"]'
+      channel: '[data-test-id="filter-group-active-channel"]',
     },
     filterField: {
       filterField: '[data-test-id*="filter-field"]',
@@ -35,12 +35,12 @@ export const PRODUCTS_LIST = {
       collection: '[data-test-id="filter-field-collections"]',
       productType: '[data-test-id="filter-field-productType"]',
       stock: '[data-test-id="filter-field-stock"]',
-      channel: '[data-test-id="filter-field-channel"]'
+      channel: '[data-test-id="filter-field-channel"]',
     },
-    filterBySearchInput: '[data-test-id="filter-field-autocomplete-input"]'
+    filterBySearchInput: '[data-test-id="filter-field-autocomplete-input"]',
   },
   nextPageButton: "[data-test='button-pagination-next']",
   previousPagePagination: "[data-test='button-pagination-back']",
   resultsOnPageSelect: "[data-test-id='PaginationRowNumberSelect']",
-  rowNumberOption: "[data-test-id='rowNumberOption']"
+  rowNumberOption: "[data-test-id='rowNumberOption']",
 };
