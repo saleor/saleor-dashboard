87808f66482448d4301d13196be84cb55912d994
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2022-08-12 17:28:02 +0200

    Don't hide transaction balance when order is not confirmed

--- src/orders/components/OrderDetailsPage/OrderDetailsPage.tsx
+++ src/orders/components/OrderDetailsPage/OrderDetailsPage.tsx
@@ -299,19 +299,15 @@ const OrderDetailsPage: React.FC<OrderDetailsPageProps> = props => {
                     />
                   </React.Fragment>
                 ))}
-                {!isOrderUnconfirmed && (
-                  <>
-                    <OrderPayment
-                      order={order}
-                      onCapture={onPaymentCapture}
-                      onMarkAsPaid={onPaymentPaid}
-                      onRefund={onPaymentRefund}
-                      onVoid={onPaymentVoid}
-                    />
-                    <CardSpacer />
-                    <Metadata data={data} onChange={changeMetadata} />
-                  </>
-                )}
+                <OrderPayment
+                  order={order}
+                  onCapture={onPaymentCapture}
+                  onMarkAsPaid={onPaymentPaid}
+                  onRefund={onPaymentRefund}
+                  onVoid={onPaymentVoid}
+                />
+                <CardSpacer />
+                <Metadata data={data} onChange={changeMetadata} />
                 <OrderHistory
                   history={order?.events}
                   orderCurrency={order?.total?.gross.currency}
