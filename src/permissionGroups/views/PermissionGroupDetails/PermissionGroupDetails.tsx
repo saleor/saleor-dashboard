f79ce8c8ef322f97fb412a4ee2f51445defa1838
Michał Droń <dron.official@yahoo.com>
2022-08-16 16:12:37 +0200

    Allow saving regardless of membersChanged value

--- src/permissionGroups/views/PermissionGroupDetails/PermissionGroupDetails.tsx
+++ src/permissionGroups/views/PermissionGroupDetails/PermissionGroupDetails.tsx
@@ -61,8 +61,6 @@ export const PermissionGroupDetails: React.FC<PermissionGroupDetailsProps> = ({
     data?.permissionGroup.users,
   );
 
-  const [membersModified, setMembersModified] = useState(false);
-
   const { search, result: searchResult, loadMore } = useStaffMemberSearch({
     variables: DEFAULT_INITIAL_SEARCH_DATA,
   });
@@ -104,7 +102,6 @@ export const PermissionGroupDetails: React.FC<PermissionGroupDetailsProps> = ({
 
   const unassignMembers = () => {
     setMembersList(membersList?.filter(m => !listElements.includes(m.id)));
-    setMembersModified(true);
     closeModal();
   };
 
@@ -153,7 +150,6 @@ export const PermissionGroupDetails: React.FC<PermissionGroupDetailsProps> = ({
         permissionGroup={data?.permissionGroup}
         permissionsExceeded={permissionsExceeded}
         members={membersList || []}
-        membersModified={membersModified}
         onAssign={() => openModal("assign")}
         onUnassign={ids => openModal("unassign", { ids })}
         errors={
@@ -198,7 +194,6 @@ export const PermissionGroupDetails: React.FC<PermissionGroupDetailsProps> = ({
             ...membersList,
             ...formData.filter(member => !membersList.includes(member)),
           ]);
-          setMembersModified(true);
           closeModal();
         }}
       />
