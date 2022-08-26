6da62ac7e728e6f8211e6c9b30435b1f4a749acc
Michał Droń <dron.official@yahoo.com>
2022-08-26 11:03:34 +0200

    Allow only 3 decimal characters in tax inputs

--- src/taxes/components/TaxInput/TaxInput.tsx
+++ src/taxes/components/TaxInput/TaxInput.tsx
@@ -1,4 +1,5 @@
-import { InputAdornment, TextField } from "@material-ui/core";
+import { InputAdornment, TextField, TextFieldProps } from "@material-ui/core";
+import { findPriceSeparator } from "@saleor/components/PriceField/utils";
 import { FormChange } from "@saleor/hooks/useForm";
 import React from "react";
 
@@ -17,6 +18,29 @@ export const TaxInput: React.FC<TaxInputProps> = ({
 }) => {
   const classes = useStyles();
 
+  const handleChange: FormChange = e => {
+    let value = e.target.value;
+    const splitCharacter = findPriceSeparator(value);
+    const [integerPart, decimalPart] = value.split(splitCharacter);
+
+    if (decimalPart?.length > 3) {
+      const shortenedDecimalPart = decimalPart.slice(0, 3);
+      value = `${integerPart}${splitCharacter}${shortenedDecimalPart}`;
+    }
+
+    change({
+      target: {
+        name: e.target.name,
+        value,
+      },
+    });
+  };
+  const handleKeyDown: TextFieldProps["onKeyDown"] = e => {
+    if (e.key === "e" || e.key === "E") {
+      e.preventDefault();
+    }
+  };
+
   return (
     <TextField
       type="number"
@@ -27,8 +51,14 @@ export const TaxInput: React.FC<TaxInputProps> = ({
         startAdornment: <InputAdornment position="start">%</InputAdornment>,
         className: classes.hideSpinboxes,
       }}
-      inputProps={{ className: classes.inputPadding, min: 0, max: 100 }}
-      onChange={change}
+      inputProps={{
+        className: classes.inputPadding,
+        min: 0,
+        max: 100,
+        step: 0.001,
+      }}
+      onChange={handleChange}
+      onKeyDown={handleKeyDown}
     />
   );
 };
