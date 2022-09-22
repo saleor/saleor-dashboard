25f18e6a22847e5a4f62146e0a71c54c3fc6a3f2
Michał Droń <dron.official@yahoo.com>
2022-09-22 11:10:28 +0200

    Connect countries view to API (#2178)
    * Add empty states
    
    * Update countries view urls
    
    * Remove unused import
    
    * Add country modal to countries view
    
    * Update schema
    
    * Implement country view mutations & error fragments
    
    * Implement tax class update mutation
    
    * Add sidebar temporary state for new configs
    
    * Remove unused imports
    
    * Wrap in form
    
    * Add savebar & fix search
    
    * Update schema
    
    * Add form wrapper
    
    * Fix types
    
    * Extract messages
    
    * Bump macaw
    
    * Update snapshots
    
    * Fix comma dangles
    
    * Update snapshots
    
    * Notify about mutation success
    
    * Add logic for mixing current and new rates
    
    * Workaround for sending null rates
    
    * Fix filling form with correct data after submitting
    
    * Handle deleting configuration
    
    * Fix selected banner
    
    * Remove leftover comment
    
    * Add handler for country configuration delete
    
    * Trigger deployment
    
    * Clean up useEffects causing infinite render loops
    
    * Sort countries from api by name
    
    * Fix card bottom padding
    
    * Remove bottom divider & fix padding
    
    * Remove scroll wrapper in side menu
    
    * Update snapshots
    
    * Remove scroll wrapper from tax classes menu
    
    * Update snapshots
    
    * Refresh form to initial onSubmit
    
    * Revert "Refresh form to initial onSubmit"
    
    This reverts commit 42414237d35086da63f4aa088c8072411429b1d8.
    
    * Allow only 3 decimal characters in tax inputs
    
    * Update snapshots
    
    * Update schema
    
    * Update types
    
    * Change logic from default tax class to null class
    
    * Fix sorting
    
    * Send empty country rates as nulls in mutation
    
    * Extract messages
    
    * Update lockfile
    
    * Update schema
    
    * Drop default tax classes
    
    * Update snapshots

--- src/taxes/components/TaxInput/TaxInput.tsx
+++ src/taxes/components/TaxInput/TaxInput.tsx
@@ -1,11 +1,12 @@
-import { InputAdornment, TextField } from "@material-ui/core";
+import { InputAdornment, TextField, TextFieldProps } from "@material-ui/core";
+import { findPriceSeparator } from "@saleor/components/PriceField/utils";
 import { FormChange } from "@saleor/hooks/useForm";
 import React from "react";
 
 import { useStyles } from "./styles";
 
 interface TaxInputProps {
-  placeholder: number | undefined;
+  placeholder: string | undefined;
   value: string | undefined;
   change: FormChange;
 }
@@ -17,18 +18,47 @@ export const TaxInput: React.FC<TaxInputProps> = ({
 }) => {
   const classes = useStyles();
 
+  const handleChange: FormChange = e => {
+    let value = e.target.value;
+    const splitCharacter = findPriceSeparator(value);
+    const [integerPart, decimalPart] = value.split(splitCharacter);
+
+    if (decimalPart?.length > 3) {
+      const shortenedDecimalPart = decimalPart.slice(0, 3);
+      value = `${integerPart}${splitCharacter}${shortenedDecimalPart}`;
+    }
+
+    change({
+      target: {
+        name: e.target.name,
+        value,
+      },
+    });
+  };
+  const handleKeyDown: TextFieldProps["onKeyDown"] = e => {
+    if (e.key === "e" || e.key === "E") {
+      e.preventDefault();
+    }
+  };
+
   return (
     <TextField
       type="number"
       fullWidth
-      placeholder={placeholder?.toString()}
+      placeholder={placeholder}
       value={value}
       InputProps={{
         startAdornment: <InputAdornment position="start">%</InputAdornment>,
         className: classes.hideSpinboxes,
       }}
-      inputProps={{ className: classes.inputPadding, min: 0, max: 100 }}
-      onChange={change}
+      inputProps={{
+        className: classes.inputPadding,
+        min: 0,
+        max: 100,
+        step: 0.001,
+      }}
+      onChange={handleChange}
+      onKeyDown={handleKeyDown}
     />
   );
 };
--- src/taxes/utils/utils.ts
+++ src/taxes/utils/utils.ts
@@ -1,19 +1,74 @@
 import {
-  CountryWithCodeFragment,
+  CountryFragment,
   TaxClassFragment,
+  TaxCountryConfigurationFragment,
   TaxRateFragment,
 } from "@saleor/graphql";
+import uniqBy from "lodash/uniqBy";
 
-export const getDefaultTaxRateInCountry = (
-  taxClasses: TaxClassFragment[] | undefined,
-  selectedCountry: CountryWithCodeFragment,
-): TaxRateFragment["rate"] | undefined =>
-  taxClasses
-    ?.find(taxClass => taxClass.isDefault)
-    .countries.find(country => country.country.code === selectedCountry.code)
-    .rate;
+export const getDefaultTaxRateInCountry = ():
+  | TaxRateFragment["rate"]
+  | undefined => 0; // @TODO: implement default tax rates in tax classes page
 
 export const encodeURIComponentOptional = (
   uriComponent: string | number | boolean | undefined,
 ): string | undefined =>
   uriComponent ? encodeURIComponent(uriComponent) : undefined;
+
+export const filterChosenCountries = (
+  countries: CountryFragment[],
+  configurations: TaxCountryConfigurationFragment[],
+) =>
+  countries.filter(
+    country =>
+      !configurations.find(config => config.country.code === country.code),
+  );
+export const mapUndefinedTaxRatesToCountries = (
+  taxConfigurations: TaxCountryConfigurationFragment[],
+  taxClasses: TaxClassFragment[],
+): TaxCountryConfigurationFragment[] =>
+  taxConfigurations
+    .map(config => {
+      if (config.taxClassCountryRates.length === taxClasses.length + 1) {
+        return {
+          ...config,
+          taxClassCountryRates: [...config.taxClassCountryRates].sort(rate =>
+            rate.taxClass ? 1 : -1,
+          ),
+        };
+      } else {
+        const taxClassCountryRates = uniqBy(
+          [
+            ...config.taxClassCountryRates,
+            ...taxClasses.map(taxClass => ({
+              taxClass,
+              rate: undefined,
+              __typename: "TaxClassCountryRate" as const,
+            })),
+          ],
+          "taxClass.id",
+        );
+        const defaultRate = taxClassCountryRates.find(
+          rate => rate.taxClass === null,
+        );
+        const parsedCountryRates = taxClassCountryRates.filter(
+          rate => rate.taxClass !== null,
+        );
+        parsedCountryRates.unshift(
+          defaultRate ?? {
+            rate: undefined,
+            taxClass: null,
+            __typename: "TaxClassCountryRate" as const,
+          },
+        );
+
+        return {
+          ...config,
+          taxClassCountryRates: parsedCountryRates,
+        };
+      }
+    })
+    .sort((a, b) => a.country.country.localeCompare(b.country.country));
+
+export const isOnlyTaxClass = (taxClasses: TaxClassFragment[]) =>
+  taxClasses.length === 1;
