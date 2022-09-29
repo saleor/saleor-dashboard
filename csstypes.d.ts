fc03fdf996422036b60fbe7bf3884599e1f8cd8c
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2022-09-29 16:10:30 +0200

    Update types for React 17

---
+++ csstypes.d.ts
@@ -0,0 +1,5 @@
+// Support for latest CSS properties types in legacy Material-UI.
+// Needed until Material-UI is removed from Macaw-UI.
+declare module "csstype" {
+  export * from "node_modules/csstype";
+}
