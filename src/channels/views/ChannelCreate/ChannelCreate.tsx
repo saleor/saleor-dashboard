57c7babdf9d0d6fd00c67083439282abd8bd0b01
Patryk Andrzejewski <vox3r69@gmail.com>
2022-10-05 12:35:37 +0200

    pick explicitly fields for mutation (#2353)

--- src/channels/views/ChannelCreate/ChannelCreate.tsx
+++ src/channels/views/ChannelCreate/ChannelCreate.tsx
@@ -63,12 +63,16 @@ export const ChannelCreateView = ({}) => {
     warehousesToDisplay,
     currencyCode,
     allocationStrategy,
-    ...rest
+    name,
+    slug,
+    defaultCountry,
   }: FormData) => {
     const createChannelMutation = createChannel({
       variables: {
         input: {
-          ...rest,
+          defaultCountry,
+          name,
+          slug,
           currencyCode: currencyCode.toUpperCase(),
           addShippingZones: shippingZonesIdsToAdd,
           addWarehouses: warehousesIdsToAdd,
