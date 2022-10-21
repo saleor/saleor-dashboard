792ae320c55b773a67d5d395f797ead490a11574
Karolina Rakoczy <rakoczy.karolina@gmail.com>
2022-10-21 13:52:19 +0400

    Fix test for creating app

--- cypress/elements/apps/appsList.js
+++ cypress/elements/apps/appsList.js
@@ -1,3 +1,4 @@
 export const APPS_LIST = {
-  createLocalAppButton: '[data-test-id="create-app"]'
+  createLocalAppButton: '[data-test-id="create-app"]',
+  webhookAndEventsTab: '[id="WEBHOOKS_AND_EVENTS"]',
 };
