c009f075810c64d6410b373b5a8a267f7d10ad9a
Patryk Andrzejewski <vox3r69@gmail.com>
2022-10-27 17:26:09 +0200

    DataGrid full-screen mode (#2453)
    * full screen mode
    
    * add product title, review
    
    * update snapshots
    
    * Update messages

---
+++ src/components/Datagrid/usePortalClasses.ts
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+
+/**
+ * Glide automatically refers to the div with an id "portal"
+ * This hook allows to add additional classes for it
+ */
+const portalRoot = document.getElementById("portal");
+
+export const usePortalClasses = ({ className }: { className: string }) => {
+  useEffect(() => {
+    portalRoot.classList.add(className);
+  }, []);
+};
