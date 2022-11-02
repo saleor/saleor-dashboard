7387019288dae6076a07271937fce3ee6462dff9
Michał Droń <dron.official@yahoo.com>
2022-11-02 12:20:25 +0100

    Use single price value instead of range in product list (#2386)
    * Render single value when amount of money is matching
    
    * Use Intl.NumberFormat in money formatting
    
    * Extract messages

--- src/components/MoneyRange/MoneyRange.tsx
+++ src/components/MoneyRange/MoneyRange.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useIntl } from "react-intl";
 
 import { LocaleConsumer } from "../Locale";
-import { formatMoney, IMoney } from "../Money";
+import { formatMoney, formatMoneyRange, IMoney } from "../Money";
 
 export interface MoneyRangeProps {
   from?: IMoney;
@@ -14,43 +14,38 @@ export const MoneyRange: React.FC<MoneyRangeProps> = ({ from, to }) => {
 
   return (
     <LocaleConsumer>
-      {({ locale }) =>
-        from && to
-          ? intl.formatMessage(
-              {
-                id: "zTdwWM",
-                defaultMessage: "{fromMoney} - {toMoney}",
-                description: "money",
-              },
-              {
-                fromMoney: formatMoney(from, locale),
-                toMoney: formatMoney(to, locale),
-              },
-            )
-          : from && !to
-          ? intl.formatMessage(
-              {
-                id: "lW5uJO",
-                defaultMessage: "from {money}",
-                description: "money",
-              },
-              {
-                money: formatMoney(from, locale),
-              },
-            )
-          : !from && to
-          ? intl.formatMessage(
-              {
-                id: "hptDxW",
-                defaultMessage: "to {money}",
-                description: "money",
-              },
-              {
-                money: formatMoney(to, locale),
-              },
-            )
-          : "-"
-      }
+      {({ locale }) => {
+        if (from && to) {
+          return from.amount === to.amount
+            ? formatMoney(from, locale)
+            : formatMoneyRange(from, to, locale);
+        }
+        if (from && !to) {
+          return intl.formatMessage(
+            {
+              id: "lW5uJO",
+              defaultMessage: "from {money}",
+              description: "money",
+            },
+            {
+              money: formatMoney(from, locale),
+            },
+          );
+        }
+        if (!from && to) {
+          return intl.formatMessage(
+            {
+              id: "hptDxW",
+              defaultMessage: "to {money}",
+              description: "money",
+            },
+            {
+              money: formatMoney(to, locale),
+            },
+          );
+        }
+        return "-";
+      }}
     </LocaleConsumer>
   );
 };
