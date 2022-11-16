2bbb458f56da217380ae73b0a651fe849c4459b5
Karolina Rakoczy <karolina.rakoczy@saleor.io>
2022-11-16 13:44:34 +0100

    Fix tests for customer registration and reset password (#2560)

--- cypress/support/api/utils/users.js
+++ cypress/support/api/utils/users.js
@@ -22,21 +22,21 @@ export function inviteStaffMemberWithFirstPermission({
  * Function mhGetMailsByRecipient first get all emails from mailhog with a timeout, and after that it finds email from recipient.
  * It cloud happened that invite email from saleor has not been received yet, so in this case the action should be retried.
  */
-export function getMailActivationLinkForUser(email, i = 0) {
+export function getMailActivationLinkForUser(email, regex, i = 0) {
+  const urlRegex = regex ? regex : /\[\w*password\w*\]\(([^\)]*)/;
   if (i > 3) {
     throw new Error(`There is no email invitation for user ${email}`);
   }
   return cy.mhGetMailsByRecipient(email).should(mails => {
     if (!mails.length) {
       cy.wait(10000);
-      getMailActivationLinkForUser(email, i + 1);
+      getMailActivationLinkForUser(email, regex, i + 1);
     } else {
       cy.wrap(mails)
         .mhFirst()
         .should("not.eq", undefined)
         .mhGetBody()
         .then(body => {
-          const urlRegex = /\[\w*password\w*\]\(([^\)]*)/;
           const bodyWithoutWhiteSpaces = body.replace(/(\r\n|\n|\r|\s)/gm, "");
           return urlRegex.exec(bodyWithoutWhiteSpaces)[1];
         });
