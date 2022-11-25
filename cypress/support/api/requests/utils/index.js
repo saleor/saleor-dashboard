acc921e97085da366c466bd2764ea974a0183b87
Karolina Rakoczy <rakoczy.karolina@gmail.com>
2022-11-25 10:17:32 +0100

    clear session data when needed

--- cypress/support/api/requests/utils/index.js
+++ cypress/support/api/requests/utils/index.js
@@ -5,42 +5,32 @@ import { urlList } from "../../../../fixtures/urlList";
 Cypress.Commands.add(
   "sendRequestWithQuery",
   (query, authorization = "auth", variables = "") => {
-    if (authorization.length !== 30) {
-      cy.request({
-        body: {
-          variables,
-          query,
-        },
-        headers: {
-          Authorization: `JWT ${window.sessionStorage.getItem(authorization)}`,
-        },
-        method: "POST",
-        url: urlList.apiUri,
-        log: true,
-      }).then(response => {
-        const respInSting = JSON.stringify(response.body);
-        if (respInSting.includes(`"errors":[{`)) {
-          cy.log(query).log(JSON.stringify(response.body));
-        }
-      });
+    let headers;
+
+    if (authorization && authorization.length !== 30) {
+      headers = {
+        Authorization: `JWT ${window.sessionStorage.getItem(authorization)}`,
+      };
+    } else if (authorization) {
+      headers = { Authorization: `Bearer ${authorization}` };
     } else {
-      cy.request({
-        body: {
-          variables,
-          query,
-        },
-        headers: {
-          Authorization: `Bearer ${authorization}`,
-        },
-        method: "POST",
-        url: urlList.apiUri,
-        log: true,
-      }).then(response => {
-        const respInSting = JSON.stringify(response.body);
-        if (respInSting.includes(`"errors":[{`)) {
-          cy.log(query).log(JSON.stringify(response.body));
-        }
-      });
+      headers = {};
     }
+
+    cy.request({
+      body: {
+        variables,
+        query,
+      },
+      headers,
+      method: "POST",
+      url: urlList.apiUri,
+      log: true,
+    }).then(response => {
+      const respInSting = JSON.stringify(response.body);
+      if (respInSting.includes(`"errors":[{`)) {
+        cy.log(query).log(JSON.stringify(response.body));
+      }
+    });
   },
 );
