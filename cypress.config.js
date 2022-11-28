6f3974b334a662e87f93bffe1b54b39fb4e39002
Karolina Rakoczy <rakoczy.karolina@gmail.com>
2022-11-28 12:35:21 +0100

    remove video if test passed, decrease video compression

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,12 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-console */
 const { defineConfig } = require("cypress");
+const _ = require("lodash");
+const fs = require("fs");
 
 module.exports = defineConfig({
   projectId: "51ef7c",
+  videoCompression: 15,
   chromeWebSecurity: false,
   videoUploadOnPasses: false,
   defaultCommandTimeout: 20000,
@@ -20,6 +24,24 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       config = require("./cypress/support/cypress-grep/plugin")(config);
       config = require("./cypress/plugins/index.js")(on, config);
+      on("after:spec", (spec, results) => {
+        if (results && results.video) {
+          // Do we have failures for any retry attempts?
+          const failures = _.some(results.tests, test =>
+            _.some(test.attempts, { state: "failed" }),
+          );
+          if (!failures) {
+            // delete the video if the spec passed and no tests retried
+            return fs.unlink(results.video, function(err) {
+              if (err) {
+                console.warn(`Could not remove video - ${err}`);
+              } else {
+                console.log("File removed:", results.video);
+              }
+            });
+          }
+        }
+      });
       return config;
     },
     baseUrl: "http://localhost:9000/",
