46bc4f68e74b2c9b400188299cb8fc42c67d5380
Karolina Rakoczy <rakoczy.karolina@gmail.com>
2022-11-29 10:49:14 +0100

    always remove videos

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,11 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-console */
 const { defineConfig } = require("cypress");
-const _ = require("lodash");
 const fs = require("fs");
 
 module.exports = defineConfig({
   projectId: "51ef7c",
-  videoCompression: false,
   chromeWebSecurity: false,
-  videoUploadOnPasses: false,
   defaultCommandTimeout: 20000,
   requestTimeout: 20000,
   viewportWidth: 1400,
@@ -26,20 +23,13 @@ module.exports = defineConfig({
       config = require("./cypress/plugins/index.js")(on, config);
       on("after:spec", (spec, results) => {
         if (results && results.video) {
-          // Do we have failures for any retry attempts?
-          const failures = _.some(results.tests, test =>
-            _.some(test.attempts, { state: "failed" }),
-          );
-          if (!failures) {
-            // delete the video if the spec passed and no tests retried
-            return fs.unlink(results.video, function(err) {
-              if (err) {
-                console.warn(`Could not remove video - ${err}`);
-              } else {
-                console.log("File removed:", results.video);
-              }
-            });
-          }
+          return fs.unlink(results.video, function(err) {
+            if (err) {
+              console.warn(`Could not remove video - ${err}`);
+            } else {
+              console.log("File removed:", results.video);
+            }
+          });
         }
       });
       return config;
