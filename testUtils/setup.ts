49cb61301effd983179a634ec789abd0d3c24573
andrzejewsky <vox3r69@gmail.com>
2022-11-30 11:16:32 +0100

    Remove snapshots

--- testUtils/setup.ts
+++ testUtils/setup.ts
@@ -1,6 +1,10 @@
+import { configure } from "@testing-library/react";
+
 document.getElementById = () => document.createElement("div");
 
 window.__SALEOR_CONFIG__ = {
   API_URL: "http://localhost:8000/graphql/",
   APP_MOUNT_URI: "/",
 };
+
+configure({ testIdAttribute: "data-test-id" });
