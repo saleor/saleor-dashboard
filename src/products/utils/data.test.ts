c646b9ee808c81fbaf4c9cb9407de93c20926352
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2022-12-01 16:29:30 +0100

    Follow AAA test convention

--- src/products/utils/data.test.ts
+++ src/products/utils/data.test.ts
@@ -4,6 +4,7 @@ type GetSelectedMediaParams = Parameters<typeof getSelectedMedia>;
 
 describe("Product media utils", () => {
   it("should return selected media in proper order when media ids passed", () => {
+    // Arrange
     const media: GetSelectedMediaParams[0] = [
       {
         id: "1",
@@ -24,7 +25,10 @@ describe("Product media utils", () => {
     ];
     const selectedIds: GetSelectedMediaParams[1] = ["1", "3", "4"];
 
+    // Act
     const result = getSelectedMedia(media, selectedIds);
+
+    // Assert
     const expectedResult = [
       {
         id: "4",
@@ -44,6 +48,7 @@ describe("Product media utils", () => {
   });
 
   it("should return empty array of media when no media ids passed", () => {
+    // Arrange
     const media: GetSelectedMediaParams[0] = [
       {
         id: "1",
@@ -64,8 +69,10 @@ describe("Product media utils", () => {
     ];
     const selectedIds: GetSelectedMediaParams[1] = [];
 
+    // Act
     const result = getSelectedMedia(media, selectedIds);
 
+    // Assert
     expect(result).toEqual([]);
   });
 });
--- src/products/utils/handlers.test.ts
+++ src/products/utils/handlers.test.ts
@@ -6,6 +6,7 @@ type HandleAssignMediaParams = Parameters<typeof handleAssignMedia>;
 
 describe("Product handlers", () => {
   it("should not alter product variant media when the same selected media ids as previously passed", async () => {
+    // Arrange
     const media: HandleAssignMediaParams[0] = ["1", "2"];
     const variant: HandleAssignMediaParams[1] = {
       id: "1",
@@ -29,13 +30,16 @@ describe("Product handlers", () => {
     const assignMedia = jest.fn(() => Promise.resolve({}));
     const unassignMedia = jest.fn(() => Promise.resolve({}));
 
+    // Act
     await handleAssignMedia(media, variant, assignMedia, unassignMedia);
 
+    // Assert
     expect(assignMedia).not.toHaveBeenCalled();
     expect(unassignMedia).not.toHaveBeenCalled();
   });
 
   it("should assign media to product variant when more then all previous selected media ids passed", async () => {
+    // Arrange
     const media: HandleAssignMediaParams[0] = ["1", "2", "3"];
     const variant: HandleAssignMediaParams[1] = {
       id: "1",
@@ -52,8 +56,10 @@ describe("Product handlers", () => {
     const assignMedia = jest.fn(() => Promise.resolve({}));
     const unassignMedia = jest.fn(() => Promise.resolve({}));
 
+    // Act
     await handleAssignMedia(media, variant, assignMedia, unassignMedia);
 
+    // Assert
     expect(assignMedia).toHaveBeenCalledTimes(2);
     expect(assignMedia).toHaveBeenCalledWith({
       variantId: "1",
@@ -67,6 +73,7 @@ describe("Product handlers", () => {
   });
 
   it("should unassign media from product variant when not all previous selected media ids passed", async () => {
+    // Arrange
     const media: HandleAssignMediaParams[0] = ["3"];
     const variant: HandleAssignMediaParams[1] = {
       id: "1",
@@ -97,8 +104,10 @@ describe("Product handlers", () => {
     const assignMedia = jest.fn(() => Promise.resolve({}));
     const unassignMedia = jest.fn(() => Promise.resolve({}));
 
+    // Act
     await handleAssignMedia(media, variant, assignMedia, unassignMedia);
 
+    // Assert
     expect(assignMedia).not.toHaveBeenCalled();
     expect(unassignMedia).toHaveBeenCalledTimes(2);
     expect(unassignMedia).toHaveBeenCalledWith({
@@ -112,6 +121,7 @@ describe("Product handlers", () => {
   });
 
   it("should assign and unassign media from product variant when not all but more selected media ids from previously selected passed", async () => {
+    // Arrange
     const media: HandleAssignMediaParams[0] = ["1", "3"];
     const variant: HandleAssignMediaParams[1] = {
       id: "1",
@@ -135,8 +145,10 @@ describe("Product handlers", () => {
     const assignMedia = jest.fn(() => Promise.resolve({}));
     const unassignMedia = jest.fn(() => Promise.resolve({}));
 
+    // Act
     await handleAssignMedia(media, variant, assignMedia, unassignMedia);
 
+    // Assert
     expect(assignMedia).toHaveBeenCalledTimes(1);
     expect(assignMedia).toHaveBeenCalledWith({
       variantId: "1",
