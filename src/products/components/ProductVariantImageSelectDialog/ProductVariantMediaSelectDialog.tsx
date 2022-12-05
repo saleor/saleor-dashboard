14a36f7ba7294e04c31e2e61f6ae856b71552014
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2022-12-05 16:55:13 +0100

    Move dialog styles to separate file

--- src/products/components/ProductVariantImageSelectDialog/ProductVariantMediaSelectDialog.tsx
+++ src/products/components/ProductVariantImageSelectDialog/ProductVariantMediaSelectDialog.tsx
@@ -9,51 +9,11 @@ import ConfirmButton from "@saleor/components/ConfirmButton";
 import { ProductMediaFragment } from "@saleor/graphql";
 import useModalDialogOpen from "@saleor/hooks/useModalDialogOpen";
 import { buttonMessages } from "@saleor/intl";
-import { makeStyles } from "@saleor/macaw-ui";
 import clsx from "clsx";
 import React, { useState } from "react";
 import { FormattedMessage } from "react-intl";
 
-const useStyles = makeStyles(
-  theme => ({
-    image: {
-      height: "100%",
-      objectFit: "contain",
-      userSelect: "none",
-      width: "100%",
-    },
-    imageContainer: {
-      background: "transparent",
-      border: "1px solid #eaeaea",
-      borderRadius: theme.spacing(),
-      cursor: "pointer",
-      height: theme.spacing(21.5),
-      overflow: "hidden",
-      padding: theme.spacing(2),
-      position: "relative",
-      transitionDuration: theme.transitions.duration.standard + "ms",
-    },
-    content: {
-      overflowY: "scroll",
-    },
-    root: {
-      display: "grid",
-      gridColumnGap: theme.spacing(2),
-      gridRowGap: theme.spacing(2),
-      gridTemplateColumns: "repeat(3, 1fr)",
-      maxWidth: "100%",
-      width: theme.breakpoints.values.lg,
-      [theme.breakpoints.down("sm")]: {
-        gridTemplateColumns: "repeat(2, 1fr)",
-      },
-    },
-    selectedImageContainer: {
-      borderColor: theme.palette.primary.main,
-      borderWidth: "2px",
-    },
-  }),
-  { name: "ProductVariantImageSelectDialog" },
-);
+import { useStyles } from "./styles";
 
 interface ProductVariantImageSelectDialogProps {
   media?: ProductMediaFragment[];
