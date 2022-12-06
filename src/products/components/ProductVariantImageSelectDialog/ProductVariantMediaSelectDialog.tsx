0741d3ca2cb2a52a298f54e4e9125dd95b474b64
Dawid <tarasiukdawid@gmail.com>
2022-12-06 12:03:41 +0100

    Improve saving variant images (#2640)
    * Improve saving variant images
    
    * Update storybook types
    
    * Add getSelectedMedia function for products
    
    * Add test for handleAssignMedia in products
    
    * Follow AAA test convention
    
    * Add confirm button to media dialog selector
    
    * Move dialog styles to separate file

--- src/products/components/ProductVariantImageSelectDialog/ProductVariantMediaSelectDialog.tsx
+++ src/products/components/ProductVariantImageSelectDialog/ProductVariantMediaSelectDialog.tsx
@@ -5,64 +5,57 @@ import {
   DialogTitle,
 } from "@material-ui/core";
 import BackButton from "@saleor/components/BackButton";
+import ConfirmButton from "@saleor/components/ConfirmButton";
 import { ProductMediaFragment } from "@saleor/graphql";
-import { makeStyles } from "@saleor/macaw-ui";
+import useModalDialogOpen from "@saleor/hooks/useModalDialogOpen";
+import { buttonMessages } from "@saleor/intl";
 import clsx from "clsx";
-import React from "react";
+import React, { useState } from "react";
 import { FormattedMessage } from "react-intl";
 
-const useStyles = makeStyles(
-  theme => ({
-    image: {
-      height: "100%",
-      objectFit: "contain",
-      userSelect: "none",
-      width: "100%",
-    },
-    imageContainer: {
-      background: "transparent",
-      border: "1px solid #eaeaea",
-      borderRadius: theme.spacing(),
-      cursor: "pointer",
-      height: theme.spacing(21.5),
-      overflow: "hidden",
-      padding: theme.spacing(2),
-      position: "relative",
-      transitionDuration: theme.transitions.duration.standard + "ms",
-    },
-    content: {
-      overflowY: "scroll",
-    },
-    root: {
-      display: "grid",
-      gridColumnGap: theme.spacing(2),
-      gridRowGap: theme.spacing(2),
-      gridTemplateColumns: "repeat(3, 1fr)",
-      maxWidth: "100%",
-      width: theme.breakpoints.values.lg,
-      [theme.breakpoints.down("sm")]: {
-        gridTemplateColumns: "repeat(2, 1fr)",
-      },
-    },
-    selectedImageContainer: {
-      borderColor: theme.palette.primary.main,
-      borderWidth: "2px",
-    },
-  }),
-  { name: "ProductVariantImageSelectDialog" },
-);
+import { useStyles } from "./styles";
 
 interface ProductVariantImageSelectDialogProps {
   media?: ProductMediaFragment[];
   selectedMedia?: string[];
   open: boolean;
-  onClose();
-  onMediaSelect(id: string);
+  onClose: () => void;
+  onConfirm: (selectedIds: string[]) => void;
 }
 
 const ProductVariantMediaSelectDialog: React.FC<ProductVariantImageSelectDialogProps> = props => {
-  const { media, open, selectedMedia, onClose, onMediaSelect } = props;
+  const {
+    media,
+    open,
+    selectedMedia: initialMedia,
+    onClose,
+    onConfirm,
+  } = props;
   const classes = useStyles(props);
+
+  const [selectedMedia, setSelectedMedia] = useState(initialMedia);
+
+  useModalDialogOpen(open, {
+    onOpen: () => setSelectedMedia(initialMedia),
+    onClose: () => setSelectedMedia(initialMedia),
+  });
+
+  const handleMediaSelect = (id: string) => {
+    const isMediaAssigned = selectedMedia.includes(id);
+
+    if (isMediaAssigned) {
+      setSelectedMedia(selectedMedia =>
+        selectedMedia.filter(mediaId => mediaId !== id),
+      );
+    } else {
+      setSelectedMedia(selectedMedia => [...selectedMedia, id]);
+    }
+  };
+
+  const handleConfirm = () => {
+    onConfirm(selectedMedia);
+    onClose();
+  };
 
   return (
     <Dialog onClose={onClose} open={open}>
@@ -90,7 +83,7 @@ const ProductVariantMediaSelectDialog: React.FC<ProductVariantImageSelectDialogP
                         selectedMedia.indexOf(mediaObj.id) !== -1,
                     },
                   ])}
-                  onClick={onMediaSelect(mediaObj.id)}
+                  onClick={() => handleMediaSelect(mediaObj.id)}
                   key={mediaObj.id}
                 >
                   <img
@@ -105,6 +98,13 @@ const ProductVariantMediaSelectDialog: React.FC<ProductVariantImageSelectDialogP
       </DialogContent>
       <DialogActions>
         <BackButton onClick={onClose} />
+        <ConfirmButton
+          transitionState="default"
+          onClick={handleConfirm}
+          data-test-id="submit"
+        >
+          <FormattedMessage {...buttonMessages.confirm} />
+        </ConfirmButton>
       </DialogActions>
     </Dialog>
   );
