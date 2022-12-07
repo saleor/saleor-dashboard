01873454af54d40d286cac494b0a7f5068b145aa
Michał Droń <dron.official@yahoo.com>
2022-12-07 11:02:02 +0100

    Fix exit form issue in product translations (#2644)
    * Fix rich text exit form issue
    
    * Add unit test for rich text submit hook
    
    * Extract logic
    
    * Fix tests
    
    Co-authored-by: andrzejewsky <vox3r69@gmail.com>

---
+++ src/translations/components/TranslationFields/useRichTextSubmit.test.ts
@@ -0,0 +1,23 @@
+import useRichText from "@saleor/utils/richText/useRichText";
+import { renderHook } from "@testing-library/react-hooks";
+
+import { useRichTextSubmit } from "./useRichTextSubmit";
+
+jest.mock("@saleor/utils/richText/useRichText", () => jest.fn());
+
+describe("useRichTextSubmit", () => {
+  it("submits value from editor succesfully", async () => {
+    // Given
+    const textEditorValue = "text editor value";
+    const getValue = jest.fn(() => textEditorValue);
+    (useRichText as jest.Mock).mockImplementation(() => ({ getValue }));
+    const submitFn = jest.fn();
+    const { result } = renderHook(() => useRichTextSubmit("initial", submitFn));
+
+    // When
+    await result.current.handleSubmit();
+
+    // Then
+    expect(submitFn).toHaveBeenCalledWith(textEditorValue);
+  });
+});
