e90a2fd7572812c1303802cbccb381e9a81203a1
Michał Droń <dron.official@yahoo.com>
2022-12-16 15:54:41 +0100

    Remove submitting from exit dirty form modal (#2820)
    * Remove submit from exit dirty form util
    
    * Extract messages
    
    * Change data-test-id attributes
    
    * Add a unit test
    
    * Change given when then to AAA
    
    * Mock packages
    
    * Change button click to user events

--- cypress/elements/shared/unsavedChangesDialog.js
+++ cypress/elements/shared/unsavedChangesDialog.js
@@ -1,4 +1,4 @@
 export const UNSAVED_CHANGES_DIALOG = {
-  leaveWithoutSavingButton: '[data-test-id="leave-without-saving"]',
-  saveAndContinueButton: '[data-test-id="save-and-continue"]'
+  keepEditingButton: '[data-test-id="keep-editing"]',
+  ignoreChangesButton: '[data-test-id="ignore-changes"]',
 };
