85655579a4fa6d951581e41cb2c62024ae952b7a
Michał Droń <dron.official@yahoo.com>
2022-12-16 13:35:09 +0100

    Add a unit test

---
+++ src/components/Datagrid/MoneyCell.test.ts
@@ -0,0 +1,54 @@
+import { Locale } from "../Locale";
+import { parseCurrency } from "./MoneyCell";
+
+describe("parseCurrency", () => {
+  it("rounds down to 3 decimals in 3 digit currency - EN locale (dot)", () => {
+    // Arrange
+    const value = "5.6777";
+    const currency = "BHD";
+    const locale = Locale.EN;
+
+    // Act
+    const parsed = parseCurrency(value, locale, currency);
+
+    // Assert
+    expect(parsed).toBe(5.677);
+  });
+
+  it("rounds down to 0 decimals in 0 digit currency - PL locale (comma)", () => {
+    // Arrange
+    const value = "12,90679";
+    const currency = "JPY";
+    const locale = Locale.PL;
+
+    // Act
+    const parsed = parseCurrency(value, locale, currency);
+
+    // Assert
+    expect(parsed).toBe(12);
+  });
+  it("rounds down to 2 decimals in 2 digit currency - EN locale (dot)", () => {
+    // Arrange
+    const value = "244.98721";
+    const currency = "PLN";
+    const locale = Locale.EN;
+
+    // Act
+    const parsed = parseCurrency(value, locale, currency);
+
+    // Assert
+    expect(parsed).toBe(244.98);
+  });
+  it("rounds down to 3 decimals in 3 digit currency - PL locale (comma)", () => {
+    // Arrange
+    const value = "0,27386";
+    const currency = "TND";
+    const locale = Locale.PL;
+
+    // Act
+    const parsed = parseCurrency(value, locale, currency);
+
+    // Assert
+    expect(parsed).toBe(0.273);
+  });
+});
