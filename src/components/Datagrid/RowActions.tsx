8621d8b08c8dcbcbc512e08fba1783491b8e97e2
poulch <chyla1988@gmail.com>
2022-12-16 20:59:39 +0100

    Datagrid - show icon button when single menu item with icon (#2862)
    * RowMenu component
    
    * Show first menu item as icon
    
    * RowAction tests
    
    * Change Edit button
    
    * Remove outline from colum picker
    
    * Change ghost color
    
    * Update enterVariantEditPage test
    
    * Update test id in test for edit button
    
    * Update editVariant selector in test

---
+++ src/components/Datagrid/RowActions.tsx
@@ -0,0 +1,51 @@
+import { IconButton, MoreHorizontalIcon } from "@saleor/macaw-ui";
+import React from "react";
+
+import CardMenu, { CardMenuItem } from "../CardMenu";
+import useStyles from "./styles";
+
+interface RowActionsProps {
+  menuItems: CardMenuItem[];
+  disabled?: boolean;
+}
+
+export const RowActions = ({ menuItems, disabled }: RowActionsProps) => {
+  const classes = useStyles();
+  const hasSingleMenuItem = menuItems.length === 1;
+  const firstMenuItem = menuItems[0];
+
+  const handleIconClick = () => {
+    firstMenuItem.onSelect();
+  };
+
+  if (!menuItems.length) {
+    return null;
+  }
+
+  return (
+    <div className={classes.rowAction}>
+      {hasSingleMenuItem && firstMenuItem.Icon ? (
+        <IconButton
+          data-test-id="row-action-button"
+          disabled={disabled}
+          onClick={handleIconClick}
+          className={classes.ghostIcon}
+          variant="ghost"
+        >
+          {firstMenuItem.Icon}
+        </IconButton>
+      ) : (
+        <CardMenu
+          disabled={disabled}
+          Icon={MoreHorizontalIcon}
+          IconButtonProps={{
+            className: classes.ghostIcon,
+            hoverOutline: false,
+            state: "default",
+          }}
+          menuItems={menuItems}
+        />
+      )}
+    </div>
+  );
+};
