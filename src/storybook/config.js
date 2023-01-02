f0154c935f1ef78ac94330994da01c76cc5d6123
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2023-01-02 13:05:06 +0100

    feat: remove not needed storybook

--- src/storybook/config.js
+++ src/storybook/config.js
@@ -14,46 +14,8 @@ function loadStories() {
   req.keys().forEach(filename => req(filename));
 
   // Components
-  require("./stories/components/ActionDialog");
-  require("./stories/components/AddressEdit");
-  require("./stories/components/AddressFormatter");
-  require("./stories/components/AssignProductDialog");
-  require("./stories/components/AutocompleteSelectMenu");
-  require("./stories/components/CardMenu");
-  require("./stories/components/Checkbox");
-  require("./stories/components/ColumnPicker");
-  require("./stories/components/Date");
-  require("./stories/components/DateTime");
-  require("./stories/components/DeleteFilterTabDialog");
-  require("./stories/components/EditableTableCell");
-  require("./stories/components/ErrorMessageCard");
   require("./stories/components/ErrorPage");
-  require("./stories/components/ExternalLink");
-  require("./stories/components/Filter");
-  require("./stories/components/Money");
-  require("./stories/components/MoneyRange");
-  require("./stories/components/MultiSelectField");
   require("./stories/components/NotFoundPage");
-  require("./stories/components/PageHeader");
-  require("./stories/components/Percent");
-  require("./stories/components/PhoneField");
-  require("./stories/components/PriceField");
-  require("./stories/components/SaveFilterTabDialog");
-  require("./stories/components/SingleSelectField");
-  require("./stories/components/Skeleton");
-  require("./stories/components/TablePagination");
-  require("./stories/components/Timeline");
-  require("./stories/components/Weight");
-  require("./stories/components/WeightRange");
-  require("./stories/components/messages");
-
-  // Attributes
-  require("./stories/attributes/AttributeBulkDeleteDialog");
-  require("./stories/attributes/AttributeDeleteDialog");
-  require("./stories/attributes/AttributeListPage");
-  require("./stories/attributes/AttributePage");
-  require("./stories/attributes/AttributeValueDeleteDialog");
-  require("./stories/attributes/AttributeValueEditDialog");
 
   // Categories
   require("./stories/categories/CategoryCreatePage");
@@ -69,14 +31,12 @@ function loadStories() {
   require("./stories/configuration/ConfigurationPage");
 
   // Customers
-  require("./stories/customers/CustomerAddressDialog");
   require("./stories/customers/CustomerAddressListPage");
   require("./stories/customers/CustomerCreatePage");
   require("./stories/customers/CustomerDetailsPage");
   require("./stories/customers/CustomerListPage");
 
   // Discounts
-  require("./stories/discounts/DiscountCountrySelectDialog");
   require("./stories/discounts/SaleCreatePage");
   require("./stories/discounts/SaleDetailsPage");
   require("./stories/discounts/SaleListPage");
@@ -88,9 +48,7 @@ function loadStories() {
   require("./stories/home/HomePage");
 
   // Navigation
-  require("./stories/navigation/MenuCreateDialog");
   require("./stories/navigation/MenuDetailsPage");
-  require("./stories/navigation/MenuItemDialog");
   require("./stories/navigation/MenuListPage");
 
   // Pages
@@ -107,27 +65,12 @@ function loadStories() {
   require("./stories/products/ProductListPage");
   require("./stories/products/ProductUpdatePage");
   require("./stories/products/ProductVariantCreatePage");
-  require("./stories/products/ProductVariantImageSelectDialog");
   require("./stories/products/ProductVariantPage");
 
   // Orders
-  require("./stories/orders/OrderBulkCancelDialog");
-  require("./stories/orders/OrderCancelDialog");
-  require("./stories/orders/OrderCustomer");
   require("./stories/orders/OrderDetailsPage");
-  require("./stories/orders/OrderDraftCancelDialog");
   require("./stories/orders/OrderDraftListPage");
   require("./stories/orders/OrderDraftPage/OrderDraftPage");
-  require("./stories/orders/OrderFulfillmentCancelDialog");
-  require("./stories/orders/OrderFulfillmentTrackingDialog");
-  require("./stories/orders/OrderHistory");
-  require("./stories/orders/OrderListPage");
-  require("./stories/orders/OrderMarkAsPaidDialog");
-  require("./stories/orders/OrderPaymentDialog");
-  require("./stories/orders/OrderPaymentVoidDialog");
-  require("./stories/orders/OrderProductAddDialog");
-  require("./stories/orders/OrderShippingMethodEditDialog");
-  require("./stories/orders/OrderInvoiceList");
 
   // Product types
   require("./stories/productTypes/ProductTypeCreatePage");
@@ -135,7 +78,6 @@ function loadStories() {
   require("./stories/productTypes/ProductTypeListPage");
 
   // Shipping
-  require("./stories/shipping/ShippingZoneCountriesAssignDialog");
   require("./stories/shipping/ShippingZoneCreatePage");
   require("./stories/shipping/ShippingZoneDetailsPage");
   require("./stories/shipping/ShippingZonesListPage");
