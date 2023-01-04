57b0b13599a24004190f18b3842ed5ef6230f385
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2023-01-04 12:47:46 +0100

    fix: storybook mocks

--- testUtils/ApolloMockedProvider.tsx
+++ testUtils/ApolloMockedProvider.tsx
@@ -1,9 +1,19 @@
 import { MockedProvider, MockedResponse } from "@apollo/client/testing";
 import React from "react";
 
-import { addressMocks, appsMocks } from "./mocks";
+import {
+  addressMocks,
+  appsMocks,
+  pageTypesMocks,
+  warehousesMocks,
+} from "./mocks";
 
-const mocks: MockedResponse[] = [...appsMocks, ...addressMocks];
+const mocks: MockedResponse[] = [
+  ...appsMocks,
+  ...addressMocks,
+  ...warehousesMocks,
+  ...pageTypesMocks,
+];
 
 export const ApolloMockedProvider = ({ children }) => (
   <MockedProvider mocks={mocks} addTypename={false}>
