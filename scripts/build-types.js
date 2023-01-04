4d0867c5ff0535b9042c893b4614e5bf06393a27
Paweł Chyła <pawel.chyla@saleor.io>
2023-01-04 09:36:47 +0100

    Refactor build types script

--- scripts/build-types.js
+++ scripts/build-types.js
@@ -2,109 +2,104 @@
 const { generate } = require("@graphql-codegen/cli");
 
 // Feature flags names that will be used as suffix for generated files
-const FEATURE_FLAGS = [];
+const FEATURE_FLAGS = ["news"];
 
-(async () => {
-  const schemaSuffixes = ["default", ...FEATURE_FLAGS];
+const schemaSuffixes = ["default", ...FEATURE_FLAGS];
 
-  for await (const rawSuffix of schemaSuffixes) {
-    const suffix = prepareSuffix(rawSuffix);
+for (const rawSuffix of schemaSuffixes) {
+  const suffix = prepareSuffix(rawSuffix);
 
-    await generate(
-      {
-        schema: process.cwd() + "/introspection" + suffix + ".json",
-        overwrite: true,
-        generates: {
-          [process.cwd() +
-          "/src/graphql/fragmentTypes" +
-          suffix +
-          ".generated.ts"]: {
-            plugins: [
-              {
-                add: {
-                  content: "/* eslint-disable */",
-                },
+  generate(
+    {
+      schema: "./introspection" + suffix + ".json",
+      overwrite: true,
+      generates: {
+        [getOutputPath("fragmentTypes", suffix)]: {
+          plugins: [
+            {
+              add: {
+                content: "/* eslint-disable */",
               },
-              "fragment-matcher",
-            ],
-            config: {
-              minify: false,
-              apolloClientVersion: 3,
             },
-          },
-          [process.cwd() +
-          "/src/graphql/typePolicies" +
-          suffix +
-          ".generated.ts"]: {
-            plugins: [
-              {
-                add: {
-                  content: "/* eslint-disable */",
-                },
-              },
-              "typescript-apollo-client-helpers",
-            ],
-          },
-          [process.cwd() + "/src/graphql/types" + suffix + ".generated.ts"]: {
-            documents: [
-              process.cwd() + "/src/**/queries" + suffix + ".ts",
-              process.cwd() + "/src/**/mutations" + suffix + ".ts",
-              process.cwd() + "/src/**/fragments/*" + suffix + ".ts",
-              process.cwd() + "/src/searches/*" + suffix + ".ts",
-            ],
-            config: {
-              nonOptionalTypename: true,
-              avoidOptionals: {
-                field: true,
-                inputValue: false,
-                object: false,
-                defaultValue: false,
-              },
-              namingConvention: {
-                enumValues: "change-case-all#upperCase",
-              },
-              onlyOperationTypes: true,
-            },
-            plugins: [
-              {
-                add: {
-                  content: "/* eslint-disable */",
-                },
-              },
-              "typescript",
-              "typescript-operations",
-            ],
-          },
-          [process.cwd() + "/src/graphql/hooks" + suffix + ".generated.ts"]: {
-            documents: [
-              process.cwd() + "/src/**/queries" + suffix + ".ts",
-              process.cwd() + "/src/**/mutations" + suffix + ".ts",
-              process.cwd() + "/src/**/fragments/*" + suffix + ".ts",
-              process.cwd() + "/src/searches/*" + suffix + ".ts",
-            ],
-            preset: "import-types",
-            presetConfig: {
-              typesPath: "./types" + suffix + ".generated",
-            },
-            config: {
-              withHooks: true,
-              apolloReactHooksImportFrom: "@saleor/hooks/graphql",
-            },
-            plugins: [
-              {
-                add: {
-                  content: "/* eslint-disable */",
-                },
-              },
-              "typescript-react-apollo",
-            ],
+            "fragment-matcher",
+          ],
+          config: {
+            minify: false,
+            apolloClientVersion: 3,
           },
         },
+        [getOutputPath("typePolicies", suffix)]: {
+          plugins: [
+            {
+              add: {
+                content: "/* eslint-disable */",
+              },
+            },
+            "typescript-apollo-client-helpers",
+          ],
+        },
+        [getOutputPath("types", suffix)]: {
+          documents: getDocumentsPaths(suffix),
+          config: {
+            nonOptionalTypename: true,
+            avoidOptionals: {
+              field: true,
+              inputValue: false,
+              object: false,
+              defaultValue: false,
+            },
+            namingConvention: {
+              enumValues: "change-case-all#upperCase",
+            },
+            onlyOperationTypes: true,
+          },
+          plugins: [
+            {
+              add: {
+                content: "/* eslint-disable */",
+              },
+            },
+            "typescript",
+            "typescript-operations",
+          ],
+        },
+        [getOutputPath("hooks", suffix)]: {
+          documents: getDocumentsPaths(suffix),
+          preset: "import-types",
+          presetConfig: {
+            typesPath: "./types" + suffix + ".generated",
+          },
+          config: {
+            withHooks: true,
+            apolloReactHooksImportFrom: "@saleor/hooks/graphql",
+          },
+          plugins: [
+            {
+              add: {
+                content: "/* eslint-disable */",
+              },
+            },
+            "typescript-react-apollo",
+          ],
+        },
       },
-      true,
-    );
-  }
-})();
+    },
+    true,
+  );
+}
+
+function getOutputPath(path, suffix) {
+  return `./src/graphql/${path}${suffix}.generated.ts`;
+}
+
+function getDocumentsPaths(suffix) {
+  return [
+    `./src/**/queries${suffix}.ts`,
+    `./src/**/mutations${suffix}.ts`,
+    `./src/**/fragments/*${suffix}.ts`,
+    `./src/searches/*${suffix}.ts`,
+  ];
+}
 
 function prepareSuffix(suffix) {
   if (suffix === "default") {
