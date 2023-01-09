87b1dedac8377255b8c767bd2fc1dbbbe1448f63
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2023-01-09 11:03:35 +0100

    Remove back button from app frame page

--- src/apps/components/AppPage/AppPage.tsx
+++ src/apps/components/AppPage/AppPage.tsx
@@ -1,10 +1,6 @@
-import { appsListPath } from "@saleor/apps/urls";
-import { Backlink } from "@saleor/components/Backlink";
 import Container from "@saleor/components/Container";
 import { AppQuery } from "@saleor/graphql";
-import { sectionNames } from "@saleor/intl";
 import React from "react";
-import { useIntl } from "react-intl";
 
 import { AppFrame } from "../AppFrame";
 import { useStyles } from "./styles";
@@ -22,14 +18,10 @@ export const AppPage: React.FC<AppPageProps> = ({
   onError,
   refetch,
 }) => {
-  const intl = useIntl();
   const classes = useStyles();
 
   return (
     <Container className={classes.container}>
-      <Backlink href={appsListPath}>
-        {intl.formatMessage(sectionNames.apps)}
-      </Backlink>
       <div className={classes.iframeContainer}>
         {url && (
           <AppFrame
