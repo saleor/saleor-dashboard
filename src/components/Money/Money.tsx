6613722df362d2be6d79eb3689c30b0d139a0f78
Michał Droń <dron.official@yahoo.com>
2023-01-10 15:36:38 +0100

    Pass extra props to money component

--- src/components/Money/Money.tsx
+++ src/components/Money/Money.tsx
@@ -24,7 +24,8 @@ export interface MoneyProps {
   money: IMoney | null;
 }
 
-export const Money: React.FC<MoneyProps> = ({ money }) => {
+export const Money: React.FC<MoneyProps> = props => {
+  const { money } = props;
   const { locale } = useLocale();
   const classes = useStyles();
 
@@ -43,7 +44,7 @@ export const Money: React.FC<MoneyProps> = ({ money }) => {
   });
 
   return (
-    <span className={classes.root}>
+    <span className={classes.root} {...props}>
       <span className={classes.currency}>{money.currency}</span>
       {amount}
     </span>
