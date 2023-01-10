5bc8e39eb7708f3f2a6f06d1df5598303708adb2
Dawid <tarasiukdawid@gmail.com>
2023-01-10 15:24:24 +0100

    Hide toolbar in app page (#2938)
    * Hide toolbar in app page
    
    * Remove back button from app frame page

---
+++ src/components/AppLayout/styles.ts
@@ -0,0 +1,110 @@
+import { makeStyles } from "@saleor/macaw-ui";
+
+import { appLoaderHeight } from "./consts";
+
+export const useStyles = makeStyles(
+  theme => ({
+    appAction: {
+      [theme.breakpoints.down("sm")]: {
+        left: 0,
+        width: "100%",
+      },
+      bottom: 0,
+      gridColumn: 2,
+      position: "sticky",
+      zIndex: 10,
+    },
+    appLoader: {
+      height: appLoaderHeight,
+      marginBottom: theme.spacing(4),
+      zIndex: 1201,
+    },
+    appLoaderPlaceholder: {
+      height: appLoaderHeight,
+      marginBottom: theme.spacing(4),
+    },
+
+    content: {
+      flex: 1,
+      [theme.breakpoints.up("md")]: {
+        width: 0, // workaround for flex children width expansion affected by their contents
+      },
+    },
+    darkThemeSwitch: {
+      [theme.breakpoints.down("sm")]: {
+        marginRight: theme.spacing(1),
+      },
+      marginRight: theme.spacing(2),
+    },
+    header: {
+      display: "grid",
+      gridTemplateAreas: `"headerAnchor headerToolbar"`,
+      [theme.breakpoints.down("sm")]: {
+        gridTemplateAreas: `"headerToolbar" 
+        "headerAnchor"`,
+      },
+      marginBottom: theme.spacing(6),
+    },
+    headerAnchor: {
+      gridArea: "headerAnchor",
+    },
+    headerToolbar: {
+      display: "flex",
+      gridArea: "headerToolbar",
+      height: 40,
+      [theme.breakpoints.down("sm")]: {
+        height: "auto",
+      },
+    },
+    root: {
+      isolation: "isolate",
+      [theme.breakpoints.up("md")]: {
+        display: "flex",
+      },
+      width: `100%`,
+    },
+    spacer: {
+      flex: 1,
+    },
+    userBar: {
+      alignItems: "center",
+      display: "flex",
+    },
+
+    view: {
+      marginLeft: 0,
+    },
+    viewMargins: {
+      paddingBottom: theme.spacing(),
+      [theme.breakpoints.up("sm")]: {
+        paddingBottom: theme.spacing(3),
+      },
+    },
+    viewContainer: {
+      minHeight: `calc(100vh - ${appLoaderHeight + 72}px - ${theme.spacing(
+        4,
+      )})`,
+    },
+  }),
+  { name: "AppLayout" },
+);
+
+export const useFullSizeStyles = makeStyles(
+  () => ({
+    content: {
+      height: "100vh",
+      display: "flex",
+      flexDirection: "column",
+    },
+    viewContainer: {
+      flex: 1,
+      display: "flex",
+      flexDirection: "column",
+    },
+    view: {
+      flex: 1,
+    },
+    viewContainerWrapper: {},
+  }),
+  { name: "AppLayoutFullSize" },
+);
