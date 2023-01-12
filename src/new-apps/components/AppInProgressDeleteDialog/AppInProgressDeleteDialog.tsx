5578d9b50ff1a446bbee4489a8720b22b272f78a
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2023-01-12 15:51:16 +0100

    Test AppInProgressDeleteDialog component

--- src/new-apps/components/AppInProgressDeleteDialog/AppInProgressDeleteDialog.tsx
+++ src/new-apps/components/AppInProgressDeleteDialog/AppInProgressDeleteDialog.tsx
@@ -3,12 +3,14 @@ import ActionDialog from "@saleor/components/ActionDialog";
 import { ConfirmButtonTransitionState } from "@saleor/macaw-ui";
 import { getStringOrPlaceholder } from "@saleor/misc";
 import React from "react";
-import { FormattedMessage, useIntl } from "react-intl";
+import { useIntl } from "react-intl";
+
+import msgs from "./messages";
 
 export interface AppInProgressDeleteDialogProps {
   confirmButtonState: ConfirmButtonTransitionState;
   open: boolean;
-  name: string;
+  name?: string | null;
   onClose: () => void;
   onConfirm: () => void;
 }
@@ -22,36 +24,28 @@ const AppInProgressDeleteDialog: React.FC<AppInProgressDeleteDialogProps> = ({
 }) => {
   const intl = useIntl();
 
+  const missingName = name === null || name === "";
+
+  const getMainText = () => {
+    if (missingName) {
+      return intl.formatMessage(msgs.deleteApp);
+    }
+    return intl.formatMessage(msgs.deleteNamedApp, {
+      name: <strong>{getStringOrPlaceholder(name)}</strong>,
+    });
+  };
+
   return (
     <ActionDialog
       confirmButtonState={confirmButtonState}
       open={open}
       onClose={onClose}
       onConfirm={onConfirm}
-      title={intl.formatMessage({
-        id: "zQX6xO",
-        defaultMessage: "Delete App",
-        description: "dialog header",
-      })}
+      title={intl.formatMessage(msgs.header)}
       variant="delete"
     >
-      <DialogContentText>
-        {["", null].includes(name) ? (
-          <FormattedMessage
-            id="6hLZNA"
-            defaultMessage="Are you sure you want to delete this app?"
-            description="delete app"
-          />
-        ) : (
-          <FormattedMessage
-            id="EWD/wU"
-            defaultMessage="Deleting {name}, you will remove installation of the app. If you are paying for app subscription, remember to unsubscribe from the app in Saleor Marketplace. Are you sure you want to delete the app?"
-            description="delete app"
-            values={{
-              name: <strong>{getStringOrPlaceholder(name)}</strong>,
-            }}
-          />
-        )}
+      <DialogContentText data-test-id="dialog-content">
+        {getMainText()}
       </DialogContentText>
     </ActionDialog>
   );
