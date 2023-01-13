e2a197ebf31e69aa7e7c197b6bfac397690b6b5a
karolm-saleor <karol.macheta@saleor.io>
2023-01-13 12:44:17 +0100

    fixed mocks and added loginViaMock

--- cypress/functional/apps.js
+++ cypress/functional/apps.js
@@ -1,21 +1,24 @@
 /// <reference types="cypress"/>
 /// <reference types="../support"/>
-import { urlList } from "../fixtures/urlList";
 
 describe("should display mocked list", () => {
   before(() => {
-    cy.clearSessionData().loginUserViaRequest();
-    // cy.mockMutation("POST", "refreshTokenWithUser", "app/refreshTokenWithUser.json");
-    // cy.mockMutation("POST", "UserDetails", "app/UserDetails.json");
-    // cy.mockMutation("POST", "BaseChannels", "app/BaseChannels.json");
-    // cy.mockMutation("POST", "ShopInfo", "app/ShopInfo.json");
-    // cy.mockMutation("POST", "AppsInstallations", "app/AppsInstallations.json");
-    // cy.mockMutation("POST", "ExtensionList", "app/ExtensionList.json");
+    cy.loginUserViaMockedToken();
+
+    cy.mockMutation(
+      "POST",
+      "refreshTokenWithUser",
+      "app/refreshTokenWithUser.json",
+    );
+    cy.mockMutation("POST", "UserDetails", "app/UserDetails.json");
+    cy.mockMutation("POST", "BaseChannels", "app/BaseChannels.json");
+    cy.mockMutation("POST", "ShopInfo", "app/ShopInfo.json");
+    cy.mockMutation("POST", "AppsInstallations", "app/AppsInstallations.json");
+    cy.mockMutation("POST", "ExtensionList", "app/ExtensionList.json");
     cy.mockMutation("POST", "AppsList", "app/AppsList.json");
-    cy.visit("/");
   });
 
   it("should show list of mocked apps", () => {
-    cy.visit(urlList.apps3rdParty);
+    cy.visit("/apps");
   });
 });
