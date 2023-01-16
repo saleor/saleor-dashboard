be1ae5c2766d9ef3685b968dc6d7be1fd7fb159d
Jonatan Witoszek <jonatan.witoszek@saleor.io>
2023-01-16 10:48:26 +0100

    Add tests for OrderAddTransaction

---
+++ src/orders/components/OrderAddTransaction/OrderAddTransaction.test.tsx
@@ -0,0 +1,36 @@
+import { order } from "@saleor/orders/fixtures";
+import Wrapper from "@test/wrapper";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import React from "react";
+
+import OrderAddTransaction from "./OrderAddTransaction";
+
+describe("OrderAddTransaction", () => {
+  it("renders skeleton when order is loading", () => {
+    render(
+      <Wrapper>
+        <OrderAddTransaction
+          order={undefined}
+          onAddTransaction={() => undefined}
+        />
+      </Wrapper>,
+    );
+    expect(screen.queryByRole("button")).not.toBeInTheDocument();
+  });
+
+  it("calls onAddTransaction when clicked", async () => {
+    const callback = jest.fn();
+
+    render(
+      <Wrapper>
+        <OrderAddTransaction order={order(null)} onAddTransaction={callback} />
+      </Wrapper>,
+    );
+    const button = await screen.findByRole("button");
+
+    expect(button).toBeInTheDocument();
+    await userEvent.click(button);
+    expect(callback).toHaveBeenCalled();
+  });
+});
