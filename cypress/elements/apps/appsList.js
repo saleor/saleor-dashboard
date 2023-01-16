3b61a70c4929d74cda50b16a502e68fd5f07a891
karolm-saleor <karol.macheta@saleor.io>
2023-01-16 10:08:42 +0100

    added 2 basic tests for check of dialog

--- cypress/elements/apps/appsList.js
+++ cypress/elements/apps/appsList.js
@@ -1,3 +1,6 @@
 export const APPS_LIST = {
   createLocalAppButton: '[data-test-id="create-app"]',
+  activeSwitch: '[data-test-id="app-active-switch"]',
+  dialogText: '[data-test-id="dialog-content"]',
+  dialogBackBtn: '[data-test-id="back"]',
 };
--- cypress/functional/apps.js
+++ cypress/functional/apps.js
@@ -1,10 +1,12 @@
 /// <reference types="cypress"/>
 /// <reference types="../support"/>
 
-describe("should display mocked list", () => {
-  before(() => {
-    cy.loginUserViaMockedToken();
+import { APPS_LIST } from "../elements/apps/appsList";
+import { BUTTON_SELECTORS } from "../elements/shared/button-selectors";
 
+describe("should display mocked list", () => {
+  beforeEach(() => {
+    cy.loginUserViaMockedToken();
     cy.mockMutation(
       "POST",
       "refreshTokenWithUser",
@@ -16,9 +18,22 @@ describe("should display mocked list", () => {
     cy.mockMutation("POST", "AppsInstallations", "app/AppsInstallations.json");
     cy.mockMutation("POST", "ExtensionList", "app/ExtensionList.json");
     cy.mockMutation("POST", "AppsList", "app/AppsList.json");
+    cy.visit("/new-apps");
   });
 
-  it("should show list of mocked apps", () => {
-    cy.visit("/apps");
+  it("should show deactivate dialog for active app", () => {
+    cy.get(APPS_LIST.activeSwitch)
+      .first()
+      .click();
+    cy.get(BUTTON_SELECTORS.submit).contains("Deactivate");
+    cy.get(APPS_LIST.dialogText).contains("Are you sure you want to disable");
+  });
+
+  it("should show activate dialog for deactived app", () => {
+    cy.get(APPS_LIST.activeSwitch)
+      .last()
+      .click();
+    cy.get(BUTTON_SELECTORS.submit).contains("Activate");
+    cy.get(APPS_LIST.dialogText).contains("Are you sure you want to activate");
   });
 });
