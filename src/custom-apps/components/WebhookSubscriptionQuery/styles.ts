8f1828860d340f37c3bff03905c860c97926fc71
Jakub Neander <oh@zaiste.net>
2023-01-24 14:59:14 +0100

    Add GraphiQL for defining subscription queries for webhooks (#2885)
    Co-authored-by: Bartłomiej Wiaduch <tukan2can@gmail.com>
    Co-authored-by: timur <timuric@gmail.com>
    Co-authored-by: Michał Droń <dron.official@yahoo.com>

---
+++ src/custom-apps/components/WebhookSubscriptionQuery/styles.ts
@@ -0,0 +1,18 @@
+import { makeStyles } from "@saleor/macaw-ui";
+
+export const useStyles = makeStyles(
+  theme => ({
+    disabled: {
+      pointerEvents: "none",
+      opacity: 0.6,
+    },
+    cardContent: {
+      height: 500,
+      padding: 0,
+    },
+    card: {
+      marginBottom: theme.spacing(2),
+    },
+  }),
+  { name: "WebhookSubscriptionQuery" },
+);
--- src/custom-apps/handlers.ts
+++ src/custom-apps/handlers.ts
@@ -3,35 +3,127 @@ import {
   WebhookEventTypeSyncEnum,
 } from "@dashboard/graphql";
 import { ChangeEvent } from "@dashboard/hooks/useForm";
+import { capitalize } from "@dashboard/misc";
 import { toggle } from "@dashboard/utils/lists";
+import {
+  InlineFragmentNode,
+  ObjectFieldNode,
+  parse,
+  print,
+  visit,
+} from "graphql";
 
 import { filterSelectedAsyncEvents } from "./utils";
 
-export const createSyncEventsSelectHandler = (
-  change: (event: ChangeEvent, cb?: () => void) => void,
-  syncEvents: WebhookEventTypeSyncEnum[],
-) => (event: ChangeEvent) => {
-  const events = toggle(event.target.value, syncEvents, (a, b) => a === b);
+export const createSyncEventsSelectHandler =
+  (
+    change: (event: ChangeEvent, cb?: () => void) => void,
+    syncEvents: WebhookEventTypeSyncEnum[],
+    setQuery: React.Dispatch<React.SetStateAction<string>>,
+  ) =>
+  (event: ChangeEvent) => {
+    const events = toggle(event.target.value, syncEvents, (a, b) => a === b);
 
-  change({
-    target: {
-      name: "syncEvents",
-      value: events,
-    },
-  });
-};
+    // Clear query
+    setQuery("");
 
-export const createAsyncEventsSelectHandler = (
-  change: (event: ChangeEvent, cb?: () => void) => void,
-  asyncEvents: WebhookEventTypeAsyncEnum[],
-) => (event: ChangeEvent) => {
-  const events = toggle(event.target.value, asyncEvents, (a, b) => a === b);
-  const filteredEvents = filterSelectedAsyncEvents(events);
+    // Clear asyncEvents
+    change({
+      target: {
+        name: "asyncEvents",
+        value: [],
+      },
+    });
 
-  change({
-    target: {
-      name: "asyncEvents",
-      value: filteredEvents,
-    },
-  });
+    change({
+      target: {
+        name: "syncEvents",
+        value: events,
+      },
+    });
+  };
+
+export const createAsyncEventsSelectHandler =
+  (
+    change: (event: ChangeEvent, cb?: () => void) => void,
+    asyncEvents: WebhookEventTypeAsyncEnum[],
+    query: string,
+    setQuery: React.Dispatch<React.SetStateAction<string>>,
+  ) =>
+  (event: ChangeEvent) => {
+    const events = toggle(event.target.value, asyncEvents, (a, b) => a === b);
+    const filteredEvents = filterSelectedAsyncEvents(events);
+
+    // Clear syncEvents
+    change({
+      target: {
+        name: "syncEvents",
+        value: [],
+      },
+    });
+
+    change({
+      target: {
+        name: "asyncEvents",
+        value: filteredEvents,
+      },
+    });
+
+    handleQuery(filteredEvents, query, setQuery);
+  };
+
+const handleQuery = (
+  events: WebhookEventTypeAsyncEnum[],
+  query: string,
+  setQuery: React.Dispatch<React.SetStateAction<string>>,
+) => {
+  if (events.length > 0 && query.length === 0) {
+    const event = events[0]
+      .toLowerCase()
+      .split("_")
+      .map(chunk => capitalize(chunk))
+      .join("");
+    setQuery(
+      print(parse(`subscription { event { ... on ${event} { __typename } } }`)),
+    );
+  }
+
+  if (query.length > 0) {
+    const ast = parse(query);
+
+    visit(ast, {
+      SelectionSet(node, _key, parent) {
+        if ((parent as ObjectFieldNode).name?.value === "event") {
+          const queryEvents = node.selections.map(
+            selection =>
+              (selection as InlineFragmentNode).typeCondition.name.value,
+          );
+          const newEvents = events
+            .map(event =>
+              event
+                .toLowerCase()
+                .split("_")
+                .map(chunk => capitalize(chunk))
+                .join(""),
+            )
+            .filter(event => !queryEvents.includes(event));
+
+          if (newEvents.length > 0) {
+            // TODO modify AST
+
+            const inserted = query.replace(/\n/g, " ").replace(
+              "   } } ",
+              newEvents
+                .map(event => ` ... on ${event} { __typename }`)
+                .join("")
+                .concat("   } } "),
+            );
+            setQuery(print(parse(inserted)));
+          }
+        }
+
+        return undefined;
+      },
+    });
+  }
 };
