805393f5feb0344449bca8f73df3a9e7a171859f
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2023-01-24 16:08:48 +0100

    feat: changes after CR

--- src/components/Sidebar/NewSidebar.tsx
+++ src/components/Sidebar/NewSidebar.tsx
@@ -1,5 +1,5 @@
 import { useUser } from "@dashboard/auth";
-import { Box, List, Text } from "@saleor/macaw-ui/next";
+import { Box, List, sprinkles, Text } from "@saleor/macaw-ui/next";
 import React from "react";
 import { useIntl } from "react-intl";
 import { Link } from "react-router-dom";
@@ -22,12 +22,19 @@ export const NewSidebar = () => {
     >
       <List as="ol">
         {menuStructure.map(menuItem => (
-          <Link to={menuItem.url} key={menuItem.id}>
-            <List.Item paddingX={4} paddingY={4} gap={5} borderRadius={3}>
-              {menuItem.icon}
-              <Text>{menuItem.label}</Text>
-            </List.Item>
-          </Link>
+          <List.Item borderRadius={3} key={menuItem.id}>
+            <Link
+              to={menuItem.url}
+              className={sprinkles({
+                padding: 4,
+              })}
+            >
+              <Box display="flex" alignItems="center" gap={5}>
+                {menuItem.icon}
+                <Text>{menuItem.label}</Text>
+              </Box>
+            </Link>
+          </List.Item>
         ))}
       </List>
     </Box>
