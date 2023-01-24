084ab17e66de2666ad7c22fae10eecd531fb9a14
Jonatan Witoszek <jonatan.witoszek@saleor.io>
2023-01-24 13:49:12 +0100

    Fix Polly.js configuration

--- testUtils/api.ts
+++ testUtils/api.ts
@@ -1,30 +1,44 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { BatchHttpLink } from "@apollo/client/link/batch-http";
 import { getApiUrl } from "@dashboard/config";
-import NodeHttpAdapter from "@pollyjs/adapter-node-http";
-import FSPersister from "@pollyjs/persister-fs";
 import { createFetch } from "@saleor/sdk";
+import { isCI } from "ci-info";
+import { get } from "env-var";
 import path from "path";
 import { setupPolly } from "setup-polly-jest";
 
+const POLLY_MODES = ["replay", "record", "passthrough", "stopped"] as const;
+
+const POLLY_MODE = get("POLLY_MODE")
+  .default(POLLY_MODES[0])
+  .asEnum(POLLY_MODES);
+
+const POLLY_RECORD_IF_MISSING = get("POLLY_RECORD_IF_MISSING")
+  .default(isCI ? "false" : "true")
+  .asBoolStrict();
+
 function setupApi() {
   setupPolly({
-    adapters: [NodeHttpAdapter],
+    adapters: [require("@pollyjs/adapter-node-http")],
     matchRequestsBy: {
       headers: false,
       url: {
         hash: false,
-        hostname: false,
+        hostname: true,
         password: false,
-        pathname: false,
+        pathname: true,
         port: false,
         protocol: false,
         query: false,
         username: false,
       },
+      body: false,
     },
-    persister: FSPersister,
+    mode: POLLY_MODE,
+    recordIfMissing: POLLY_RECORD_IF_MISSING,
+    persister: require("@pollyjs/persister-fs"),
     persisterOptions: {
+      keepUnusedRequests: false,
       fs: {
         recordingsDir: path.resolve(__dirname, "../recordings"),
       },
