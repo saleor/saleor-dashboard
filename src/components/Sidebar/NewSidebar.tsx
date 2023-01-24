1e6b53e2496fab5dcb6f9f085d6941880371b4f4
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2023-01-24 16:48:51 +0100

    Basic sidebar version (#3039)

--- src/components/Sidebar/NewSidebar.tsx
+++ src/components/Sidebar/NewSidebar.tsx
@@ -1,3 +1,42 @@
+import { useUser } from "@dashboard/auth";
+import { Box, List, sprinkles, Text } from "@saleor/macaw-ui/next";
 import React from "react";
+import { useIntl } from "react-intl";
+import { Link } from "react-router-dom";
 
-export const NewSidebar = () => <div>Work in progress</div>;
+import useMenuStructure from "./useMenuStructure";
+
+export const NewSidebar = () => {
+  const intl = useIntl();
+  const { user } = useUser();
+  const [menuStructure] = useMenuStructure(intl, user);
+
+  return (
+    <Box
+      backgroundColor="subdued"
+      as="aside"
+      padding={5}
+      borderColor="neutralPlain"
+      borderRightWidth={1}
+      borderRightStyle="solid"
+    >
+      <List as="ol">
+        {menuStructure.map(menuItem => (
+          <List.Item borderRadius={3} key={menuItem.id}>
+            <Link
+              to={menuItem.url}
+              className={sprinkles({
+                padding: 4,
+              })}
+            >
+              <Box display="flex" alignItems="center" gap={5}>
+                {menuItem.icon}
+                <Text>{menuItem.label}</Text>
+              </Box>
+            </Link>
+          </List.Item>
+        ))}
+      </List>
+    </Box>
+  );
+};
