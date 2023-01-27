98a56551d5b5bc270e2bfdc69258bc048efd6ac7
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2023-01-27 10:00:55 +0100

    feat: use avatars from macaw

--- src/components/Sidebar/MountingPoint.tsx
+++ src/components/Sidebar/MountingPoint.tsx
@@ -1,19 +1,10 @@
 import sideBarDefaultLogo from "@assets/images/sidebar-default-logo.png";
-import { Box, Text } from "@saleor/macaw-ui/next";
+import { Avatar, Box, Text } from "@saleor/macaw-ui/next";
 import React from "react";
 
 export const MountingPoint = () => (
   <Box display="flex" gap={5} paddingX={6} paddingY={7}>
-    {/* TODO: use Avatar when https://github.com/saleor/macaw-ui/pull/225 is merged */}
-    <Box
-      __width="24px"
-      __height="24px"
-      as="img"
-      src={sideBarDefaultLogo}
-      alt="Saleor dashboard logo"
-      borderRadius={2}
-    />
-
+    <Avatar.Store src={sideBarDefaultLogo} scheme="decorative2" size="small" />
     <Text variant="bodyEmp" size="medium">
       Saleor Dashboard
     </Text>
--- src/components/Sidebar/UserControls.tsx
+++ src/components/Sidebar/UserControls.tsx
@@ -1,7 +1,6 @@
-import sideBarDefaultLogo from "@assets/images/sidebar-default-logo.png";
 import { useUser } from "@dashboard/auth";
-import { getUserName } from "@dashboard/misc";
-import { Box, MoreOptionsIcon, Text } from "@saleor/macaw-ui/next";
+import { getUserInitials, getUserName } from "@dashboard/misc";
+import { Avatar, Box, MoreOptionsIcon, Text } from "@saleor/macaw-ui/next";
 import React from "react";
 
 export const UserControls = () => {
@@ -20,16 +19,12 @@ export const UserControls = () => {
       justifyContent="space-between"
     >
       <Box display="flex" gap={5} alignItems="center">
-        {/* TODO: use Avatar when https://github.com/saleor/macaw-ui/pull/225 is merged */}
-        <Box
-          __width="32px"
-          __height="32px"
-          __borderRadius="50%"
-          as="img"
-          src={sideBarDefaultLogo}
-          alt="Saleor dashboard logo"
+        <Avatar.User
+          initials={getUserInitials(user)}
+          scheme="decorative2"
+          src={user?.avatar?.url}
         />
-        <Text variant="bodyEmp" size="medium" as="p">
+        <Text variant="bodyEmp" size="medium">
           {getUserName(user, true)}
         </Text>
       </Box>
