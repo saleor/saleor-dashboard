998794b26c81be08243aab947acbccce740599c5
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2023-01-31 09:41:15 +0100

    Add Sidebar Drawer (#3068)

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,11 +1,32 @@
-import { useFlags } from "@dashboard/hooks/useFlags";
+import { Box, Drawer, MenuIcon } from "@saleor/macaw-ui/next";
 import React from "react";
 
-import { LegacySidebar } from "./legacy";
-import { NewSidebar } from "./NewSidebar";
+import { SidebarContent } from "./Content";
 
-export const Sidebar = () => {
-  const { enableNewSidebar } = useFlags(["enableNewSidebar"]);
-
-  return enableNewSidebar.enabled ? <NewSidebar /> : <LegacySidebar />;
-};
+export const Sidebar = () => (
+  <>
+    <Box
+      display={{ mobile: "none", tablet: "none", desktop: "block" }}
+      height="100%"
+    >
+      <SidebarContent />
+    </Box>
+    <Box display={{ mobile: "block", tablet: "block", desktop: "none" }}>
+      <Drawer>
+        <Drawer.Trigger>
+          <Box
+            as="button"
+            borderWidth={0}
+            backgroundColor="interactiveNeutralHighlightDefault"
+            cursor="pointer"
+          >
+            <MenuIcon />
+          </Box>
+        </Drawer.Trigger>
+        <Drawer.Content backgroundColor="subdued">
+          <SidebarContent />
+        </Drawer.Content>
+      </Drawer>
+    </Box>
+  </>
+);
