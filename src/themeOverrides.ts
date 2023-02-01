4ca8e09103e36505dc41d77d9097b2c23f5de1e9
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2023-02-01 11:19:31 +0100

    Add theme & palette overrides (#3081)

--- src/themeOverrides.ts
+++ src/themeOverrides.ts
@@ -1,12 +1,84 @@
 import { Theme } from "@material-ui/core/styles";
+import { dark, light, Themes } from "@saleor/macaw-ui";
+import { vars } from "@saleor/macaw-ui/next";
 
-const breakpoints = ({
+const breakpoints = {
   keys: ["xs", "sm", "md", "lg", "xl"],
   values: { lg: 1680, md: 1280, sm: 600, xl: 1920, xs: 0 },
-} as unknown) as Theme["breakpoints"];
+} as unknown as Theme["breakpoints"];
 
-const themeOverrides: Partial<Theme> = {
+export const themeOverrides: Partial<Theme> = {
   breakpoints,
+  overrides: {
+    MuiCard: {
+      root: {
+        border: 0,
+        backgroundColor: vars.colors.background.surfaceNeutralPlain,
+      },
+    },
+    MuiPaper: {
+      root: {
+        backgroundColor: vars.colors.background.plain,
+        color: vars.colors.foreground.textNeutralDefault,
+      },
+    },
+    MuiTable: {
+      root: {
+        tableLayout: "auto",
+      },
+    },
+    MuiIconButton: {
+      edgeEnd: {
+        marginRight: 0,
+      },
+    },
+    MuiCardHeader: {
+      action: {
+        marginRight: 0,
+      },
+      root: {
+        paddingLeft: 0,
+        paddingRight: 0,
+      },
+    },
+    MuiCardContent: {
+      root: {
+        paddingLeft: 0,
+        paddingRight: 0,
+      },
+    },
+    MuiListItem: {
+      root: {
+        paddingLeft: 0,
+        paddingRight: 0,
+      },
+      gutters: {
+        paddingLeft: 0,
+        paddingRight: 0,
+      },
+    },
+    MuiCardActions: {
+      root: {
+        paddingLeft: 0,
+        paddingRight: 0,
+      },
+    },
+  },
 };
 
-export default themeOverrides;
+export const paletteOverrides: Themes = {
+  light: {
+    ...light,
+    background: {
+      ...light.background,
+      default: vars.colors.background.plain,
+    },
+  },
+  dark: {
+    ...dark,
+    background: {
+      ...dark.background,
+      default: vars.colors.background.plain,
+    },
+  },
+};
