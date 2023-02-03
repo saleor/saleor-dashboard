e8b14672f140720001072d4ec93c0e3b96611022
Karolina Rakoczy <rakoczy.karolina@gmail.com>
2023-02-03 16:06:20 +0100

    add authorization token to releases/latest request

--- .github/workflows/getEnvironmentVariables.js
+++ .github/workflows/getEnvironmentVariables.js
@@ -9,9 +9,12 @@ program
   .name("Approve PR")
   .description("Approve and merge PR if patch release")
   .option("--version <version>", "version of a project")
-  .option("--token <token>", "token fo login to cloud")
+  .option("--repo_token <token>", "token for login to cloud")
   .action(async options => {
-    const isOldVersion = await checkIfOldVersion(options.version);
+    const isOldVersion = await checkIfOldVersion(
+      options.version,
+      options.repo_token,
+    );
     core.setOutput("IS_OLD_VERSION", isOldVersion);
 
     if (!isPatchRelease(options.version)) {
@@ -176,17 +179,17 @@ async function getDomainForUpdatedEnvironment(environmentId, token) {
   return responseInJson.domain;
 }
 
-async function checkIfOldVersion(version) {
-  const newestVersion = await getTheNewestVersion();
+async function checkIfOldVersion(version, token) {
+  const newestVersion = await getTheNewestVersion(token);
   const howManyVersionsBehind =
     getFormattedVersion(newestVersion) - getFormattedVersion(version);
   //All versions besides last three are old versions
   return howManyVersionsBehind > 2 ? "true" : "false";
 }
 
-async function getTheNewestVersion() {
+async function getTheNewestVersion(token) {
   const octokit = new Octokit({
-    auth: process.env.GITHUB_TOKEN,
+    auth: token,
   });
 
   const response = await octokit.request(
