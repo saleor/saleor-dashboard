2ddbb8bf9a3a373d4934838bfa0411a3c4b91d78
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2023-02-06 14:05:45 +0100

    Improve Polly.js config (#3040)
    * Fix Polly.js configuration
    
    * Update test ids
    
    * Fix CR comments
    
    * Remove env var

--- testUtils/api.ts
+++ testUtils/api.ts
@@ -1,30 +1,55 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { BatchHttpLink } from "@apollo/client/link/batch-http";
 import { getApiUrl } from "@dashboard/config";
-import NodeHttpAdapter from "@pollyjs/adapter-node-http";
-import FSPersister from "@pollyjs/persister-fs";
 import { createFetch } from "@saleor/sdk";
+import isCI from "is-ci";
 import path from "path";
 import { setupPolly } from "setup-polly-jest";
 
+const POLLY_MODES = ["replay", "record", "passthrough", "stopped"] as const;
+
+function getPollyMode() {
+  const env = process.env.POLLY_MODE as typeof POLLY_MODES[number];
+  if (!env) {
+    return POLLY_MODES[0]; // replay
+  }
+  if (POLLY_MODES.includes(env)) {
+    return env;
+  }
+  console.warn(`Unrecognised POLLY_MODE env variable value: ${env}`);
+  return POLLY_MODES[0]; // replay
+}
+
+function getPollyRecordIfMissing() {
+  const env = process.env.POLLY_RECORD_IF_MISSING;
+  if (!env) {
+    return !isCI;
+  }
+  return env === "true";
+}
+
 function setupApi() {
   setupPolly({
-    adapters: [NodeHttpAdapter],
+    adapters: [require("@pollyjs/adapter-node-http")],
     matchRequestsBy: {
       headers: false,
       url: {
         hash: false,
-        hostname: false,
+        hostname: true,
         password: false,
-        pathname: false,
+        pathname: true,
         port: false,
         protocol: false,
         query: false,
         username: false,
       },
+      body: false,
     },
-    persister: FSPersister,
+    mode: getPollyMode(),
+    recordIfMissing: getPollyRecordIfMissing(),
+    persister: require("@pollyjs/persister-fs"),
     persisterOptions: {
+      keepUnusedRequests: false,
       fs: {
         recordingsDir: path.resolve(__dirname, "../recordings"),
       },
