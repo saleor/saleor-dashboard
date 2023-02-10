2f2720209a188c0e2a16fe88338e01e642229bd6
Michał Droń <dron.official@yahoo.com>
2023-02-10 14:32:28 +0100

    Replace tables with new macaw lists

--- src/new-apps/components/InstalledAppList/InstalledAppList.tsx
+++ src/new-apps/components/InstalledAppList/InstalledAppList.tsx
@@ -1,12 +1,11 @@
 import { AppInstallation, InstalledApp } from "@dashboard/new-apps/types";
 import { ListProps } from "@dashboard/types";
-import { Table, TableBody } from "@material-ui/core";
 import { Skeleton } from "@material-ui/lab";
+import { List } from "@saleor/macaw-ui/next";
 import React from "react";
 
 import InstalledAppListRow from "../InstalledAppListRow";
 import NotInstalledAppListRow from "../NotInstalledAppListRow";
-import { useStyles } from "./styles";
 
 interface InstalledAppListProps extends ListProps {
   appList?: InstalledApp[];
@@ -17,33 +16,29 @@ const InstalledAppList: React.FC<InstalledAppListProps> = ({
   appList,
   appInstallationList,
 }) => {
-  const classes = useStyles();
-
   if (!appList || !appInstallationList) {
     return <Skeleton />;
   }
 
   return (
-    <Table className={classes.table}>
-      <TableBody>
-        {appInstallationList?.map(({ appInstallation, logo, isExternal }) => (
-          <NotInstalledAppListRow
-            key={appInstallation.id}
-            appInstallation={appInstallation}
-            isExternal={isExternal}
-            logo={logo}
-          />
-        ))}
-        {appList.map(({ app, isExternal, logo }) => (
-          <InstalledAppListRow
-            key={app.id}
-            app={app}
-            isExternal={isExternal}
-            logo={logo}
-          />
-        ))}
-      </TableBody>
-    </Table>
+    <List>
+      {appInstallationList?.map(({ appInstallation, logo, isExternal }) => (
+        <NotInstalledAppListRow
+          key={appInstallation.id}
+          appInstallation={appInstallation}
+          isExternal={isExternal}
+          logo={logo}
+        />
+      ))}
+      {appList.map(({ app, isExternal, logo }) => (
+        <InstalledAppListRow
+          key={app.id}
+          app={app}
+          isExternal={isExternal}
+          logo={logo}
+        />
+      ))}
+    </List>
   );
 };
 
