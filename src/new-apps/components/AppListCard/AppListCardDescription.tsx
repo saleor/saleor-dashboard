f2d9a3d3b0e8db301e77eee5a1a0abc9911dddd7
Michał Droń <dron.official@yahoo.com>
2023-02-13 13:47:10 +0100

    Improve logo rendering

--- src/new-apps/components/AppListCard/AppListCardDescription.tsx
+++ src/new-apps/components/AppListCard/AppListCardDescription.tsx
@@ -2,48 +2,53 @@ import { GetV2SaleorAppsResponse } from "@dashboard/new-apps/marketplace.types";
 import { Box, Text } from "@saleor/macaw-ui/next";
 import React from "react";
 
-import { useDescriptionStyles } from "./styles";
-
 interface AppListCardDescriptionProps {
   app: GetV2SaleorAppsResponse.SaleorApp;
 }
 
 const AppListCardDescription: React.FC<AppListCardDescriptionProps> = ({
   app,
-}) => {
-  const classes = useDescriptionStyles();
-
-  return (
-    <Box>
+}) => (
+  <Box>
+    <Box
+      display="flex"
+      flexDirection="row"
+      alignItems="center"
+      marginBottom={8}
+      gap={6}
+    >
       <Box
+        width={13}
+        height={13}
         display="flex"
-        flexDirection="row"
-        alignItems="center"
-        marginBottom={8}
+        placeItems="center"
+        borderRadius={3}
+        style={{
+          backgroundColor: app.logo.color,
+        }}
+        data-test-id="app-logo"
       >
-        <div
-          className={classes.logo}
-          style={{
-            backgroundColor: app.logo.color,
-          }}
-          data-test-id="app-logo"
-        >
-          {app.logo.source && <img src={app.logo.source} alt="App logo" />}
-          {!app.logo.source && (
-            <Text variant="bodyEmp" as="h2" data-test-id="app-logo-placeholder">
-              {app.name.en[0] || ""}
-            </Text>
-          )}
-        </div>
-        <Text variant="bodyEmp" size="medium" color="textNeutralDefault">
-          {app.name.en}
-        </Text>
+        {app.logo.source && <img src={app.logo.source} alt="App logo" />}
+        {!app.logo.source && (
+          <Text
+            variant="bodyEmp"
+            size="large"
+            as="h2"
+            data-test-id="app-logo-placeholder"
+            color="textNeutralContrasted"
+          >
+            {app.name.en[0] || ""}
+          </Text>
+        )}
       </Box>
-      <Text size="small" variant="body" color="textNeutralSubdued">
-        {app.description.en}
+      <Text variant="bodyEmp" size="medium" color="textNeutralDefault">
+        {app.name.en}
       </Text>
     </Box>
-  );
-};
+    <Text size="small" variant="body" color="textNeutralSubdued">
+      {app.description.en}
+    </Text>
+  </Box>
+);
 AppListCardDescription.displayName = "AppListCardDescription";
 export default AppListCardDescription;
