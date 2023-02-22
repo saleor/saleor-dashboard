e87d5a997712b7ce121b72a7c1df0184aefbf823
Bartłomiej Wiaduch <tukan2can@gmail.com>
2023-02-22 16:31:09 +0100

    Add test for getUnavailableObjects

---
+++ src/components/DryRun/utils.test.ts
@@ -0,0 +1,24 @@
+import { getUnavailableObjects } from "./utils";
+
+describe("getUnavailableObjects", () => {
+  it("should return unavailable for dry run events from provided query", () => {
+    const query = `
+    subscription {
+      event {
+        ... on ProductUpdated {
+          __typename
+        }
+        ... on ProductDeleted {
+          __typename
+        }
+        ... on AddressUpdated {
+          __typename
+        }
+      }
+    }`;
+
+    const events = getUnavailableObjects(query);
+
+    expect(events).toEqual(["AddressUpdated"]);
+  });
+});
