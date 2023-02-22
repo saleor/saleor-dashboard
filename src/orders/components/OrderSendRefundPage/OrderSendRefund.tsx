0a82caf885adc53bf64a980d1720f07c88f36301
Jonatan Witoszek <jonatan.witoszek@saleor.io>
2023-02-22 14:26:51 +0100

    Update SendRefundPage with new page layout

--- src/orders/components/OrderSendRefundPage/OrderSendRefund.tsx
+++ src/orders/components/OrderSendRefundPage/OrderSendRefund.tsx
@@ -1,17 +1,17 @@
-import { Backlink } from "@dashboard/components/Backlink";
+import { Content } from "@dashboard/components/AppLayout/Content";
+import { DetailedContent } from "@dashboard/components/AppLayout/DetailedContent";
+import { RightSidebar } from "@dashboard/components/AppLayout/RightSidebar";
+import { TopNav } from "@dashboard/components/AppLayout/TopNav";
 import CardSpacer from "@dashboard/components/CardSpacer";
 import CardTitle from "@dashboard/components/CardTitle";
-import { Grid } from "@dashboard/components/Grid";
 import Hr from "@dashboard/components/Hr";
-import PageHeader from "@dashboard/components/PageHeader";
 import Skeleton from "@dashboard/components/Skeleton";
 import {
   CreateManualTransactionRefundMutationVariables,
   OrderDetailsFragment,
 } from "@dashboard/graphql/transactions";
-import { orderMessages } from "@dashboard/orders/messages";
 import { orderUrl } from "@dashboard/orders/urls";
-import { Card, CardContent, Container } from "@material-ui/core";
+import { Card, CardContent } from "@material-ui/core";
 import { ConfirmButtonTransitionState } from "@saleor/macaw-ui";
 import React from "react";
 import { FormattedMessage } from "react-intl";
@@ -47,114 +47,97 @@ const OrderSendRefundPage: React.FC<OrderSendRefundPageProps> = ({
   const transactions = order?.transactions ?? [];
 
   return (
-    <Container>
-      <Backlink href={orderUrl(order?.id)}>
-        {order?.number ? (
-          <FormattedMessage
-            {...orderMessages.headerOrderNumber}
-            values={{
-              orderNumber: order?.number,
-            }}
-          />
-        ) : (
-          <FormattedMessage {...orderMessages.headerOrder} />
-        )}
-      </Backlink>
-      <PageHeader
+    <DetailedContent>
+      <TopNav
+        href={orderUrl(order?.id)}
         title={<FormattedMessage {...refundPageMessages.pageSubtitle} />}
       />
-      <Grid>
-        <div>
-          {loading && transactions.length === 0 && (
-            <>
-              <Card>
-                <CardContent className={classes.cardLoading} />
-              </Card>
-              <CardSpacer />
-            </>
-          )}
-          {transactions.map(transaction => (
-            <TransactionCard
-              key={transaction.id}
-              transaction={transaction}
-              totalRemainingGrant={order?.totalRemainingGrant}
-              orderId={order?.id}
-            />
-          ))}
-          <ManualRefundCard
-            currency={currency}
-            submitState={addManualRefundState}
-            error={addManualRefundError}
-            onAddTransaction={({ amount, description, pspReference }) => {
-              onAddManualRefund({
-                currency,
-                description,
-                amount,
-                orderId: order?.id,
-                pspReference,
-              });
-            }}
+      <Content>
+        {loading && transactions.length === 0 && (
+          <>
+            <Card>
+              <CardContent className={classes.cardLoading} />
+            </Card>
+            <CardSpacer />
+          </>
+        )}
+        {transactions.map(transaction => (
+          <TransactionCard
+            key={transaction.id}
+            transaction={transaction}
+            totalRemainingGrant={order?.totalRemainingGrant}
+            orderId={order?.id}
           />
-        </div>
-        <div>
-          <Card>
-            <CardTitle
-              title={<FormattedMessage {...refundPageMessages.refundBalance} />}
-            />
-            <CardContent>
-              <ul className={classes.dataList}>
-                <DataLine
-                  label={
-                    <FormattedMessage {...refundPageMessages.totalCaptured} />
-                  }
-                >
-                  <DataLineMoney money={order?.totalCharged} />
+        ))}
+        <ManualRefundCard
+          currency={currency}
+          submitState={addManualRefundState}
+          error={addManualRefundError}
+          onAddTransaction={({ amount, description, pspReference }) => {
+            onAddManualRefund({
+              currency,
+              description,
+              amount,
+              orderId: order?.id,
+              pspReference,
+            });
+          }}
+        />
+      </Content>
+      <RightSidebar>
+        <Card>
+          <CardTitle
+            title={<FormattedMessage {...refundPageMessages.refundBalance} />}
+          />
+          <CardContent>
+            <ul className={classes.dataList}>
+              <DataLine
+                label={
+                  <FormattedMessage {...refundPageMessages.totalCaptured} />
+                }
+              >
+                <DataLineMoney money={order?.totalCharged} />
+              </DataLine>
+              <DataLine
+                label={
+                  <FormattedMessage {...refundPageMessages.grantedRefund} />
+                }
+              >
+                <DataLineMoney money={order?.totalGrantedRefund} />
+              </DataLine>
+              <DataLine
+                label={
+                  <FormattedMessage {...refundPageMessages.pendingRefunds} />
+                }
+              >
+                <DataLineMoney money={order?.totalRefundPending} />
+              </DataLine>
+            </ul>
+          </CardContent>
+          <Hr />
+          <CardTitle
+            title={
+              <FormattedMessage {...refundPageMessages.balanceAfterRequests} />
+            }
+          />
+          <CardContent>
+            {loading && <Skeleton />}
+            <ul className={classes.dataList}>
+              {order?.transactions.map(transaction => (
+                <DataLine label={transaction.type}>
+                  <DataLineMoney money={transaction.refundedAmount} />
                 </DataLine>
-                <DataLine
-                  label={
-                    <FormattedMessage {...refundPageMessages.grantedRefund} />
-                  }
-                >
-                  <DataLineMoney money={order?.totalGrantedRefund} />
-                </DataLine>
-                <DataLine
-                  label={
-                    <FormattedMessage {...refundPageMessages.pendingRefunds} />
-                  }
-                >
-                  <DataLineMoney money={order?.totalRefundPending} />
-                </DataLine>
-              </ul>
-            </CardContent>
-            <Hr />
-            <CardTitle
-              title={
-                <FormattedMessage
-                  {...refundPageMessages.balanceAfterRequests}
-                />
-              }
-            />
-            <CardContent>
-              {loading && <Skeleton />}
-              <ul className={classes.dataList}>
-                {order?.transactions.map(transaction => (
-                  <DataLine label={transaction.type}>
-                    <DataLineMoney money={transaction.refundedAmount} />
-                  </DataLine>
-                ))}
-                <DataLine
-                  label={<FormattedMessage {...refundPageMessages.result} />}
-                >
-                  <DataLineSettled
-                    unsettledMoney={order?.totalRemainingGrant}
-                  />
-                </DataLine>
-              </ul>
-            </CardContent>
-          </Card>
-        </div>
-      </Grid>
-    </Container>
+              ))}
+              <DataLine
+                label={<FormattedMessage {...refundPageMessages.result} />}
+              >
+                <DataLineSettled unsettledMoney={order?.totalRemainingGrant} />
+              </DataLine>
+            </ul>
+          </CardContent>
+        </Card>
+      </RightSidebar>
+    </DetailedContent>
   );
 };
 
