f9bf42ee59f38634c95a2c7ef9dcabfb36412ec3
Bartłomiej Wiaduch <tukan2can@gmail.com>
2023-02-22 15:44:24 +0100

    Refactor getWebhookTypes

--- src/custom-apps/components/WebhookEvents/utils.ts
+++ src/custom-apps/components/WebhookEvents/utils.ts
@@ -6,34 +6,23 @@ import {
 type Actions = string[];
 
 export const getWebhookTypes = (webhookEvents: string[]) => {
-  const webhookTypes: Record<string, Actions> = {};
   const multiWords = ["DRAFT_ORDER", "GIFT_CARD", "ANY_EVENTS"];
 
-  const setObject = (
-    keywords: string[],
-    object: string,
-    objectLength: number,
-  ) => {
-    const events = webhookTypes[object] || [];
-    const event = keywords.slice(objectLength).join("_");
-    events.push(!!event.length ? event : object);
-    webhookTypes[object] = events;
-  };
-
-  webhookEvents.forEach(key => {
+  return webhookEvents.reduce((acc, key) => {
     const keywords = key.split("_");
-
-    // handle 2 word exceptions
     const multiKeyword = keywords.slice(0, 2).join("_");
-    if (multiWords.includes(multiKeyword)) {
-      setObject(keywords, multiKeyword, 2);
-      return;
-    }
 
-    setObject(keywords, keywords[0], 1);
-  });
+    const [keyword, sliceSize] = multiWords.includes(multiKeyword)
+      ? [multiKeyword, 2]
+      : [keywords[0], 1];
 
-  return webhookTypes;
+    const event = keywords.slice(sliceSize).join("_");
+    const events = acc[keyword] || [];
+    events.push(!!event.length ? event : multiKeyword);
+    acc[keyword] = events;
+
+    return acc;
+  }, {});
 };
 
 export const AsyncWebhookTypes: Record<string, Actions> = getWebhookTypes(
