11a6a2986133c17062be0bb255703acaaad5f5e0
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2023-02-28 09:39:57 +0100

    fix: after rebase

--- src/new-apps/components/AppPage/AppPage.tsx
+++ src/new-apps/components/AppPage/AppPage.tsx
@@ -1,7 +1,9 @@
+import { DetailedContent } from "@dashboard/components/AppLayout/DetailedContent";
 import { AppQuery } from "@dashboard/graphql";
 import React from "react";
 
 import { AppFrame } from "../AppFrame";
+import { AppPageNav } from "./AppPageNav";
 import { useStyles } from "./styles";
 
 export interface AppPageProps {
@@ -20,17 +22,25 @@ export const AppPage: React.FC<AppPageProps> = ({
   const classes = useStyles();
 
   return (
-    <div className={classes.iframeContainer}>
-      {url && data.id && data.accessToken && (
-        <AppFrame
-          src={url}
-          appToken={data?.accessToken}
-          onError={onError}
-          appId={data?.id}
-          refetch={refetch}
-        />
-      )}
-    </div>
+    <DetailedContent useSingleColumn>
+      <AppPageNav
+        name={data?.name}
+        supportUrl={data?.supportUrl}
+        homepageUrl={data?.homepageUrl}
+      />
+
+      <div className={classes.iframeContainer}>
+        {url && data.id && data.accessToken && (
+          <AppFrame
+            src={url}
+            appToken={data?.accessToken ?? ""}
+            onError={onError}
+            appId={data?.id ?? ""}
+            refetch={refetch}
+          />
+        )}
+      </div>
+    </DetailedContent>
   );
 };
 
