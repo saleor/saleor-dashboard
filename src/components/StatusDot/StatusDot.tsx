c19305fce0b2e5a38b62fbb91ffd1bd91d6e9632
Michał Droń <dron.official@yahoo.com>
2023-03-09 12:44:06 +0100

    Extract status color to function

--- src/components/StatusDot/StatusDot.tsx
+++ src/components/StatusDot/StatusDot.tsx
@@ -1,19 +1,26 @@
-import { Box } from "@saleor/macaw-ui/next";
+import { Box, Sprinkles } from "@saleor/macaw-ui/next";
 import React from "react";
 
 export interface StatusDotProps {
   status: "default" | "error";
 }
 
+const getStatusColor = (
+  status: StatusDotProps["status"],
+): Sprinkles["backgroundColor"] => {
+  switch (status) {
+    case "error":
+      return "interactiveCriticalDefault";
+    default:
+      return "decorativeSurfacePlain2";
+  }
+};
+
 export const StatusDot: React.FC<StatusDotProps> = ({ status }) => (
   <Box
     width={4}
     height={4}
     borderRadius="50%"
-    backgroundColor={
-      status === "default"
-        ? "decorativeSurfacePlain2"
-        : "interactiveCriticalDefault"
-    }
+    backgroundColor={getStatusColor(status)}
   />
 );
