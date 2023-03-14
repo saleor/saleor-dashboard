8ad85da0635491014c7cd659672b66b649aa7947
Paweł Chyła <pawel.chyla@saleor.io>
2023-03-14 12:04:29 +0100

    Fix switching view

--- src/products/components/ProductListPage/ProductListPage.tsx
+++ src/products/components/ProductListPage/ProductListPage.tsx
@@ -218,7 +218,7 @@ export const ProductListPage: React.FC<ProductListPageProps> = props => {
             })}
             actions={
               <Switch
-                defaultValue="datagrid"
+                defaultValue={storedProductListViewType}
                 onValueChange={value => {
                   setProductListViewType(value as ProductListViewType);
                 }}
