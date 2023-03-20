9aa74982c9231d9179cd0283ca429c34c84ff226
wojteknowacki <wojciech.nowacki@saleor.io>
2023-03-20 12:26:38 +0100

    canvas temporary fix - percy should do the rest of the job when it is ready

--- cypress/e2e/products/images.js
+++ cypress/e2e/products/images.js
@@ -2,10 +2,9 @@
 /// <reference types="../../support"/>
 
 import { PRODUCT_DETAILS } from "../../elements/catalog/products/product-details";
-import { PRODUCTS_LIST } from "../../elements/catalog/products/products-list";
 import { SHARED_ELEMENTS } from "../../elements/shared/sharedElements";
 import { demoProductsNames } from "../../fixtures/products";
-import { productDetailsUrl, urlList } from "../../fixtures/urlList";
+import { productDetailsUrl } from "../../fixtures/urlList";
 import { getFirstProducts } from "../../support/api/requests/Product";
 import { deleteCollectionsStartsWith } from "../../support/api/utils/catalog/collectionsUtils";
 import {
@@ -18,39 +17,6 @@ describe("Tests for images", () => {
   beforeEach(() => {
     cy.clearSessionData().loginUserViaRequest();
   });
-
-  it(
-    "Images on product list should be displayed",
-    { tags: ["@products", "@allEnv", "@stable"] },
-    () => {
-      cy.addAliasToGraphRequest("ProductList")
-        .visit(urlList.products)
-        .wait("@ProductList")
-        .its("response.body.data.products.edges")
-        .then(products => {
-          cy.get(SHARED_ELEMENTS.skeleton).should("not.exist");
-          cy.get(PRODUCTS_LIST.productImage)
-            .each($image => {
-              cy.wrap($image)
-                .invoke("attr", "src")
-                .then(imageUrl => {
-                  cy.request(imageUrl);
-                })
-                .then(respImage => {
-                  expect(respImage.status).to.eq(200);
-                });
-            })
-            .then(images => {
-              const expectedProductsSvgAvatars =
-                products.length - images.length;
-              cy.get(PRODUCTS_LIST.imageIcon).should(
-                "have.length",
-                expectedProductsSvgAvatars,
-              );
-            });
-        });
-    },
-  );
 
   it(
     "Should display product image",
