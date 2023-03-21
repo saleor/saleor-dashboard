2dca5d92436619ff24f5c9b9a301ab9af785d68c
Michał Droń <droniu@droniu.dev>
2023-03-21 16:07:52 +0100

    Refactor media section in product details (#3395)
    * Bump macaw-ui
    
    * Refactor ProductMedia

---
+++ src/products/components/ProductMedia/messages.ts
@@ -0,0 +1,14 @@
+import { defineMessages } from "react-intl";
+
+export const messages = defineMessages({
+  media: {
+    id: "/Mcvt4",
+    defaultMessage: "Media",
+    description: "section header",
+  },
+  upload: {
+    id: "mGiA6q",
+    defaultMessage: "Upload",
+    description: "modal button upload",
+  },
+});
