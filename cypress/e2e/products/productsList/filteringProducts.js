66755b6936db15ac874d55aec4d70f5c4c7a477a
wojteknowacki <wojciech.nowacki@saleor.io>
2023-03-23 16:11:14 +0100

    fixing failing tests

--- cypress/e2e/products/productsList/filteringProducts.js
+++ cypress/e2e/products/productsList/filteringProducts.js
@@ -3,7 +3,7 @@
 
 import faker from "faker";
 
-import { PRODUCTS_LIST } from "../../../elements/catalog/products/products-list";
+import { SHARED_ELEMENTS } from "../../../elements";
 import { urlList } from "../../../fixtures/urlList";
 import { createCollection } from "../../../support/api/requests/Collections";
 import { updateProduct } from "../../../support/api/requests/Product";
@@ -24,7 +24,7 @@ import {
 } from "../../../support/pages/catalog/products/productsListPage";
 
 describe("As an admin I should be able to filter products", () => {
-  const startsWith = "CyFilterProducts-";
+  const startsWith = "ACyFilterProducts-";
   const name = `${startsWith}${faker.datatype.number()}`;
   const stockQuantity = 747;
   const price = 342;
@@ -92,9 +92,7 @@ describe("As an admin I should be able to filter products", () => {
   });
 
   beforeEach(() => {
-    cy.clearSessionData()
-      .loginUserViaRequest()
-      .visit(urlList.products);
+    cy.clearSessionData().loginUserViaRequest().visit(urlList.products);
   });
 
   const filterProductsBy = [
@@ -107,11 +105,8 @@ describe("As an admin I should be able to filter products", () => {
       `should filter products by ${filterBy.type}. TC: ${filterBy.testCase}`,
       { tags: ["@productsList", "@allEnv", "@stable"] },
       () => {
-        cy.expectSkeletonIsVisible().waitForProgressBarToNotExist();
         selectFilterOption(filterBy.type, name);
-        cy.getTextFromElement(PRODUCTS_LIST.productsNames).then(product => {
-          expect(product).to.includes(name);
-        });
+        cy.get(SHARED_ELEMENTS.dataGridTable).contains(name).should("exist");
       },
     );
   });
@@ -120,7 +115,6 @@ describe("As an admin I should be able to filter products", () => {
     "should filter products out of stock. TC: SALEOR_2604",
     { tags: ["@productsList", "@allEnv", "@stable"] },
     () => {
-      cy.expectSkeletonIsVisible();
       const productOutOfStock = `${startsWith}${faker.datatype.number()}`;
       createProductInChannel({
         name: productOutOfStock,
@@ -132,16 +126,11 @@ describe("As an admin I should be able to filter products", () => {
         categoryId: category.id,
         price,
       });
-      cy.waitForProgressBarToNotExist();
       selectChannel(channel.slug);
       selectProductsOutOfStock();
-      cy.searchInTable(productOutOfStock)
-        .get(PRODUCTS_LIST.productsNames)
-        .should("have.length", 1)
-        .getTextFromElement(PRODUCTS_LIST.productsNames)
-        .then(product => {
-          expect(product).to.includes(productOutOfStock);
-        });
+      cy.get(SHARED_ELEMENTS.dataGridTable)
+        .contains(productOutOfStock)
+        .should("exist");
     },
   );
 });
