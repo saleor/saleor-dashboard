d7e475fa803365fa46a4d57536225ae05d1b42bb
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2023-04-07 15:36:02 +0200

    backport #3390 to 3.8

--- src/auth/hooks/useAuthProvider.ts
+++ src/auth/hooks/useAuthProvider.ts
@@ -42,12 +42,8 @@ export function useAuthProvider({
   notify,
   apolloClient,
 }: UseAuthProviderOpts): UserContext {
-  const {
-    login,
-    getExternalAuthUrl,
-    getExternalAccessToken,
-    logout,
-  } = useAuth();
+  const { login, getExternalAuthUrl, getExternalAccessToken, logout } =
+    useAuth();
   const navigate = useNavigator();
   const { authenticated, authenticating, user } = useAuthState();
   const [requestedExternalPluginId] = useLocalStorage(
@@ -189,6 +185,7 @@ export function useAuthProvider({
         }
       } else {
         setErrors(["externalLoginError"]);
+        await handleLogout();
       }
 
       await logoutNonStaffUser(result.data.externalObtainAccessTokens);
