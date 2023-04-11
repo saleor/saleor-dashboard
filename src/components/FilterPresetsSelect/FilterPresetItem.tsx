3d9054727dc6ce5f7982eb4643bf8f10eb50a9dc
Paweł Chyła <pawel.chyla@saleor.io>
2023-04-11 09:12:23 +0200

    Fix list keyboard navigation

--- src/components/FilterPresetsSelect/FilterPresetItem.tsx
+++ src/components/FilterPresetsSelect/FilterPresetItem.tsx
@@ -18,47 +18,44 @@ export const FilterPresetItem = ({
 
   return (
     <Dropdown.Item>
-      <Box
+      <List.Item
+        paddingLeft={4}
+        paddingRight={11}
+        paddingY={3}
+        gap={6}
+        borderRadius={2}
         position="relative"
+        display="flex"
+        justifyContent="space-between"
+        onClick={onSelect}
         onMouseOver={() => setHasHover(true)}
         onMouseLeave={() => setHasHover(false)}
       >
-        <List.Item
-          paddingLeft={4}
-          paddingRight={11}
-          paddingY={3}
-          gap={6}
-          borderRadius={2}
-          display="flex"
-          justifyContent="space-between"
-          onClick={onSelect}
-        >
-          <Text ellipsis variant={isActive ? "bodyStrong" : "body"}>
-            {children}
-          </Text>
-          {hasHover && (
-            <Box
-              cursor="pointer"
-              zIndex="2"
-              position="absolute"
-              __top="50%"
-              right={4}
-              __transform="translateY(-50%)"
-              onClick={onRemove}
-              display="flex"
-              alignItems="center"
-            >
-              <RemoveIcon
-                color={{
-                  default: "iconNeutralSubdued",
-                  hover: "iconNeutralPlain",
-                  focusVisible: "iconNeutralPlain",
-                }}
-              />
-            </Box>
-          )}
-        </List.Item>
-      </Box>
+        <Text ellipsis variant={isActive ? "bodyStrong" : "body"}>
+          {children}
+        </Text>
+        {hasHover && (
+          <Box
+            cursor="pointer"
+            zIndex="2"
+            position="absolute"
+            __top="50%"
+            right={4}
+            __transform="translateY(-50%)"
+            onClick={onRemove}
+            display="flex"
+            alignItems="center"
+          >
+            <RemoveIcon
+              color={{
+                default: "iconNeutralSubdued",
+                hover: "iconNeutralPlain",
+                focusVisible: "iconNeutralPlain",
+              }}
+            />
+          </Box>
+        )}
+      </List.Item>
     </Dropdown.Item>
   );
 };
