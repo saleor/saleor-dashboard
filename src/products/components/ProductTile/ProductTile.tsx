ebb820f7735beb6670b664ea8445aacbe986cfd7
timur <timuric@gmail.com>
2023-04-15 15:13:04 +0200

    Realign hover effect

--- src/products/components/ProductTile/ProductTile.tsx
+++ src/products/components/ProductTile/ProductTile.tsx
@@ -1,6 +1,7 @@
 import { StatusDot } from "@dashboard/components/StatusDot/StatusDot";
 import { ProductListQuery } from "@dashboard/graphql";
 import { RelayToFlat } from "@dashboard/types";
+import { useTheme } from "@saleor/macaw-ui";
 import { Box, ProductsIcons, sprinkles, Text } from "@saleor/macaw-ui/next";
 import React from "react";
 
@@ -15,7 +16,7 @@ const commonThumbnailProps = {
   borderColor: "neutralHighlight",
   borderStyle: "solid",
   borderWidth: 1,
-  marginBottom: 8,
+  marginBottom: 4,
   borderRadius: 3,
   aspectRatio: "1 / 1",
 } as const;
@@ -23,58 +24,84 @@ const commonThumbnailProps = {
 export const ProductTile: React.FC<ProductTileProps> = ({
   product,
   onClick,
-}) => (
-  <Box
-    display="flex"
-    flexDirection="column"
-    onClick={onClick}
-    cursor="pointer"
-    backgroundColor={{ hover: "interactiveNeutralHighlightHovering" }}
-    borderRadius={4}
-    padding={5}
-    data-test-id={`product-tile-${product.id}`}
-  >
-    {product.thumbnail ? (
-      <Box
-        {...commonThumbnailProps}
-        as="img"
-        alt={product.name}
-        objectFit="scale-down"
-        src={product.thumbnail.url}
-      />
-    ) : (
-      <Box
-        {...commonThumbnailProps}
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        backgroundColor="surfaceNeutralSubdued"
-      >
-        <Box __width="40%" __height="40%">
-          <ProductsIcons
-            size="fill"
-            color="iconNeutralDisabled"
-            data-test-id={`placeholder-svg-${product.id}`}
-          />
-        </Box>
-      </Box>
-    )}
-    <Text
-      ellipsis
-      color="textNeutralSubdued"
-      variant="caption"
-      size="small"
-      className={sprinkles({ paddingY: 2 })}
+}) => {
+  const { spacing } = useTheme();
+
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      onClick={onClick}
+      cursor="pointer"
+      borderRadius={3}
+      position="relative"
+      data-test-id={`product-tile-${product.id}`}
     >
-      {product.productType.name}
-    </Text>
-    <Box display="flex" justifyContent="space-between" marginTop={2}>
-      <Text ellipsis color="textNeutralDefault" variant="bodyEmp" size="small">
-        {product.name}
-      </Text>
-      <Box padding={5}>
-        <StatusDot status={getTileStatus(product.channelListings)} />
+      {product.thumbnail ? (
+        <Box
+          {...commonThumbnailProps}
+          as="img"
+          alt={product.name}
+          objectFit="scale-down"
+          src={product.thumbnail.url}
+        />
+      ) : (
+        <Box
+          {...commonThumbnailProps}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          backgroundColor="surfaceNeutralSubdued"
+        >
+          <Box __width="40%" __height="40%">
+            <ProductsIcons
+              size="fill"
+              color="iconNeutralDisabled"
+              data-test-id={`placeholder-svg-${product.id}`}
+            />
+          </Box>
+        </Box>
+      )}
+      <Box display="flex" alignItems="center">
+        <Box paddingRight={3}>
+          <StatusDot status={getTileStatus(product.channelListings)} />
+        </Box>
+        <Text
+          ellipsis
+          color="textNeutralSubdued"
+          variant="caption"
+          size="small"
+          alignItems="center"
+          className={sprinkles({ paddingY: 2 })}
+        >
+          {product.productType.name}
+        </Text>
       </Box>
+      <Box display="flex" justifyContent="space-between" marginTop={2}>
+        <Text
+          ellipsis
+          color="textNeutralDefault"
+          variant="bodyEmp"
+          size="small"
+        >
+          {product.name}
+        </Text>
+      </Box>
+      <Box
+        position={"absolute"}
+        style={{
+          margin: "auto",
+          right: `calc(${spacing(2)} / -2)`,
+          top: `calc(${spacing(2)} / -2)`,
+          width: `calc(100% + ${spacing(2)})`,
+          height: `calc(100% + ${spacing(2)})`,
+          opacity: 0.1,
+        }}
+        borderRadius={5}
+        backgroundColor={{
+          hover: "highlightDim",
+        }}
+      ></Box>
     </Box>
-  </Box>
-);
+  );
+};
