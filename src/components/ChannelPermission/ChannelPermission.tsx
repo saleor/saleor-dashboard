a05abb1bb5b5f93f46f16c51d12a2facae489df5
Paweł Chyła <pawel.chyla@saleor.io>
2023-04-17 14:29:32 +0200

    Handle events and data in channel permission component

--- src/components/ChannelPermission/ChannelPermission.tsx
+++ src/components/ChannelPermission/ChannelPermission.tsx
@@ -8,7 +8,7 @@ import {
   ListItemText,
   Typography,
 } from "@material-ui/core";
-import React from "react";
+import React, { ChangeEvent } from "react";
 import { useIntl } from "react-intl";
 
 import CardTitle from "../CardTitle/CardTitle";
@@ -17,14 +17,64 @@ interface ChannelPermissionProps {
   channels: ChannelFragment[];
   description: string;
   fullAccessLabel: string;
+  disabled: boolean;
+  data: {
+    hasAllChannels: boolean;
+    channels: ChannelFragment[];
+  };
+  onChange: (event: ChangeEvent<any>, cb?: () => void) => void;
 }
 
 export const ChannelPermission = ({
   channels,
   description,
   fullAccessLabel,
+  disabled,
+  onChange,
+  data,
 }: ChannelPermissionProps) => {
   const intl = useIntl();
+
+  const handleAllChannelsChange = () => {
+    onChange({
+      target: {
+        name: "hasAllChannels",
+        value: !data.hasAllChannels,
+      },
+    } as any);
+
+    onChange({
+      target: {
+        name: "channels",
+        value: !data.hasAllChannels ? channels : [],
+      },
+    } as any);
+  };
+
+  const handleChannelChange = (channel: ChannelFragment) => () => {
+    const hasChannelChecked = data.channels.find(
+      chan => chan.id === channel.id,
+    );
+
+    if (hasChannelChecked) {
+      onChange({
+        target: {
+          name: "channels",
+          value: data.channels.filter(chan => chan.id !== channel.id),
+        },
+      } as any);
+    } else {
+      onChange({
+        target: {
+          name: "channels",
+          value: [...data.channels, channel],
+        },
+      } as any);
+    }
+  };
+
+  const isChannelChecked = (channel: ChannelFragment) =>
+    !!data.channels.find(chan => chan.id === channel.id);
 
   return (
     <Card>
@@ -36,14 +86,19 @@ export const ChannelPermission = ({
       ></CardTitle>
       <CardContent>
         <Typography variant="body2">{description}</Typography>
-        <ListItem role={undefined} dense button>
+        <ListItem
+          role={undefined}
+          onClick={handleAllChannelsChange}
+          dense
+          button
+        >
           <ListItemIcon>
             <Checkbox
               data-test-id="full-access"
               color="secondary"
               edge="start"
-              checked={false}
-              disabled={false}
+              checked={data.hasAllChannels}
+              disabled={disabled}
               tabIndex={-1}
               disableRipple
               inputProps={{ "aria-labelledby": "fullAccess" }}
@@ -61,12 +116,13 @@ export const ChannelPermission = ({
             role={undefined}
             dense
             button
+            onClick={handleChannelChange(channel)}
           >
             <ListItemIcon>
               <Checkbox
                 color="secondary"
                 edge="start"
-                checked={false}
+                checked={isChannelChecked(channel)}
                 tabIndex={-1}
                 disableRipple
                 name={channel.name}
