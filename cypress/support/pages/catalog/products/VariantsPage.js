b9692d08cffa5e7e660862dd6268d94305356cd4
wojteknowacki <wojciech.nowacki@saleor.io>
2023-04-26 09:43:28 +0200

    fixes product variants failing tests SALEOR_2901 and SALEOR_2902

--- cypress/support/pages/catalog/products/VariantsPage.js
+++ cypress/support/pages/catalog/products/VariantsPage.js
@@ -81,7 +81,6 @@ export function fillUpVariantDetails({
     .each(input => {
       cy.wrap(input).type(costPrice);
     });
-
   if (variantName) {
     cy.get(VARIANTS_SELECTORS.variantNameInput).type(variantName);
   }
