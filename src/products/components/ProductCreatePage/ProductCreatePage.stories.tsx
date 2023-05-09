efd45603ece52666a10a3ab0e29940e9e203bc44
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2023-05-09 12:03:16 +0200

    fix: after CR

--- src/products/components/ProductCreatePage/ProductCreatePage.stories.tsx
+++ src/products/components/ProductCreatePage/ProductCreatePage.stories.tsx
@@ -1,13 +1,18 @@
 import { channelsList } from "@dashboard/channels/fixtures";
 import { createChannelsData } from "@dashboard/channels/utils";
 import { fetchMoreProps } from "@dashboard/fixtures";
-import { ProductErrorCode } from "@dashboard/graphql";
+import {
+  ProductChannelListingErrorFragment,
+  ProductErrorCode,
+  ProductErrorWithAttributesFragment,
+} from "@dashboard/graphql";
 import {
   productTypes,
   productTypeSearch,
 } from "@dashboard/productTypes/fixtures";
 import { taxClasses } from "@dashboard/taxes/fixtures";
 import { warehouseList } from "@dashboard/warehouses/fixtures";
+import { ConfirmButtonTransitionState } from "@saleor/macaw-ui";
 import React from "react";
 
 import { product as productFixture } from "../../fixtures";
@@ -21,88 +26,51 @@ export default {
   title: "Products / Create product",
 };
 
-export const Default = () => (
-  <ProductCreatePage
-    channelsErrors={[]}
-    currentChannels={channels}
-    allChannelsCount={5}
-    loading={false}
-    errors={[]}
-    header="Add product"
-    collections={product.collections}
-    fetchCategories={() => undefined}
-    fetchCollections={() => undefined}
-    fetchProductTypes={() => undefined}
-    fetchAttributeValues={() => undefined}
-    fetchMoreCategories={fetchMoreProps}
-    fetchMoreCollections={fetchMoreProps}
-    fetchMoreProductTypes={fetchMoreProps}
-    fetchMoreAttributeValues={fetchMoreProps}
-    productTypes={productTypes}
-    categories={[product.category]}
-    onChannelsChange={() => undefined}
-    onSubmit={() => undefined}
-    openChannelsModal={() => undefined}
-    saveButtonBarState="default"
-    warehouses={warehouseList}
-    onWarehouseConfigure={() => undefined}
-    taxClasses={taxClasses}
-    fetchMoreTaxClasses={undefined}
-    weightUnit="kg"
-    referencePages={[]}
-    referenceProducts={[]}
-    attributeValues={[]}
-    onAssignReferencesClick={() => undefined}
-    onCloseDialog={() => undefined}
-    onSelectProductType={() => undefined}
-    onAttributeSelectBlur={() => undefined}
-  />
-);
+const props = {
+  channelsErrors: [] as ProductChannelListingErrorFragment[],
+  currentChannels: channels,
+  allChannelsCount: 5,
+  loading: false,
+  errors: [] as ProductErrorWithAttributesFragment[],
+  header: "Add product",
+  collections: product.collections,
+  fetchCategories: () => undefined,
+  fetchCollections: () => undefined,
+  fetchProductTypes: () => undefined,
+  fetchAttributeValues: () => undefined,
+  fetchMoreCategories: fetchMoreProps,
+  fetchMoreCollections: fetchMoreProps,
+  fetchMoreProductTypes: fetchMoreProps,
+  fetchMoreAttributeValues: fetchMoreProps,
+  productTypes,
+  categories: [product.category],
+  onChannelsChange: () => undefined,
+  onSubmit: () => undefined,
+  openChannelsModal: () => undefined,
+  saveButtonBarState: "default" as ConfirmButtonTransitionState,
+  warehouses: warehouseList,
+  onWarehouseConfigure: () => undefined,
+  taxClasses,
+  fetchMoreTaxClasses: undefined,
+  weightUnit: "kg",
+  referencePages: [],
+  referenceProducts: [],
+  attributeValues: [],
+  onAssignReferencesClick: () => undefined,
+  onCloseDialog: () => undefined,
+  onSelectProductType: () => undefined,
+  onAttributeSelectBlur: () => undefined,
+};
+
+export const Default = () => <ProductCreatePage {...props} />;
 
 export const WhenLoading = () => (
-  <ProductCreatePage
-    channelsErrors={[]}
-    currentChannels={channels}
-    allChannelsCount={5}
-    loading={true}
-    errors={[]}
-    header="Add product"
-    collections={product.collections}
-    fetchCategories={() => undefined}
-    fetchCollections={() => undefined}
-    fetchProductTypes={() => undefined}
-    fetchAttributeValues={() => undefined}
-    fetchMoreCategories={fetchMoreProps}
-    fetchMoreCollections={fetchMoreProps}
-    fetchMoreProductTypes={fetchMoreProps}
-    fetchMoreAttributeValues={fetchMoreProps}
-    productTypes={productTypes}
-    categories={[product.category]}
-    onChannelsChange={() => undefined}
-    onSubmit={() => undefined}
-    openChannelsModal={() => undefined}
-    saveButtonBarState="default"
-    warehouses={undefined}
-    onWarehouseConfigure={() => undefined}
-    taxClasses={taxClasses}
-    fetchMoreTaxClasses={undefined}
-    weightUnit="kg"
-    referencePages={[]}
-    referenceProducts={[]}
-    attributeValues={[]}
-    onAssignReferencesClick={() => undefined}
-    onCloseDialog={() => undefined}
-    onSelectProductType={() => undefined}
-    onAttributeSelectBlur={() => undefined}
-  />
+  <ProductCreatePage {...props} loading={true} />
 );
 
 export const FormErrors = () => (
   <ProductCreatePage
-    channelsErrors={[]}
-    currentChannels={channels}
-    allChannelsCount={5}
-    loading={false}
+    {...props}
     errors={(
       ["attributes", "name", "productType", "category", "sku"] as Array<
         keyof ProductCreateFormData | "attributes"
@@ -117,34 +85,5 @@ export const FormErrors = () => (
       field,
       message: "Attributes invalid",
     }))}
-    header="Add product"
-    collections={product.collections}
-    fetchCategories={() => undefined}
-    fetchCollections={() => undefined}
-    fetchProductTypes={() => undefined}
-    fetchAttributeValues={() => undefined}
-    fetchMoreCategories={fetchMoreProps}
-    fetchMoreCollections={fetchMoreProps}
-    fetchMoreProductTypes={fetchMoreProps}
-    selectedProductType={productTypeSearch}
-    fetchMoreAttributeValues={fetchMoreProps}
-    productTypes={productTypes}
-    categories={[product.category]}
-    onChannelsChange={() => undefined}
-    onSubmit={() => undefined}
-    openChannelsModal={() => undefined}
-    saveButtonBarState="default"
-    warehouses={warehouseList}
-    onWarehouseConfigure={() => undefined}
-    taxClasses={taxClasses}
-    fetchMoreTaxClasses={undefined}
-    weightUnit="kg"
-    referencePages={[]}
-    referenceProducts={[]}
-    attributeValues={[]}
-    onAssignReferencesClick={() => undefined}
-    onCloseDialog={() => undefined}
-    onSelectProductType={() => undefined}
-    onAttributeSelectBlur={() => undefined}
   />
 );
