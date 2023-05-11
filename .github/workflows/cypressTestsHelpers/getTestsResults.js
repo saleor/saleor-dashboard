0dfca1abfe32c5160f2d3ae52bea7b7656d116b0
Karolina Rakoczy <rakoczy.karolina@gmail.com>
2023-05-11 10:05:19 +0200

    Create new file with helpers

--- .github/workflows/cypressTestsHelpers/getTestsResults.js
+++ .github/workflows/cypressTestsHelpers/getTestsResults.js
@@ -1,34 +1,15 @@
-const core = require("@actions/core");
 const { Command } = require("commander");
 const { GraphQLClient } = require("graphql-request");
 
 const program = new Command();
 const client = new GraphQLClient("https://dashboard.cypress.io/graphql");
 
-program
-  .name("Approve PR")
-  .description("Approve and merge PR if patch release")
-  .option("--dashboard_url <dashboard_url>", "Cypress dashboard url")
-  .action(async options => {
-    const data = await getTestsStatusAndId(options.dashboard_url);
-
-    let testsStatus = data.status;
-
-    if (testsStatus === "FAILED") {
-      const testCases = await getFailedTestCases(data.runId);
-      if (testCases.length >= 20) {
-        core.setOutput("notifySlack", "true");
-      }
-    } else if (testsStatus === "FAILED") {
-      core.setOutput("notifySlack", "true");
-    }
-  })
-  .parse();
+const statusAndID = dashboardUrl => getTestsStatusAndId(dashboardUrl);
+const failedTestCases = runId => getFailedTestCases(runId);
 
 async function getTestsStatusAndId(dashboardUrl) {
   const getProjectRegex = /\/projects\/([^\/]*)/;
   const getRunRegex = /\/runs\/([^\/]*)/;
-
   const requestVariables = {
     projectId: dashboardUrl.match(getProjectRegex)[1],
     buildNumber: dashboardUrl.match(getRunRegex)[1],
@@ -94,3 +75,5 @@ async function getFailedTestCases(runId) {
       });
   });
 }
+
+module.exports = { statusAndID, failedTestCases };
