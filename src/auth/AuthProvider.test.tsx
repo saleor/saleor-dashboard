cc109f3b8b5757c2ff0f11af91f9b5a46e418eec
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2023-05-18 10:54:30 +0200

    Logout user that do not have permissions (#3559)

--- src/auth/AuthProvider.test.tsx
+++ src/auth/AuthProvider.test.tsx
@@ -1,41 +1,11 @@
-/** @jest-environment setup-polly-jest/jest-environment-jsdom */
-
-import { getApiUrl } from "@dashboard/config";
-import { createSaleorClient, SaleorProvider } from "@saleor/sdk";
-import setupApi from "@test/api";
+import { useApolloClient } from "@apollo/client";
+import { useUserDetailsQuery } from "@dashboard/graphql";
+import useNotifier from "@dashboard/hooks/useNotifier";
+import { useAuth, useAuthState } from "@saleor/sdk";
 import { act, renderHook } from "@testing-library/react-hooks";
-import React from "react";
-import { IntlProvider } from "react-intl";
-import { MemoryRouter as Router } from "react-router-dom";
+import { useIntl } from "react-intl";
 
 import { useAuthProvider } from "./hooks/useAuthProvider";
-
-const apolloClient = setupApi();
-
-function renderAuthProvider() {
-  const intl = {
-    formatMessage: ({ defaultMessage }) => defaultMessage,
-  };
-  const notify = jest.fn();
-  const saleorClient = createSaleorClient({
-    apiUrl: getApiUrl(),
-    channel: "",
-  });
-  const wrapper = ({ children }) => (
-    <IntlProvider defaultLocale="en" locale="en">
-      <Router>
-        <SaleorProvider client={saleorClient}>{children}</SaleorProvider>
-      </Router>
-    </IntlProvider>
-  );
-
-  const { result } = renderHook(
-    () => useAuthProvider({ intl: intl as any, notify, apolloClient }),
-    { wrapper },
-  );
-
-  return result;
-}
 
 const adminCredentials = {
   email: "admin@example.com",
@@ -53,43 +23,202 @@ beforeEach(() => {
   sessionStorage.clear();
 });
 
-describe("User", () => {
-  it("will be logged in if has valid credentials", async () => {
-    const hook = renderAuthProvider();
+jest.mock("react-intl", () => ({
+  useIntl: jest.fn(() => ({
+    formatMessage: jest.fn(x => x.defaultMessage),
+  })),
+  defineMessages: jest.fn(x => x),
+}));
 
-    await act(async () => {
-      const result = await hook.current.login(
+jest.mock("@saleor/sdk", () => ({
+  useAuth: jest.fn(() => ({
+    login: jest.fn(() => ({
+      data: {
+        tokenCreate: {
+          errors: [],
+          user: {
+            userPermissions: [
+              {
+                code: "MANAGE_USERS",
+                name: "Handle checkouts",
+              },
+            ],
+          },
+        },
+      },
+    })),
+    logout: jest.fn(),
+  })),
+  useAuthState: jest.fn(() => undefined),
+}));
+jest.mock("@apollo/client", () => ({
+  useApolloClient: jest.fn(() => ({
+    clearStore: jest.fn(),
+  })),
+  ApolloError: jest.fn(),
+}));
+
+jest.mock("@dashboard/graphql", () => ({
+  useUserDetailsQuery: jest.fn(() => ({
+    data: undefined,
+  })),
+}));
+
+jest.mock("@dashboard/hooks/useNotifier", () => ({
+  __esModule: true,
+  default: jest.fn(() => () => undefined),
+}));
+
+jest.mock("@dashboard/hooks/useNavigator", () => ({
+  __esModule: true,
+  default: jest.fn(() => () => undefined),
+}));
+jest.mock("@dashboard/hooks/useLocalStorage", () => ({
+  __esModule: true,
+  default: jest.fn(() => []),
+}));
+jest.mock("@dashboard/auth", () => ({
+  useUser: jest.fn(),
+}));
+jest.mock("use-react-router", () => ({
+  __esModule: true,
+  default: jest.fn(() => ({
+    location: {},
+  })),
+}));
+
+describe("AuthProvider", () => {
+  it("Staff user will be logged in if has valid credentials", async () => {
+    // Arrange
+    const intl = useIntl();
+    const notify = useNotifier();
+    const apolloClient = useApolloClient();
+
+    (useAuthState as jest.Mock).mockImplementation(() => ({
+      authenticated: true,
+      authenticating: false,
+      user: {
+        isStaff: true,
+      },
+    }));
+    (useUserDetailsQuery as jest.Mock).mockImplementation(() => ({
+      data: {
+        me: {
+          email: adminCredentials.email,
+          isStaff: true,
+        },
+      },
+    }));
+
+    // Act
+    const hook = renderHook(() =>
+      useAuthProvider({ intl, notify, apolloClient }),
+    );
+    await act(async () =>
+      hook.result.current.login(
         adminCredentials.email,
         adminCredentials.password,
-      );
-      expect(result.user?.email).toBe(adminCredentials.email);
-    });
-    expect(hook.current.authenticated).toBe(true);
+      ),
+    );
+
+    // Assert
+    expect(hook.result.current.user?.email).toBe(adminCredentials.email);
+    expect(hook.result.current.authenticated).toBe(true);
   });
 
-  it("will not be logged in if doesn't have valid credentials", async () => {
-    const hook = renderAuthProvider();
+  it("User will not be logged in if doesn't have valid credentials", async () => {
+    // Arrange
+    const intl = useIntl();
+    const notify = useNotifier();
+    const apolloClient = useApolloClient();
 
-    await act(async () => {
-      const result = await hook.current.login(
-        adminCredentials.email,
-        "NotAValidPassword123!",
-      );
-      expect(result.user).toBe(null);
-    });
-    expect(hook.current.authenticated).toBe(false);
+    (useAuthState as jest.Mock).mockImplementation(() => ({
+      authenticated: false,
+      authenticating: false,
+    }));
+    (useUserDetailsQuery as jest.Mock).mockImplementation(() => ({
+      data: {
+        me: null,
+      },
+    }));
+
+    // Act
+    const hook = renderHook(() =>
+      useAuthProvider({ intl, notify, apolloClient }),
+    );
+
+    // Assert
+    expect(hook.result.current.user).toBe(null);
+    expect(hook.result.current.authenticated).toBe(false);
   });
 
-  it("will not be logged in if is non-staff", async () => {
-    const hook = renderAuthProvider();
+  it("Non-staff user will not be logged in", async () => {
+    // Arrange
+    const intl = useIntl();
+    const notify = useNotifier();
+    const apolloClient = useApolloClient();
 
-    await act(async () => {
-      const result = await hook.current.login(
+    (useAuthState as jest.Mock).mockImplementation(() => ({
+      authenticated: false,
+      authenticating: false,
+    }));
+    (useUserDetailsQuery as jest.Mock).mockImplementation(() => ({
+      data: {
+        me: {
+          email: nonStaffUserCredentials.email,
+          isStaff: false,
+        },
+      },
+    }));
+
+    // Act
+    const hook = renderHook(() =>
+      useAuthProvider({ intl, notify, apolloClient }),
+    );
+    await act(async () =>
+      hook.result.current.login(
         nonStaffUserCredentials.email,
         nonStaffUserCredentials.password,
-      );
-      expect(result.user).toBe(null);
-    });
-    expect(hook.current.authenticated).toBe(false);
+      ),
+    );
+
+    // Assert
+    expect(hook.result.current.errors).toEqual([]);
+    expect(hook.result.current.authenticated).toBe(false);
+  });
+
+  it("Should logout user without userPermissions", async () => {
+    const intl = useIntl();
+    const notify = useNotifier();
+    const apolloClient = useApolloClient();
+
+    (useAuth as jest.Mock).mockImplementation(() => ({
+      login: jest.fn(() => ({
+        data: {
+          tokenCreate: {
+            errors: [],
+            user: {
+              userPermissions: [],
+            },
+          },
+        },
+      })),
+      logout: jest.fn(),
+    }));
+
+    // Act
+    const hook = renderHook(() =>
+      useAuthProvider({ intl, notify, apolloClient }),
+    );
+    await act(async () =>
+      hook.result.current.login(
+        nonStaffUserCredentials.email,
+        nonStaffUserCredentials.password,
+      ),
+    );
+
+    // Assert
+    expect(hook.result.current.errors).toEqual(["noPermissionsError"]);
+    expect(hook.result.current.authenticated).toBe(false);
   });
 });
