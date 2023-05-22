ad61a8bb8a053e817e4db56dc2741dc4c8e004ac
wojteknowacki <wojciech.nowacki@saleor.io>
2023-05-22 16:43:25 +0200

    tests for adding, deleteing and updating public and prvate metadata for fullfilled orders

--- cypress/support/api/utils/index.js
+++ cypress/support/api/utils/index.js
@@ -1,8 +1,14 @@
 export { deleteChannelsStartsWith, getDefaultChannel } from "./channelsUtils";
-export { createOrder, createReadyToFulfillOrder } from "./ordersUtils";
 export { createShipping, deleteShippingStartsWith } from "./shippingUtils";
 export {
   getDefaultTaxClass,
   updateTaxConfigurationForChannel,
 } from "./taxesUtils";
 export * as productsUtils from "./products/productsUtils";
+
+export {
+  createFulfilledOrder,
+  createOrder,
+  createReadyToFulfillOrder,
+  createUnconfirmedOrder,
+} from "./ordersUtils";
--- cypress/support/pages/index.js
+++ cypress/support/pages/index.js
@@ -1,2 +1,21 @@
 export * as ordersOperationsHelpers from "./ordersOperations";
 export * as transactionsOrderUtils from "./ordersTransactionUtils";
+export {
+  addMetadataField,
+  addPrivateMetadataFieldFulfillmentOrder,
+  addPublicMetadataFieldFulfillmentOrder,
+  deletePrivateFulfillmentMetadata,
+  deletePublicFulfillmentMetadata,
+  expandPrivateFulfillmentMetadata,
+  expandPublicFulfillmentMetadata,
+  updatePrivateMetadataFieldFulfillmentOrder,
+  updatePublicMetadataFieldFulfillmentOrder,
+} from "./catalog/metadataComponent";
+export { selectChannelInPicker } from "./channelsPage";
+export { finalizeDraftOrder } from "./draftOrderPage";
+export {
+  addNewProductToOrder,
+  applyFixedLineDiscountForProduct,
+  changeQuantityOfProducts,
+  deleteProductFromGridTableOnIndex,
+} from "./ordersOperations";
