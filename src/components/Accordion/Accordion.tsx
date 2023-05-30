f021342d656d26bc3cc0f7658d4284770fe36b8f
Paweł Chyła <pawel.chyla@saleor.io>
2023-05-30 14:43:58 +0200

    Revert "Delete old accordion"
    This reverts commit 8615c9671174ea6bdf259cbf9a74cba3480781b6.

---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,82 @@
+import { Typography } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import RemoveIcon from "@material-ui/icons/Remove";
+import { IconButton, makeStyles } from "@saleor/macaw-ui";
+import clsx from "clsx";
+import React from "react";
+
+import Hr from "../Hr";
+
+const useStyles = makeStyles(
+  theme => ({
+    content: {
+      padding: theme.spacing(3, 0),
+    },
+    expandButton: {
+      position: "relative",
+      right: theme.spacing(-2),
+      top: theme.spacing(0.5),
+    },
+    root: {
+      border: `1px solid ${theme.palette.divider}`,
+      borderRadius: 12,
+      padding: theme.spacing(0, 3),
+    },
+    title: {
+      display: "flex",
+      justifyContent: "space-between",
+    },
+    titleText: {
+      padding: theme.spacing(2, 0),
+    },
+  }),
+  {
+    name: "Accordion",
+  },
+);
+
+export interface AccordionProps {
+  className?: string;
+  initialExpand?: boolean;
+  quickPeek?: React.ReactNode;
+  title: string;
+}
+
+const Accordion: React.FC<AccordionProps> = ({
+  children,
+  className,
+  initialExpand,
+  quickPeek,
+  title,
+  ...props
+}) => {
+  const classes = useStyles({});
+  const [expanded, setExpanded] = React.useState(!!initialExpand);
+
+  return (
+    <div className={clsx(classes.root, className)} {...props}>
+      <div className={classes.title}>
+        <Typography className={classes.titleText}>{title}</Typography>
+        <div className={classes.expandButton}>
+          <IconButton
+            variant="secondary"
+            onClick={() => setExpanded(!expanded)}
+          >
+            {expanded ? <RemoveIcon /> : <AddIcon />}
+          </IconButton>
+        </div>
+      </div>
+      {(expanded || !!quickPeek) && (
+        <>
+          <Hr />
+          <div className={classes.content}>
+            {quickPeek ? (expanded ? children : quickPeek) : children}
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+Accordion.displayName = "Accordion";
+export default Accordion;
