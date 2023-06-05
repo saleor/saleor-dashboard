17a330f8132ff0d43890b8d4438fc41706571391
Michał Droń <droniu@droniu.dev>
2023-06-05 07:48:24 +0200

    Replace switch with if statements

--- src/components/Datagrid/ColumnPicker/ColumnPickerAvailableNodes.tsx
+++ src/components/Datagrid/ColumnPicker/ColumnPickerAvailableNodes.tsx
@@ -26,45 +26,38 @@ export const ColumnPickerAvailableNodes = ({
   const areNodesEmpty = currentCategory.availableNodes?.length === 0;
 
   const renderNodes = () => {
-    switch (true) {
-      case areNodesLoading:
-        return (
-          <Box
-            width="100%"
-            height="100%"
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-          >
-            <CircularProgress />
-          </Box>
-        );
-      case areNodesEmpty:
-        return (
-          <Text size="small" color="textNeutralSubdued">
-            <FormattedMessage {...messages.noResultsFound} />
-          </Text>
-        );
-      default:
-        return currentCategory.availableNodes.map(node => (
-          <Box
-            display="flex"
-            alignItems="center"
-            padding={5}
-            gap={6}
-            key={node.id}
-          >
-            <Checkbox
-              onCheckedChange={() => changeHandler(node.id)}
-              checked={columnPickerSettings.includes(node.id)}
-            >
-              <Text size="small" color="textNeutralSubdued">
-                {node.title}
-              </Text>
-            </Checkbox>
-          </Box>
-        ));
+    if (areNodesLoading) {
+      return (
+        <Box
+          width="100%"
+          height="100%"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <CircularProgress />
+        </Box>
+      );
     }
+    if (areNodesEmpty) {
+      return (
+        <Text size="small" color="textNeutralSubdued">
+          <FormattedMessage {...messages.noResultsFound} />
+        </Text>
+      );
+    }
+    return currentCategory.availableNodes.map(node => (
+      <Box display="flex" alignItems="center" padding={5} gap={6} key={node.id}>
+        <Checkbox
+          onCheckedChange={() => changeHandler(node.id)}
+          checked={columnPickerSettings.includes(node.id)}
+        >
+          <Text size="small" color="textNeutralSubdued">
+            {node.title}
+          </Text>
+        </Checkbox>
+      </Box>
+    ));
   };
 
   return (
