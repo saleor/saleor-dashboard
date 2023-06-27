66d8a02ebcacdf3bebedfe0e0c557bd3c9a5fce9
andrzejewsky <vox3r69@gmail.com>
2023-06-27 16:58:11 +0200

    Feature flag

--- src/components/AppLayout/ListFilters/ListFilters.tsx
+++ src/components/AppLayout/ListFilters/ListFilters.tsx
@@ -1,4 +1,5 @@
 import { FilterErrorMessages, IFilter } from "@dashboard/components/Filter";
+import { useFlag } from "@dashboard/featureFlags";
 import { FilterProps, SearchPageProps } from "@dashboard/types";
 import { Box } from "@saleor/macaw-ui/next";
 import React, { ReactNode } from "react";
@@ -26,37 +27,45 @@ export const ListFilters = ({
   onFilterAttributeFocus,
   errorMessages,
   actions,
-}: ListFiltersProps) => (
-  <>
-    <Box
-      display="grid"
-      gridTemplateColumns={2}
-      gap={4}
-      paddingBottom={2}
-      paddingX={6}
-    >
-      <Box display="flex" alignItems="center" gap={4}>
-        <ExpressionFilters />
-        <FiltersSelect
-          errorMessages={errorMessages}
-          menu={filterStructure}
-          currencySymbol={currencySymbol}
-          onFilterAdd={onFilterChange}
-          onFilterAttributeFocus={onFilterAttributeFocus}
-        />
+}: ListFiltersProps) => {
+  const isProductPage = window.location.pathname.includes("/products");
+  const productListingPageFiltersFlag = useFlag("product_filters");
+  const filtersEnabled = isProductPage && productListingPageFiltersFlag.enabled;
 
-        <Box __width="320px">
-          <SearchInput
-            initialSearch={initialSearch}
-            placeholder={searchPlaceholder}
-            onSearchChange={onSearchChange}
-          />
+  return (
+    <>
+      <Box
+        display="grid"
+        gridTemplateColumns={2}
+        gap={4}
+        paddingBottom={2}
+        paddingX={6}
+      >
+        <Box display="flex" alignItems="center" gap={4}>
+          {filtersEnabled ? (
+            <ExpressionFilters />
+          ) : (
+            <FiltersSelect
+              errorMessages={errorMessages}
+              menu={filterStructure}
+              currencySymbol={currencySymbol}
+              onFilterAdd={onFilterChange}
+              onFilterAttributeFocus={onFilterAttributeFocus}
+            />
+          )}
+          <Box __width="320px">
+            <SearchInput
+              initialSearch={initialSearch}
+              placeholder={searchPlaceholder}
+              onSearchChange={onSearchChange}
+            />
+          </Box>
+        </Box>
+        <Box display="flex" justifyContent="flex-end">
+          {actions}
         </Box>
       </Box>
-      <Box display="flex" justifyContent="flex-end">
-        {actions}
-      </Box>
-    </Box>
-  </>
-);
+    </>
+  );
+};
 ListFilters.displayName = "FilterBar";
