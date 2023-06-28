029745ffe2888ad050c2a52f45c5fbb8ecc5fdc2
Paweł Chyła <pawel.chyla@saleor.io>
2023-06-28 09:35:03 +0200

    getAttributeValueFields utils

---
+++ src/attributes/components/AttributeValueEditDialog/utils.ts
@@ -0,0 +1,19 @@
+import { AttributeValueEditDialogFormData } from "@dashboard/attributes/utils/data";
+
+export const getAttributeValueFields = (
+  attributeValue: AttributeValueEditDialogFormData | null,
+  isSwatch: boolean,
+): Partial<Omit<AttributeValueEditDialogFormData, "name">> => {
+  if (attributeValue?.fileUrl) {
+    return {
+      fileUrl: attributeValue?.fileUrl,
+      contentType: attributeValue?.contentType,
+    };
+  }
+
+  if (attributeValue?.value && isSwatch) {
+    return { value: attributeValue?.value ?? "" };
+  }
+
+  return {};
+};
