e60c35578ba5955d8d48bf02b80b0458e3f8d825
wojteknowacki <wojciech.nowacki@saleor.io>
2023-07-21 15:52:16 +0200

    test for updating presets

--- cypress/e2e/products/productsList/productPresets.js
+++ cypress/e2e/products/productsList/productPresets.js
@@ -2,10 +2,14 @@
 /// <reference types="../../../support"/>
 
 import { PRODUCTS_LIST } from "../../../elements/catalog/products/products-list";
-import { urlList } from "../../../fixtures/urlList";
+import { SEARCH } from "../../../elements/shared";
+import { LOCAL_STORAGE_KEYS, urlList } from "../../../fixtures/";
 import { ensureCanvasStatic } from "../../../support/customCommands/sharedElementsOperations/canvas";
 import {
   addPresetWithName,
+  clickSavedPresetContain,
+  clickShowSavedPresetsButton,
+  clickUpdatePresetButton,
   confirmActivePresetName,
   confirmGridRowsContainsText,
   searchItems,
@@ -14,13 +18,14 @@ import {
 describe("As a user I should be able to save selected filters with search queries under the given name", () => {
   beforeEach(() => {
     cy.clearSessionData().loginUserViaRequest();
-    cy.visit(urlList.products);
   });
 
   it(
     "should be able to add preset. TC: SALEOR_3802",
     { tags: ["@productsList", "@allEnv", "@stable"] },
     () => {
+      cy.visit(urlList.products);
+
       ensureCanvasStatic(PRODUCTS_LIST.dataGridTable).then(() => {
         cy.assertCanvasRowsNumber(PRODUCTS_LIST.dataGridTable, 21);
       });
@@ -31,6 +36,31 @@ describe("As a user I should be able to save selected filters with search querie
       ensureCanvasStatic(PRODUCTS_LIST.dataGridTable).then(() => {
         confirmGridRowsContainsText(presetName);
         confirmActivePresetName(presetName);
+      });
+    },
+  );
+  it(
+    "should be able to update preset. TC: SALEOR_2713",
+    { tags: ["@productsList", "@allEnv", "@stable"] },
+    () => {
+      const searchQuery = "bean";
+      // space is needed since we add second part of query into input
+      const updatedSearchQuery = " juice";
+      window.localStorage.setItem(
+        LOCAL_STORAGE_KEYS.keys.productPresets,
+        `[{"data":"query=${searchQuery}","name":"${searchQuery}"}]`,
+      );
+      cy.visit(urlList.products);
+      ensureCanvasStatic(PRODUCTS_LIST.dataGridTable);
+      clickShowSavedPresetsButton();
+      clickSavedPresetContain(searchQuery);
+      cy.get(SEARCH.searchInput).click().type(updatedSearchQuery);
+      ensureCanvasStatic(PRODUCTS_LIST.dataGridTable);
+      clickUpdatePresetButton();
+      ensureCanvasStatic(PRODUCTS_LIST.dataGridTable).then(() => {
+        expect(
+          localStorage.getItem(LOCAL_STORAGE_KEYS.keys.productPresets),
+        ).to.contains(`query=${searchQuery}%20${updatedSearchQuery.trim()}`);
       });
     },
   );
