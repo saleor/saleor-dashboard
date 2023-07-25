553f5b37ec21688430133b10f518300d6155c733
Paweł Chyła <pawel.chyla@saleor.io>
2023-07-25 15:32:09 +0200

    Fix max width border in atagrids

--- src/components/Datagrid/Datagrid.tsx
+++ src/components/Datagrid/Datagrid.tsx
@@ -33,7 +33,6 @@ import React, {
 } from "react";
 import { FormattedMessage } from "react-intl";
 
-import { contentMaxWidth } from "../AppLayout/consts";
 import { CardMenuItem } from "../CardMenu";
 import { ColumnPickerProps } from "../ColumnPicker";
 import { FullScreenContainer } from "./components/FullScreenContainer";
@@ -495,8 +494,6 @@ export const Datagrid: React.FC<DatagridProps> = ({
                   borderTopWidth={1}
                   borderTopStyle="solid"
                   borderColor="neutralPlain"
-                  __maxWidth={contentMaxWidth}
-                  margin="auto"
                 />
                 <DataEditor
                   {...datagridProps}
