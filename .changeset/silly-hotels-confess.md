cef20ad59c5e3c01d8c6a942933d689f9345f805
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2023-08-02 08:16:15 +0200

    chore: add changeset

---
+++ .changeset/silly-hotels-confess.md
@@ -0,0 +1,5 @@
+---
+"saleor-dashboard": patch
+---
+
+Experimental filters: add unit tests and bump UI library
--- src/components/ConditionalFilter/ValueProvider/useUrlValueProvider.ts
+++ src/components/ConditionalFilter/ValueProvider/useUrlValueProvider.ts
@@ -37,7 +37,6 @@ export const useUrlValueProvider = (
   params.delete("sort");
   params.delete("activeTab");
 
-  console.log("params", params.toString(), locationSearch);
   const tokenizedUrl = new TokenArray(params.toString());
   const fetchingParams = tokenizedUrl.getFetchingParams();
 
