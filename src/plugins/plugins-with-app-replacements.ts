a25a8db3d02eb8c1a8cf8b8bc86abdeb568c1238
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-08-03 15:53:58 +0200

    Inform about app replacements of plugins in plugins page (#4047)
    * Inform about app replacements of plugins in plugins page
    
    * Change Alert implementation to macaw Box

---
+++ src/plugins/plugins-with-app-replacements.ts
@@ -0,0 +1,20 @@
+/**
+ * Provides a static list of plugins IDs that are already re-implemented
+ * as apps and available in the App Store.
+ *
+ * In the future they will be officially deprecated, but now called "with app replacements"
+ *
+ * Ids are part of plugins query
+ * https://docs.saleor.io/docs/3.x/api-reference/miscellaneous/queries/plugins
+ */
+export const getPluginsWithAppReplacementsIds = () => {
+  return [
+    "mirumee.payments.adyen",
+    "mirumee.taxes.avalara",
+    "mirumee.invoicing",
+    "mirumee.notifications.sendgrid_email",
+    "saleor.payments.stripe",
+    "mirumee.payments.stripe",
+    "mirumee.notifications.user_email",
+  ];
+};
