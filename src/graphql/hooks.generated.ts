f9a6160ce0807e9b3e6abdfd943520e2452ea21b
timur <timuric@gmail.com>
2023-08-16 14:23:44 +0200

    Build types

--- src/graphql/hooks.generated.ts
+++ src/graphql/hooks.generated.ts
@@ -2252,7 +2252,7 @@ export const ProductWithChannelListingsFragmentDoc = gql`
     fragment ProductWithChannelListings on Product {
   id
   name
-  thumbnail {
+  thumbnail(size: 1024) {
     url
   }
   productType {
