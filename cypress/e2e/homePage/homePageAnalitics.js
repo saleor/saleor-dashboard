eaa79f21ba16aa84c0bc1ae3e0e3e0371370e815
wojteknowacki <wojciech.nowacki@saleor.io>
2023-08-24 09:14:07 +0200

    wip - adding screenshots

--- cypress/e2e/homePage/homePageAnalitics.js
+++ cypress/e2e/homePage/homePageAnalitics.js
@@ -164,7 +164,7 @@ describe("As an admin I want to see correct information on dashboard home page",
       cy.contains(
         HOMEPAGE_SELECTORS.productsOutOfStock,
         productsOutOfStockRegexp,
-      ).should("be.visible");
+      ).should("not.be.visible");
     },
   );
 });
