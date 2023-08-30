54cec8049e7a173f78d08593a0e67905aa013985
wojteknowacki <wojciech.nowacki@saleor.io>
2023-08-31 00:08:54 +0200

    wip - video and screenshots path fix

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -178,8 +178,8 @@ Cypress.on("test:after:run", (test, runnable) => {
       filename = `${titleToFileName(parent.title)} -- ${filename}`;
       parent = parent.parent;
     }
-    filename += `${titleToFileName(test.title)} (failed).png`;
+    filename += `${titleToFileName(test.title)}(failed).png`;
     addContext({ test }, `mochareports/${Cypress.spec.name}/${filename}`);
   }
-  addContext({ test }, `videos/${Cypress.spec.name}.mp4`);
+  addContext({ test }, `mochareports/videos/${Cypress.spec.name}.mp4`);
 });
