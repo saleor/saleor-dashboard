6e399728d3853e1ea3b4a58e393dce7fff0a7b1c
Paweł Chyła <chyla1988@gmail.com>
2023-08-30 12:15:49 +0200

    Fix assign warehouses in variant page (#4146)

---
+++ src/hooks/useFetchAllWarehouse.ts
@@ -0,0 +1,30 @@
+import {
+  WarehouseListDocument,
+  WarehouseListQuery,
+  WarehouseListQueryVariables,
+} from "@dashboard/graphql";
+import { makeFetchAll } from "@dashboard/hooks/makeFetchAll";
+
+export const useFetchAllWarehouses = makeFetchAll<
+  WarehouseListQuery,
+  WarehouseListQueryVariables
+>(
+  WarehouseListDocument,
+  "warehouses",
+  (previousResult, { fetchMoreResult }) => {
+    if (!fetchMoreResult) {
+      return previousResult;
+    }
+
+    const previousEdges = previousResult?.warehouses?.edges ?? [];
+    const fetchMoreEdges = fetchMoreResult?.warehouses?.edges ?? [];
+
+    if (fetchMoreResult?.warehouses?.edges) {
+      fetchMoreResult.warehouses.edges = [...previousEdges, ...fetchMoreEdges];
+
+      return { ...fetchMoreResult };
+    }
+
+    return previousResult;
+  },
+);
