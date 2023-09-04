9e3775afcecd7b4f7b4b40bf76bf4e909a7dc690
Michalina Graczyk <michalina.graczyk@saleor.io>
2023-09-04 16:31:08 +0200

    Add Plawright POC

---
+++ tests/example.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from "@playwright/test";
+
+test("has title", async ({ page }) => {
+  await page.goto("/dashboard");
+  //await page.click('[href="link:///dashboard/"]');
+  await page.fill('[data-test-id="email"]', process.env.CYPRESS_USER_NAME);
+  await page.fill(
+    '[data-test-id="password"]',
+    process.env.CYPRESS_USER_PASSWORD,
+  );
+  await page.click('[data-test-id="submit"]');
+
+  await expect(page).toHaveTitle("Dashboard | Saleor e-commerce");
+});
