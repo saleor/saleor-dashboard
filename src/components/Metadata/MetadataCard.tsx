a62e78625ab96baffaf56f84b0306341ca3e0439
Paweł Chyła <pawel.chyla@saleor.io>
2023-09-05 13:36:12 +0200

    Wrapp metadata into accordion

--- src/components/Metadata/MetadataCard.tsx
+++ src/components/Metadata/MetadataCard.tsx
@@ -1,25 +1,12 @@
-import TableRowLink from "@dashboard/components/TableRowLink";
 import { MetadataInput } from "@dashboard/graphql";
 import { FormChange } from "@dashboard/hooks/useForm";
-import { Table, TableBody, TableCell, TableHead } from "@material-ui/core";
-import {
-  Box,
-  Button,
-  ChervonDownIcon,
-  ChervonUpIcon,
-  Input,
-  Text,
-  Textarea,
-  TrashBinIcon,
-  vars,
-} from "@saleor/macaw-ui/next";
+import { Accordion, Box, Button, Skeleton, Text } from "@saleor/macaw-ui/next";
 import React from "react";
 import { FormattedMessage, useIntl } from "react-intl";
 
 import { DashboardCard } from "../Card";
-import Skeleton from "../Skeleton";
+import { MetadataCardTable } from "./MetadataCardTable";
 import { EventDataAction } from "./types";
-import { nameInputPrefix, nameSeparator, valueInputPrefix } from "./utils";
 
 export interface MetadataCardProps {
   data: MetadataInput[];
@@ -33,183 +20,91 @@ export const MetadataCard: React.FC<MetadataCardProps> = ({
   onChange,
 }) => {
   const intl = useIntl();
-  const [expanded, setExpanded] = React.useState(false);
+  const title = isPrivate
+    ? {
+        id: "ETHnjq",
+        defaultMessage: "Private Metadata",
+        description: "header",
+      }
+    : {
+        id: "VcI+Zh",
+        defaultMessage: "Metadata",
+        description: "header",
+      };
 
   return (
     <DashboardCard
+      paddingTop={6}
       data-test-id="metadata-editor"
       data-test-is-private={isPrivate}
-      data-test-expanded={expanded}
-      gap={0}
     >
-      <DashboardCard.Title>
-        <Box
-          display="flex"
-          justifyContent="space-between"
-          onClick={() => setExpanded(!expanded)}
-          cursor="pointer"
-        >
-          {isPrivate
-            ? intl.formatMessage({
-                id: "ETHnjq",
-                defaultMessage: "Private Metadata",
-                description: "header",
-              })
-            : intl.formatMessage({
-                id: "VcI+Zh",
-                defaultMessage: "Metadata",
-                description: "header",
-              })}
-          <Button
-            variant="secondary"
-            data-test-id="expand"
-            type="button"
-            icon={expanded ? <ChervonUpIcon /> : <ChervonDownIcon />}
-          />
-        </Box>
-      </DashboardCard.Title>
-      {data === undefined ? (
-        <DashboardCard.Content>
-          <Skeleton />
-        </DashboardCard.Content>
-      ) : (
-        <>
-          {data.length > 0 && (
-            <DashboardCard.Content>
-              <Text variant="caption" color="textNeutralSubdued">
-                <FormattedMessage
-                  id="2+v1wX"
-                  defaultMessage="{number,plural,one{{number} string} other{{number} strings}}"
-                  description="number of metadata fields in model"
-                  values={{
-                    number: data.length,
-                  }}
-                />
-              </Text>
-            </DashboardCard.Content>
-          )}
-          {expanded && (
-            <>
-              {data.length === 0 ? (
-                <DashboardCard.Content>
+      <DashboardCard.Content>
+        <Accordion>
+          <Accordion.Item value="metadata-accordion">
+            <Accordion.Trigger>
+              <Box display="flex" flexDirection="column" gap={2}>
+                <Text variant="heading">{intl.formatMessage(title)}</Text>
+
+                {data?.length > 0 && (
                   <Text variant="caption" color="textNeutralSubdued">
                     <FormattedMessage
-                      id="cY6H2C"
-                      defaultMessage="No metadata created for this element. Use the button below to add new metadata field."
-                      description="empty metadata text"
+                      id="2+v1wX"
+                      defaultMessage="{number,plural,one{{number} string} other{{number} strings}}"
+                      description="number of metadata fields in model"
+                      values={{
+                        number: data.length,
+                      }}
                     />
                   </Text>
-                </DashboardCard.Content>
+                )}
+              </Box>
+
+              <Accordion.TriggerButton />
+            </Accordion.Trigger>
+            <Accordion.Content>
+              {data === undefined ? (
+                <Skeleton />
               ) : (
-                <Table>
-                  <TableHead>
-                    <TableRowLink>
-                      <TableCell style={{ paddingLeft: vars.spacing[6] }}>
-                        <Text variant="caption" color="textNeutralSubdued">
-                          <FormattedMessage
-                            id="nudPsY"
-                            defaultMessage="Field"
-                            description="metadata field name, header"
-                          />
-                        </Text>
-                      </TableCell>
-                      <TableCell style={{ paddingLeft: vars.spacing[8] }}>
-                        <Text variant="caption" color="textNeutralSubdued">
-                          <FormattedMessage
-                            id="LkuDEb"
-                            defaultMessage="Value"
-                            description="metadata field value, header"
-                          />
-                        </Text>
-                      </TableCell>
-                      <TableCell
-                        style={{
-                          textAlign: "end",
-                          paddingRight: vars.spacing[6],
-                        }}
-                      >
-                        <Text variant="caption" color="textNeutralSubdued">
-                          <FormattedMessage
-                            id="nEixpu"
-                            defaultMessage="Actions"
-                            description="table action"
-                          />
-                        </Text>
-                      </TableCell>
-                    </TableRowLink>
-                  </TableHead>
-                  <TableBody>
-                    {data.map((field, fieldIndex) => (
-                      <TableRowLink data-test-id="field" key={fieldIndex}>
-                        <TableCell style={{ paddingLeft: vars.spacing[6] }}>
-                          <Input
-                            width="100%"
-                            size="small"
-                            aria-label={`${nameInputPrefix}${nameSeparator}${fieldIndex}`}
-                            name={`${nameInputPrefix}${nameSeparator}${fieldIndex}`}
-                            onChange={onChange}
-                            value={field.key}
-                          />
-                        </TableCell>
-                        <TableCell>
-                          <Textarea
-                            width="100%"
-                            rows={1}
-                            size="small"
-                            aria-label={`${valueInputPrefix}${nameSeparator}${fieldIndex}`}
-                            name={`${valueInputPrefix}${nameSeparator}${fieldIndex}`}
-                            onChange={onChange}
-                            value={field.value}
-                          />
-                        </TableCell>
-                        <TableCell style={{ paddingRight: vars.spacing[6] }}>
-                          <Box display="flex" justifyContent="flex-end">
-                            <Button
-                              variant="secondary"
-                              data-test-id={"delete-field-" + fieldIndex}
-                              onClick={() =>
-                                onChange({
-                                  target: {
-                                    name: EventDataAction.delete,
-                                    value: fieldIndex,
-                                  },
-                                })
-                              }
-                              type="button"
-                              icon={<TrashBinIcon />}
-                            />
-                          </Box>
-                        </TableCell>
-                      </TableRowLink>
-                    ))}
-                  </TableBody>
-                </Table>
+                <>
+                  {data.length === 0 ? (
+                    <Text variant="caption" color="textNeutralSubdued">
+                      <FormattedMessage
+                        id="cY6H2C"
+                        defaultMessage="No metadata created for this element. Use the button below to add new metadata field."
+                        description="empty metadata text"
+                      />
+                    </Text>
+                  ) : (
+                    <MetadataCardTable data={data} onChange={onChange} />
+                  )}
+
+                  <Button
+                    marginTop={2}
+                    paddingLeft={6}
+                    variant="secondary"
+                    data-test-id="add-field"
+                    type="button"
+                    onClick={() =>
+                      onChange({
+                        target: {
+                          name: EventDataAction.add,
+                          value: null,
+                        },
+                      })
+                    }
+                  >
+                    <FormattedMessage
+                      id="GiDxS4"
+                      defaultMessage="Add Field"
+                      description="add metadata field,button"
+                    />
+                  </Button>
+                </>
               )}
-              <DashboardCard.Content marginTop={2} paddingLeft={6}>
-                <Button
-                  variant="secondary"
-                  data-test-id="add-field"
-                  type="button"
-                  onClick={() =>
-                    onChange({
-                      target: {
-                        name: EventDataAction.add,
-                        value: null,
-                      },
-                    })
-                  }
-                >
-                  <FormattedMessage
-                    id="GiDxS4"
-                    defaultMessage="Add Field"
-                    description="add metadata field,button"
-                  />
-                </Button>
-              </DashboardCard.Content>
-            </>
-          )}
-        </>
-      )}
+            </Accordion.Content>
+          </Accordion.Item>
+        </Accordion>
+      </DashboardCard.Content>
     </DashboardCard>
   );
 };
