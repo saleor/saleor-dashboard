a1b93123c72491dbbf19556b532f82066be9b0e0
Patryk Andrzejewski <vox3r69@gmail.com>
2023-09-07 13:12:48 +0200

    Fix layouts for translation edition pages (#4178)
    * Fix translation pages
    
    * Fix translation pages

--- src/translations/components/TranslationsMenuItemPage/TranslationsMenuItemPage.tsx
+++ src/translations/components/TranslationsMenuItemPage/TranslationsMenuItemPage.tsx
@@ -72,30 +72,32 @@ const TranslationsMenuItemPage: React.FC<TranslationsMenuItemPageProps> = ({
           }
         />
       </TopNav>
-      <TranslationFields
-        activeField={activeField}
-        disabled={disabled}
-        initialState={true}
-        title={intl.formatMessage(commonMessages.generalInformations)}
-        fields={[
-          {
-            displayName: intl.formatMessage({
-              id: "0Vyr8h",
-              defaultMessage: "Name",
-              description: "menu item name",
-            }),
-            name: TranslationInputFieldName.name,
-            translation: data?.translation?.name || null,
-            type: "short" as "short",
-            value: data?.menuItem.name,
-          },
-        ]}
-        saveButtonState={saveButtonState}
-        richTextResetKey={languageCode}
-        onEdit={onEdit}
-        onDiscard={onDiscard}
-        onSubmit={onSubmit}
-      />
+      <DetailPageLayout.Content>
+        <TranslationFields
+          activeField={activeField}
+          disabled={disabled}
+          initialState={true}
+          title={intl.formatMessage(commonMessages.generalInformations)}
+          fields={[
+            {
+              displayName: intl.formatMessage({
+                id: "0Vyr8h",
+                defaultMessage: "Name",
+                description: "menu item name",
+              }),
+              name: TranslationInputFieldName.name,
+              translation: data?.translation?.name || null,
+              type: "short" as "short",
+              value: data?.menuItem.name,
+            },
+          ]}
+          saveButtonState={saveButtonState}
+          richTextResetKey={languageCode}
+          onEdit={onEdit}
+          onDiscard={onDiscard}
+          onSubmit={onSubmit}
+        />
+      </DetailPageLayout.Content>
     </DetailPageLayout>
   );
 };
