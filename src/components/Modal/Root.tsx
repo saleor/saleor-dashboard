129d2057df5d0f8e65085086e73ba28726d3b0f4
Paweł Chyła <chyla1988@gmail.com>
2023-09-15 15:40:42 +0200

    Dashboard modal (#4198)

---
+++ src/components/Modal/Root.tsx
@@ -0,0 +1,6 @@
+import { Modal, ModalRootProps } from "@saleor/macaw-ui/next";
+import React from "react";
+
+export const Root = ({ children, ...rest }: ModalRootProps) => {
+  return <Modal {...rest}>{children}</Modal>;
+};
