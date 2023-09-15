ba2e73bef2920bf19532d15194998be6eb2f397f
Paweł Chyła <pawel.chyla@saleor.io>
2023-09-15 15:52:13 +0200

    Use DashboardModal

--- src/orders/components/OrderMetadataDialog/OrderMetadataDialog.tsx
+++ src/orders/components/OrderMetadataDialog/OrderMetadataDialog.tsx
@@ -1,7 +1,8 @@
 import { Metadata } from "@dashboard/components/Metadata";
+import { DashboardModal } from "@dashboard/components/Modal";
 import { OrderLineFragment } from "@dashboard/graphql";
 import { buttonMessages, commonMessages } from "@dashboard/intl";
-import { Box, Button, Modal, Text } from "@saleor/macaw-ui/next";
+import { Button } from "@saleor/macaw-ui/next";
 import React from "react";
 import { FormattedMessage } from "react-intl";
 
@@ -17,42 +18,34 @@ export const OrderMetadataDialog = ({
   data,
 }: OrderMetadataDialogProps) => {
   return (
-    <Modal open={open} onChange={onClose}>
-      <Modal.Content>
-        <Box
-          backgroundColor="surfaceNeutralPlain"
-          boxShadow="modal"
-          borderRadius={2}
-          __left="50%"
-          __top="50%"
-          position="fixed"
-          __maxHeight="90vh"
-          __width="min(850px, 90vw)"
-          overflowY="auto"
-          __transform="translate(-50%, -50%)"
-          paddingY={4}
-        >
-          <Text paddingX={6} variant="heading" size="large">
-            <FormattedMessage {...commonMessages.metadata} />:{" "}
-            {data?.productName ?? ""}
-          </Text>
+    <DashboardModal open={open} onChange={onClose}>
+      <DashboardModal.Content
+        __maxHeight="90vh"
+        __width="min(850px, 90vw)"
+        overflowY="auto"
+        gap={0}
+        paddingX={0}
+      >
+        <DashboardModal.Title paddingX={6}>
+          <FormattedMessage {...commonMessages.metadata} />:{" "}
+          {data?.productName ?? ""}
+        </DashboardModal.Title>
 
-          <Metadata
-            readonly={true}
-            onChange={() => {}}
-            data={{
-              metadata: data?.variant?.metadata ?? [],
-              privateMetadata: data?.variant?.privateMetadata ?? [],
-            }}
-          />
+        <Metadata
+          readonly={true}
+          onChange={() => {}}
+          data={{
+            metadata: data?.variant?.metadata ?? [],
+            privateMetadata: data?.variant?.privateMetadata ?? [],
+          }}
+        />
 
-          <Box paddingX={6} display="flex" justifyContent="flex-end">
-            <Button data-test-id="back" variant="secondary" onClick={onClose}>
-              <FormattedMessage {...buttonMessages.close} />
-            </Button>
-          </Box>
-        </Box>
-      </Modal.Content>
-    </Modal>
+        <DashboardModal.Actions paddingX={6}>
+          <Button data-test-id="back" variant="secondary" onClick={onClose}>
+            <FormattedMessage {...buttonMessages.close} />
+          </Button>
+        </DashboardModal.Actions>
+      </DashboardModal.Content>
+    </DashboardModal>
   );
 };
