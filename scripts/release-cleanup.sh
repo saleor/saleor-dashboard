66560268240718588b0151a41c5f034b49bb870f
andrzejewsky <vox3r69@gmail.com>
2023-09-21 14:17:45 +0200

    Cleanup

--- scripts/release-cleanup.sh
+++ scripts/release-cleanup.sh
@@ -4,17 +4,9 @@
 
 set -e
 
-npx changeset status --verbose --output=status.json || true
-
-VERSION=$(cat status.json | jq '.releases | .[] | select(.name=="saleor-dashboard" and (.type=="minor" or .type=="major")) | .newVersion' | xargs)
-rm status.json || true
-
 git fetch --all
-git checkout main
 git ls-tree --name-only "$1"~1 .changeset/ | grep .md | xargs git rm --ignore-unmatch --
 
-# If there are minor changes (minor release), prepare a new dev version for main
-if [ ! -z "$VERSION" ]; then
-  NEW_VERSION=$(npx semver $VERSION -i minor)
-  npm version "$NEW_VERSION-dev" --git-tag-version false
-fi
+CURRENT_VERSION=$(gh api repos/saleor/saleor-dashboard/releases/latest | jq .tag_name | xargs)
+NEW_VERSION=$(npx semver $CURRENT_VERSION -i minor)
+npm version "$NEW_VERSION-dev" --git-tag-version false
