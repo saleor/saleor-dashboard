7eeebb9d5fbe86d74cef55ba23d4d06d53c444b9
andrzejewsky <vox3r69@gmail.com>
2023-10-02 15:38:32 +0200

    Fix bunch of errors that are loged in console

---
+++ .changeset/nasty-fishes-sit.md
@@ -0,0 +1,5 @@
+---
+"saleor-dashboard": patch
+---
+
+Remove attributes error, pill reference, editor js error
--- src/components/Pill/Pill.tsx
+++ src/components/Pill/Pill.tsx
@@ -3,7 +3,7 @@ import { getStatusColor } from "@dashboard/misc";
 import { makeStyles, Pill as MacawuiPill, PillProps } from "@saleor/macaw-ui";
 import { useTheme, vars } from "@saleor/macaw-ui/next";
 import clsx from "clsx";
-import React from "react";
+import React, { Ref } from "react";
 
 const useStyles = makeStyles<{
   color: PillProps["color"];
@@ -21,16 +21,24 @@ const useStyles = makeStyles<{
 // Main purpose of this component is to override default Pill component
 // from macaw-ui to add custom styles
 // TODO: migrate to Pill component from new macaw-ui when it will be ready
-export const Pill = ({ color, ...props }: PillProps) => {
-  const { theme: currentTheme } = useTheme();
-  const backgroundColor = getStatusColor(color, currentTheme);
-  const classes = useStyles({
-    color: backgroundColor.startsWith("#")
-      ? backgroundColor
-      : vars.colors.background[backgroundColor],
-  });
+export const Pill = React.forwardRef(
+  ({ color, ...props }: PillProps, ref: Ref<HTMLDivElement>) => {
+    const { theme: currentTheme } = useTheme();
+    const backgroundColor = getStatusColor(color, currentTheme);
+    const classes = useStyles({
+      color: backgroundColor.startsWith("#")
+        ? backgroundColor
+        : vars.colors.background[backgroundColor],
+    });
 
-  return (
-    <MacawuiPill {...props} className={clsx(classes.pill, props.className)} />
-  );
-};
+    return (
+      <MacawuiPill
+        {...props}
+        ref={ref}
+        className={clsx(classes.pill, props.className)}
+      />
+    );
+  },
+);
+
+Pill.displayName = "Pill";
