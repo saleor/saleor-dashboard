8d7438671ea1027f1f2f681ffb6cb7d9554fdade
andrzejewsky <vox3r69@gmail.com>
2023-10-06 12:38:52 +0200

    Change codegen config to TS

---
+++ codegen.ts
@@ -0,0 +1,19 @@
+import { CodegenConfig } from '@graphql-codegen/cli'
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const config: CodegenConfig = {
+  schema: process.env.API_URI,
+  // documents: ['src/**/*.tsx'],
+  generates: {
+    './introspection.json': {
+      plugins: ['introspection']
+    },
+    './schema.graphql': {
+      plugins: ['schema-ast']
+    }
+  }
+}
+ 
+export default config
