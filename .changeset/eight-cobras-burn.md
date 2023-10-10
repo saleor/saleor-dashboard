3ba325565c1c1211f036594dd3eb7b9492f4d1ac
Patryk Andrzejewski <vox3r69@gmail.com>
2023-10-10 12:20:52 +0200

    Change codegen config to TS (#4304)
    * Change codegen config to TS
    
    * Change codegen config to TS
    
    * Remove comment
    
    * Remove old config

---
+++ .changeset/eight-cobras-burn.md
@@ -0,0 +1,5 @@
+---
+"saleor-dashboard": patch
+---
+
+Change gql codegen config to typescript and reflect env vars
---
+++ codegen.ts
@@ -0,0 +1,18 @@
+import { CodegenConfig } from '@graphql-codegen/cli'
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const config: CodegenConfig = {
+  schema: process.env.API_URI,
+  generates: {
+    './introspection.json': {
+      plugins: ['introspection']
+    },
+    './schema.graphql': {
+      plugins: ['schema-ast']
+    }
+  }
+}
+ 
+export default config
