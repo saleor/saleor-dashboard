40bc8f24993a7baa3518f53b6cecb69b4cb1f09a
wojteknowacki <wojciech.nowacki@saleor.io>
2023-10-17 14:26:43 +0200

    Home page critical test migration to playwright

--- playwright/pages/homePage.ts
+++ playwright/pages/homePage.ts
@@ -1,4 +1,4 @@
-import type { Locator, Page } from "@playwright/test";
+import { expect, Locator, Page } from "@playwright/test";
 
 export class HomePage {
   readonly page: Page;
@@ -10,8 +10,13 @@ export class HomePage {
   readonly ordersReadyToFulfill: Locator;
   readonly paymentsWaitingForCapture: Locator;
   readonly productsOutOfStock: Locator;
+  readonly channelSelect: Locator;
+  readonly channelOptions: Locator;
+
   constructor(page: Page) {
     this.page = page;
+    this.channelSelect = page.getByTestId("app-channel-select");
+    this.channelOptions = page.locator("[data-test-id*='select-field-option']");
     this.welcomeMessage = page.getByTestId("home-header");
     this.sales = page.getByTestId("sales-analytics");
     this.orders = page.getByTestId("orders-analytics");
@@ -23,5 +28,26 @@ export class HomePage {
   }
   async goto() {
     await this.page.goto("/");
+  }
+
+  async clickChannelSelectButton() {
+    await this.channelSelect.click();
+  }
+
+  async selectDifferentChannelThanGiven(defaultChannelName: string) {
+    await this.channelOptions
+      .filter({ hasNotText: defaultChannelName })
+      .first()
+      .click();
+  }
+
+  async expectHomePageElementsToBeVisible() {
+    await expect(this.sales).toBeVisible();
+    await expect(this.orders).toBeVisible();
+    await expect(this.activity).toBeVisible();
+    await expect(this.topProducts).toBeVisible();
+    await expect(this.ordersReadyToFulfill).toBeVisible();
+    await expect(this.paymentsWaitingForCapture).toBeVisible();
+    await expect(this.productsOutOfStock).toBeVisible();
   }
 }
