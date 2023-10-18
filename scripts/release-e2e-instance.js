2f0f952871a944a8b3efc1acad8bdc3a0eab0ba7
andrzejewsky <vox3r69@gmail.com>
2023-10-18 17:03:20 +0200

    E2e pool

--- scripts/release-e2e-instance.js
+++ scripts/release-e2e-instance.js
@@ -2,6 +2,7 @@ const util = require("node:util");
 const execFile = util.promisify(require("node:child_process").exec);
 
 async function run() {
-  await execFile(`npx saleor env update ${name} --name=${name}-BUSY`);
+  const name = process.argv[2]
+  await execFile(`npx saleor env update ${name}-BUSY --name=${name}`);
 }
 run();
