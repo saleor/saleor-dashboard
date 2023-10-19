0a6d50066303e74528a48b468a8c9a794c976003
wojteknowacki <wojciech.nowacki@saleor.io>
2023-10-19 09:44:13 +0200

    bump workers to 4 on CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 4 : undefined,
   reporter: "html",
   timeout: 60000,
   // webServer: {
