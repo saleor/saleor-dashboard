35994b8ed9d3a878d529ba12ec2428fae3c635b9
andrzejewsky <vox3r69@gmail.com>
2023-10-31 12:22:44 +0100

    Instances

--- scripts/release-e2e-instance.js
+++
@@ -1,8 +0,0 @@
-const util = require("node:util");
-const exec = util.promisify(require("node:child_process").exec);
-
-async function run() {
-  const name = process.argv[2]
-  await exec(`npx saleor env update ${name}-BUSY --name=${name}`);
-}
-run();
