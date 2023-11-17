771f94d3559a6989667f6ea42b0c957dcfbaf4a1
HigorSantiago <higorsantiago@alu.ufc.br>
2023-11-17 20:12:33 -0300

    4447-refactor-code-smell-force-update

--- src/components/Sidebar/user/FeatureFlagsModal/ToggleButton.tsx
+++ src/components/Sidebar/user/FeatureFlagsModal/ToggleButton.tsx
@@ -1,6 +1,7 @@
 import { Name } from "@dashboard/featureFlags/availableFlags";
 import { Button, Spinner } from "@saleor/macaw-ui-next";
 import React, { useState } from "react";
+import { useHistory, useLocation } from "react-router";
 
 import { usePersistence } from "./usePersistence";
 
@@ -13,11 +14,15 @@ export const ToggleButton = ({ isEnabled, flagSlug }: ToggleButtonProps) => {
   const { toggleFlag } = usePersistence();
   const [loading, setLoading] = useState(false);
 
+  const location = useLocation();
+  const history = useHistory();
+  const currentRoute = location.pathname;
+
   const handleToggleClick = async () => {
     setLoading(true);
     await toggleFlag(flagSlug);
 
-    window.location.reload();
+    history.replace(currentRoute);
   };
 
   if (loading) {
