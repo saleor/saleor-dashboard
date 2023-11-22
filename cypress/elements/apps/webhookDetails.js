284fecdc11a09dd9bba9333176c3746067c114dd
wojteknowacki <wojciech.nowacki@saleor.io>
2023-11-22 12:00:49 +0100

    removed duplicated selector

--- cypress/elements/apps/webhookDetails.js
+++ cypress/elements/apps/webhookDetails.js
@@ -2,6 +2,5 @@ export const WEBHOOK_DETAILS = {
   nameInput: '[name="name"]',
   targetUrlInput: '[name="targetUrl"]',
   webhookObjects: '[data-test-id="webhook-objects-items"]',
-  webhookObjects: '[data-test-id="webhook-objects-items"]',
   webhookEventsCheckboxes: '[data-test-id="events-checkbox"]',
 };
