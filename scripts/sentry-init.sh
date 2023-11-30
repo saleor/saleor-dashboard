b70ee614e2f0481ad92292d2cc8bb6f925ae61ac
andrzejewsky <vox3r69@gmail.com>
2023-11-30 17:15:12 +0100

    Set sentry release

--- scripts/sentry-init.sh
+++
@@ -1,8 +0,0 @@
-if [[ -z "${SENTRY_ORG}" ]]; then
-  echo "Sentry not definied. Skipping initializing..."
-else 
-  VER=$(cat package.json | jq -r .version)
-  COMMIT=$(git rev-parse --short HEAD)
-  SENTRY_RELEASE="$VER-$COMMIT"
-  echo "Sentry release: $SENTRY_RELEASE"
-fi
