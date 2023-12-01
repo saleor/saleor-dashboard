0e8d1c5014a19dccf7f6b4ca0bd93dccade8413e
andrzejewsky <vox3r69@gmail.com>
2023-12-01 11:03:31 +0100

    Sentry scripts

--- scripts/sentry-upload.sh
+++ scripts/sentry-upload.sh
@@ -4,7 +4,7 @@ if [[ -z "${SENTRY_RELEASE}" ]]; then
   echo "Sentry not definied. Skipping uploading..."
 else 
   echo "Sentry processing... ($SENTRY_RELEASE)"
-  npm run sentry:inject
-  npm run sentry:upload
-  npm run sentry:clean
+  # npm run sentry:inject
+  # npm run sentry:upload
+  # npm run sentry:clean
 fi
