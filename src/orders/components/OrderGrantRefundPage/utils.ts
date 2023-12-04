d2952e129977665b9f51fd8f7af2618ce8297958
Michał Droń <droniu@droniu.dev>
2023-12-04 17:57:18 +0100

    Fix refunding fulfilled items (#4483)

--- src/orders/components/OrderGrantRefundPage/utils.ts
+++ src/orders/components/OrderGrantRefundPage/utils.ts
@@ -36,8 +36,8 @@ export const getFulfilmentSubtitle = (
 export const prepareLineData = (lines: Map<string, ReducerOrderLine>): Line[] =>
   Array.from(lines.entries())
     .filter(([_, line]) => line.isDirty)
-    .map(([id, line]) => ({
-      id,
+    .map(([, line]) => ({
+      id: line.orderLineId,
       quantity: line.selectedQuantity,
     }));
 
