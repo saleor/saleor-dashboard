d093ebcc42be84fa2e14e9ca9205c66f1e04fc89
Paweł Chyła <pawel.chyla@saleor.io>
2024-01-02 12:13:55 +0100

    Improve details form submission

---
+++ src/discounts/components/DiscountSavebar/DiscountSavebar.tsx
@@ -0,0 +1,33 @@
+import { ConfirmButtonTransitionState } from "@dashboard/components/ConfirmButton";
+import Savebar from "@dashboard/components/Savebar";
+import React from "react";
+import { useFormContext } from "react-hook-form";
+
+interface DiscountSavebarProps {
+  disabled: boolean;
+  onCancel: () => void;
+  onSubmit: () => void;
+  onDelete: () => void;
+  submitButtonState: ConfirmButtonTransitionState;
+}
+
+export const DiscountSavebar = ({
+  disabled,
+  onSubmit,
+  onDelete,
+  onCancel,
+  submitButtonState,
+}: DiscountSavebarProps) => {
+  const { formState } = useFormContext();
+  const { isDirty } = formState;
+
+  return (
+    <Savebar
+      disabled={disabled}
+      onCancel={onCancel}
+      onSubmit={isDirty ? onSubmit : undefined}
+      onDelete={onDelete}
+      state={submitButtonState}
+    />
+  );
+};
