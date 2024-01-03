730f1a0c775c97541e2d4af6c552fe97bb97e7a7
Michał Droń <droniu@droniu.dev>
2024-01-03 19:58:36 +0100

    Rewrite utils tests to follow AAA

--- src/orders/components/OrderReturnPage/utils.test.ts
+++ src/orders/components/OrderReturnPage/utils.test.ts
@@ -2,37 +2,56 @@ import { getReturnRefundValue } from "./utils";
 
 describe("getReturnRefundValue", () => {
   it("should return empty string if autoGrantRefund is false", () => {
-    const result = getReturnRefundValue({
+    // Arrange
+    const testData = {
       autoGrantRefund: false,
       isAmountDirty: false,
       customRefundValue: undefined,
       amountData: undefined,
-    });
+    };
+
+    // Act
+    const result = getReturnRefundValue(testData);
+
+    // Assert
     expect(result).toEqual("");
   });
 
   it("should return customRefundValue as string if autoGrantRefund is true and isAmountDirty is true", () => {
-    const result = getReturnRefundValue({
+    // Arrange
+    const testData = {
       autoGrantRefund: true,
       isAmountDirty: true,
       customRefundValue: 123,
       amountData: undefined,
-    });
+    };
+
+    // Act
+    const result = getReturnRefundValue(testData);
+
+    // Assert
     expect(result).toEqual("123");
   });
 
   it("should return empty string if autoGrantRefund is true, isAmountDirty is true, but customRefundValue is undefined", () => {
-    const result = getReturnRefundValue({
+    // Arrange
+    const testData = {
       autoGrantRefund: true,
       isAmountDirty: true,
       customRefundValue: undefined,
       amountData: undefined,
-    });
+    };
+
+    // Act
+    const result = getReturnRefundValue(testData);
+
+    // Assert
     expect(result).toEqual("");
   });
 
   it("should return refundTotalAmount as string if autoGrantRefund is true, isAmountDirty is false, and amountData is defined", () => {
-    const result = getReturnRefundValue({
+    // Arrange
+    const testData = {
       autoGrantRefund: true,
       isAmountDirty: false,
       customRefundValue: undefined,
@@ -54,17 +73,28 @@ describe("getReturnRefundValue", () => {
           currency: "USD",
         },
       },
-    });
+    };
+
+    // Act
+    const result = getReturnRefundValue(testData);
+
+    // Assert
     expect(result).toEqual("456.78");
   });
 
   it("should return empty string if autoGrantRefund is true, isAmountDirty is false, but amountData is undefined", () => {
-    const result = getReturnRefundValue({
+    // Arrange
+    const testData = {
       autoGrantRefund: true,
       isAmountDirty: false,
       customRefundValue: undefined,
       amountData: undefined,
-    });
+    };
+
+    // Act
+    const result = getReturnRefundValue(testData);
+
+    // Assert
     expect(result).toEqual("");
   });
 });
