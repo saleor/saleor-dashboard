c09ef4d43e5ce58be4418396af1bd376bb63174e
Paweł Chyła <chyla1988@gmail.com>
2024-01-30 16:05:48 +0100

    Support order conditions in rule summary (#4647)
    * Refactor rule model
    
    * Add changeset
    
    * Add discount type component
    
    * Add type to initial form values
    
    * Bump macaw
    
    * Refactor Add button
    
    * Update test
    
    * Refactor DiscountRule isLoaded
    
    * Add type support
    
    * Remve useeffect
    
    * Add changeset
    
    * Extract messages
    
    * Init context
    
    * Context intro
    
    * Remove empty import
    
    * Fix tests, fix useDiscountRulesContext imports
    
    * Add changeset
    
    * Support mutliple condition types
    
    * Update label map after rule add
    
    * Add sorting
    
    * Extract messages
    
    * Add changeset
    
    * Refactor RuleList
    
    * Refactor rule summary to support order conditions
    
    * Update test
    
    * Add changeset
    
    * Improve useVariantOptions
    
    * Remove unused hooks

---
+++ src/discounts/components/DiscountRules/componenets/RulesList/components/RuleSummary/components/RuleConditionsChips/formatLable.ts
@@ -0,0 +1,42 @@
+import { Locale } from "@dashboard/components/Locale";
+import {
+  formatMoney as formatMoneyUtils,
+  formatMoneyRange as formatMoneyRangeUtils,
+} from "@dashboard/components/Money";
+import { Condition, isTuple } from "@dashboard/discounts/models";
+
+export const formatMoneyRange = (
+  condition: Condition,
+  currencySymbol: string,
+  locale: Locale,
+) => {
+  if (!isTuple(condition.value)) {
+    return "";
+  }
+
+  return formatMoneyRangeUtils(
+    {
+      amount: Number(condition.value[0]),
+      currency: currencySymbol,
+    },
+    {
+      amount: Number(condition.value[1]),
+      currency: currencySymbol,
+    },
+    locale,
+  );
+};
+
+export const formatMoney = (
+  condition: Condition,
+  currencySymbol: string,
+  locale: Locale,
+) => {
+  return formatMoneyUtils(
+    {
+      amount: Number(condition.value),
+      currency: currencySymbol,
+    },
+    locale,
+  );
+};
