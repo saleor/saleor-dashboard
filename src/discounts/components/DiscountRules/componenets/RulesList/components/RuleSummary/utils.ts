7bee47fd0984794cde4a6d187e202862b4b1a294
Paweł Chyła <pawel.chyla@saleor.io>
2024-01-30 13:33:52 +0100

    Refactor rule summary to support order conditions

--- src/discounts/components/DiscountRules/componenets/RulesList/components/RuleSummary/utils.ts
+++ src/discounts/components/DiscountRules/componenets/RulesList/components/RuleSummary/utils.ts
@@ -3,17 +3,18 @@ import {
   hueToPillColorLight,
   stringToHue,
 } from "@dashboard/components/Datagrid/customCells/PillCell";
-import { Condition, isArrayOfOptions, Rule } from "@dashboard/discounts/models";
+import { isArrayOfOptions, Rule } from "@dashboard/discounts/models";
 import { DefaultTheme, Option } from "@saleor/macaw-ui-next";
 
+import { EnrichCondition } from "./components/RuleConditionsChips/useEnrichConditions";
+
 const MAX_ITEMS_TO_SHOW = 3;
-export type OptionWithConditionType = Option & { type: string };
 
 export const splitConditions = (
-  conditions: OptionWithConditionType[],
+  conditions: Option[],
 ): {
-  conditionsInSummary: OptionWithConditionType[];
-  conditionsInTooltip: OptionWithConditionType[];
+  conditionsInSummary: Option[];
+  conditionsInTooltip: Option[];
 } => {
   const conditionsInSummary = conditions.slice(0, MAX_ITEMS_TO_SHOW);
   const conditionsInTooltip = conditions.slice(MAX_ITEMS_TO_SHOW);
@@ -25,16 +26,25 @@ export const splitConditions = (
 };
 
 export const mapConditionToOption = (
-  conditions: Condition[],
-): OptionWithConditionType[] => {
-  return conditions.reduce<OptionWithConditionType[]>((acc, condition) => {
-    if (isArrayOfOptions(condition.value)) {
+  conditions: EnrichCondition[],
+): Option[] => {
+  return conditions.reduce<Option[]>((acc, condition) => {
+    if (
+      isArrayOfOptions(condition.value) &&
+      condition.inputType === "multiselect"
+    ) {
+      // Flat each condition array of Options
       acc.push(
-        ...condition.value.map<OptionWithConditionType>(value => ({
-          ...value,
-          type: condition.id ?? "",
+        ...condition.value.map(conditionValue => ({
+          value: conditionValue.label,
+          label: condition.label ?? condition.id ?? "",
         })),
       );
+    } else {
+      acc.push({
+        label: condition.label ?? condition.id ?? "",
+        value: condition.value?.toString() ?? condition.id ?? "",
+      });
     }
 
     return acc;
@@ -51,6 +61,6 @@ export const conditionTypeToHue = (type: string, theme: DefaultTheme) => {
 export const hasNoRuleConditions = (rule: Rule) => {
   return (
     !rule.conditions.length ||
-    rule.conditions.every(condition => !condition.value?.length)
+    rule.conditions.every(condition => !condition?.value?.length)
   );
 };
