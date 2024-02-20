f8266ad175e305c69ff64bc4bf2adc47aa033180
karola312 <rakoczy.karolina@gmail.com>
2024-02-20 12:06:21 +0100

    Fix faker

--- playwright/tests/customers.spec.ts
+++ playwright/tests/customers.spec.ts
@@ -6,20 +6,16 @@ import { CUSTOMERS, CUSTOMER_ADDRESS } from "@data/e2eTestData";
 
 test.use({ storageState: "playwright/.auth/admin.json" });
 let customersPage: CustomersPage;
-let firstName: string;
-let lastName: string;
-let email: string;
-let note: string;
 
 test.beforeEach(({ page }) => {
   customersPage = new CustomersPage(page);
-  firstName = faker.name.firstName();
-  lastName = faker.name.lastName();
-  email = faker.internet.email();
-  note = faker.lorem.sentence();
 });
 
 test("TC: SALEOR_119 Create customer @e2e @customer", async () => {
+  const firstName = faker.name.firstName();
+  const lastName = faker.name.lastName();
+  const email = faker.internet.email();
+  const note = faker.lorem.sentence();
   await customersPage.goToCustomersListView();
   await customersPage.clickOnCreateCustomer()
   await customersPage.fillName(firstName, lastName)
@@ -36,6 +32,10 @@ test("TC: SALEOR_120 Delete customer @e2e @customer", async () => {
   await customersPage.successBanner.waitFor({ state: "visible" });
 });
 test("TC: SALEOR_121 Update customer account info @e2e @customer", async () => {
+  const firstName = faker.name.firstName();
+  const lastName = faker.name.lastName();
+  const email = faker.internet.email();
+  const note = faker.lorem.sentence();
   await customersPage.gotoCustomerDetailsPage(CUSTOMERS.editCustomer.id);
   await customersPage.fillNote(note)
   await customersPage.fillName(firstName, lastName)
