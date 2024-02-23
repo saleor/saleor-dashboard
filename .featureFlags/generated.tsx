da65fa9449d9169943cf72d25fc9ce813a740f4d
Michał Droń <droniu@droniu.dev>
2024-02-23 12:10:09 +0100

    Fix flag

--- .featureFlags/generated.tsx
+++ .featureFlags/generated.tsx
@@ -1,18 +1,16 @@
 // @ts-nocheck
 
-import G71535 from "./images/discounts-list.png"
-import W10796 from "./images/filters.png"
-import Y13160 from "./images/filters.png"
+import E60240 from "./images/discounts-list.png"
+import T97919 from "./images/filters.png"
 
-const discounts_rules = () => (<><p><img src={G71535} alt="Discount rules"/></p>
+const discounts_rules = () => (<><p><img src={E60240} alt="Discount rules"/></p>
 <p>Apply the new discounts rules to narrow your promotions audience.
 Set up conditions and channels that must be fulfilled to apply defined reward.</p>
 </>)
-const improved_refunds = () => (<><p><img src={W10796} alt="new filters"/></p>
-<p>Experience new refund flow supporting multiple transactions.</p>
+const improved_refunds = () => (<><p>Experience new refund flow supporting multiple transactions.</p>
 
 </>)
-const product_filters = () => (<><p><img src={Y13160} alt="new filters"/></p>
+const product_filters = () => (<><p><img src={T97919} alt="new filters"/></p>
 <p>Experience the new look and enhanced abilities of new fitering mechanism.
 Easily combine any criteria you want, and quickly browse their values.</p>
 </>)
--- .featureFlags/improved-refunds.md
+++ .featureFlags/improved-refunds.md
@@ -6,8 +6,6 @@ payload: "default"
 visible: false
 ---
 
-![new filters](./images/filters.png)
-
 Experience new refund flow supporting multiple transactions.
 
-<!-- TODO: replace with actual image later on -->
+<!-- TODO: add image later on -->
