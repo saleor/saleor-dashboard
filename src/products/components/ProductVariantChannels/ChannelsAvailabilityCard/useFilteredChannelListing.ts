c1237bd10e43f5aafe20c8e2099f8b2b23c78253
Paweł Chyła <pawel.chyla@saleor.io>
2024-03-11 11:30:40 +0100

    Fix undefined channel listing array

---
+++ src/products/components/ProductVariantChannels/ChannelsAvailabilityCard/useFilteredChannelListing.ts
@@ -0,0 +1,23 @@
+interface ChannelListing {
+  channel: {
+    id: string;
+  };
+}
+
+export const useFilteredChannelListing = ({
+  channelListing,
+  allAvailableListings,
+}: {
+  allAvailableListings: Array<{ id: string }>;
+  channelListing: ChannelListing[] | null | undefined;
+}) => {
+  if (!channelListing) {
+    return [];
+  }
+
+  const listingIds = allAvailableListings.map(lst => lst.id);
+
+  return channelListing.filter(channel =>
+    listingIds.includes(channel.channel.id),
+  );
+};
