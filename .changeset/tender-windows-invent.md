f77fe1712b94515212cf3c3f4feca372484596cd
Paweł Chyła <chyla1988@gmail.com>
2024-03-11 09:57:07 +0100

    Prevent warehouses fetch when no channels in variant details (#4712)
    * Prevent warehouses fetch when no channels
    
    * Add changeset
    
    * useAllwarehouses hook with tests

---
+++ .changeset/tender-windows-invent.md
@@ -0,0 +1,5 @@
+---
+"saleor-dashboard": patch
+---
+
+Prevent unnecessary warehouses fetch in product variant details when variant has not channel assigned
--- src/products/views/ProductVariant/ProductVariant.tsx
+++ src/products/views/ProductVariant/ProductVariant.tsx
@@ -29,7 +29,6 @@ import {
   useVariantMediaUnassignMutation,
   useVariantUpdateMutation,
 } from "@dashboard/graphql";
-import { useFetchAllWarehouses } from "@dashboard/hooks/useFetchAllWarehouse";
 import useNavigator from "@dashboard/hooks/useNavigator";
 import useNotifier from "@dashboard/hooks/useNotifier";
 import useOnSetDefaultVariant from "@dashboard/hooks/useOnSetDefaultVariant";
@@ -59,6 +58,7 @@ import {
 } from "../../urls";
 import { mapFormsetStockToStockInput } from "../../utils/data";
 import { createVariantReorderHandler } from "./../ProductUpdate/handlers";
+import { useAllWarehouses } from "./useAllWarehouses";
 import { useSubmitChannels } from "./useSubmitChannels";
 
 interface ProductUpdateProps {
@@ -141,15 +141,7 @@ export const ProductVariant: React.FC<ProductUpdateProps> = ({
   const variant = data?.productVariant;
   const channels = createVariantChannels(variant);
 
-  const warehouses = useFetchAllWarehouses({
-    displayLoader: true,
-    variables: {
-      first: 100,
-      filter: {
-        channels: channels.map(channel => channel.id),
-      },
-    },
-  });
+  const warehouses = useAllWarehouses(channels);
 
   const [deactivatePreorder, deactivatePreoderOpts] =
     useProductVariantPreorderDeactivateMutation({});
@@ -324,7 +316,7 @@ export const ProductVariant: React.FC<ProductUpdateProps> = ({
         placeholderImage={placeholderImg}
         variant={variant}
         header={variant?.name || variant?.sku}
-        warehouses={mapEdgesToItems(warehouses?.data?.warehouses) || []}
+        warehouses={warehouses}
         onDelete={() => openModal("remove")}
         onSubmit={handleSubmit}
         onWarehouseConfigure={() => navigate(warehouseAddPath)}
