4ab8d5683e12c29f1d2ae4162b55b878b9af391b
Paweł Chyła <chyla1988@gmail.com>
2024-03-11 13:17:13 +0100

    Fix: Dashboard crashing when entering variants (#4715)
    * Fix undefined channel listing array
    
    * Add changeset

---
+++ src/products/components/ProductVariantChannels/ChannelsAvailabilityCard/useFilteredChannelListing.test.ts
@@ -0,0 +1,40 @@
+import { useFilteredChannelListing } from "./useFilteredChannelListing";
+
+describe("useFilteredChannelListing", () => {
+  it("should return empty array if channelListing is null or undefined", () => {
+    // Arrange
+    const allAvailableListings = [{ id: "1" }, { id: "2" }];
+    const channelListing = null;
+
+    // Act
+    const result = useFilteredChannelListing({
+      allAvailableListings,
+      channelListing,
+    });
+
+    // Assert
+    expect(result).toEqual([]);
+  });
+
+  it("should return array with filtered channel listings", () => {
+    // Arrange
+    const allAvailableListings = [{ id: "1" }, { id: "2" }, { id: "3" }];
+    const channelListing = [
+      { channel: { id: "1" } },
+      { channel: { id: "3" } },
+      { channel: { id: "4" } },
+    ];
+
+    // Act
+    const result = useFilteredChannelListing({
+      allAvailableListings,
+      channelListing,
+    });
+
+    // Assert
+    expect(result).toEqual([
+      { channel: { id: "1" } },
+      { channel: { id: "3" } },
+    ]);
+  });
+});
