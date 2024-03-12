563b86557ba5417e0a62adb09b665da9afe3422e
Anna Szczęch <30683248+szczecha@users.noreply.github.com>
2024-03-12 08:56:47 +0000

    [E2E][TEST] Add tests for thirdparty apps (#4708)
    * changeset
    
    * add tests for installing and removing app
    
    * add test ids

---
+++ playwright/pages/appInstallationPage.ts
@@ -0,0 +1,13 @@
+import type { Page } from "@playwright/test";
+
+export class AppInstallationPage {
+    readonly page: Page;
+
+    constructor(
+        page: Page,
+        readonly appInstallationPageHeader = page.getByTestId("app-installation-page-header"),
+        readonly installAppButton = page.getByTestId("install-app-button"),
+    ) {
+        this.page = page;
+    }
+}
