04c879f18ec4b29941b1661ed520f941a21d37e3
yellowee <renata.gajzlerowicz@gmail.com>
2024-03-18 13:49:37 +0100

    applying requested changes - storageState()

--- playwright/api/basics.ts
+++ playwright/api/basics.ts
@@ -1,59 +1,55 @@
 import { APIRequestContext } from "@playwright/test";
 
-const URL = process.env.API_URI || "";
-interface Data {
-  query: string;
+interface User {
+    email: string;
+    password: string;
 }
 
-interface User {
-  email: string;
-  password: string;
-}
 interface TokenCreateResponse {
-  tokenCreate: {
-    token: string;
-    refreshToken: string;
-    errors: [
-      {
-        message: string;
-        code: string;
-      },
-    ];
-    user: {
-      id: string;
+    tokenCreate: {
+        token: string;
+        refreshToken: string;
+        errors: [
+            {
+                message: string;
+                code: string;
+            },
+        ];
+        user: {
+            id: string;
+        };
     };
-  };
 }
 
 interface ApiResponse<T> {
-  data: T;
+    data: T;
 }
 
 export class BasicApiService {
-  readonly request: APIRequestContext;
+    readonly request: APIRequestContext;
 
-  constructor(request: APIRequestContext) {
-    this.request = request;
-  }
-  async logInUserViaApi(user: User) {
-    const query = `mutation TokenAuth{
-        tokenCreate(email: "${user.email}", password: "${user.password}") {
-          token
-          refreshToken
-          errors: errors {
-            code
-            message
-          }
-          user {
-            id
-          }
-        }
-      }`;
+    constructor(request: APIRequestContext) {
+        this.request = request;
+    }
 
+    async logInUserViaApi(user: User): Promise<ApiResponse<TokenCreateResponse>> {
+        const query = `mutation TokenAuth{
+            tokenCreate(email: "${user.email}", password: "${user.password}") {
+                token
+                refreshToken
+                errors: errors {
+                    code
+                    message
+                }
+                user {
+                    id
+                }
+            }
+        }`;
 
-    const loginResponse = await this.request.post(URL, { data: {query} });
-    const loginResponseJson = await loginResponse.json();
-    
-    return loginResponseJson as ApiResponse<TokenCreateResponse>;
-  }
+        const loginResponse = await this.request.post(process.env.API_URI || "", { data: { query } });
+        const loginResponseJson = await loginResponse.json();
+
+        return loginResponseJson as ApiResponse<TokenCreateResponse>;
+    }
 }
--- playwright/tests/auth.setup.ts
+++ playwright/tests/auth.setup.ts
@@ -1,94 +1,42 @@
-import { test as setup } from "@playwright/test";
+import { APIRequestContext, test as setup } from "@playwright/test";
 import { BasicApiService } from "@api/basics";
 import fs from 'fs';
 import path from 'path';
-import { UserPermissionType, USER_PERMISSION, permissions } from "@data/userPermissions";
+import { USER_PERMISSION, UserPermissionType, permissions } from "@data/userPermissions";
 
-
-const getTemplate = (refreshToken: string) =>  {
-  const currentDate = new Date();
-currentDate.setFullYear(currentDate.getFullYear() + 1);
- const expires = Math.floor(currentDate.getTime() / 1000);
-
-  return {
-  "cookies": [
-    {
-      "name": "refreshToken",
-      "value": refreshToken,
-      "domain": process.env.API_URI!,
-      "path": "/",
-      expires,
-      "httpOnly": true,
-      "secure": true,
-      "sameSite": "None"
-    }
-  ],
-  "origins": [
-    {
-      "origin": process.env.BASE_URL!,
-      "localStorage": [
-        {
-          "name": "requestedExternalPluginId",
-          "value": "null"
-        },
-        {
-          "name": "notifiedAboutNavigator",
-          "value": "true"
-        },
-        {
-          "name": "_saleorRefreshToken",
-          "value": refreshToken
-        },
-        {
-          "name": "channel",
-          "value": "Q2hhbm5lbDoyMjQz"
-        },
-        {
-          "name": "externalLoginFallbackUri",
-          "value": "null"
-        },
-        {
-          "name": "macaw-ui-theme",
-          "value": "light"
-        }
-      ]
-    }
-  ]
-}}
-
-setup("Authenticate as admin via API", async ({ request }) => {
+const authenticateAndSaveState = async (request: APIRequestContext, email: string, password: string, filePath: string) => {
     const basicApiService = new BasicApiService(request);
+    await basicApiService.logInUserViaApi({ email, password });
 
-    const response = await basicApiService.logInUserViaApi({
-        email: process.env.E2E_USER_NAME!,
-        password: process.env.E2E_USER_PASSWORD!
+    const loginJsonInfo = await request.storageState();
+
+    loginJsonInfo.origins.push({
+        origin: process.env.BASE_URL!,
+        localStorage: [
+            {
+                name: "_saleorRefreshToken",
+                value: loginJsonInfo.cookies[0].value,
+            }
+        ]
     });
-
+    fs.writeFileSync(filePath, JSON.stringify(loginJsonInfo, null, 2));
+};
+const authSetup = async ( request: APIRequestContext,email: string, password: string, fileName: string) => {
     const tempDir = path.join(__dirname, '../.auth');
     if (!fs.existsSync(tempDir)) {
         fs.mkdirSync(tempDir, { recursive: true });
     }
-    const tempFilePath = path.join(tempDir, 'admin.json');
-
-    fs.writeFileSync(tempFilePath, JSON.stringify(getTemplate(response.data.tokenCreate.refreshToken), null, 2));
+    const tempFilePath = path.join(tempDir, fileName);
+    await authenticateAndSaveState(request, email, password, tempFilePath);
+};
+setup("Authenticate as admin via API", async ({ request }) => {
+    await authSetup(request, process.env.E2E_USER_NAME!, process.env.E2E_USER_PASSWORD!, 'admin.json');
 });
 
 const user: UserPermissionType = USER_PERMISSION;
 const password: string = process.env.E2E_PERMISSIONS_USERS_PASSWORD!;
 for (const permission of permissions) {
-setup(`Authenticate as ${permission} user via API`, async ({ request }) => {
-    const basicApiService = new BasicApiService(request);
-
-    const response = await basicApiService.logInUserViaApi({
-        email: user[permission],
-        password: password
+    setup(`Authenticate as ${permission} user via API`, async ({ request }) => {
+        await authSetup(request, user[permission], password, `${permission}.json`);
     });
-
-    const tempDir = path.join(__dirname, '../.auth');
-    if (!fs.existsSync(tempDir)) {
-        fs.mkdirSync(tempDir, { recursive: true });
-    }
-    const tempFilePath = path.join(tempDir, `${permission}.json`);
-
-    fs.writeFileSync(tempFilePath, JSON.stringify(getTemplate(response.data.tokenCreate.refreshToken), null, 2));
-})}
+}
