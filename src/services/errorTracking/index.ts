9cdb1f68187fec3037d2085a5ee47e7594f06403
Paweł Chyła <chyla1988@gmail.com>
2024-03-18 13:14:55 +0100

    Add sentry source map tracking and release (#4731)
    * Add sentry source map tracking and release
    
    * Throw error
    
    * Remove old scripts
    
    * Remove throw button
    
    * Add changeset
    
    * Fix sentry in storybook

--- src/services/errorTracking/index.ts
+++ src/services/errorTracking/index.ts
@@ -6,6 +6,7 @@ const errorTracker = ErrorTrackerFactory(
   SentryAdapter({
     dsn: process.env.SENTRY_DSN,
     environment: process.env.ENVIRONMENT,
+    release: process.env.RELEASE_NAME,
   }),
 );
 
