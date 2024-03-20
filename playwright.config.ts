154d51a890654fc0549a1763b7ff333006d6150c
yellowee <renata.gajzlerowicz@gmail.com>
2024-03-20 12:17:50 +0100

    adding condition to not create json file if it does exist

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,35 +8,29 @@ export default defineConfig({
   testDir: "playwright/tests",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  // TODO hardcoded values should be extracted to ENVs
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 2 : 2,
+  workers: process.env.CI ? 4 : 2,
   reporter: process.env.CI ? "blob" : "html",
-  timeout: 60000 ,
-  expect: { timeout: 10000 },
-  // webServer: {
-  //   command: "npm run dev",
-  //   url: "http://localhost:9000/",
-  //   reuseExistingServer: !process.env.CI,
-  // },
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  maxFailures: 5,
+  timeout: process.env.CI ? 45000 : 60000,
   use: {
     baseURL: process.env.BASE_URL,
-    trace: "on-first-retry",
+    trace: process.env.CI ? "on-first-retry" : "off",
     screenshot: "only-on-failure",
     testIdAttribute: "data-test-id",
     video: process.env.CI ? "retain-on-failure" : "off",
+    headless: true,
   },
-
-  /* Configure projects for major browsers */
   projects: [
-    { name: "setup", testMatch: /.*\.setup\.ts/ },
-
     {
-      // if new project added make sure to add dependency as below
+    name: "setup",
+    testMatch: /.*\.setup\.ts/
+    },
+    {
       dependencies: ["setup"],
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
   ],
 });
+
