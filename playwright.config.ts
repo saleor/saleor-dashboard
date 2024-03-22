a1d6ffce5d87bd221c78959f3506fe77e0474cba
yellowee <renata.gajzlerowicz@gmail.com>
2024-03-22 08:09:25 +0100

    updating yml files

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,16 +1,17 @@
 import dotenv from "dotenv";
-
 import { defineConfig, devices } from "@playwright/test";
 
 dotenv.config();
-
+const env = process.env;
+const DEFAULT_RETRIES = '0';
+const DEFAULT_WORKERS = '2';
 export default defineConfig({
   testDir: "playwright/tests",
   fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 2 : 2,
-  reporter: process.env.CI
+  forbidOnly: !!env.CI,
+  retries: parseInt(env.RETRIES || DEFAULT_RETRIES),
+  workers: parseInt(env.WORKERS || DEFAULT_WORKERS),
+    reporter: process.env.CI
     ? [
         ["blob"],
         ["github"],
@@ -28,26 +29,26 @@ export default defineConfig({
         ],
       ]
     : [["html"], ["list"]],
-  timeout: 60000,
   expect: { timeout: 10000 },
+  maxFailures: 5,
+  timeout: env.CI ? 45000 : 60000,
   use: {
-    baseURL: process.env.BASE_URL,
-    trace: process.env.CI ? "on-first-retry" : "off",
+    baseURL: env.BASE_URL || '',
+    trace: env.CI ? "on-first-retry" : "off",
     screenshot: "only-on-failure",
     testIdAttribute: "data-test-id",
-    video: process.env.CI ? "retain-on-failure" : "off",
+    video: env.CI ? "retain-on-failure" : "off",
     headless: true,
   },
   projects: [
     {
-    name: "setup",
-    testMatch: /.*\.setup\.ts/
+      name: "setup",
+      testMatch: /.*\.setup\.ts/
     },
     {
-      dependencies: ["setup"],
       name: "chromium",
+      dependencies: ["setup"],
       use: { ...devices["Desktop Chrome"] },
     },
   ],
 });
-
