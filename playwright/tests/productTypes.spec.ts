5a303b29643f6796ca9bf7a9f583674f3d19418f
Anna Szczech <anna.m.szczech@gmail.com>
2024-04-03 15:50:05 +0200

    add assertions

--- playwright/tests/productTypes.spec.ts
+++ playwright/tests/productTypes.spec.ts
@@ -17,6 +17,7 @@ test("TC: SALEOR_1 Create basic product type @e2e @product-type", async ({
   await productTypePage.makeProductShippableWithWeight();
   await productTypePage.clickSaveButton();
   await productTypePage.expectSuccessBanner();
+  await expect(productTypePage.nameInput).toHaveValue(productTypeName);
 });
 
 test("TC: SALEOR_2 Create gift card product type @e2e @product-type", async ({
@@ -29,24 +30,31 @@ test("TC: SALEOR_2 Create gift card product type @e2e @product-type", async ({
   await productTypePage.selectGiftCardButton();
   await productTypePage.clickSaveButton();
   await productTypePage.expectSuccessBanner();
+  await expect(productTypePage.nameInput).toHaveValue(productTypeName);
 });
 
 test("TC: SALEOR_184 As a admin I can edit product type @e2e @product-type", async ({
   page,
 }) => {
   const productTypePage = new ProductTypePage(page);
+
   const updatedProductTypeName = `updated-e2e-product-type-${faker.datatype.number()}`;
+
   await productTypePage.gotoExistingProductTypePage(PRODUCT_TYPES.productTypeToBeEdited.id);
   await productTypePage.updateProductTypeName(updatedProductTypeName);
   await productTypePage.makeProductShippableWithWeight();
   await productTypePage.clickSaveButton();
   await productTypePage.expectSuccessBanner();
+  await expect(productTypePage.isShippingRequired).toBeChecked();
+  await expect(productTypePage.shippingWeightInput).toHaveValue("10");
+  await expect(productTypePage.nameInput).toHaveValue(updatedProductTypeName);
 });
 
 test("TC: SALEOR_185 As a admin user I can delete product type with assigned products @e2e @product-type", async ({
   page,
 }) => {
   const productTypePage = new ProductTypePage(page);
+
   const productTypeName = PRODUCT_TYPES.productTypeToBeRemoved.name;
 
   await productTypePage.gotoExistingProductTypePage(PRODUCT_TYPES.productTypeToBeRemoved.id);
@@ -62,6 +70,7 @@ test("TC: SALEOR_186 As a admin user I can delete several product types @e2e @pr
   page,
 }) => {
   const productTypePage = new ProductTypePage(page);
+
   const rowsToBeDeleted = PRODUCT_TYPES.productTypesToBeBulkDeleted.ids;
   const productTypeNames = PRODUCT_TYPES.productTypesToBeBulkDeleted.names;
 
@@ -71,6 +80,7 @@ test("TC: SALEOR_186 As a admin user I can delete several product types @e2e @pr
   await productTypePage.clickBulkDeleteButton();
   await productTypePage.deleteProductTypeDialog.clickConfirmDeleteButton();
   await productTypePage.expectSuccessBanner();
+  await productTypePage.productTypeList.waitFor({ state: "visible", timeout: 50000 });
   await expect(productTypePage.productTypeList).not.toContainText(productTypeNames[0]);
   await expect(productTypePage.productTypeList).not.toContainText(productTypeNames[1]);
 });
