4cf6451affc954022da1338813df961b054d088b
Karolina Rakoczy <karolina.rakoczy@saleor.io>
2024-04-05 13:05:38 +0200

    cherry pick 5f1ac2a49227ae3b7dfbce7bf2da5440e4e4f95e

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,8 +11,25 @@ export default defineConfig({
   // TODO hardcoded values should be extracted to ENVs
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 2 : 2,
-  reporter: process.env.CI ? "blob" : "html",
-  timeout: 60000 ,
+  reporter: process.env.CI
+    ? [
+        ["blob"],
+        ["github"],
+        [
+          "playwright-testmo-reporter",
+          {
+            outputFile: "testmo/testmo.xml", // Optional: Output file path. Defaults to 'testmo.xml'.
+            embedBrowserType: true, // Optional: Embed browser type in the XML file. Defaults to false.
+            embedTestSteps: true, // Optional: Embed test steps in the XML file. Defaults to true.
+            testStepCategories: ["hook", "expect", "pw:api", "test.step"], // Optional: Test step categories to include in the XML file. Defaults to ["hook","expect","pw:api","test.step"]. Possible options are "hook", "expect", "pw:api", "test.step".
+            testTitleDepth: 1, // Optional: Test case title depth to report in the XML file. Defaults to 1. Increase this to 2 include suite name. Increase this even further to include the path.
+            attachmentBasePathCallback: () =>
+              process.env.URL_TO_RUN ? process.env.URL_TO_RUN : "",
+          },
+        ],
+      ]
+    : [["html"], ["list"]],
+  timeout: 60000,
   expect: { timeout: 10000 },
   workers: process.env.CI ? 2 : 2,
   reporter: process.env.CI
