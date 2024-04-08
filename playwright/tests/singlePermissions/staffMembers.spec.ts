0d090fd4c1c2682b7f1d613fa298a3ed2520f2b3
Renata <renata.gajzlerowicz@gmail.com>
2024-04-08 11:05:44 +0200

    fixing flaky tests (#4772) (#4778)

--- playwright/tests/singlePermissions/staffMembers.spec.ts
+++ playwright/tests/singlePermissions/staffMembers.spec.ts
@@ -1,5 +1,3 @@
-import { URL_LIST } from "@data/url";
-import { BasePage } from "@pages/basePage";
 import { ConfigurationPage } from "@pages/configurationPage";
 import { MainMenuPage } from "@pages/mainMenuPage";
 import { PermissionGroupsPage } from "@pages/permissionGroupsPage";
@@ -8,33 +6,30 @@ import { expect, test } from "@playwright/test";
 
 test.use({ storageState: "playwright/.auth/staff.json" });
 
-test("TC: SALEOR_19 User should be able to navigate to staff members list page as a staff member using STAFF permission @e2e", async ({
-  page,
-  request,
-}) => {
-  const mainMenuPage = new MainMenuPage(page);
-  const basePage = new BasePage(page);
-  const configurationPage = new ConfigurationPage(page);
-  const staffMembersPage = new StaffMembersPage(page, request);
+let staffMembersPage: StaffMembersPage;
+let mainMenuPage: MainMenuPage;
+let configurationPage: ConfigurationPage;
+let permissionGroupsPage: PermissionGroupsPage;
 
-  await page.goto(URL_LIST.configuration);
+test.beforeEach(async ({ page, request }) => {
+  staffMembersPage = new StaffMembersPage(page, request);
+  mainMenuPage = new MainMenuPage(page);
+  configurationPage = new ConfigurationPage(page);
+  permissionGroupsPage = new PermissionGroupsPage(page);
+  await configurationPage.gotoConfigurationView();
+  await configurationPage.waitForDOMToFullyLoad();
+});
+
+test("TC: SALEOR_19 User should be able to navigate to staff members list page as a staff member using STAFF permission @e2e", async () => {
   await configurationPage.openStaffMembers();
   await expect(staffMembersPage.inviteStaffMembersButton).toBeVisible();
   await mainMenuPage.expectMenuItemsCount(3);
-  await basePage.expectGridToBeAttached();
+  await staffMembersPage.expectGridToBeAttached();
 });
 
-test("TC: SALEOR_20 User should be able to navigate to permission group list page as a staff member using STAFF permission @e2e", async ({
-  page,
-}) => {
-  const mainMenuPage = new MainMenuPage(page);
-  const basePage = new BasePage(page);
-  const configurationPage = new ConfigurationPage(page);
-  const permissionGroupsPage = new PermissionGroupsPage(page);
-
-  await page.goto(URL_LIST.configuration);
+test("TC: SALEOR_20 User should be able to navigate to permission group list page as a staff member using STAFF permission @e2e", async () => {
   await configurationPage.openPermissionGroups();
   await expect(permissionGroupsPage.createPermissionGroupButton).toBeVisible();
   await mainMenuPage.expectMenuItemsCount(3);
-  await basePage.expectGridToBeAttached();
+  await permissionGroupsPage.expectGridToBeAttached();
 });
