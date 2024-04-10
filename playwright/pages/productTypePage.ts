e2e5c716c2dd6371c680aa42dfd15ff712c80989
Anna Szczęch <30683248+szczecha@users.noreply.github.com>
2024-04-10 11:20:27 +0200

    [E2E] Tests for product types (#4757)
    * initial commit with changelog
    
    * add test for update product type
    
    * add test to delete several product types
    
    * add test to remove product type with asigned product
    
    * fix numering and assertion after removing types
    
    * typo
    
    * update test data
    
    * add assertions

--- playwright/pages/productTypePage.ts
+++ playwright/pages/productTypePage.ts
@@ -1,10 +1,12 @@
 import { URL_LIST } from "@data/url";
 import { BasePage } from "@pages/basePage";
+import { DeleteDialog } from "@pages/dialogs/deleteDialog";
 import type { Page } from "@playwright/test";
 
-export class ProductTypePage {
+export class ProductTypePage extends BasePage {
   readonly page: Page;
-  basePage: BasePage;
+  readonly basePage: BasePage;
+  readonly deleteProductTypeDialog: DeleteDialog;
 
   constructor(
     page: Page,
@@ -24,12 +26,22 @@ export class ProductTypePage {
       "variant-selection-checkbox",
     ),
     readonly saveButton = page.getByTestId("button-bar-confirm"),
+    readonly bulkDeleteButton = page.getByTestId("bulk-delete-product-types"),
+    readonly productTypeList = page.getByTestId("product-types-list"),
+    readonly rowCheckbox = page.getByTestId("checkbox"),
   ) {
+    super(page);
     this.page = page;
     this.basePage = new BasePage(page);
+    this.deleteProductTypeDialog = new DeleteDialog(page);
   }
 
   async typeProductTypeName(name: string) {
+    await this.nameInput.fill(name);
+  }
+
+  async updateProductTypeName(name: string) {
+    await this.nameInput.clear();
     await this.nameInput.fill(name);
   }
   async makeProductShippableWithWeight(weight: string = "10") {
@@ -58,5 +70,23 @@ export class ProductTypePage {
 
   async clickCreateProductTypeButton() {
     await this.addProductTypeButton.click();
+  }
+
+  async gotoExistingProductTypePage(productTypeId: string) {
+    const existingProductTypeUrl = URL_LIST.productTypes + productTypeId;
+    await console.log(
+      "Navigating to product type details: " + existingProductTypeUrl,
+    );
+    await this.page.goto(existingProductTypeUrl);
+  }
+  async clickBulkDeleteButton() {
+    await this.bulkDeleteButton.click();
+  }
+
+  async checkProductTypesOnList(listRows: string[]) {
+    for (const row of listRows) {
+      const rowLocator = this.page.getByTestId(`id-${row}`);
+      await rowLocator.locator("input").click();
+    }
   }
 }
