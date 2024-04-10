ac29fee4fb6f1e07cff1b66f98e8631943c60043
Anna Szczęch <30683248+szczecha@users.noreply.github.com>
2024-04-10 16:11:54 +0200

    [E2E] Use dummy app in delete app test (#4789)
    * replace app
    
    * update test data

--- playwright/tests/apps.spec.ts
+++ playwright/tests/apps.spec.ts
@@ -51,11 +51,11 @@ test.skip("TC: SALEOR_119 User should be able to install and configure app from 
 test("TC: SALEOR_120 User should be able to delete thirdparty app @e2e", async () => {
   await appPage.goToExistingAppPage(APPS.appToBeDeleted.id);
   await appPage.pageHeader.waitFor({ state: "visible", timeout: 10000 });
-  await expect(appPage.pageHeader).toContainText("Adyen");
+  await expect(appPage.pageHeader).toContainText("Saleor QA App");
   await appPage.deleteButton.click();
   await appPage.deleteAppDialog.clickDeleteButton();
   await appsPage.expectSuccessBanner();
   await appsPage.waitForDOMToFullyLoad();
   await expect(appsPage.installedAppRow.first()).toBeVisible();
-  await expect(appsPage.appAdyen).not.toBeVisible();
+  await expect(appsPage.appQA).not.toBeVisible();
 });
