288ba56dbba2d362076239c01e058d7b2399a5f7
Karolina Rakoczy <rakoczy.karolina@gmail.com>
2024-04-15 10:35:42 +0200

    Revert "send playwright tests results (#4784)"
    This reverts commit 051cc92f676d4d3fee3e33d9592ee2281208669a.

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,27 +9,9 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 2 : 2,
-  reporter: process.env.CI
-    ? [
-        ["blob"],
-        ["github"],
-        [
-          "playwright-testmo-reporter",
-          {
-            outputFile: "testmo/testmo.xml", // Optional: Output file path. Defaults to 'testmo.xml'.
-            embedBrowserType: true, // Optional: Embed browser type in the XML file. Defaults to false.
-            embedTestSteps: true, // Optional: Embed test steps in the XML file. Defaults to true.
-            testStepCategories: ["hook", "expect", "pw:api", "test.step"], // Optional: Test step categories to include in the XML file. Defaults to ["hook","expect","pw:api","test.step"]. Possible options are "hook", "expect", "pw:api", "test.step".
-            testTitleDepth: 1, // Optional: Test case title depth to report in the XML file. Defaults to 1. Increase this to 2 include suite name. Increase this even further to include the path.
-            attachmentBasePathCallback: () =>
-              process.env.URL_TO_RUN ? process.env.URL_TO_RUN : "",
-          },
-        ],
-      ]
-    : [["html"], ["list"]],
+  workers: process.env.CI ? 2 : undefined,
+  reporter: process.env.CI ? "blob" : "html",
   timeout: 60000,
-  expect: { timeout: 10000 },
   // webServer: {
   //   command: "npm run dev",
   //   url: "http://localhost:9000/",
