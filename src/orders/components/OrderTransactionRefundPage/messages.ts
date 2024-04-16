01b7fd8d834cae84c1f31c332ba17b6f837d1a68
Paweł Chyła <pawel.chyla@saleor.io>
2024-04-16 09:04:28 +0200

    Define messages

---
+++ src/orders/components/OrderTransactionRefundPage/messages.ts
@@ -0,0 +1,12 @@
+import { defineMessages } from "react-intl";
+
+export const refundMessages = defineMessages({
+  qtyTitle: {
+    id: "OK/KE0",
+    defaultMessage: "Select quantities to refund",
+  },
+  transactionTitle: {
+    id: "laHYed",
+    defaultMessage: "Select transaction you want to refund",
+  },
+});
