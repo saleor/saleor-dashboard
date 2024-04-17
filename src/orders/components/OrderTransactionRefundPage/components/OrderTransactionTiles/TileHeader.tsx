ec2787ca6ad66f9b96d8fdc2d21739095f5b391f
Paweł Chyła <pawel.chyla@saleor.io>
2024-04-17 14:37:03 +0200

    Make whole row clickable

--- src/orders/components/OrderTransactionRefundPage/components/OrderTransactionTiles/TileHeader.tsx
+++ src/orders/components/OrderTransactionRefundPage/components/OrderTransactionTiles/TileHeader.tsx
@@ -13,30 +13,35 @@ interface TileHeaderProps {
 export const TileHeader: React.FC<TileHeaderProps> = ({
   transaction,
   isDisabled = false,
-}) => (
-  <RadioGroup.Item
-    id={transaction.id}
-    value={transaction.id}
-    paddingX={4}
-    disabled={isDisabled}
-    width="100%"
-  >
-    <Box display="flex" justifyContent="space-between" width="100%">
-      <Text
-        size={5}
-        fontWeight="medium"
-        padding={4}
-        color={isDisabled ? "defaultDisabled" : "default1"}
-      >
-        {transaction.name === "" ? (
-          <FormattedMessage
-            {...transactionRefundTilesMessages.defaultTransactionName}
-          />
-        ) : (
-          transaction.name
-        )}
-      </Text>
-      {/* TODO: Need to use radix to make whole row clickable */}
-    </Box>
-  </RadioGroup.Item>
-);
+}) => {
+  return (
+    <RadioGroup.Item
+      position="relative"
+      id={transaction.id}
+      value={transaction.id}
+      paddingX={4}
+      disabled={isDisabled}
+      width="100%"
+      display="grid"
+      __gridTemplateColumns="max-content 1fr"
+    >
+      <Box display="flex" justifyContent="space-between">
+        <Text
+          size={5}
+          fontWeight="medium"
+          padding={4}
+          color={isDisabled ? "defaultDisabled" : "default1"}
+        >
+          {transaction.name === "" ? (
+            <FormattedMessage
+              {...transactionRefundTilesMessages.defaultTransactionName}
+            />
+          ) : (
+            transaction.name
+          )}
+        </Text>
+        {/* TODO: Need to use radix to make whole row clickable */}
+      </Box>
+    </RadioGroup.Item>
+  );
+};
