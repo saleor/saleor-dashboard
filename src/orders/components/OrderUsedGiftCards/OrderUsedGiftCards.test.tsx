c957d9c234b2cb4f4f5e9faa4ece7767d73f6299
Paweł Chyła <chyla1988@gmail.com>
2024-04-23 12:12:02 +0200

    Fix show multiple gift cards in order details (#4814)
    * Show multiple order gift cards
    
    * Extract ThemeWrapper
    
    * Write tests
    
    * Rename to OrderUsedGiftCards, add this to order summary
    
    * Add changeset
    
    * Fix for comma
    
    * Update tests

---
+++ src/orders/components/OrderUsedGiftCards/OrderUsedGiftCards.test.tsx
@@ -0,0 +1,75 @@
+import { OrderDetailsFragment } from "@dashboard/graphql";
+import { ThemeWrapper } from "@test/themeWrapper";
+import { render, screen } from "@testing-library/react";
+import React, { ReactNode } from "react";
+import { IntlProvider } from "react-intl";
+import { BrowserRouter } from "react-router-dom";
+
+import { messages } from "./messages";
+import { OrderUsedGiftCards } from "./OrderUsedGiftCards";
+
+const mockGiftCards = [
+  {
+    id: "1",
+    last4CodeChars: "LTD2",
+  },
+  {
+    id: "2",
+    last4CodeChars: "FRTG",
+  },
+] as OrderDetailsFragment["giftCards"];
+
+const Wrapper = ({ children }: { children: ReactNode }) => {
+  return (
+    <BrowserRouter>
+      <ThemeWrapper>
+        <IntlProvider
+          locale="EN"
+          messages={{
+            paidWithGiftCard: messages.usedGiftCard.defaultMessage,
+          }}
+        >
+          {children}
+        </IntlProvider>
+      </ThemeWrapper>
+    </BrowserRouter>
+  );
+};
+
+describe("OrderUsedGiftCards", () => {
+  it("should render single gift card", () => {
+    // Arrange & Act
+    render(<OrderUsedGiftCards giftCards={[mockGiftCards[0]]} />, {
+      wrapper: Wrapper,
+    });
+
+    // Assert
+    expect(screen.getByText(/paid with gift card/i)).toBeInTheDocument();
+    expect(screen.getByRole("link", { name: "LTD2" })).toHaveAttribute(
+      "href",
+      "/gift-cards/1",
+    );
+    expect(screen.getByText(/LTD2/)).toBeInTheDocument();
+  });
+
+  it("should render multipe gift cards", () => {
+    // Arrange & Act
+    render(
+      <OrderUsedGiftCards giftCards={[mockGiftCards[0], mockGiftCards[1]]} />,
+      { wrapper: Wrapper },
+    );
+
+    // Assert
+    expect(screen.getByText(/paid with gift card/i)).toBeInTheDocument();
+    expect(screen.getByText(/LTD2/)).toBeInTheDocument();
+    expect(screen.getByRole("link", { name: "LTD2," })).toHaveAttribute(
+      "href",
+      "/gift-cards/1",
+    );
+    expect(screen.getByText(/FRTG/)).toBeInTheDocument();
+    expect(screen.getByRole("link", { name: "FRTG" })).toHaveAttribute(
+      "href",
+      "/gift-cards/2",
+    );
+  });
+});
---
+++ src/orders/components/OrderUsedGiftCards/messages.ts
@@ -0,0 +1,9 @@
+import { defineMessages } from "react-intl";
+
+export const messages = defineMessages({
+  usedGiftCard: {
+    id: "yivxZJ",
+    defaultMessage: "Paid with Gift Card: ({link})",
+    description: "order payment",
+  },
+});
