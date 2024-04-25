df70e737561135b02a085edd963c78886e69abf2
karola312 <rakoczy.karolina@gmail.com>
2024-04-25 13:16:13 +0200

    Fix script

--- .github/workflows/getUnusedEnvironments.js
+++ .github/workflows/getUnusedEnvironments.js
@@ -1,10 +1,12 @@
 module.exports = () => {
-  const OPEN_PRS = JSON.parse(process.env.OPEN_PRS).map(function (v) {
+  const openPullRequestsInString = process.env.OPEN_PRS;
+  console.log(openPullRequestsInString);
+  const OPEN_PRS = JSON.parse(openPullRequestsInString).map(function (v) {
     return v.toLowerCase();
   });
-  const ENVIRONMENTS_FOR_PR_TESTING = JSON.parse(
+  const ENVIRONMENTS_FOR_PR_TESTING = JSON.parse(JSON.parse(
     process.env.ENVIRONMENTS_FOR_PR_TESTING,
-  );
+  ));
   const prsToRemove = [];
   ENVIRONMENTS_FOR_PR_TESTING.forEach(environment => {
     if (!OPEN_PRS.includes(environment.name)) {
