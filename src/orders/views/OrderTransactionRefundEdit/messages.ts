16b47e2bffcfe2165129529e8f5e8be922ba57b8
Michał Droń <droniu@droniu.dev>
2024-05-06 10:21:55 +0200

    Fix multiple validation messages on refund view (#4818)
    * Handle REQUIRED duplicate error on refund create
    
    * Add custom validation
    
    * Extract messages
    
    * Changeset
    
    * Iterate over array only once
    
    * CR fixes
    
    * Refactor onComplete function
    
    * Extract messages
    
    ---------
    
    Co-authored-by: Wojciech Mista <wojciech.mista@hotmail.com>

---
+++ src/orders/views/OrderTransactionRefundEdit/messages.ts
@@ -0,0 +1,14 @@
+import { defineMessages } from "react-intl";
+
+export const transactionRefundEditMessages = defineMessages({
+  noAmountError: {
+    defaultMessage: "Amount should be greater than 0",
+    id: "FX7ts2",
+    description: "error message",
+  },
+  savedDraft: {
+    defaultMessage: "Saved draft",
+    id: "vqu+23",
+    description: "success notifier message",
+  },
+});
