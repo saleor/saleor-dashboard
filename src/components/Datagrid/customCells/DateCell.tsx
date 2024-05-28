2e1733a54deedd00aa3da88a8704e7b6612b9d96
Paweł Chyła <pawel.chyla@saleor.io>
2024-05-28 15:38:38 +0200

    Improve with all date formats

--- src/components/Datagrid/customCells/DateCell.tsx
+++ src/components/Datagrid/customCells/DateCell.tsx
@@ -15,9 +15,17 @@ interface DateCellProps {
 export type DateCell = CustomCell<DateCellProps>;
 
 export const dateCellRenderer = (locale: Locale): CustomRenderer<DateCell> => {
-  const dateFormater = new Intl.DateTimeFormat(locale, {
-    dateStyle: "short",
-  });
+  const dateFormaters = {
+    full: new Intl.DateTimeFormat(locale, {
+      dateStyle: "full",
+    }),
+    long: new Intl.DateTimeFormat(locale, {
+      dateStyle: "long",
+    }),
+    short: new Intl.DateTimeFormat(locale, {
+      dateStyle: "short",
+    }),
+  };
 
   const timeFromater = new Intl.DateTimeFormat(locale, {
     timeStyle: "short",
@@ -39,17 +47,45 @@ export const dateCellRenderer = (locale: Locale): CustomRenderer<DateCell> => {
         return;
       }
 
-      const dateFormat = dateFormater.format(date);
       const time = timeFromater.format(date);
 
       ctx.textAlign = "left";
 
-      const justifyToRight = false;
+      let justifyToRight = true;
+
+      const fullFormater = dateFormaters.full.format(date);
+      const fullFormaterWidth = ctx.measureText(`${fullFormater} ${time}`).width;
+      const longFormater = dateFormaters.long.format(date);
+      const shortFormater = dateFormaters.short.format(date);
+
+      const cellWidth = rect.width - theme.cellHorizontalPadding * 2;
+      let displayDate: string | undefined = fullFormater;
+
+      if (cellWidth < fullFormaterWidth) {
+        const candidateFormats = [
+          {
+            format: longFormater,
+            width: ctx.measureText(`${longFormater} ${time}`).width,
+          },
+          {
+            format: shortFormater,
+            width: ctx.measureText(`${shortFormater} ${time}`).width,
+          },
+        ];
+
+        displayDate = candidateFormats.find(format => format.width <= cellWidth)?.format;
+
+        if (!displayDate) {
+          displayDate = shortFormater;
+          justifyToRight = false;
+        }
+      }
+
       const bias = getMiddleCenterBias(ctx, theme);
 
       ctx.fillStyle = theme.textDark;
       ctx.fillText(
-        dateFormat,
+        displayDate,
         rect.x + theme.cellHorizontalPadding,
         rect.y + rect.height / 2 + bias,
       );
@@ -60,7 +96,7 @@ export const dateCellRenderer = (locale: Locale): CustomRenderer<DateCell> => {
         time,
         justifyToRight
           ? rect.x + rect.width - theme.cellHorizontalPadding
-          : rect.x + theme.cellHorizontalPadding + ctx.measureText(dateFormat).width + 5,
+          : rect.x + theme.cellHorizontalPadding + ctx.measureText(displayDate).width + 5,
         rect.y + rect.height / 2 + bias,
       );
 
