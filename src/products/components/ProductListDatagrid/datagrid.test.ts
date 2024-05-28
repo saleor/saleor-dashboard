d8814bd284e83093757a8ffcdec859d8028aa5ca
Paweł Chyła <chyla1988@gmail.com>
2024-05-28 13:29:16 +0200

    Fix product list crash when description contains empty object, remove html tags and &nbsp; (#4887)
    * Improve description in product datagrid
    
    * Add changeset
    
    * Fix typo
    
    * Remove comment
    
    * Cut too long description

--- src/products/components/ProductListDatagrid/datagrid.test.ts
+++ src/products/components/ProductListDatagrid/datagrid.test.ts
@@ -15,11 +15,37 @@ describe("getDescriptionValue", () => {
     ).toBe("");
   });
 
-  it("should return empty string when description contains &nbsp", () => {
+  it("should replace all &nbsp; with empty string", () => {
     expect(
       getDescriptionValue(
-        '{"time": 1637142885936, "blocks": [{"data": {"text": "&nbsp;"}, "type": "paragraph"}], "version": "2.20.0"}',
+        '{"time": 1637142885936, "blocks": [{"data": {"text": "&nbsp;&nbsp;&nbsp;&nbsp;description&nbsp;&nbsp;"}, "type": "paragraph"}], "version": "2.20.0"}',
       ),
-    ).toBe("");
+    ).toBe("description");
+  });
+
+  it("should replace all html tags with empty string", () => {
+    expect(
+      getDescriptionValue(
+        '{"time": 1637142885936, "blocks": [{"data": {"text": "<b><a href=http://fooflw.pl>Link</a><i> description</i></b>"}, "type": "paragraph"}], "version": "2.20.0"}',
+      ),
+    ).toBe("Link description");
+  });
+
+  it("should omit blocks with empty text", () => {
+    expect(
+      getDescriptionValue(
+        '{"time": 1634014163888, "blocks": [{"data": {"text": ""}, "type": "heading"},{"data": {"text": ""}, "type": "paragraph"},{"data": {"text": "description"}, "type": "paragraph"}], "version": "2.20.0"}',
+      ),
+    ).toBe("description");
+  });
+
+  it("should cut description when too long", () => {
+    expect(
+      getDescriptionValue(
+        '{"time": 1634014163888, "blocks": [{"data": {"text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus, nisi sed dapibus eleifend, nisl tellus tempor mi, tristique pretium."}, "type": "heading"},{"data": {"text": ""}, "type": "paragraph"},{"data": {"text": "description"}, "type": "paragraph"}], "version": "2.20.0"}',
+      ),
+    ).toBe(
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus, nisi sed dapibus eleifend, nisl ...",
+    );
   });
 });
