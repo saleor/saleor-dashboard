f307e4e8d97c5760d3996be8eb3ec05553256f01
andrzejewsky <vox3r69@gmail.com>
2024-05-28 10:26:28 +0200

    Change API_URI to API_URL

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ export const SW_INTERVAL = parseInt(process.env.SW_INTERVAL ?? "300", 10);
 export const IS_CLOUD_INSTANCE = window.__SALEOR_CONFIG__.IS_CLOUD_INSTANCE === "true";
 
 export const getAppsConfig = () => ({
-  marketplaceApiUri: window.__SALEOR_CONFIG__.APPS_MARKETPLACE_API_URI,
+  marketplaceApiUri: window.__SALEOR_CONFIG__.APPS_MARKETPLACE_API_URL,
   tunnelUrlKeywords: window.__SALEOR_CONFIG__.APPS_TUNNEL_URL_KEYWORDS?.split(";") || [
     ".ngrok.io",
     ".saleor.live",
