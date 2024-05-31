05d75c490d9da7144b616c308f212c22f24c3df1
Paweł Chyła <pawel.chyla@saleor.io>
2024-05-31 12:40:19 +0200

    Refactor DateCell

--- src/components/Datagrid/customCells/DateCell.tsx
+++ src/components/Datagrid/customCells/DateCell.tsx
@@ -4,6 +4,8 @@ import {
   CustomRenderer,
   getMiddleCenterBias,
   GridCellKind,
+  Rectangle,
+  Theme,
 } from "@glideapps/glide-data-grid";
 
 export const numberCellEmptyValue = Symbol("date-cell-empty-value");
@@ -14,8 +16,14 @@ interface DateCellProps {
 
 export type DateCell = CustomCell<DateCellProps>;
 
+type DateFormaters = {
+  full: Intl.DateTimeFormat;
+  long: Intl.DateTimeFormat;
+  short: Intl.DateTimeFormat;
+};
+
 export const dateCellRenderer = (locale: Locale): CustomRenderer<DateCell> => {
-  const dateFormaters = {
+  const dateFormaters: DateFormaters = {
     full: new Intl.DateTimeFormat(locale, {
       dateStyle: "full",
     }),
@@ -47,60 +55,138 @@ export const dateCellRenderer = (locale: Locale): CustomRenderer<DateCell> => {
         return;
       }
 
-      const time = timeFromater.format(date);
+      const cellWidth = getCellWidth(rect, theme);
+      const textY = getCellMiddleY(ctx, rect, theme);
+      const textXStart = getCellX(rect, theme);
 
-      ctx.textAlign = "left";
+      const displayTime = timeFromater.format(date);
 
-      let justifyToRight = true;
+      const { displayDate, justifyToRight } = formatDate({
+        date,
+        formaters: dateFormaters,
+        time: displayTime,
+        ctx,
+        cellWidth,
+      });
 
-      const fullFormater = dateFormaters.full.format(date);
-      const fullFormaterWidth = ctx.measureText(`${fullFormater} ${time}`).width;
-      const longFormater = dateFormaters.long.format(date);
-      const shortFormater = dateFormaters.short.format(date);
+      drawText({
+        ctx,
+        text: displayDate,
+        x: textXStart,
+        y: textY,
+        align: "left",
+        color: theme.textDark,
+      });
 
-      const cellWidth = rect.width - theme.cellHorizontalPadding * 2;
-      let displayDate: string | undefined = fullFormater;
+      const dateTextXEnd = textXStart + ctx.measureText(displayDate).width;
 
-      if (cellWidth < fullFormaterWidth) {
-        const candidateFormats = [
-          {
-            format: longFormater,
-            width: ctx.measureText(`${longFormater} ${time}`).width,
-          },
-          {
-            format: shortFormater,
-            width: ctx.measureText(`${shortFormater} ${time}`).width,
-          },
-        ];
-
-        displayDate = candidateFormats.find(format => format.width <= cellWidth)?.format;
-
-        if (!displayDate) {
-          displayDate = shortFormater;
-          justifyToRight = false;
-        }
-      }
-
-      const bias = getMiddleCenterBias(ctx, theme);
-
-      ctx.fillStyle = theme.textDark;
-      ctx.fillText(
-        displayDate,
-        rect.x + theme.cellHorizontalPadding,
-        rect.y + rect.height / 2 + bias,
-      );
-
-      ctx.fillStyle = theme.textLight;
-      ctx.textAlign = justifyToRight ? "right" : "left";
-      ctx.fillText(
-        time,
-        justifyToRight
-          ? rect.x + rect.width - theme.cellHorizontalPadding
-          : rect.x + theme.cellHorizontalPadding + ctx.measureText(displayDate).width + 5,
-        rect.y + rect.height / 2 + bias,
-      );
+      drawText({
+        ctx,
+        text: displayTime,
+        x: calculateTimeX({ rect, theme, justifyToRight, dateTextXEnd }),
+        y: textY,
+        align: justifyToRight ? "right" : "left",
+        color: theme.textLight,
+      });
 
       return true;
     },
   };
 };
+
+function formatDate({
+  cellWidth,
+  ctx,
+  date,
+  formaters,
+  time,
+}: {
+  date: Date;
+  formaters: DateFormaters;
+  time: string;
+  ctx: CanvasRenderingContext2D;
+  cellWidth: number;
+}): { displayDate: string; justifyToRight: boolean } {
+  const fullFormater = formaters.full.format(date);
+  const longFormater = formaters.long.format(date);
+  const shortFormater = formaters.short.format(date);
+
+  if (cellWidth >= getTextWidth(ctx, fullFormater, time)) {
+    return { displayDate: fullFormater, justifyToRight: true };
+  }
+
+  const candidateFormats = [
+    {
+      format: longFormater,
+      width: getTextWidth(ctx, longFormater, time),
+    },
+    {
+      format: shortFormater,
+      width: getTextWidth(ctx, shortFormater, time),
+    },
+  ];
+
+  const displayFormat = candidateFormats.find(format => format.width <= cellWidth)?.format;
+
+  if (!displayFormat) {
+    return { displayDate: shortFormater, justifyToRight: false };
+  }
+
+  return { displayDate: displayFormat, justifyToRight: true };
+}
+
+function drawText({
+  align,
+  color,
+  ctx,
+  text,
+  x,
+  y,
+}: {
+  ctx: CanvasRenderingContext2D;
+  text: string;
+  x: number;
+  y: number;
+  align: CanvasTextAlign;
+  color: string;
+}) {
+  ctx.fillStyle = color;
+  ctx.textAlign = align;
+  ctx.fillText(text, x, y);
+}
+
+function getCellMiddleY(ctx: CanvasRenderingContext2D, rect: Rectangle, theme: Theme) {
+  const bias = getMiddleCenterBias(ctx, theme);
+
+  return rect.y + rect.height / 2 + bias;
+}
+
+function getCellWidth(rect: Rectangle, theme: Theme) {
+  return rect.width - theme.cellHorizontalPadding * 2;
+}
+
+function getCellX(rect: Rectangle, theme: Theme) {
+  return rect.x + theme.cellHorizontalPadding;
+}
+
+function getTextWidth(ctx: CanvasRenderingContext2D, date: string, time: string) {
+  return ctx.measureText(`${date} ${time}`).width;
+}
+
+function calculateTimeX({
+  rect,
+  justifyToRight,
+  theme,
+  dateTextXEnd,
+}: {
+  rect: Rectangle;
+  theme: Theme;
+  justifyToRight: boolean;
+  dateTextXEnd: number;
+}) {
+  if (justifyToRight) {
+    return rect.x + rect.width - theme.cellHorizontalPadding;
+  }
+
+  return dateTextXEnd + 5;
+}
