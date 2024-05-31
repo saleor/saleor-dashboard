f3e3f9d88b776c534c6d5cd275e88f52f8e0ef13
Paweł Chyła <pawel.chyla@saleor.io>
2024-05-31 12:41:18 +0200

    wrap  function in useRowAnchor into useCallback

--- src/components/Datagrid/hooks/useRowAnchor.ts
+++ src/components/Datagrid/hooks/useRowAnchor.ts
@@ -1,9 +1,11 @@
 import { getAppMountUri } from "@dashboard/config";
 import useDebounce from "@dashboard/hooks/useDebounce";
 import { DataEditorProps, GridMouseEventArgs, Item } from "@glideapps/glide-data-grid";
-import { useRef } from "react";
+import { useCallback, useRef } from "react";
 
 import { preventRowClickOnSelectionCheckbox } from "../utils";
+
+const DEBOUNCE_TIME = 100;
 
 export const useRowAnchor = ({
   getRowAnchorUrl,
@@ -14,38 +16,39 @@ export const useRowAnchor = ({
 }) => {
   const rowAnchorRef = useRef<HTMLAnchorElement | null>(null);
 
-  const setRowAnchorRef = (element: HTMLAnchorElement) => {
+  const setRowAnchorRef = useCallback((element: HTMLAnchorElement) => {
     rowAnchorRef.current = element;
-  };
+  }, []);
 
-  const setSAnchorPosition = (args: GridMouseEventArgs) => {
-    if (args.kind !== "cell" || !rowAnchorRef.current || !getRowAnchorUrl) {
-      return;
-    }
+  const setSAnchorPosition = useCallback(
+    useDebounce((args: GridMouseEventArgs) => {
+      if (args.kind !== "cell" || !rowAnchorRef.current || !getRowAnchorUrl) {
+        return;
+      }
 
-    const href = getRowAnchorUrl(args.location);
+      const href = getRowAnchorUrl(args.location);
 
-    if (!href) {
-      return;
-    }
+      if (!href) {
+        return;
+      }
 
-    if (preventRowClickOnSelectionCheckbox(rowMarkers, args.location[0])) {
-      return;
-    }
+      if (preventRowClickOnSelectionCheckbox(rowMarkers, args.location[0])) {
+        return;
+      }
 
-    rowAnchorRef.current.style.left = `${window.scrollX + args.bounds.x}px`;
-    rowAnchorRef.current.style.width = `${args.bounds.width}px`;
-    rowAnchorRef.current.style.top = `${window.scrollY + args.bounds.y}px`;
-    rowAnchorRef.current.style.height = `${args.bounds.height}px`;
-    rowAnchorRef.current.href = getAppMountUri() + (href.startsWith("/") ? href.slice(1) : href);
-    rowAnchorRef.current.dataset.reactRouterPath = href;
-  };
-
-  const debouncedHandleRowHref = useDebounce(setSAnchorPosition, 500);
+      rowAnchorRef.current.style.left = `${window.scrollX + args.bounds.x}px`;
+      rowAnchorRef.current.style.width = `${args.bounds.width}px`;
+      rowAnchorRef.current.style.top = `${window.scrollY + args.bounds.y}px`;
+      rowAnchorRef.current.style.height = `${args.bounds.height}px`;
+      rowAnchorRef.current.href = getAppMountUri() + (href.startsWith("/") ? href.slice(1) : href);
+      rowAnchorRef.current.dataset.reactRouterPath = href;
+    }, DEBOUNCE_TIME),
+    [getRowAnchorUrl, rowMarkers],
+  );
 
   return {
     rowAnchorRef,
     setRowAnchorRef,
-    setSAnchorPosition: debouncedHandleRowHref,
+    setSAnchorPosition,
   };
 };
