ae9aa634a7738819aa70d8beb9d5aee229487771
Paweł Chyła <chyla1988@gmail.com>
2024-06-07 12:26:31 +0200

    Fix product variant crash when many warehouses by load on scroll (#4932)
    * Fetch warehouses async
    
    * Update create and update page
    
    * Improve loader and button
    
    * useInfityScroll hook
    
    * Center loader
    
    * Improve edge cases in infinity scroll
    
    * Improve useWarehouseSearch variables typing
    
    * Keep warehouses fetch in view component
    
    * Fix infinity loading
    
    * Add changesset
    
    * Fix typos
    
    * Remove not usedd hook useAllWarehouses
    
    * Use event listener insted of expose onScroll funtion
    
    * use scroll  instead
    
    * Back to scroll again Yeah!
    
    * Fix loading more on init

--- src/products/components/ProductStocks/ProductStocks.tsx
+++ src/products/components/ProductStocks/ProductStocks.tsx
@@ -10,26 +10,17 @@ import { renderCollection } from "@dashboard/misc";
 import { getFormErrors, getProductErrorMessage } from "@dashboard/utils/errors";
 import createNonNegativeValueChangeHandler from "@dashboard/utils/handlers/nonNegativeValueChangeHandler";
 import { Table, TableBody, TableCell, TableHead } from "@material-ui/core";
-import {
-  Box,
-  Button,
-  Checkbox,
-  Dropdown,
-  Input,
-  List,
-  Text,
-  TrashBinIcon,
-  vars,
-} from "@saleor/macaw-ui-next";
-import React from "react";
+import { Box, Button, Checkbox, Input, Text, TrashBinIcon, vars } from "@saleor/macaw-ui-next";
+import React, { useMemo } from "react";
 import { FormattedMessage, useIntl } from "react-intl";
 
+import { ProductStocksAssignWarehouses } from "./components/ProductStocksAssignWarehouses";
 import { messages } from "./messages";
 
 export interface ProductStockFormsetData {
   quantityAllocated: number;
 }
-export type ProductStockInput = FormsetAtomicData<ProductStockFormsetData, string>;
+export type ProductStockInput = FormsetAtomicData<ProductStockFormsetData, string, string>;
 export interface ProductStockFormData {
   sku: string;
   trackInventory: boolean;
@@ -49,9 +40,11 @@ export interface ProductStocksProps {
   warehouses: WarehouseFragment[];
   onChange: FormsetChange;
   onFormDataChange: FormChange;
-  onWarehouseStockAdd: (warehouseId: string) => void;
+  onWarehouseStockAdd: (warehouseId: string, warehouseName: string) => void;
   onWarehouseStockDelete: (warehouseId: string) => void;
   onWarehouseConfigure: () => void;
+  fetchMoreWarehouses: () => void;
+  hasMoreWarehouses: boolean;
 }
 
 export const ProductStocks: React.FC<ProductStocksProps> = ({
@@ -60,21 +53,27 @@ export const ProductStocks: React.FC<ProductStocksProps> = ({
   hasVariants,
   errors,
   stocks,
-  warehouses,
   productVariantChannelListings = [],
+  warehouses,
+  hasMoreWarehouses,
   onChange,
   onFormDataChange,
   onWarehouseStockAdd,
   onWarehouseStockDelete,
   onWarehouseConfigure,
+  fetchMoreWarehouses,
 }) => {
   const intl = useIntl();
   const [lastStockRowFocus, setLastStockRowFocus] = React.useState(false);
-  const warehousesToAssign =
-    warehouses?.filter(warehouse => !stocks.some(stock => stock.id === warehouse.id)) || [];
   const formErrors = getFormErrors(["sku"], errors);
-  const handleWarehouseStockAdd = (warehouseId: string) => {
-    onWarehouseStockAdd(warehouseId);
+
+  const stocksIds = useMemo(() => stocks.map(stock => stock.id), [stocks]);
+
+  const warehousesToAssign =
+    warehouses?.filter(warehouse => !stocksIds.includes(warehouse.id)) || [];
+
+  const handleWarehouseStockAdd = (warehouseId: string, warehouseName: string) => {
+    onWarehouseStockAdd(warehouseId, warehouseName);
     setLastStockRowFocus(true);
   };
   const handleStockInputFocus = (input: HTMLDivElement) => {
@@ -225,46 +224,13 @@ export const ProductStocks: React.FC<ProductStocksProps> = ({
 
         {productVariantChannelListings?.length > 0 &&
           warehouses?.length > 0 &&
-          warehousesToAssign.length > 0 && (
-            <Dropdown>
-              <Dropdown.Trigger>
-                <Button
-                  type="button"
-                  variant="secondary"
-                  marginTop={5}
-                  data-test-id="assign-warehouse-button"
-                >
-                  <FormattedMessage {...messages.assignWarehouse} />
-                </Button>
-              </Dropdown.Trigger>
-
-              <Dropdown.Content align="end">
-                <Box>
-                  <List
-                    id="warehouse-list"
-                    padding={2}
-                    borderRadius={4}
-                    boxShadow="defaultOverlay"
-                    backgroundColor="default1"
-                    __maxHeight={400}
-                    overflowY="auto"
-                  >
-                    {warehousesToAssign.map(warehouse => (
-                      <Dropdown.Item key={warehouse.id}>
-                        <List.Item
-                          paddingX={1.5}
-                          paddingY={2}
-                          borderRadius={4}
-                          onClick={() => handleWarehouseStockAdd(warehouse.id)}
-                        >
-                          <Text>{warehouse.name}</Text>
-                        </List.Item>
-                      </Dropdown.Item>
-                    ))}
-                  </List>
-                </Box>
-              </Dropdown.Content>
-            </Dropdown>
+          (warehousesToAssign.length > 0 || hasMoreWarehouses) && (
+            <ProductStocksAssignWarehouses
+              warehousesToAssign={warehousesToAssign}
+              hasMoreWarehouses={hasMoreWarehouses}
+              loadMoreWarehouses={fetchMoreWarehouses}
+              onWarehouseSelect={handleWarehouseStockAdd}
+            />
           )}
       </DashboardCard.Content>
     </DashboardCard>
