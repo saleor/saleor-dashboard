9d4263ab7a62c9385035d7a5b89fc8230cf8e3e1
Wojciech <wojciech.mista@hotmail.com>
2024-06-21 11:57:33 +0200

    keep one playground version

--- src/components/DevModePanel/useDevModeKeyTrigger.ts
+++ src/components/DevModePanel/useDevModeKeyTrigger.ts
@@ -5,9 +5,7 @@ type DevModeKeyTriggerCallback = ({ shift }: { shift: boolean }) => void;
 export const useDevModeKeyTrigger = (callbackHandler: DevModeKeyTriggerCallback) => {
   useEffect(() => {
     const handler = (event: KeyboardEvent) => {
-      if (event.shiftKey && event.metaKey && event.code === "Quote") {
-        callbackHandler({ shift: true });
-      } else if (event.metaKey && event.code === "Quote") {
+      if (event.metaKey && event.code === "Quote") {
         callbackHandler({ shift: false });
       } else if (event.ctrlKey && event.code === "Quote") {
         callbackHandler({ shift: false });
