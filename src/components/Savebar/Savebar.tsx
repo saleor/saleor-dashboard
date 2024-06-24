2111f930520da4c49ff65cc881740ed6d9e9fe1d
Paweł Chyła <chyla1988@gmail.com>
2024-06-24 10:17:08 +0200

    Implement new savebar (#4975)
    * Savebar init
    
    * Use portal to embaded savebar into layout
    
    * Use ConfirmButton component
    
    * Remove not used imports
    
    * Make transition state required
    
    * Restore old savebar
    
    * Refactor files
    
    * Add changeset
    
    * Show old savebar
    
    * Improve changeset

---
+++ src/components/Savebar/Savebar.tsx
@@ -0,0 +1,30 @@
+import { Root as Portal } from "@radix-ui/react-portal";
+import { Box } from "@saleor/macaw-ui-next";
+import React from "react";
+
+import { savebarHeight } from "../AppLayout/consts";
+import { useSavebarRef } from "./SavebarRefContext";
+
+interface SavebarProps {
+  children: React.ReactNode;
+}
+
+export const SavebarRoot = ({ children }: SavebarProps) => {
+  const { anchor } = useSavebarRef();
+
+  if (!anchor.current) {
+    return null;
+  }
+
+  return (
+    <Portal container={anchor.current} asChild>
+      <Box __height={savebarHeight} backgroundColor="default1">
+        <Box display="flex" alignItems="center" gap={3} paddingX={6} height="100%">
+          {children}
+        </Box>
+      </Box>
+    </Portal>
+  );
+};
+
+export const Spacer = () => <Box __flex={1} />;
