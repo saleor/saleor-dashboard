b955312251b625ac68e42687fb582a7184811713
Paweł Chyła <pawel.chyla@saleor.io>
2024-07-04 11:19:49 +0200

    Update channel locator

--- playwright/pages/channelsPage.ts
+++ playwright/pages/channelsPage.ts
@@ -34,7 +34,7 @@ export class ChannelPage extends BasePage {
       .first(),
     readonly slugNameInput = page.getByTestId("slug-name-input"),
     readonly channelCurrencySelect = page.getByTestId("channel-currency-select-input"),
-    readonly countrySelect = page.getByTestId("country-select-input").locator("input"),
+    readonly countrySelect = page.getByTestId("country-select-input"),
   ) {
     super(page);
     this.page = page;
