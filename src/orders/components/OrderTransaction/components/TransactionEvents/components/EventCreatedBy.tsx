1e31e24ffdeb3903601ef8675758c1257f29a88c
Wojciech <wojciech.mista@hotmail.com>
2024-07-04 14:40:47 +0200

    unify colors of avatars

--- src/orders/components/OrderTransaction/components/TransactionEvents/components/EventCreatedBy.tsx
+++ src/orders/components/OrderTransaction/components/TransactionEvents/components/EventCreatedBy.tsx
@@ -1,9 +1,10 @@
 // @ts-strict-ignore
 import { AppUrls } from "@dashboard/apps/urls";
+import { UserAvatar } from "@dashboard/components/UserAvatar";
 import { AppAvatarFragment, StaffMemberAvatarFragment } from "@dashboard/graphql";
 import { getUserInitials, getUserName } from "@dashboard/misc";
 import { staffMemberDetailsUrl } from "@dashboard/staff/urls";
-import { Avatar, makeStyles } from "@saleor/macaw-ui";
+import { Box } from "@saleor/macaw-ui-next";
 import React from "react";
 import { Link } from "react-router-dom";
 
@@ -11,22 +12,7 @@ interface EventCreatedByProps {
   createdBy: StaffMemberAvatarFragment | AppAvatarFragment | null;
 }
 
-const useStyles = makeStyles(
-  theme => ({
-    userWrapper: {
-      display: "inline-flex",
-      height: "100%",
-      gap: theme.spacing(1),
-      alignItems: "center",
-      justifyContent: "flex-end",
-    },
-  }),
-  { name: "EventCreatedBy" },
-);
-
 export const EventCreatedBy: React.FC<EventCreatedByProps> = ({ createdBy }) => {
-  const classes = useStyles();
-
   if (!createdBy) {
     return null;
   }
@@ -36,9 +22,18 @@ export const EventCreatedBy: React.FC<EventCreatedByProps> = ({ createdBy }) => 
   }
 
   return (
-    <Link to={staffMemberDetailsUrl(createdBy.id)} className={classes.userWrapper}>
-      <Avatar initials={getUserInitials(createdBy)} avatar={createdBy?.avatar?.url} />
+    <Box
+      as={Link}
+      // @ts-expect-error - Box inherits props from "Link" but it's not typed
+      to={staffMemberDetailsUrl(createdBy.id)}
+      display="inline-flex"
+      height="100%"
+      gap={2}
+      alignItems="center"
+      justifyContent="flex-end"
+    >
+      <UserAvatar initials={getUserInitials(createdBy)} src={createdBy?.avatar?.url} />
       {getUserName(createdBy, true)}
-    </Link>
+    </Box>
   );
 };
