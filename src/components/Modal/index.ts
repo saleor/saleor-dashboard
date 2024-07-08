8369c8b29cc6183fa09330b2a7d49895e8926511
Paweł Chyła <pawel.chyla@saleor.io>
2024-07-08 09:10:12 +0200

    Add new action dialog

--- src/components/Modal/index.ts
+++ src/components/Modal/index.ts
@@ -4,6 +4,8 @@ import { Content } from "./Content";
 import { Root } from "./Root";
 import { Title } from "./Title";
 
+export const DASHBOARD_MODAL_WIDTH = 600;
+
 export const DashboardModal = Object.assign(Root, {
   Title,
   Content,
