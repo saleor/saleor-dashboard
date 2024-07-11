744a7aa9c764c995e34da612e0c1fde472f291dc
Wojciech <wojciech.mista@hotmail.com>
2024-07-11 15:41:02 +0200

    app install page

--- src/components/Card/Title.tsx
+++ src/components/Card/Title.tsx
@@ -3,10 +3,11 @@ import React from "react";
 
 interface TitleProps {
   paddingX?: Sprinkles["paddingX"];
+  dataTestId?: string;
 }
 
-export const Title: React.FC<TitleProps> = ({ children, paddingX }) => (
-  <Box paddingX={paddingX ?? 6} paddingTop={6}>
+export const Title: React.FC<TitleProps> = ({ children, paddingX, dataTestId }) => (
+  <Box paddingX={paddingX ?? 6} paddingTop={6} data-test-id={dataTestId}>
     <Text size={5} fontWeight="bold">
       {children}
     </Text>
