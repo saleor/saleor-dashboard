c99dfc0b1e843e6f50306435a0cad6a1887b9454
Wojciech <wojciech.mista@hotmail.com>
2024-07-11 21:33:39 +0200

    attributes

--- src/components/Card/index.ts
+++ src/components/Card/index.ts
@@ -1,5 +1,6 @@
 import { Content } from "./Content";
 import { Root } from "./Root";
 import { Title } from "./Title";
+import { Toolbar } from "./Toolbar";
 
-export const DashboardCard = Object.assign(Root, { Title, Content });
+export const DashboardCard = Object.assign(Root, { Title, Content, Toolbar });
