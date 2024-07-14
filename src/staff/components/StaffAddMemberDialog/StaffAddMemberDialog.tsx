77187c574e34c83a9b0b4366142b4ade6968544e
Paweł Chyła <pawel.chyla@saleor.io>
2024-07-14 12:39:40 +0200

    Migrate OrderChangeWarehouseDialog and StaffAddMemberDialog

--- src/staff/components/StaffAddMemberDialog/StaffAddMemberDialog.tsx
+++ src/staff/components/StaffAddMemberDialog/StaffAddMemberDialog.tsx
@@ -2,16 +2,15 @@
 import BackButton from "@dashboard/components/BackButton";
 import { ConfirmButton, ConfirmButtonTransitionState } from "@dashboard/components/ConfirmButton";
 import Form from "@dashboard/components/Form";
-import FormSpacer from "@dashboard/components/FormSpacer";
+import { DashboardModal } from "@dashboard/components/Modal";
 import { SearchPermissionGroupsQuery, StaffErrorFragment } from "@dashboard/graphql";
 import { SubmitPromise } from "@dashboard/hooks/useForm";
 import useModalDialogErrors from "@dashboard/hooks/useModalDialogErrors";
 import { commonMessages } from "@dashboard/intl";
 import { FetchMoreProps, RelayToFlat, SearchPageProps } from "@dashboard/types";
 import { getFormErrors } from "@dashboard/utils/errors";
-import { Dialog, DialogActions, DialogContent, DialogTitle, TextField } from "@material-ui/core";
-import { makeStyles } from "@saleor/macaw-ui";
-import { vars } from "@saleor/macaw-ui-next";
+import { TextField } from "@material-ui/core";
+import { Box } from "@saleor/macaw-ui-next";
 import React from "react";
 import { FormattedMessage, useIntl } from "react-intl";
 
@@ -30,27 +29,6 @@ const initialForm: AddMemberFormData = {
   lastName: "",
   permissionGroups: [],
 };
-const useStyles = makeStyles(
-  theme => ({
-    hr: {
-      backgroundColor: vars.colors.border.default1,
-      border: "none",
-      height: 1,
-      marginBottom: 0,
-    },
-    sectionTitle: {
-      fontWeight: 600 as const,
-      marginBottom: theme.spacing(),
-      marginTop: theme.spacing(2),
-    },
-    textFieldGrid: {
-      display: "grid",
-      gridColumnGap: theme.spacing(2),
-      gridTemplateColumns: "1fr 1fr",
-    },
-  }),
-  { name: "StaffAddMemberDialog" },
-);
 
 interface StaffAddMemberDialogProps extends SearchPageProps {
   availablePermissionGroups: RelayToFlat<SearchPermissionGroupsQuery["search"]>;
@@ -65,7 +43,6 @@ interface StaffAddMemberDialogProps extends SearchPageProps {
 
 const StaffAddMemberDialog: React.FC<StaffAddMemberDialogProps> = props => {
   const { confirmButtonState, errors, onClose, onConfirm, open } = props;
-  const classes = useStyles(props);
   const dialogErrors = useModalDialogErrors(errors, open);
   const intl = useIntl();
   const formErrors = getFormErrors(["firstName", "lastName", "email"], dialogErrors);
@@ -78,55 +55,54 @@ const StaffAddMemberDialog: React.FC<StaffAddMemberDialogProps> = props => {
   });
 
   return (
-    <Dialog onClose={onClose} open={open}>
+    <DashboardModal onChange={onClose} open={open}>
       <Form
         data-test-id="invite-staff-member-dialog-form"
         initial={initialForm}
         onSubmit={onConfirm}
       >
         {({ change, data: formData }) => (
-          <>
-            <DialogTitle disableTypography>
+          <DashboardModal.Content __width={400} overflow="hidden">
+            <DashboardModal.Title>
               <FormattedMessage
                 id="23g7PY"
                 defaultMessage="Invite Staff Member"
                 description="dialog header"
               />
-            </DialogTitle>
-            <DialogContent>
-              <div className={classes.textFieldGrid}>
-                <TextField
-                  data-test-id="first-name-input"
-                  {...getFieldProps("firstName")}
-                  type="text"
-                  value={formData.firstName}
-                  onChange={change}
-                />
-                <TextField
-                  data-test-id="last-name-input"
-                  {...getFieldProps("lastName")}
-                  type="text"
-                  value={formData.lastName}
-                  onChange={change}
-                />
-              </div>
-              <FormSpacer />
+            </DashboardModal.Title>
+
+            <Box display="flex" gap={3} justifyContent="space-between">
               <TextField
-                data-test-id="email-input"
-                fullWidth
-                {...getFieldProps("email")}
-                type="email"
-                value={formData.email}
+                data-test-id="first-name-input"
+                {...getFieldProps("firstName")}
+                type="text"
+                value={formData.firstName}
                 onChange={change}
-                FormHelperTextProps={
-                  {
-                    "data-test-id": "email-text-input-helper-text",
-                  } as ExtendedFormHelperTextProps
-                }
               />
-            </DialogContent>
-            <hr className={classes.hr} />
-            <DialogActions>
+              <TextField
+                data-test-id="last-name-input"
+                {...getFieldProps("lastName")}
+                type="text"
+                value={formData.lastName}
+                onChange={change}
+              />
+            </Box>
+
+            <TextField
+              data-test-id="email-input"
+              fullWidth
+              {...getFieldProps("email")}
+              type="email"
+              value={formData.email}
+              onChange={change}
+              FormHelperTextProps={
+                {
+                  "data-test-id": "email-text-input-helper-text",
+                } as ExtendedFormHelperTextProps
+              }
+            />
+
+            <DashboardModal.Actions>
               <BackButton onClick={onClose} />
               <ConfirmButton
                 data-test-id="submit"
@@ -135,11 +111,11 @@ const StaffAddMemberDialog: React.FC<StaffAddMemberDialogProps> = props => {
               >
                 <FormattedMessage id="hw9Fah" defaultMessage="Send invite" description="button" />
               </ConfirmButton>
-            </DialogActions>
-          </>
+            </DashboardModal.Actions>
+          </DashboardModal.Content>
         )}
       </Form>
-    </Dialog>
+    </DashboardModal>
   );
 };
 
