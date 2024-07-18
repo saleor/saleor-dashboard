f5b9d6eb295e536f8fb3f21eec6813795016ff06
Wojciech <wojciech.mista@hotmail.com>
2024-07-18 10:06:47 +0200

    optimize code

--- src/components/Card/Header.tsx
+++ src/components/Card/Header.tsx
@@ -1,19 +1,21 @@
-import { Box, BoxProps, PropsWithBox } from "@saleor/macaw-ui-next";
+import { Box, PropsWithBox } from "@saleor/macaw-ui-next";
 import React from "react";
-
-const toolbarWrapperStyles = {
-  display: "flex",
-  flexDirection: "row",
-  alignItems: "center",
-  justifyContent: "space-between",
-} as BoxProps;
 
 export const Header: React.FC<PropsWithBox<{ children: React.ReactNode }>> = ({
   children,
   className,
   ...rest
 }) => (
-  <Box paddingX={6} paddingTop={6} className={className} {...toolbarWrapperStyles} {...rest}>
+  <Box
+    paddingX={6}
+    paddingTop={6}
+    className={className}
+    display="flex"
+    flexDirection="row"
+    alignItems="center"
+    justifyContent="space-between"
+    {...rest}
+  >
     {children}
   </Box>
 );
