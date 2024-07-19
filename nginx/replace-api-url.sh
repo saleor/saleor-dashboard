76ec6704016ae0cbee51c38accbe10bbcd3cd5a1
Yasin Uslu <nepjua@gmail.com>
2024-07-19 13:07:30 +0300

    Make the script more robust

--- nginx/replace-api-url.sh
+++ nginx/replace-api-url.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
 # Replaces environment variables in the bundle's index.html file with their respective values.
 # This script is automatically picked up by the nginx entrypoint on startup.
@@ -10,20 +10,21 @@ INDEX_BUNDLE_PATH="/app/dashboard/index.html"
 # Function to replace environment variables
 replace_env_var() {
   var_name=$1
-  var_value=$(eval echo \$$var_name)
+  var_value=$(eval echo \$"$var_name")
   if [ -n "$var_value" ]; then
     echo "Setting $var_name to: $var_value"
-    sed -i "s#$var_name:.*#$var_name: \"$var_value\",#" "$INDEX_BUNDLE_PATH"
+    sed -i "s#$var_name: \".*\"#$var_name: \"$var_value\"#" "$INDEX_BUNDLE_PATH"
   else
     echo "No $var_name provided, using defaults."
   fi
 }
 
 # Replace each environment variable
+replace_env_var "API_URL"
 replace_env_var "APP_MOUNT_URI"
 replace_env_var "APPS_MARKETPLACE_API_URL"
 replace_env_var "APPS_TUNNEL_URL_KEYWORDS"
-replace_env_var "STATIC_URL"
-replace_env_var "API_URL"
+replace_env_var "IS_CLOUD_INSTANCE"
+replace_env_var "LOCALE_CODE"
 
 echo "Environment variable replacement complete."
