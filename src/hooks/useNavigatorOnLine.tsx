9b0b3c30073f54db9fef38f68a945dbb39507f39
Paweł Chyła <pawel.chyla@saleor.io>
2024-07-30 13:59:24 +0200

    useNavigatorOnLine hook

---
+++ src/hooks/useNavigatorOnLine.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+
+const getOnLineStatus = () =>
+  typeof navigator !== "undefined" && typeof navigator.onLine === "boolean"
+    ? navigator.onLine
+    : true;
+
+export const useNavigatorOnLine = () => {
+  const [status, setStatus] = React.useState(getOnLineStatus());
+
+  const setOnline = () => setStatus(true);
+  const setOffline = () => setStatus(false);
+
+  React.useEffect(() => {
+    window.addEventListener("online", setOnline);
+    window.addEventListener("offline", setOffline);
+
+    return () => {
+      window.removeEventListener("online", setOnline);
+      window.removeEventListener("offline", setOffline);
+    };
+  }, []);
+
+  return status;
+};
