26634981803c5efc0659915a9f851ee965c639e9
Wojciech <wojciech.mista@hotmail.com>
2024-08-09 10:40:19 +0200

    improve tests

--- src/giftCards/GiftCardUpdate/GiftCardHistory/hooks/useGiftCardHistoryEvents.test.tsx
+++ src/giftCards/GiftCardUpdate/GiftCardHistory/hooks/useGiftCardHistoryEvents.test.tsx
@@ -23,74 +23,77 @@ const mockUser = {
 };
 
 mockUseUser.mockReturnValue({ user: mockUser });
-mockUseGiftCardEventsQuery.mockImplementation(value => {
-  if (value.skip) {
-    return {
-      data: undefined,
-    };
-  }
-
-  if (!value.variables.canSeeApp || !value.variables.canSeeUser) {
-    return {
-      data: undefined,
-    };
-  }
-
-  return {
-    data: {
-      giftCard: {
-        events: [{ id: "event1" }, { id: "event2" }],
-      },
-    },
-  };
-});
-
-describe("useGiftCardEventsQuery (mock implementation)", () => {
-  it("should return gift card events", () => {
-    // Arrange & Act
-    const { result } = renderHook(() =>
-      useGiftCardEventsQuery({
-        variables: { id: "giftCardId", canSeeApp: true, canSeeUser: true },
-        skip: false,
-      }),
-    );
-
-    // Assert
-    expect(result.current.data).toEqual({
-      giftCard: { events: [{ id: "event1" }, { id: "event2" }] },
-    });
-  });
-
-  it("should return undefined without permissions", () => {
-    // Arrange & Act
-    const { result } = renderHook(() =>
-      useGiftCardEventsQuery({
-        variables: { id: "giftCardId", canSeeApp: false, canSeeUser: false },
-        skip: false,
-      }),
-    );
-
-    // Assert
-    expect(result.current.data).toBeUndefined();
-  });
-
-  it("should return undefined when it is skipped", () => {
-    // Arrange & Act
-    const { result } = renderHook(() =>
-      useGiftCardEventsQuery({
-        variables: { id: "giftCardId", canSeeApp: true, canSeeUser: true },
-        skip: true,
-      }),
-    );
-
-    // Assert
-    expect(result.current.data).toBeUndefined();
-  });
-});
 
 describe("useGiftCardHistoryEvents", () => {
   it("should return gift card events", () => {
     // Arrange
+    mockUseGiftCardEventsQuery.mockImplementation(() => ({
+      data: {
+        giftCard: {
+          events: [
+            { id: "event1", app: {}, user: {} },
+            { id: "event2", app: {}, user: {} },
+          ],
+        },
+      },
+    }));
+
+    const wrapper = ({ children }: { children: React.ReactNode }) => (
+      <GiftCardDetailsContext.Provider value={{ giftCard: mockGiftCard, loading: false }}>
+        {children}
+      </GiftCardDetailsContext.Provider>
+    );
+
+    // Act
+    const { result } = renderHook(() => useGiftCardHistoryEvents(), { wrapper });
+
+    // Assert
+    expect(result.current.id).toBe("giftCardId");
+    expect(result.current.events).toEqual([
+      { id: "event1", app: {}, user: {} },
+      { id: "event2", app: {}, user: {} },
+    ]);
+    expect(mockUseGiftCardEventsQuery).toBeCalledWith({
+      variables: { id: "giftCardId", canSeeApp: true, canSeeUser: true },
+      skip: false,
+    });
+  });
+
+  it("should return undefined when there is no gift card", () => {
+    // Arrange
+    mockUseGiftCardEventsQuery.mockImplementation(() => ({
+      data: undefined,
+    }));
+
+    const wrapper = ({ children }: { children: React.ReactNode }) => (
+      <GiftCardDetailsContext.Provider value={{ giftCard: undefined, loading: false }}>
+        {children}
+      </GiftCardDetailsContext.Provider>
+    );
+
+    // Act
+    const { result } = renderHook(() => useGiftCardHistoryEvents(), { wrapper });
+
+    // Assert
+    expect(result.current.id).toBeUndefined();
+    expect(result.current.events).toBeUndefined();
+    expect(mockUseGiftCardEventsQuery).toBeCalledWith({
+      variables: undefined,
+      skip: true,
+    });
+  });
+
+  it("should not return app and user when user does not have permissions", () => {
+    // Arrange
+    mockUseUser.mockReturnValue({ user: { userPermissions: [] } });
+    mockUseGiftCardEventsQuery.mockImplementation(() => ({
+      data: {
+        giftCard: {
+          events: [{ id: "event1" }, { id: "event2" }],
+        },
+      },
+    }));
+
     const wrapper = ({ children }: { children: React.ReactNode }) => (
       <GiftCardDetailsContext.Provider value={{ giftCard: mockGiftCard, loading: false }}>
         {children}
@@ -103,39 +106,9 @@ describe("useGiftCardHistoryEvents", () => {
     // Assert
     expect(result.current.id).toBe("giftCardId");
     expect(result.current.events).toEqual([{ id: "event1" }, { id: "event2" }]);
-  });
-
-  it("should return undefined when there is no gift card", () => {
-    // Arrange
-    const wrapper = ({ children }: { children: React.ReactNode }) => (
-      <GiftCardDetailsContext.Provider value={{ giftCard: undefined, loading: false }}>
-        {children}
-      </GiftCardDetailsContext.Provider>
-    );
-
-    // Act
-    const { result } = renderHook(() => useGiftCardHistoryEvents(), { wrapper });
-
-    //
-    expect(result.current.id).toBeUndefined();
-    expect(result.current.events).toBeUndefined();
-  });
-
-  it("should return undefined when user does not have permissions", () => {
-    // Arrange
-    mockUseUser.mockReturnValue({ user: { userPermissions: [] } });
-
-    const wrapper = ({ children }: { children: React.ReactNode }) => (
-      <GiftCardDetailsContext.Provider value={{ giftCard: mockGiftCard, loading: false }}>
-        {children}
-      </GiftCardDetailsContext.Provider>
-    );
-
-    // Act
-    const { result } = renderHook(() => useGiftCardHistoryEvents(), { wrapper });
-
-    // Assert
-    expect(result.current.id).toBe("giftCardId");
-    expect(result.current.events).toBeUndefined();
+    expect(mockUseGiftCardEventsQuery).toBeCalledWith({
+      variables: { id: "giftCardId", canSeeApp: false, canSeeUser: false },
+      skip: false,
+    });
   });
 });
