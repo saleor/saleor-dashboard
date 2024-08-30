7215d68697dce9bc343de6084629f03baa4bbfcc
Wojciech <wojciech.mista@hotmail.com>
2024-08-30 08:28:19 +0200

    change skeleton height

--- src/orders/components/OrderHistory/HistoryComponentLoader.tsx
+++ src/orders/components/OrderHistory/HistoryComponentLoader.tsx
@@ -5,7 +5,7 @@ export const HistoryComponentLoader = () => (
   <Box>
     <Skeleton __height="32px" __marginBottom="20px" />
     <Skeleton __height="16px" __marginBottom="18px" />
-    <Skeleton __height="20px" __marginBottom="18px" />
+    <Skeleton __height="16px" __marginBottom="18px" />
     <Skeleton __height="16px" __marginBottom="18px" />
   </Box>
 );
