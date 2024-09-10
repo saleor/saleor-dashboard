87241c97b988715f72322d7a454fc71b8c91b9d2
Wojciech <wojciech.mista@hotmail.com>
2024-09-10 10:03:36 +0200

    retain traces on first failure

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,28 +18,28 @@ export default defineConfig({
   workers: parseInt(env.WORKERS || DEFAULT_WORKERS),
   reporter: process.env.CI
     ? [
-      ["blob"],
-      ["github"],
-      [
-        "playwright-testmo-reporter",
-        {
-          outputFile: "testmo/testmo.xml", // Optional: Output file path. Defaults to 'testmo.xml'.
-          embedBrowserType: true, // Optional: Embed browser type in the XML file. Defaults to false.
-          embedTestSteps: true, // Optional: Embed test steps in the XML file. Defaults to true.
-          testStepCategories: ["hook", "expect", "pw:api", "test.step"], // Optional: Test step categories to include in the XML file. Defaults to ["hook","expect","pw:api","test.step"]. Possible options are "hook", "expect", "pw:api", "test.step".
-          testTitleDepth: 1, // Optional: Test case title depth to report in the XML file. Defaults to 1. Increase this to 2 include suite name. Increase this even further to include the path.
-          attachmentBasePathCallback: () =>
-            process.env.URL_TO_RUN ? process.env.URL_TO_RUN : "",
-        },
-      ],
-    ]
+        ["blob"],
+        ["github"],
+        [
+          "playwright-testmo-reporter",
+          {
+            outputFile: "testmo/testmo.xml", // Optional: Output file path. Defaults to 'testmo.xml'.
+            embedBrowserType: true, // Optional: Embed browser type in the XML file. Defaults to false.
+            embedTestSteps: true, // Optional: Embed test steps in the XML file. Defaults to true.
+            testStepCategories: ["hook", "expect", "pw:api", "test.step"], // Optional: Test step categories to include in the XML file. Defaults to ["hook","expect","pw:api","test.step"]. Possible options are "hook", "expect", "pw:api", "test.step".
+            testTitleDepth: 1, // Optional: Test case title depth to report in the XML file. Defaults to 1. Increase this to 2 include suite name. Increase this even further to include the path.
+            attachmentBasePathCallback: () =>
+              process.env.URL_TO_RUN ? process.env.URL_TO_RUN : "",
+          },
+        ],
+      ]
     : [["html"], ["list"]],
   expect: { timeout: 5000 },
   maxFailures: 10,
   timeout: 30000,
   use: {
     baseURL: env.BASE_URL || "",
-    trace: env.CI ? "on-all-retries" : "on",
+    trace: env.CI ? "retain-on-failure" : "on",
     screenshot: "only-on-failure",
     testIdAttribute: "data-test-id",
     video: env.CI ? "retain-on-failure" : "off",
@@ -54,7 +54,7 @@ export default defineConfig({
       name: "e2e",
       dependencies: ["setup"],
       use: { ...devices["Desktop Chrome"] },
-      testIgnore: "playwright/tests/apps.spec.ts"
+      testIgnore: "playwright/tests/apps.spec.ts",
     },
     {
       name: "apps-e2e",
