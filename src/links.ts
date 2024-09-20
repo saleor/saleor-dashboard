24ce14ece8b9e8f3ce577212173bfc4e60d8cc96
Wojciech <wojciech.mista@hotmail.com>
2024-09-20 10:43:22 +0200

    change link

--- src/links.ts
+++ src/links.ts
@@ -1,2 +1,2 @@
 export const TECHNICAL_HELP_CTA_URL =
-  "https://saleor.typeform.com/talk-with-us?&utm_button=Book%20a%20demo%20&utm_page=%2F";
+  "https://www.getclockwise.com/c/rian-dillon-saleor-io/short-call-with-saleor";
