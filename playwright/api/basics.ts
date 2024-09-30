b43f9da9261a9e22ea8fe5e2190705dbcc2cdcbd
Michalina Graczyk <michalina.graczyk@saleor.io>
2024-09-30 22:22:38 +0200

    Add in-memory lock mechanism for logInUserViaApi

--- playwright/api/basics.ts
+++ playwright/api/basics.ts
@@ -28,37 +28,60 @@ interface ApiResponse<T> {
 export class BasicApiService {
   readonly request: APIRequestContext;
 
+  private static lock: Promise<void> | null = null;
+
   constructor(request: APIRequestContext) {
     this.request = request;
   }
 
   async logInUserViaApi(user: User): Promise<ApiResponse<TokenCreateResponse>> {
-    const query = `mutation TokenAuth{
-            tokenCreate(email: "${user.email}", password: "${user.password}") {
-                token
-                refreshToken
-                errors: errors {
-                    code
-                    message
-                }
-                user {
-                    id
-                }
-            }
-        }`;
-    const loginResponse = await this.request.post(process.env.API_URL || "", {
-      data: { query },
-    });
-    const loginResponseJson: { data: TokenCreateResponse } = await loginResponse.json();
-
-    if (loginResponseJson.data.tokenCreate.errors?.length > 0) {
-      const errorMessages = loginResponseJson.data.tokenCreate.errors
-        .map(e => e.message)
-        .join(", ");
-
-      throw new Error(`Login failed: ${errorMessages}`);
+    // Acquire the global lock.
+    while (BasicApiService.lock) {
+      await BasicApiService.lock;
     }
 
-    return loginResponseJson as ApiResponse<TokenCreateResponse>;
+    let releaseLock: () => void = () => {
+      // Dummy release
+    };
+
+    // Create a new lock promise.
+    BasicApiService.lock = new Promise<void>(resolve => {
+      releaseLock = resolve;
+    });
+
+    try {
+      const query = `mutation TokenAuth{
+        tokenCreate(email: "${user.email}", password: "${user.password}") {
+          token
+          refreshToken
+          errors: errors {
+            code
+            message
+          }
+          user {
+            id
+          }
+        }
+      }`;
+
+      const loginResponse = await this.request.post(process.env.API_URL || "", {
+        data: { query },
+      });
+
+      const loginResponseJson: { data: TokenCreateResponse } = await loginResponse.json();
+
+      if (loginResponseJson.data.tokenCreate.errors?.length > 0) {
+        const errorMessages = loginResponseJson.data.tokenCreate.errors
+          .map(e => e.message)
+          .join(", ");
+
+        throw new Error(`Login failed: ${errorMessages}`);
+      }
+
+      return loginResponseJson as ApiResponse<TokenCreateResponse>;
+    } finally {
+      releaseLock();
+      BasicApiService.lock = null;
+    }
   }
 }
