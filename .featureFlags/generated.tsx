bed9d59481a143817dd12f6cc7194a4a4b16c2d2
Paweł Chyła <pawel.chyla@saleor.io>
2024-10-01 11:56:38 +0200

    Remove order and product filtering ff

--- .featureFlags/generated.tsx
+++ .featureFlags/generated.tsx
@@ -1,15 +1,13 @@
 // @ts-nocheck
 
-import B33780 from "./images/discounts-list.png"
-import M00128 from "./images/improved_refunds.png"
-import U43059 from "./images/order-filters.png"
-import N27424 from "./images/filters.png"
+import R80834 from "./images/discounts-list.png"
+import E27647 from "./images/improved_refunds.png"
 
-const discounts_rules = () => (<><p><img src={B33780} alt="Discount rules"/></p>
+const discounts_rules = () => (<><p><img src={R80834} alt="Discount rules"/></p>
 <p>Apply the new discounts rules to narrow your promotions audience.
 Set up conditions and channels that must be fulfilled to apply defined reward.</p>
 </>)
-const improved_refunds = () => (<><p><img src={M00128} alt="Improved refunds"/></p>
+const improved_refunds = () => (<><p><img src={E27647} alt="Improved refunds"/></p>
 <h3 id="enable-the-enhanced-refund-feature-to-streamline-your-refund-process">Enable the enhanced refund feature to streamline your refund process:</h3>
 <ul>
 <li><p>• Choose between automatic calculations based on selected items or enter refund amounts directly for overcharges and custom adjustments.</p>
@@ -18,14 +16,6 @@ const improved_refunds = () => (<><p><img src={M00128} alt="Improved refunds"/><
 </li>
 </ul>
 
-</>)
-const order_filters = () => (<><p><img src={U43059} alt="new filters"/></p>
-<p>Experience the new look and enhanced abilities of new orders filtering mechanism.
-Easily combine any criteria you want, and quickly browse their values.</p>
-</>)
-const product_filters = () => (<><p><img src={N27424} alt="new filters"/></p>
-<p>Experience the new look and enhanced abilities of new fitering mechanism.
-Easily combine any criteria you want, and quickly browse their values.</p>
 </>)
 
 export const AVAILABLE_FLAGS = [{
@@ -41,24 +31,6 @@ export const AVAILABLE_FLAGS = [{
   name: "improved_refunds",
   displayName: "Improved refunds",
   component: improved_refunds,
-  visible: true,
-  content: {
-    enabled: true,
-    payload: "default",
-  }
-},{
-  name: "order_filters",
-  displayName: "Orders filtering",
-  component: order_filters,
-  visible: true,
-  content: {
-    enabled: true,
-    payload: "default",
-  }
-},{
-  name: "product_filters",
-  displayName: "Products filtering",
-  component: product_filters,
   visible: true,
   content: {
     enabled: true,
