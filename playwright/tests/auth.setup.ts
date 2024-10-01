7b7260ccde1d123fe3b6646269acb474cc662932
andrzejewsky <vox3r69@gmail.com>
2024-10-01 16:15:47 +0200

    Apply to other tests

--- playwright/tests/auth.setup.ts
+++ playwright/tests/auth.setup.ts
@@ -1,8 +1,13 @@
-// import { test as setup } from "@playwright/test";
-// import { getStorageState } from "utils/auth";
+import { permissions } from "@data/userPermissions";
+import { test as setup } from "@playwright/test";
+import { getStorageState } from "utils/auth";
 
-// setup.describe.configure({ mode: "serial" });
+setup.describe.configure({ mode: "serial" });
 
-// setup("Authenticate as admin via API", async () => {
-//   await getStorageState("admin");
-// });
+setup("Authenticate as admin via API", async () => {
+  await permissions
+    .map(permissionName => () => getStorageState(permissionName))
+    .forEach(async permissionPromise => {
+      await permissionPromise();
+    });
+});
