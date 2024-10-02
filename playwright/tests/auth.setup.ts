17b4bcb528e37a8b38cefc0f6b5a3f820f405194
andrzejewsky <vox3r69@gmail.com>
2024-10-02 16:16:07 +0200

    Ci fixes

--- playwright/tests/auth.setup.ts
+++ playwright/tests/auth.setup.ts
@@ -2,7 +2,7 @@ import { permissions } from "@data/userPermissions";
 import { test as setup } from "@playwright/test";
 import { getStorageState } from "utils/auth";
 
-setup("Authenticate for each permission type via API @setup", async ({ page }, workerInfo) => {
+setup("Authenticate for each permission type via API @e2e", async ({ page }, workerInfo) => {
   console.log("Saving file for: admin");
   await getStorageState("admin");
 
