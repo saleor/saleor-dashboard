1c734fb9c60cd3b46a6cf733d1244cce32b09b60
Paweł Chyła <pawel.chyla@saleor.io>
2024-12-05 09:35:56 +0100

    Fix map shipping translations to translation entities

--- src/translations/views/EntityLists/utils.ts
+++ src/translations/views/EntityLists/utils.ts
@@ -1,3 +1,36 @@
-export function sumCompleted(list: any[]): number {
+import { ShippingMethodTranslationsQuery } from "@dashboard/graphql";
+import { TranslatableEntity } from "@dashboard/translations/components/TranslationsEntitiesList";
+import { mapEdgesToItems } from "@dashboard/utils/maps";
+
+export function mapTranslationsToEntities(
+  data: ShippingMethodTranslationsQuery | undefined,
+): TranslatableEntity[] {
+  if (!data?.translations) {
+    return [];
+  }
+
+  const items = mapEdgesToItems(data.translations);
+
+  if (!items) {
+    return [];
+  }
+
+  return items.reduce((acc, node) => {
+    if (node.__typename === "ShippingMethodTranslatableContent") {
+      acc.push({
+        completion: {
+          current: sumCompleted([node.translation?.name, node.translation?.description]),
+          max: 2,
+        },
+        id: node?.shippingMethod?.id ?? "",
+        name: node?.name,
+      });
+    }
+
+    return acc;
+  }, [] as TranslatableEntity[]);
+}
+
+function sumCompleted(list: any[]): number {
   return list.reduce((acc, field) => acc + (field ? 1 : 0), 0);
 }
