7681c5a2d3752ac0547b93849a058e18cb37e68c
Paweł Chyła <pawel.chyla@saleor.io>
2025-01-08 12:05:00 +0100

    Fix isCloudInstance

--- src/components/ProductAnalytics/index.tsx
+++ src/components/ProductAnalytics/index.tsx
@@ -13,7 +13,7 @@ const useConfig = () => {
     },
   } satisfies Partial<PostHogConfig>;
   const apiKey = process.env.POSTHOG_KEY;
-  const isCloudInstance = true;
+  const isCloudInstance = process.env.IS_CLOUD_INSTANCE;
   const canRenderAnalytics = () => {
     if (!isCloudInstance) {
       return false;
