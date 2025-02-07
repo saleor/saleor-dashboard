103bb23c6abbd3e0a47aa4d66cc74b7880316f1f
Paweł Chyła <pawel.chyla@saleor.io>
2025-02-07 14:25:40 +0100

    Generate flag

--- .featureFlags/generated.tsx
+++ .featureFlags/generated.tsx
@@ -1,18 +1,14 @@
 // @ts-nocheck
 
-import A63601 from "./images/content-filters.png"
-import I43385 from "./images/discounts-list.png"
-import U21592 from "./images/improved_refunds.png"
+import G13186 from "./images/discounts-list.png"
+import T68829 from "./images/improved_refunds.png"
+import F51530 from "./images/page-filters.png"
 
-const content_filters = () => (<><p><img src={A63601} alt="new filters"/>
-Experience the new look and enhanced abilities of new fitering mechanism.
-Easily combine any criteria you want, and quickly browse their values.</p>
-</>)
-const discounts_rules = () => (<><p><img src={I43385} alt="Discount rules"/></p>
+const discounts_rules = () => (<><p><img src={G13186} alt="Discount rules"/></p>
 <p>Apply the new discounts rules to narrow your promotions audience.
 Set up conditions and channels that must be fulfilled to apply defined reward.</p>
 </>)
-const improved_refunds = () => (<><p><img src={U21592} alt="Improved refunds"/></p>
+const improved_refunds = () => (<><p><img src={T68829} alt="Improved refunds"/></p>
 <h3 id="enable-the-enhanced-refund-feature-to-streamline-your-refund-process">Enable the enhanced refund feature to streamline your refund process:</h3>
 <ul>
 <li><p>• Choose between automatic calculations based on selected items or enter refund amounts directly for overcharges and custom adjustments.</p>
@@ -22,17 +18,12 @@ const improved_refunds = () => (<><p><img src={U21592} alt="Improved refunds"/><
 </ul>
 
 </>)
+const pages_filters = () => (<><p><img src={F51530} alt="new filters"/>
+Experience the new look and enhanced abilities of new fitering mechanism.
+Easily combine any criteria you want, and quickly browse their values.</p>
+</>)
 
 export const AVAILABLE_FLAGS = [{
-  name: "content_filters",
-  displayName: "Contents filtering",
-  component: content_filters,
-  visible: true,
-  content: {
-    enabled: true,
-    payload: "default",
-  }
-},{
   name: "discounts_rules",
   displayName: "Discounts rules",
   component: discounts_rules,
@@ -45,6 +36,15 @@ export const AVAILABLE_FLAGS = [{
   name: "improved_refunds",
   displayName: "Improved refunds",
   component: improved_refunds,
+  visible: true,
+  content: {
+    enabled: true,
+    payload: "default",
+  }
+},{
+  name: "pages_filters",
+  displayName: "Page filtering",
+  component: pages_filters,
   visible: true,
   content: {
     enabled: true,
--- .featureFlags/pages_filters.md
+++ .featureFlags/pages_filters.md
@@ -1,11 +1,11 @@
 ---
-name: content_filters
-displayName: Contents filtering
+name: pages_filters
+displayName: Page filtering
 enabled: true
 payload: "default"
 visible: true
 ---
 
-![new filters](./images/content-filters.png)
+![new filters](./images/page-filters.png)
 Experience the new look and enhanced abilities of new fitering mechanism.
 Easily combine any criteria you want, and quickly browse their values.
