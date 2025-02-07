e2229e9d8ef793ea75fc714da2ff1c4839a6a7fb
Paweł Chyła <pawel.chyla@saleor.io>
2025-02-07 11:46:17 +0100

    Generate flag

---
+++ .featureFlags/content_filters.md
@@ -0,0 +1,11 @@
+---
+name: content_filters
+displayName: Contents filtering
+enabled: true
+payload: "default"
+visible: true
+---
+
+![new filters](./images/content-filters.png)
+Experience the new look and enhanced abilities of new fitering mechanism.
+Easily combine any criteria you want, and quickly browse their values.
--- .featureFlags/generated.tsx
+++ .featureFlags/generated.tsx
@@ -1,13 +1,18 @@
 // @ts-nocheck
 
-import V24493 from "./images/discounts-list.png"
-import J49162 from "./images/improved_refunds.png"
+import A63601 from "./images/content-filters.png"
+import I43385 from "./images/discounts-list.png"
+import U21592 from "./images/improved_refunds.png"
 
-const discounts_rules = () => (<><p><img src={V24493} alt="Discount rules"/></p>
+const content_filters = () => (<><p><img src={A63601} alt="new filters"/>
+Experience the new look and enhanced abilities of new fitering mechanism.
+Easily combine any criteria you want, and quickly browse their values.</p>
+</>)
+const discounts_rules = () => (<><p><img src={I43385} alt="Discount rules"/></p>
 <p>Apply the new discounts rules to narrow your promotions audience.
 Set up conditions and channels that must be fulfilled to apply defined reward.</p>
 </>)
-const improved_refunds = () => (<><p><img src={J49162} alt="Improved refunds"/></p>
+const improved_refunds = () => (<><p><img src={U21592} alt="Improved refunds"/></p>
 <h3 id="enable-the-enhanced-refund-feature-to-streamline-your-refund-process">Enable the enhanced refund feature to streamline your refund process:</h3>
 <ul>
 <li><p>• Choose between automatic calculations based on selected items or enter refund amounts directly for overcharges and custom adjustments.</p>
@@ -19,6 +24,15 @@ const improved_refunds = () => (<><p><img src={J49162} alt="Improved refunds"/><
 </>)
 
 export const AVAILABLE_FLAGS = [{
+  name: "content_filters",
+  displayName: "Contents filtering",
+  component: content_filters,
+  visible: true,
+  content: {
+    enabled: true,
+    payload: "default",
+  }
+},{
   name: "discounts_rules",
   displayName: "Discounts rules",
   component: discounts_rules,
