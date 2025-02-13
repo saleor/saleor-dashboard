5ba2bee3f7bfc53d48be7fdfd50bf6090fa7da23
Karolina Rakoczy <rakoczy.karolina@gmail.com>
2025-02-13 10:38:24 +0100

    push flaky test report

---
+++ .github/actions/push-flaky-test-report/send-report-to-repo.js
@@ -0,0 +1,23 @@
+import { Octokit } from "octokit";
+import * as fs from "fs";
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+});
+
+
+const myJsonFile = fs.readFileSync("ctrf-reports/merged-report.json", {
+  encoding: "base64",
+});
+
+const path = `newReports/test-report-${new Date().valueOf()}`
+
+await octokit.request("PUT /repos/{owner}/{repo}/contents/{path}", {
+  owner: "saleor",
+  repo: "qa-helpers",
+  path: path,
+  message: "push new saleor-dashboard test report",
+  content: myJsonFile,
+  headers: {
+    "X-GitHub-Api-Version": "2022-11-28",
+  },
+});
