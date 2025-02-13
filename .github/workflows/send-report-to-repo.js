c59af0daa5ae4643e33bbe12255480e272fa4b53
Karolina Rakoczy <rakoczy.karolina@gmail.com>
2025-02-13 12:20:05 +0100

    Ad commander

--- .github/workflows/send-report-to-repo.js
+++ .github/workflows/send-report-to-repo.js
@@ -1,24 +1,31 @@
-const { Octokit } = require("octokit")
+const { Octokit } = require("octokit");
 const fs = require("fs");
+const { Command } = require("commander");
+const program = new Command();
 
-const octokit = new Octokit({
-  auth: process.env.GITHUB_TOKEN,
-});
+program
+  .name("Push tests results to repository")
+  .description("Push test result to QA repository")
+  .action(async () => {
+    const octokit = new Octokit({
+      auth: process.env.GITHUB_TOKEN,
+    });
 
+    const myJsonFile = fs.readFileSync("ctrf-reports/merged-report.json", {
+      encoding: "base64",
+    });
 
-const myJsonFile = fs.readFileSync("ctrf-reports/merged-report.json", {
-  encoding: "base64",
-});
+    const path = `newReports/test-report-${new Date().valueOf()}`;
 
-const path = `newReports/test-report-${new Date().valueOf()}`
-
-await octokit.request("PUT /repos/{owner}/{repo}/contents/{path}", {
-  owner: "saleor",
-  repo: "qa-helpers",
-  path: path,
-  message: "push new saleor-dashboard test report",
-  content: myJsonFile,
-  headers: {
-    "X-GitHub-Api-Version": "2022-11-28",
-  },
-});
+    await octokit.request("PUT /repos/{owner}/{repo}/contents/{path}", {
+      owner: "saleor",
+      repo: "qa-helpers",
+      path: path,
+      message: "push new saleor-dashboard test report",
+      content: myJsonFile,
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    });
+  })
+  .parse();
