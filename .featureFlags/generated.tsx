c959da1fed3069e822f8b6b8bd3ed9aced4211e5
Wojciech <wojciech.mista@hotmail.com>
2025-02-13 12:05:01 +0100

    add feature flag

--- .featureFlags/generated.tsx
+++ .featureFlags/generated.tsx
@@ -1,35 +1,40 @@
 // @ts-nocheck
 
-import W24585 from "./images/collection-filters.jpg"
-import L16463 from "./images/customers-filters.png"
-import L94878 from "./images/discounts-list.png"
-import E09437 from "./images/draft-orders-filters.png"
-import E78766 from "./images/gift-cards-filters.png"
-import X68306 from "./images/improved_refunds.png"
-import E85468 from "./images/page-filters.png"
-import B37301 from "./images/vouchers-filters.png"
+import R36053 from "./images/app-alerts.jpg"
+import F06132 from "./images/collection-filters.jpg"
+import R59677 from "./images/customers-filters.png"
+import O14545 from "./images/discounts-list.png"
+import F93900 from "./images/draft-orders-filters.png"
+import C15255 from "./images/gift-cards-filters.png"
+import J66056 from "./images/improved_refunds.png"
+import H71146 from "./images/page-filters.png"
+import G61362 from "./images/vouchers-filters.png"
 
-const collection_filters = () => (<><p><img src={W24585} alt="new filters"/>
+const app_alerts = () => (<><p><img src={R36053} alt="new filters"/>
+Experience new notifications displaying alerts for apps in the Dashboard. 
+Get meaningful information when Saleor detects issues with an app.</p>
+</>)
+const collection_filters = () => (<><p><img src={F06132} alt="new filters"/>
 Experience the new look and enhanced abilities of new filtering mechanism.
 Easily combine any criteria you want, and quickly browse their values.</p>
 </>)
-const customers_filters = () => (<><p><img src={L16463} alt="new filters"/>
+const customers_filters = () => (<><p><img src={R59677} alt="new filters"/>
 Experience the new look and enhanced abilities of new fitering mechanism.
 Easily combine any criteria you want, and quickly browse their values.</p>
 </>)
-const discounts_rules = () => (<><p><img src={L94878} alt="Discount rules"/></p>
+const discounts_rules = () => (<><p><img src={O14545} alt="Discount rules"/></p>
 <p>Apply the new discounts rules to narrow your promotions audience.
 Set up conditions and channels that must be fulfilled to apply defined reward.</p>
 </>)
-const draft_orders_filters = () => (<><p><img src={E09437} alt="new filters"/>
+const draft_orders_filters = () => (<><p><img src={F93900} alt="new filters"/>
 Experience the new look and enhanced abilities of new fitering mechanism.
 Easily combine any criteria you want, and quickly browse their values.</p>
 </>)
-const gift_cards_filters = () => (<><p><img src={E78766} alt="new filters"/>
+const gift_cards_filters = () => (<><p><img src={C15255} alt="new filters"/>
 Experience the new look and enhanced abilities of new fitering mechanism.
 Easily combine any criteria you want, and quickly browse their values.</p>
 </>)
-const improved_refunds = () => (<><p><img src={X68306} alt="Improved refunds"/></p>
+const improved_refunds = () => (<><p><img src={J66056} alt="Improved refunds"/></p>
 <h3 id="enable-the-enhanced-refund-feature-to-streamline-your-refund-process">Enable the enhanced refund feature to streamline your refund process:</h3>
 <ul>
 <li><p>• Choose between automatic calculations based on selected items or enter refund amounts directly for overcharges and custom adjustments.</p>
@@ -39,16 +44,25 @@ const improved_refunds = () => (<><p><img src={X68306} alt="Improved refunds"/><
 </ul>
 
 </>)
-const pages_filters = () => (<><p><img src={E85468} alt="new filters"/>
+const pages_filters = () => (<><p><img src={H71146} alt="new filters"/>
 Experience the new look and enhanced abilities of new fitering mechanism.
 Easily combine any criteria you want, and quickly browse their values.</p>
 </>)
-const vouchers_filters = () => (<><p><img src={B37301} alt="new filters"/>
+const vouchers_filters = () => (<><p><img src={G61362} alt="new filters"/>
 Experience the new look and enhanced abilities of new fitering mechanism.
 Easily combine any criteria you want, and quickly browse their values.</p>
 </>)
 
 export const AVAILABLE_FLAGS = [{
+  name: "app_alerts",
+  displayName: "App alerts",
+  component: app_alerts,
+  visible: true,
+  content: {
+    enabled: true,
+    payload: "default",
+  }
+},{
   name: "collection_filters",
   displayName: "Collection filtering",
   component: collection_filters,
