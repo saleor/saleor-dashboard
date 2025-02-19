bf57167b163d6d709615568258e9eb7f078a834f
Paweł Chyła <pawel.chyla@saleor.io>
2025-02-19 11:07:27 +0100

    Add tests

---
+++ src/components/AppLayout/ContextualLinks/useContextualLink.test.tsx
@@ -0,0 +1,71 @@
+import { renderHook } from "@testing-library/react-hooks";
+import React, { ReactNode } from "react";
+import { IntlProvider } from "react-intl";
+
+import { useContextualLink } from "./useContextualLink";
+
+jest.mock("@dashboard/components/ProductAnalytics/useAnalytics", () => ({
+  useAnalytics: jest.fn().mockReturnValue({ trackEvent: jest.fn() }),
+}));
+
+const wrapper = ({ children }: { children: ReactNode }) => (
+  <IntlProvider locale="EN">{children}</IntlProvider>
+);
+
+describe("useContextualLink", () => {
+  it("should return correct link for staff_members", () => {
+    // Act
+    const { result } = renderHook(() => useContextualLink("staff_members"), { wrapper });
+
+    // Assert
+    expect(result).toMatchSnapshot();
+  });
+
+  it("should return correct link for extending_saleor", () => {
+    // Act
+    const { result } = renderHook(() => useContextualLink("extending_saleor"), { wrapper });
+
+    // Assert
+    expect(result).toMatchSnapshot();
+  });
+
+  it("should return correct link for dev_panel", () => {
+    // Act
+    const { result } = renderHook(() => useContextualLink("dev_panel"), { wrapper });
+
+    // Assert
+    expect(result).toMatchSnapshot();
+  });
+
+  it("should return correct link for order_list", () => {
+    // Act
+    const { result } = renderHook(() => useContextualLink("order_list"), { wrapper });
+
+    // Assert
+    expect(result).toMatchSnapshot();
+  });
+
+  it("should return correct link for product_list", () => {
+    // Act
+    const { result } = renderHook(() => useContextualLink("product_list"), { wrapper });
+
+    // Assert
+    expect(result).toMatchSnapshot();
+  });
+
+  it("should return correct link for webhooks_events", () => {
+    // Act
+    const { result } = renderHook(() => useContextualLink("webhooks_events"), { wrapper });
+
+    // Assert
+    expect(result).toMatchSnapshot();
+  });
+
+  it("should return correct link for gift_cards", () => {
+    // Act
+    const { result } = renderHook(() => useContextualLink("gift_cards"), { wrapper });
+
+    // Assert
+    expect(result).toMatchSnapshot();
+  });
+});
