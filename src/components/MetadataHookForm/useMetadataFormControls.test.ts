3626e8e041338d5b41ab3379a8389a82760f3b1f
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2025-02-21 14:04:05 +0100

    Simplify tests

--- src/components/MetadataHookForm/useMetadataFormControls.test.ts
+++ src/components/MetadataHookForm/useMetadataFormControls.test.ts
@@ -28,7 +28,6 @@ describe("useMetadataFormControls", () => {
       {
         key: "key",
         value: "value",
-        // hook-form adds id for lists
         id: expect.any(String),
       },
     ]);
@@ -41,296 +40,238 @@ describe("useMetadataFormControls", () => {
     ]);
   });
 
-  describe("metadata change handler", () => {
-    it("handles update action for metadata key", () => {
-      const formMethods = renderHook(() =>
-        useForm<MetadataFormData>({
-          values: {
-            metadata: [{ key: "old-key", value: "test-value" }],
-            privateMetadata: [],
-          },
-        }),
-      );
-
-      const { result } = renderHook(() =>
-        useMetadataFormControls({
-          control: formMethods.result.current.control,
-          trigger: formMethods.result.current.trigger,
-          getValues: formMethods.result.current.getValues,
-        }),
-      );
-
-      const event: ChangeEvent = {
-        target: {
-          name: `metadata:0:${EventDataField.name}`,
-          value: "new-key",
+  it("handles update action for metadata and privateMetadata keys", () => {
+    const formMethods = renderHook(() =>
+      useForm<MetadataFormData>({
+        values: {
+          metadata: [{ key: "old-key", value: "test-value" }],
+          privateMetadata: [{ key: "old-private-key", value: "private-test-value" }],
         },
-      };
+      }),
+    );
 
-      act(() => {
-        result.current.handleMetadataChange(event);
-      });
+    const { result } = renderHook(() =>
+      useMetadataFormControls({
+        control: formMethods.result.current.control,
+        trigger: formMethods.result.current.trigger,
+        getValues: formMethods.result.current.getValues,
+      }),
+    );
 
-      expect(formMethods.result.current.getValues("metadata.0")).toEqual({
-        key: "new-key",
-        value: "test-value",
-      });
+    const metadataEvent: ChangeEvent = {
+      target: {
+        name: `metadata:0:${EventDataField.name}`,
+        value: "new-key",
+      },
+    };
+
+    const privateMetadataEvent: ChangeEvent = {
+      target: {
+        name: `privateMetadata:0:${EventDataField.name}`,
+        value: "new-private-key",
+      },
+    };
+
+    act(() => {
+      result.current.handleMetadataChange(metadataEvent);
+      result.current.handlePrivateMetadataChange(privateMetadataEvent);
     });
 
-    it("handles update action for metadata value", () => {
-      const formMethods = renderHook(() =>
-        useForm<MetadataFormData>({
-          values: {
-            metadata: [{ key: "test-key", value: "old-value" }],
-            privateMetadata: [],
-          },
-        }),
-      );
+    expect(formMethods.result.current.getValues("metadata.0")).toEqual({
+      key: "new-key",
+      value: "test-value",
+    });
+    expect(formMethods.result.current.getValues("privateMetadata.0")).toEqual({
+      key: "new-private-key",
+      value: "private-test-value",
+    });
+  });
 
-      const { result } = renderHook(() =>
-        useMetadataFormControls({
-          control: formMethods.result.current.control,
-          trigger: formMethods.result.current.trigger,
-          getValues: formMethods.result.current.getValues,
-        }),
-      );
-
-      const event: ChangeEvent = {
-        target: {
-          name: `metadata:0:${EventDataField.value}`,
-          value: "new-value",
+  it("handles update action for metadata and privateMetadata values", () => {
+    const formMethods = renderHook(() =>
+      useForm<MetadataFormData>({
+        values: {
+          metadata: [{ key: "test-key", value: "old-value" }],
+          privateMetadata: [{ key: "private-test-key", value: "old-private-value" }],
         },
-      };
+      }),
+    );
 
-      act(() => {
-        result.current.handleMetadataChange(event);
-      });
+    const { result } = renderHook(() =>
+      useMetadataFormControls({
+        control: formMethods.result.current.control,
+        trigger: formMethods.result.current.trigger,
+        getValues: formMethods.result.current.getValues,
+      }),
+    );
 
-      expect(formMethods.result.current.getValues("metadata.0")).toEqual({
-        key: "test-key",
+    const metadataEvent: ChangeEvent = {
+      target: {
+        name: `metadata:0:${EventDataField.value}`,
         value: "new-value",
-      });
+      },
+    };
+
+    const privateMetadataEvent: ChangeEvent = {
+      target: {
+        name: `privateMetadata:0:${EventDataField.value}`,
+        value: "new-private-value",
+      },
+    };
+
+    act(() => {
+      result.current.handleMetadataChange(metadataEvent);
+      result.current.handlePrivateMetadataChange(privateMetadataEvent);
     });
 
-    it("handles add action for metadata", () => {
-      const formMethods = renderHook(() =>
-        useForm<MetadataFormData>({
-          values: {
-            metadata: [],
-            privateMetadata: [],
-          },
-        }),
-      );
-
-      const { result } = renderHook(() =>
-        useMetadataFormControls({
-          control: formMethods.result.current.control,
-          trigger: formMethods.result.current.trigger,
-          getValues: formMethods.result.current.getValues,
-        }),
-      );
-
-      const event: ChangeEvent = {
-        target: {
-          name: EventDataAction.add,
-          value: "",
-        },
-      };
-
-      act(() => {
-        result.current.handleMetadataChange(event);
-      });
-
-      expect(formMethods.result.current.getValues("metadata")).toEqual([
-        {
-          key: "",
-          value: "",
-        },
-      ]);
+    expect(formMethods.result.current.getValues("metadata.0")).toEqual({
+      key: "test-key",
+      value: "new-value",
     });
-
-    it("handles delete action for metadata", () => {
-      const formMethods = renderHook(() =>
-        useForm<MetadataFormData>({
-          values: {
-            metadata: [{ key: "test", value: "value" }],
-            privateMetadata: [],
-          },
-        }),
-      );
-
-      const { result } = renderHook(() =>
-        useMetadataFormControls({
-          control: formMethods.result.current.control,
-          trigger: formMethods.result.current.trigger,
-          getValues: formMethods.result.current.getValues,
-        }),
-      );
-
-      const event: ChangeEvent = {
-        target: {
-          name: EventDataAction.delete,
-          value: 0,
-        },
-      };
-
-      act(() => {
-        result.current.handleMetadataChange(event);
-      });
-
-      expect(formMethods.result.current.getValues("metadata")).toEqual([]);
+    expect(formMethods.result.current.getValues("privateMetadata.0")).toEqual({
+      key: "private-test-key",
+      value: "new-private-value",
     });
   });
 
-  describe("private metadata change handler", () => {
-    it("handles update action for private metadata key", () => {
-      const formMethods = renderHook(() =>
-        useForm<MetadataFormData>({
-          values: {
-            metadata: [],
-            privateMetadata: [{ key: "old-key", value: "test-value" }],
-          },
-        }),
-      );
-
-      const { result } = renderHook(() =>
-        useMetadataFormControls({
-          control: formMethods.result.current.control,
-          trigger: formMethods.result.current.trigger,
-          getValues: formMethods.result.current.getValues,
-        }),
-      );
-
-      const event: ChangeEvent = {
-        target: {
-          name: `privateMetadata:0:${EventDataField.name}`,
-          value: "new-key",
+  it("handles add action for metadata and privateMetadata", () => {
+    const formMethods = renderHook(() =>
+      useForm<MetadataFormData>({
+        values: {
+          metadata: [],
+          privateMetadata: [],
         },
-      };
+      }),
+    );
 
-      act(() => {
-        result.current.handlePrivateMetadataChange(event);
-      });
+    const { result } = renderHook(() =>
+      useMetadataFormControls({
+        control: formMethods.result.current.control,
+        trigger: formMethods.result.current.trigger,
+        getValues: formMethods.result.current.getValues,
+      }),
+    );
 
-      expect(formMethods.result.current.getValues("privateMetadata.0")).toEqual({
-        key: "new-key",
-        value: "test-value",
-      });
+    const event: ChangeEvent = {
+      target: {
+        name: EventDataAction.add,
+        value: "",
+      },
+    };
+
+    act(() => {
+      result.current.handleMetadataChange(event);
+      result.current.handlePrivateMetadataChange(event);
     });
 
-    it("handles add action for private metadata", () => {
-      const formMethods = renderHook(() =>
-        useForm<MetadataFormData>({
-          values: {
-            metadata: [],
-            privateMetadata: [],
-          },
-        }),
-      );
-
-      const { result } = renderHook(() =>
-        useMetadataFormControls({
-          control: formMethods.result.current.control,
-          trigger: formMethods.result.current.trigger,
-          getValues: formMethods.result.current.getValues,
-        }),
-      );
-
-      const event: ChangeEvent = {
-        target: {
-          name: EventDataAction.add,
-          value: "",
-        },
-      };
-
-      act(() => {
-        result.current.handlePrivateMetadataChange(event);
-      });
-
-      expect(formMethods.result.current.getValues("privateMetadata")).toEqual([
-        {
-          key: "",
-          value: "",
-        },
-      ]);
-    });
-
-    it("handles delete action for private metadata", () => {
-      const formMethods = renderHook(() =>
-        useForm<MetadataFormData>({
-          values: {
-            metadata: [],
-            privateMetadata: [{ key: "test", value: "value" }],
-          },
-        }),
-      );
-
-      const { result } = renderHook(() =>
-        useMetadataFormControls({
-          control: formMethods.result.current.control,
-          trigger: formMethods.result.current.trigger,
-          getValues: formMethods.result.current.getValues,
-        }),
-      );
-
-      const event: ChangeEvent = {
-        target: {
-          name: EventDataAction.delete,
-          value: 0,
-        },
-      };
-
-      act(() => {
-        result.current.handlePrivateMetadataChange(event);
-      });
-
-      expect(formMethods.result.current.getValues("privateMetadata")).toEqual([]);
-    });
+    expect(formMethods.result.current.getValues("metadata")).toEqual([
+      {
+        key: "",
+        value: "",
+      },
+    ]);
+    expect(formMethods.result.current.getValues("privateMetadata")).toEqual([
+      {
+        key: "",
+        value: "",
+      },
+    ]);
   });
 
-  describe("metadata validation", () => {
-    it("validates and rejects duplicate keys in metadata", async () => {
-      const { result: reactHookForm } = renderHook(() =>
-        useForm<MetadataFormData>({
-          values: {
-            metadata: [{ key: "test", value: "value1" }],
-            privateMetadata: [],
-          },
-        }),
-      );
-
-      const { result: metadataFormControls } = renderHook(() =>
-        useMetadataFormControls({
-          control: reactHookForm.current.control,
-          trigger: reactHookForm.current.trigger,
-          getValues: reactHookForm.current.getValues,
-        }),
-      );
-
-      const eventAdd: ChangeEvent = {
-        target: {
-          name: EventDataAction.add,
-          value: "new-value",
+  it("handles delete action for metadata and privateMetadata", () => {
+    const formMethods = renderHook(() =>
+      useForm<MetadataFormData>({
+        values: {
+          metadata: [{ key: "test", value: "value" }],
+          privateMetadata: [{ key: "private-test", value: "private-value" }],
         },
-      };
-      const eventUpdate: ChangeEvent = {
-        target: {
-          name: `metadata:1:${EventDataField.name}`,
-          value: "test",
-        },
-      };
+      }),
+    );
 
-      await act(async () => {
-        metadataFormControls.current.handleMetadataChange(eventAdd);
-        metadataFormControls.current.handleMetadataChange(eventUpdate);
-      });
+    const { result } = renderHook(() =>
+      useMetadataFormControls({
+        control: formMethods.result.current.control,
+        trigger: formMethods.result.current.trigger,
+        getValues: formMethods.result.current.getValues,
+      }),
+    );
 
-      expect(reactHookForm.current.formState.errors.metadata).toMatchObject({
-        root: {
-          type: "validate",
-          message: "Metadata keys must be unique, remove duplicate key",
+    const event: ChangeEvent = {
+      target: {
+        name: EventDataAction.delete,
+        value: 0,
+      },
+    };
+
+    act(() => {
+      result.current.handleMetadataChange(event);
+      result.current.handlePrivateMetadataChange(event);
+    });
+
+    expect(formMethods.result.current.getValues("metadata")).toEqual([]);
+    expect(formMethods.result.current.getValues("privateMetadata")).toEqual([]);
+  });
+
+  it("validates and rejects duplicate keys in both metadata and privateMetadata", async () => {
+    const { result: reactHookForm } = renderHook(() =>
+      useForm<MetadataFormData>({
+        values: {
+          metadata: [{ key: "test", value: "value1" }],
+          privateMetadata: [{ key: "private-test", value: "private-value1" }],
         },
-      });
+      }),
+    );
+
+    const { result: metadataFormControls } = renderHook(() =>
+      useMetadataFormControls({
+        control: reactHookForm.current.control,
+        trigger: reactHookForm.current.trigger,
+        getValues: reactHookForm.current.getValues,
+      }),
+    );
+
+    const eventAdd: ChangeEvent = {
+      target: {
+        name: EventDataAction.add,
+        value: "",
+      },
+    };
+
+    const metadataUpdate: ChangeEvent = {
+      target: {
+        name: `metadata:1:${EventDataField.name}`,
+        value: "test",
+      },
+    };
+
+    const privateMetadataUpdate: ChangeEvent = {
+      target: {
+        name: `privateMetadata:1:${EventDataField.name}`,
+        value: "private-test",
+      },
+    };
+
+    await act(async () => {
+      // Add
+      metadataFormControls.current.handleMetadataChange(eventAdd);
+      metadataFormControls.current.handlePrivateMetadataChange(eventAdd);
+
+      // Update
+      metadataFormControls.current.handleMetadataChange(metadataUpdate);
+      metadataFormControls.current.handlePrivateMetadataChange(privateMetadataUpdate);
+    });
+
+    expect(reactHookForm.current.formState.errors.metadata).toMatchObject({
+      root: {
+        type: "validate",
+        message: "Metadata keys must be unique, remove duplicate key",
+      },
+    });
+    expect(reactHookForm.current.formState.errors.privateMetadata).toMatchObject({
+      root: {
+        type: "validate",
+        message: "Metadata keys must be unique, remove duplicate key",
+      },
     });
   });
 });
