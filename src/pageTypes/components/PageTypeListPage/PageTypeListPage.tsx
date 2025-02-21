eafaf9eda19706f5c5bbe813bf2fa3a9c1dd9339
Paweł Chyła <chyla1988@gmail.com>
2025-02-21 14:50:23 +0100

    Migrate from tabs to filter presets in page types list (#5423)
    * init
    
    * Change SearchInput
    
    * Add changeset
    
    * Extract messages
    
    * Update copy

--- src/pageTypes/components/PageTypeListPage/PageTypeListPage.tsx
+++ src/pageTypes/components/PageTypeListPage/PageTypeListPage.tsx
@@ -1,13 +1,15 @@
+import SearchInput from "@dashboard/components/AppLayout/ListFilters/components/SearchInput";
 import { TopNav } from "@dashboard/components/AppLayout/TopNav";
-import { Button } from "@dashboard/components/Button";
 import { DashboardCard } from "@dashboard/components/Card";
+import { FilterPresetsSelect } from "@dashboard/components/FilterPresetsSelect";
 import { ListPageLayout } from "@dashboard/components/Layouts";
-import SearchBar from "@dashboard/components/SearchBar";
 import { configurationMenuUrl } from "@dashboard/configuration";
 import { PageTypeFragment } from "@dashboard/graphql";
+import useNavigator from "@dashboard/hooks/useNavigator";
 import { sectionNames } from "@dashboard/intl";
 import { pageTypeAddUrl, PageTypeListUrlSortField } from "@dashboard/pageTypes/urls";
-import React from "react";
+import { Box, Button, ChevronRightIcon } from "@saleor/macaw-ui-next";
+import React, { useState } from "react";
 import { FormattedMessage, useIntl } from "react-intl";
 
 import {
@@ -24,8 +26,11 @@ export interface PageTypeListPageProps
     ListActions,
     SearchPageProps,
     SortPage<PageTypeListUrlSortField>,
-    TabPageProps {
+    Omit<TabPageProps, "onTabDelete"> {
   pageTypes: PageTypeFragment[];
+  onTabUpdate: (tabName: string) => void;
+  onTabDelete: (id: number) => void;
+  hasPresetsChanged: () => boolean;
 }
 
 const PageTypeListPage: React.FC<PageTypeListPageProps> = ({
@@ -36,38 +41,71 @@ const PageTypeListPage: React.FC<PageTypeListPageProps> = ({
   onTabChange,
   onTabDelete,
   onTabSave,
+  onTabUpdate,
+  hasPresetsChanged,
   tabs,
   ...listProps
 }) => {
   const intl = useIntl();
+  const navigate = useNavigator();
+  const [isFilterPresetOpen, setFilterPresetOpen] = useState(false);
 
   return (
     <ListPageLayout>
-      <TopNav href={configurationMenuUrl} title={intl.formatMessage(sectionNames.pageTypes)}>
-        <Button variant="primary" href={pageTypeAddUrl} data-test-id="create-page-type">
+      <TopNav
+        isAlignToRight={false}
+        withoutBorder
+        href={configurationMenuUrl}
+        title={intl.formatMessage(sectionNames.pageTypes)}
+      >
+        <Box __flex={1} display="flex" justifyContent="space-between" alignItems="center">
+          <Box display="flex">
+            <Box marginX={3} display="flex" alignItems="center">
+              <ChevronRightIcon />
+            </Box>
+
+            <FilterPresetsSelect
+              presetsChanged={hasPresetsChanged()}
+              onSelect={onTabChange}
+              onRemove={onTabDelete}
+              onUpdate={onTabUpdate}
+              savedPresets={tabs}
+              activePreset={currentTab}
+              onSelectAll={onAll}
+              onSave={onTabSave}
+              isOpen={isFilterPresetOpen}
+              onOpenChange={setFilterPresetOpen}
+              selectAllLabel={intl.formatMessage({
+                id: "ER0uUn",
+                defaultMessage: "All page types",
+                description: "select all page types preset label",
+              })}
+            />
+          </Box>
+        </Box>
+
+        <Button
+          onClick={() => navigate(pageTypeAddUrl)}
+          variant="primary"
+          data-test-id="create-page-type"
+        >
           <FormattedMessage id="6JlXeD" defaultMessage="Create page type" description="button" />
         </Button>
       </TopNav>
-      <DashboardCard>
-        <SearchBar
-          allTabLabel={intl.formatMessage({
-            id: "oVDZUb",
-            defaultMessage: "All Page Types",
-            description: "tab name",
-          })}
-          currentTab={currentTab}
-          initialSearch={initialSearch}
-          searchPlaceholder={intl.formatMessage({
-            id: "umsU70",
-            defaultMessage: "Search Page Type",
-          })}
-          tabs={tabs}
-          onAll={onAll}
-          onSearchChange={onSearchChange}
-          onTabChange={onTabChange}
-          onTabDelete={onTabDelete}
-          onTabSave={onTabSave}
-        />
+      <DashboardCard gap={0}>
+        <Box paddingX={6} marginBottom={2}>
+          <Box __width="320px">
+            <SearchInput
+              initialSearch={initialSearch}
+              placeholder={intl.formatMessage({
+                id: "3UUjQv",
+                defaultMessage: "Search page types...",
+              })}
+              onSearchChange={onSearchChange}
+            />
+          </Box>
+        </Box>
+
         <PageTypeList {...listProps} />
       </DashboardCard>
     </ListPageLayout>
