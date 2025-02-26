fd747d16db4cae56430d2ba22404280a2bf1a927
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2025-02-26 17:15:22 +0100

    Show notification on metadata save

--- src/orders/components/OrderMetadataDialog/useHandleSubmit.ts
+++ src/orders/components/OrderMetadataDialog/useHandleSubmit.ts
@@ -1,7 +1,10 @@
 import { MetadataFormData } from "@dashboard/components/Metadata";
 import { useUpdateMetadataMutation, useUpdatePrivateMetadataMutation } from "@dashboard/graphql";
+import useNotifier from "@dashboard/hooks/useNotifier";
+import { commonMessages } from "@dashboard/intl";
 import createMetadataUpdateHandler from "@dashboard/utils/handlers/metadataUpdateHandler";
 import { useMemo, useRef } from "react";
+import { useIntl } from "react-intl";
 
 import { OrderMetadataDialogData } from "./OrderMetadataDialog";
 
@@ -10,6 +13,8 @@ export const useHandleOrderLineMetadataSubmit = ({
 }: {
   initialData: OrderMetadataDialogData | undefined;
 }) => {
+  const notify = useNotifier();
+  const intl = useIntl();
   const [updateMetadata] = useUpdateMetadataMutation({});
   const [updatePrivateMetadata] = useUpdatePrivateMetadataMutation({});
 
@@ -28,10 +33,17 @@ export const useHandleOrderLineMetadataSubmit = ({
     );
   }, [initialData, updateMetadata, updatePrivateMetadata]);
 
-  const onSubmit = (data: MetadataFormData) => {
+  const onSubmit = async (data: MetadataFormData) => {
     submittedData.current = data;
 
-    return submitHandler(data);
+    const errors = await submitHandler(data);
+
+    if (Array.isArray(errors) && errors.length === 0) {
+      notify({
+        status: "success",
+        text: intl.formatMessage(commonMessages.savedChanges),
+      });
+    }
   };
 
   return { onSubmit, lastSubmittedData: submittedData.current };
