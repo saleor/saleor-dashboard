3915500604929dd7be107c2f13518abe1c93acc4
Wojciech Mista <wojciech.mista@hotmail.com>
2025-03-17 12:50:56 +0100

    Add sorting and use latest attempt date in event delivery (#5477)
    * add sorting and use latest
    
    * unify datetime format
    
    * i18n
    
    * expand only one webhook

--- src/components/EventTime/EventTime.tsx
+++ src/components/EventTime/EventTime.tsx
@@ -1,7 +1,7 @@
 import useLocale from "@dashboard/hooks/useLocale";
 import React from "react";
 
-const EventTime: React.FC<{ date: string }> = ({ date }) => {
+const EventTime: React.FC<{ date: string; showSeconds?: boolean }> = ({ date, showSeconds }) => {
   const { locale } = useLocale();
   const intl = new Intl.DateTimeFormat(locale, {
     timeZoneName: "short",
@@ -10,6 +10,7 @@ const EventTime: React.FC<{ date: string }> = ({ date }) => {
     day: "numeric",
     hour: "2-digit",
     minute: "2-digit",
+    second: showSeconds ? "2-digit" : undefined,
   });
 
   return <time dateTime={date}>{intl.format(new Date(date))}</time>;
