4b9a8b52697484f560bc0d7455bfa88596bc121f
Wojciech <wojciech.mista@hotmail.com>
2025-03-17 11:54:45 +0100

    clear form after submit

--- src/auth/components/LoginPage/form.tsx
+++ src/auth/components/LoginPage/form.tsx
@@ -12,6 +12,7 @@ export interface UseLoginFormResult {
   change: FormChange;
   data: LoginFormData;
   submit: () => SubmitPromise;
+  reset: () => void;
 }
 
 export interface LoginFormProps {
@@ -32,12 +33,13 @@ const getLoginFormData = () => {
 
 function useLoginForm(onSubmit: (data: LoginFormData) => SubmitPromise): UseLoginFormResult {
   const form = useForm(getLoginFormData());
-  const { change, data } = form;
+  const { change, data, reset } = form;
   const handleFormSubmit = useHandleFormSubmit({ onSubmit });
   const submit = async () => handleFormSubmit(data);
 
   return {
     change,
+    reset,
     data,
     submit,
   };
@@ -48,6 +50,8 @@ const LoginForm: React.FC<LoginFormProps> = ({ children, onSubmit }) => {
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     await props.submit();
+
+    props.reset(); // clear fields after submit
   };
 
   return <form onSubmit={handleSubmit}>{children(props)}</form>;
