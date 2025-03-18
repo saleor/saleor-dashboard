f7115e09e3e76b0c06cea5b6146eb9a2e21cb478
Paweł Chyła <chyla1988@gmail.com>
2025-03-18 11:49:39 +0100

    Extensions routes and menu setup under ff (#5479)
    * Prepare flag
    
    * Prepare base routing
    
    * Show extensions section in sidebar under ff
    
    * Block extensions routes
    
    * Add changeset

---
+++ src/extensions/index.tsx
@@ -0,0 +1,47 @@
+import { Route } from "@dashboard/components/Router";
+import { WindowTitle } from "@dashboard/components/WindowTitle";
+import { exploreExtensionsPath, installedExtensionsPath } from "@dashboard/extensions/urls";
+import { useFlag } from "@dashboard/featureFlags";
+import useNavigator from "@dashboard/hooks/useNavigator";
+import { sectionNames } from "@dashboard/intl";
+import React from "react";
+import { useIntl } from "react-intl";
+import { Switch } from "react-router-dom";
+
+const ExploreExtensions = () => {
+  return (
+    <div>
+      <h1>Explore Extensions</h1>
+    </div>
+  );
+};
+
+const InstalledExtensions = () => {
+  return (
+    <div>
+      <h1>Installed Extensions</h1>
+    </div>
+  );
+};
+
+export const ExtensionsSection = () => {
+  const intl = useIntl();
+  const navigate = useNavigator();
+  const { enabled: isExtensionsEnabled } = useFlag("extensions");
+
+  if (!isExtensionsEnabled) {
+    navigate("/", { replace: true });
+
+    return <>Redirecting...</>;
+  }
+
+  return (
+    <>
+      <WindowTitle title={intl.formatMessage(sectionNames.extensions)} />
+      <Switch>
+        <Route exact path={exploreExtensionsPath} component={ExploreExtensions} />
+        <Route exact path={installedExtensionsPath} component={InstalledExtensions} />
+      </Switch>
+    </>
+  );
+};
