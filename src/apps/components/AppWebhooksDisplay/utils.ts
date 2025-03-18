10598ebfc5f145ad1d404dc57997018e14efb191
Wojciech <wojciech.mista@hotmail.com>
2025-03-18 18:16:01 +0100

    sort webhooks by latest attempt

--- src/apps/components/AppWebhooksDisplay/utils.ts
+++ src/apps/components/AppWebhooksDisplay/utils.ts
@@ -3,9 +3,25 @@ import { AppWebhookDeliveriesQuery } from "@dashboard/graphql";
 export type Webhook = NonNullable<NonNullable<AppWebhookDeliveriesQuery["app"]>["webhooks"]>[0];
 
 export const sortWebhooksByDeliveries = (a: Webhook, b: Webhook) => {
-  const deliveriesA = a.eventDeliveries?.edges?.length ?? 0;
-  const deliveriesB = b.eventDeliveries?.edges?.length ?? 0;
+  const getLatestDate = (webhook: Webhook) => {
+    const attemptDate =
+      webhook.eventDeliveries?.edges?.[0]?.node?.attempts?.edges?.[0]?.node?.createdAt;
+    const deliveryDate = webhook.eventDeliveries?.edges?.[0]?.node?.createdAt;
+
+    return attemptDate || deliveryDate;
+  };
+
+  const dateA = getLatestDate(a);
+  const dateB = getLatestDate(b);
+
+  if (!dateA && !dateB) {
+    return 0;
+  }
+
+  if (!dateA) return 1;
+
+  if (!dateB) return -1;
 
   // desc
-  return deliveriesB - deliveriesA;
+  return new Date(dateB).getTime() - new Date(dateA).getTime();
 };
