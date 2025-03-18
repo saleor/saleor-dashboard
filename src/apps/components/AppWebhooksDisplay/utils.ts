d6777e906f4d56793203e1ec7732b362012f2629
Wojciech <wojciech.mista@hotmail.com>
2025-03-18 18:31:59 +0100

    small improvements

--- src/apps/components/AppWebhooksDisplay/utils.ts
+++ src/apps/components/AppWebhooksDisplay/utils.ts
@@ -2,15 +2,19 @@ import { AppWebhookDeliveriesQuery } from "@dashboard/graphql";
 
 export type Webhook = NonNullable<NonNullable<AppWebhookDeliveriesQuery["app"]>["webhooks"]>[0];
 
-export const sortWebhooksByDeliveries = (a: Webhook, b: Webhook) => {
-  const getLatestDate = (webhook: Webhook) => {
-    const attemptDate =
-      webhook.eventDeliveries?.edges?.[0]?.node?.attempts?.edges?.[0]?.node?.createdAt;
-    const deliveryDate = webhook.eventDeliveries?.edges?.[0]?.node?.createdAt;
+const getLatestDate = (webhook: Webhook): string | undefined => {
+  const attemptDate: string | undefined =
+    webhook.eventDeliveries?.edges?.[0]?.node?.attempts?.edges?.[0]?.node?.createdAt;
+  const deliveryDate: string | undefined = webhook.eventDeliveries?.edges?.[0]?.node?.createdAt;
 
-    return attemptDate || deliveryDate;
-  };
+  if (attemptDate && deliveryDate) {
+    return attemptDate > deliveryDate ? attemptDate : deliveryDate;
+  }
 
+  return attemptDate || deliveryDate;
+};
+
+export const sortWebhooksByDeliveries = (a: Webhook, b: Webhook): number => {
   const dateA = getLatestDate(a);
   const dateB = getLatestDate(b);
 
