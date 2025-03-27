5714e42c7e3a5f2b467bcedafcd311557a34fa60
Paweł Chyła <pawel.chyla@saleor.io>
2025-03-27 14:35:06 +0100

    Fix e2e tests

--- playwright/pages/extensionsPage.ts
+++ playwright/pages/extensionsPage.ts
@@ -2,7 +2,7 @@ import { URL_LIST } from "@data/url";
 import { BasePage } from "@pages/basePage";
 import { expect, Page } from "@playwright/test";
 
-export class AppsPage extends BasePage {
+export class ExtensionsPage extends BasePage {
   readonly page: Page;
 
   readonly basePage: BasePage;
@@ -10,16 +10,13 @@ export class AppsPage extends BasePage {
   constructor(
     page: Page,
     readonly installExternalAppButton = page.getByTestId("add-app-from-manifest"),
-    readonly appLogo = page.getByTestId("app-logo"),
-    readonly installedAppsList = page.getByTestId("apps-installed"),
-    readonly availableAppsList = page.getByTestId("apps-available"),
-    readonly upcomingAppsList = page.getByTestId("apps-upcoming"),
+    readonly installedExtensionsList = page.getByTestId("apps-installed"),
+    readonly availableExtensions = page.getByTestId("extensions-list"),
 
     readonly appManifestUrlInput = page.getByTestId("manifest-url-input").locator("input"),
     readonly installAppFromManifestButton = page.getByTestId("install-app-from-manifest"),
     readonly installedAppRow = page.getByTestId("apps:installed-app-row"),
     readonly appKlaviyo = page.getByTestId("app-klaviyo"),
-    readonly appAdyen = page.getByTestId("app-adyen"),
     readonly appQA = page.getByTestId("app-saleorqa app"),
     readonly installationPendingLabel = page.getByTestId("app-pending-label").first(),
     readonly availableAppsLoader = page.getByTestId("available-apps-loader"),
@@ -29,8 +26,8 @@ export class AppsPage extends BasePage {
     this.basePage = new BasePage(page);
   }
 
-  async gotoAppsList() {
-    await this.page.goto(URL_LIST.apps);
+  async gotoExtensionsList() {
+    await this.page.goto(URL_LIST.extensions);
   }
 
   async typeManifestUrl(manifestUrl: string) {
