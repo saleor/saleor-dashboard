fb4038dbe5dbe90073e4f6e9639cf2ff8c902b91
Paweł Chyła <chyla1988@gmail.com>
2025-03-28 10:25:16 +0100

    Extesnions: update e2e tests and turn on feture flag (#5513)
    * Turn on ff
    
    * Fix e2e tests
    
    * Update test

--- playwright/tests/singlePermissions/readonlyExtensionsAccess.spec.ts
+++ playwright/tests/singlePermissions/readonlyExtensionsAccess.spec.ts
@@ -2,7 +2,7 @@ import { URL_LIST } from "@data/url";
 import { permissions } from "@data/userPermissions";
 import { AppDetailsPage } from "@pages/appDetailsPage";
 import { AppPage } from "@pages/appPageThirdparty";
-import { AppsPage } from "@pages/appsPage";
+import { ExtensionsPage } from "@pages/extensionsPage";
 import { MainMenuPage } from "@pages/mainMenuPage";
 import { expect } from "@playwright/test";
 import { test } from "utils/testWithPermission";
@@ -13,27 +13,23 @@ const permissionList = permissions.filter(item => item !== permissionToExclude);
 for (const permission of permissionList) {
   test.use({ permissionName: permission });
 
-  test(`TC: SALEOR_131 User with ${permission} permissions should have readonly access to Apps #e2e #apps`, async ({
+  test(`TC: SALEOR_131 User with ${permission} permissions should have readonly access to installed extensions #e2e #apps`, async ({
     page,
   }) => {
     const mainMenuPage = new MainMenuPage(page);
-    const appsPage = new AppsPage(page);
+    const extensionsPage = new ExtensionsPage(page);
     const appPage = new AppPage(page);
     const appDetailsPage = new AppDetailsPage(page);
 
     await page.goto(URL_LIST.homePage);
-    await mainMenuPage.openApps();
-    await appsPage.waitForContentLoad();
+    await mainMenuPage.openExtensions();
+    await extensionsPage.waitForContentLoad();
 
-    await expect(appsPage.installExternalAppButton).not.toBeVisible();
+    await expect(extensionsPage.installExternalAppButton).not.toBeVisible();
 
-    const appLists = [appsPage.installedAppsList, appsPage.availableAppsList];
+    await expect(extensionsPage.installedExtensionsList).toBeVisible();
 
-    for (const appList of appLists) {
-      await expect(appList).toBeVisible();
-    }
-
-    await appsPage.installedAppRow.first().click();
+    await extensionsPage.installedAppRow.first().click();
     await expect(appPage.appSettingsButton).toBeVisible();
     await appPage.appSettingsButton.click();
     await appPage.waitContentLoad();
@@ -48,5 +44,20 @@ for (const permission of permissionList) {
     for (const button of buttons) {
       await expect(button).toBeDisabled();
     }
+  });
+
+  test(`TC: SALEOR_131 User with ${permission} permissions should have readonly access to explore extensions #e2e #apps`, async ({
+    page,
+  }) => {
+    const mainMenuPage = new MainMenuPage(page);
+    const extensionsPage = new ExtensionsPage(page);
+
+    await page.goto(URL_LIST.homePage);
+    await mainMenuPage.openExploreExtensions();
+    await extensionsPage.waitForContentLoad();
+
+    await expect(extensionsPage.installExternalAppButton).not.toBeVisible();
+
+    await expect(extensionsPage.availableExtensions).toBeVisible();
   });
 }
