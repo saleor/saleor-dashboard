967e9bdf920c7fe54aa901cfd14eaa605ac3d2d8
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2025-04-25 17:40:27 +0200

    Restore unintended changes

--- src/welcomePage/WelcomePageTilesContainer/WelcomePageTilesContainer.tsx
+++ src/welcomePage/WelcomePageTilesContainer/WelcomePageTilesContainer.tsx
@@ -3,7 +3,7 @@ import { Box } from "@saleor/macaw-ui-next";
 import React from "react";
 import { useIntl } from "react-intl";
 
-import { CommunityLiveUpdate, getTilesData } from "./tileData";
+import { getTilesData } from "./tileData";
 import { WelcomePageInfoTile } from "./WelcomePageInfoTile";
 
 export const WelcomePageTilesContainer = () => {
@@ -25,7 +25,6 @@ export const WelcomePageTilesContainer = () => {
       gap={6}
       marginTop={7}
     >
-      <CommunityLiveUpdate onTileButtonClick={handleTileButtonClick} />
       {tiles.map(tile => (
         <WelcomePageInfoTile key={tile.id} {...tile} />
       ))}
