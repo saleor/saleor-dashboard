92ef408b68db9f23d26a1e23f129c3c5f74d6a31
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2025-04-25 15:50:30 +0200

    New extensions install page (#5522)
    * Add install custom extension page
    
    * Add InputWithPlaceholder component
    
    * Fix navbar height
    
    * Add HookFormInput, debounced auto submit
    
    * Add loading state, fix auto submit to always trigger onSubmit
    
    * Add immediate submit after paste
    
    * WIP: Manifest details
    
    * Add installation functionality
    
    * Add manifest details content
    
    * Add messages
    
    * Add back button functionality
    
    * Don't use history from react-router
    
    * Clear manifest data when input changes
    
    * Add matching line color
    
    * Fix message and privacy link
    
    * Remove unused file
    
    * Move files
    
    * Improve border-radius
    
    * Fix auto-submit + change
    
    * WIP: refactor
    
    * WIP: refactor
    
    * Refactor
    
    * Remove unused code
    
    * Extract messages
    
    * Remove unused code
    
    * Fix TS error with css modules
    
    * Fix TS error
    
    * Refactor
    
    * Refactor CR
    
    * Add missing translation, fix extract-messages command
    
    * CR add comments
    
    * Fix TS file
    
    * Add tests
    
    * Fix TS types
    
    * Add hooks tests
    
    * Add tests for useFetchManifest
    
    * Add tests for useInstallApp
    
    * Add tests for auto-submit hook, fix issues on unmount
    
    * forwardRef
    
    * Add tests for InstallCustomExtension, fix bug with onPaste
    
    * Fix failing test
    
    * Fix failing test
    
    * Add changeset
    
    * Fixed TypeScript error with generic + forwardRef
    
    * Simplify onPaste debounce management logic
    
    * CR
    
    * CR
    
    * Revert "Simplify onPaste debounce management logic"
    
    This reverts commit a0f862852eefb1c400d71cf2185f29341f9f2c08.
    
    * Update test text assertion
    
    ---------
    
    Co-authored-by: Paweł Chyła <chyla1988@gmail.com>

---
+++ src/components/HookFormInput/HookFormInput.tsx
@@ -0,0 +1,54 @@
+import { ErrorCircle } from "@dashboard/icons/ErrorCircle";
+import { fixedForwardRef } from "@dashboard/utils/ref";
+import { Box } from "@saleor/macaw-ui-next";
+import React, { ComponentProps } from "react";
+import { FieldValues, useController, UseControllerProps } from "react-hook-form";
+
+import { InputWithPlaceholder } from "../InputWithPlaceholder/InputWithPlaceholder";
+
+const HookFormInputInner = <TFormValues extends FieldValues>(
+  {
+    name,
+    rules,
+    control,
+    disabled,
+    defaultValue,
+    shouldUnregister,
+    ...componentProps
+  }: UseControllerProps<TFormValues> & ComponentProps<typeof InputWithPlaceholder>,
+  ref: React.ForwardedRef<HTMLInputElement>,
+) => {
+  const { field, fieldState } = useController({
+    name,
+    rules,
+    control,
+    disabled,
+    defaultValue,
+    shouldUnregister,
+  });
+
+  return (
+    <Box display="flex" flexDirection="column" gap={2}>
+      <InputWithPlaceholder
+        {...field}
+        error={!!fieldState.error}
+        aria-invalid={!!fieldState.error}
+        aria-errormessage={fieldState.error?.message}
+        {...componentProps}
+        ref={ref}
+      />
+      {fieldState.error && (
+        <Box color="default2" display="flex" alignItems="center" gap={2} aria-hidden="true">
+          <ErrorCircle width="16px" height="16px" />
+          {fieldState.error.message}
+        </Box>
+      )}
+    </Box>
+  );
+};
+
+export const HookFormInput = fixedForwardRef(HookFormInputInner);
+
+// Cannot use generic + forwardRef and Component.displayName in the same type
+// See fixedForwardRef
+(HookFormInput as any).displayName = "HookFormInput";
