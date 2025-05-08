590bc8299d607f9136974a522f2a4ea2cbb82dbc
Wojciech <wojciech.mista@hotmail.com>
2025-05-08 15:25:54 +0200

    return/replace

--- src/orders/components/OrderReturnPage/OrderReturnRefundItemsCard/MaximalButton.tsx
+++ src/orders/components/OrderReturnPage/OrderReturnRefundItemsCard/MaximalButton.tsx
@@ -1,34 +1,21 @@
-import { Button } from "@dashboard/components/Button";
-import { makeStyles } from "@saleor/macaw-ui";
+import { Button } from "@saleor/macaw-ui-next";
 import React from "react";
 import { FormattedMessage } from "react-intl";
-
-const useStyles = makeStyles(
-  theme => ({
-    button: {
-      marginBottom: theme.spacing(1),
-      marginTop: theme.spacing(3),
-    },
-  }),
-  { name: "MaximalButton" },
-);
 
 interface MaximalButtonProps {
   onClick: () => void;
 }
 
-const MaximalButton: React.FC<MaximalButtonProps> = ({ onClick }) => {
-  const classes = useStyles({});
-
-  return (
-    <Button
-      className={classes.button}
-      onClick={onClick}
-      data-test-id="set-maximal-quantity-unfulfilled-button"
-    >
-      <FormattedMessage id="2W4EBM" defaultMessage="Set maximal quantities" description="button" />
-    </Button>
-  );
-};
+const MaximalButton: React.FC<MaximalButtonProps> = ({ onClick }) => (
+  <Button
+    variant="secondary"
+    onClick={onClick}
+    data-test-id="set-maximal-quantity-unfulfilled-button"
+    marginBottom={3}
+    size="small"
+  >
+    <FormattedMessage id="2W4EBM" defaultMessage="Set maximal quantities" description="button" />
+  </Button>
+);
 
 export default MaximalButton;
