df7b08ea0c1f4dd24ec6c0f63c7c86d7c17698c8
Wojciech <wojciech.mista@hotmail.com>
2025-05-12 11:38:34 +0200

    radio channel update

--- src/channels/components/ChannelAllocationStrategy/styles.ts
+++ src/channels/components/ChannelAllocationStrategy/styles.ts
@@ -2,10 +2,6 @@ import { makeStyles } from "@saleor/macaw-ui";
 
 export const useStyles = makeStyles(
   theme => ({
-    option: {
-      marginTop: theme.spacing(-0.25),
-      marginBottom: theme.spacing(),
-    },
     tooltipIcon: {
       fill: theme.palette.type === "dark" ? "#FAFAFA" : "#28234A",
       fillOpacity: 0.6,
