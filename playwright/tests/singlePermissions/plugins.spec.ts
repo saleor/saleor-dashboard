3da674267702cf1585967306d760ee8430ccf1df
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2025-05-12 17:12:01 +0200

    Update plugins test

--- playwright/tests/singlePermissions/plugins.spec.ts
+++ playwright/tests/singlePermissions/plugins.spec.ts
@@ -1,23 +1,47 @@
-import { ConfigurationPage } from "@pages/configurationPage";
+import { ExtensionsPage } from "@pages/extensionsPage";
 import { MainMenuPage } from "@pages/mainMenuPage";
-import { PluginsPage } from "@pages/pluginsPage";
 import { expect } from "@playwright/test";
 import { test } from "utils/testWithPermission";
 
 test.use({ permissionName: "plugin" });
 
-let configurationPage: ConfigurationPage;
 let mainMenuPage: MainMenuPage;
-let pluginsPage: PluginsPage;
+let extensionsPage: ExtensionsPage;
 
-test.beforeEach(({ page }) => {
-  configurationPage = new ConfigurationPage(page);
+const expectedExtensions = [
+  "Admin emails",
+  "Adyen",
+  "Avalara",
+  "Dummy",
+  "OpenID Connect",
+  "Stripe",
+  "User emails",
+  "Webhooks",
+];
+
+test.beforeEach(async ({ page }) => {
   mainMenuPage = new MainMenuPage(page);
-  pluginsPage = new PluginsPage(page);
+  extensionsPage = new ExtensionsPage(page);
+
+  // TODO: Remove this once removed feature flag
+  await page.goto("/"); // Navigate to root to set localStorage on correct origin
+  await page.evaluate(() => {
+    localStorage.setItem("FF_extensions_dev", '{"enabled":true,"payload":"default"}');
+  });
+  await page.reload(); // Ensure the app picks up the flags and renders the sidebar
 });
-test("TC: SALEOR_16 User should be able to navigate to plugin list as a staff member using PLUGINS permission #e2e", async () => {
-  await configurationPage.goToConfigurationView();
-  await configurationPage.openPlugins();
-  await expect(pluginsPage.pluginRow.first()).toBeVisible();
-  await mainMenuPage.expectMenuItemsCount(3);
+
+test("TC: SALEOR_16 User should be able to navigate to installed extensions and view plugin details as a staff member using PLUGINS permission #e2e", async ({
+  page,
+}) => {
+  await extensionsPage.gotoExtensionsList();
+  await expect(extensionsPage.installedExtensionsList).toBeVisible();
+  await extensionsPage.expectInstalledPluginRowsCount(expectedExtensions.length);
+
+  await Promise.all(
+    expectedExtensions.map(name => extensionsPage.expectPluginRowVisibleByName(name)),
+  );
+
+  await extensionsPage.clickViewDetailsByPluginName("Admin emails");
+  await extensionsPage.expectPluginDetailsViewVisible();
 });
