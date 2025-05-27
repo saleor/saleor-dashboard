6003df0fe52267dbe4c0df2b41a2952d8738a4bc
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2025-05-27 15:28:25 +0200

    Show correct plugin installation state on explore extensions

--- src/extensions/views/ExploreExtensions/hooks/useExploreExtensions.ts
+++ src/extensions/views/ExploreExtensions/hooks/useExploreExtensions.ts
@@ -1,16 +1,50 @@
+import { useUserPermissions } from "@dashboard/auth/hooks/useUserPermissions";
 import { getExtensionsConfig } from "@dashboard/config";
 import { ExtensionData, ExtensionsGroups } from "@dashboard/extensions/types";
 import { useFlag } from "@dashboard/featureFlags";
-import { InstalledAppFragment, useInstalledAppsQuery } from "@dashboard/graphql";
+import {
+  InstalledAppFragment,
+  PermissionEnum,
+  PluginsQuery,
+  useInstalledAppsQuery,
+  usePluginsQuery,
+} from "@dashboard/graphql";
 import { mapEdgesToItems } from "@dashboard/utils/maps";
 
 import { useAppStoreExtensions } from "./useAppStoreExtensions";
 
 const byAppType = (extension: ExtensionData) => extension.type === "APP";
 
-const toExtension = (extension: ExtensionData, installedApps: InstalledAppFragment[]) => {
+const isPluginEnabled = (plugin: PluginsQuery["plugins"]["edges"][0] | undefined) => {
+  if (!plugin) {
+    return false;
+  }
+
+  if (plugin?.node?.globalConfiguration) {
+    return plugin.node.globalConfiguration.active;
+  }
+
+  if (plugin?.node?.channelConfigurations) {
+    return plugin.node.channelConfigurations?.some(config => config.active);
+  }
+
+  return false;
+};
+
+const toExtension = (
+  extension: ExtensionData,
+  installedApps: InstalledAppFragment[],
+  allPlugins: PluginsQuery["plugins"]["edges"] | undefined,
+) => {
   // Implement checking is plugin installed in phase 3
-  if (extension.type === "PLUGIN") return extension;
+  if (extension.type === "PLUGIN") {
+    return {
+      ...extension,
+      installed: allPlugins
+        ? isPluginEnabled(allPlugins?.find(plugin => plugin.node.id === extension.id))
+        : false,
+    };
+  }
 
   const installedApp = installedApps.find(
     installedExtension => installedExtension.identifier === extension.id,
@@ -27,14 +61,20 @@ const toExtension = (extension: ExtensionData, installedApps: InstalledAppFragme
   };
 };
 
-const getFilteredExtensions = (
-  extensions: ExtensionData[],
-  installedApps: InstalledAppFragment[],
-  showAllExtensions: boolean,
-) => {
+const getFilteredExtensions = ({
+  extensions,
+  installedApps,
+  showAllExtensions,
+  allPlugins,
+}: {
+  extensions: ExtensionData[];
+  installedApps: InstalledAppFragment[];
+  showAllExtensions: boolean;
+  allPlugins: PluginsQuery["plugins"]["edges"] | undefined;
+}) => {
   const filteredExtensions = showAllExtensions ? extensions : extensions.filter(byAppType);
 
-  return filteredExtensions.map(extension => toExtension(extension, installedApps));
+  return filteredExtensions.map(extension => toExtension(extension, installedApps, allPlugins));
 };
 
 export const useExploreExtensions = () => {
@@ -44,6 +84,20 @@ export const useExploreExtensions = () => {
       first: 100,
     },
   });
+  const { enabled: isExtensionsDevEnabled } = useFlag("extensions_dev");
+  const userPermissions = useUserPermissions();
+  const hasManagePluginsPermission = !!userPermissions?.find(
+    ({ code }) => code === PermissionEnum.MANAGE_PLUGINS,
+  );
+
+  const { data: plugins } = usePluginsQuery({
+    displayLoader: true,
+    variables: {
+      first: 100,
+    },
+    skip: !isExtensionsDevEnabled || !hasManagePluginsPermission,
+  });
+
   const { enabled: showAllExtensions } = useFlag("extensions_dev");
 
   const installedApps = mapEdgesToItems(installedAppsData?.apps) ?? [];
@@ -53,7 +107,12 @@ export const useExploreExtensions = () => {
       group,
       {
         title: extensions.title,
-        items: getFilteredExtensions(extensions.items, installedApps, showAllExtensions),
+        items: getFilteredExtensions({
+          extensions: extensions.items,
+          installedApps,
+          showAllExtensions,
+          allPlugins: plugins?.plugins?.edges || [],
+        }),
       },
     ]),
   ) as ExtensionsGroups;
