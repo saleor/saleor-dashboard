b42f1f30276efbf2322647144bebe081ea3a0bb4
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2025-05-30 18:22:41 +0200

    Fix failing test

--- playwright/tests/singlePermissions/channelsWebhooks.spec.ts
+++ playwright/tests/singlePermissions/channelsWebhooks.spec.ts
@@ -1,5 +1,6 @@
 import { ChannelPage } from "@pages/channelsPage";
 import { ConfigurationPage } from "@pages/configurationPage";
+import { HomePage } from "@pages/homePage";
 import { MainMenuPage } from "@pages/mainMenuPage";
 import { expect } from "@playwright/test";
 import { test } from "utils/testWithPermission";
@@ -9,11 +10,16 @@ test.use({ permissionName: "channel" });
 let channelPage: ChannelPage;
 let mainMenuPage: MainMenuPage;
 let configurationPage: ConfigurationPage;
+let home: HomePage;
 
 test.beforeEach(async ({ page }) => {
   channelPage = new ChannelPage(page);
   mainMenuPage = new MainMenuPage(page);
   configurationPage = new ConfigurationPage(page);
+  home = new HomePage(page);
+
+  await home.goto();
+  await home.welcomeMessage.waitFor({ state: "visible", timeout: 30000 });
 });
 
 test("TC: SALEOR_11 User should be able to navigate to channel list as a staff member using CHANNEL permission #e2e", async () => {
