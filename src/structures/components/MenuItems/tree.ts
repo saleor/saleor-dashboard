e525950238f6846cee5f325842adad896e84e21e
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2025-06-11 17:09:15 +0200

    Update modeling URLs (#5644)

---
+++ src/structures/components/MenuItems/tree.ts
@@ -0,0 +1,89 @@
+// @ts-strict-ignore
+
+import { MenuTreeItem } from "@dashboard/structures/types";
+import { getPatch } from "fast-array-diff";
+
+export type TreeOperationType = "move" | "remove";
+export interface TreeOperation {
+  id: string;
+  type: TreeOperationType;
+  parentId?: string;
+  sortOrder?: number;
+  simulatedMove?: boolean;
+}
+
+export function getDiff(originalTree: MenuTreeItem[], newTree: MenuTreeItem[]): TreeOperation[] {
+  const originalMap = treeToMap(originalTree, "root");
+  const newMap = treeToMap(newTree, "root");
+  const diff: TreeOperation[] = Object.keys(newMap).flatMap(key => {
+    const originalNode = originalMap[key];
+    const newNode = newMap[key];
+    const patch = getPatch(originalNode, newNode);
+
+    if (patch.length > 0) {
+      const addedNode = patch.find(operation => operation.type === "add");
+      const removedNode = patch.find(operation => operation.type === "remove");
+
+      if (addedNode) {
+        const changedParent = originalNode.length !== newNode.length;
+        const sortOrder = removedNode ? addedNode.newPos - removedNode.oldPos : addedNode.newPos;
+
+        // This exists because backend doesn't recongize the position of the new node
+        // when it's moved from child to parent and/or up
+        // We have to make an additional move so that backend can sort the new tree correctly
+        // because without it the new node goes to the end of the parent node array by default
+        // SimulatedMove is removed before submit
+        if (changedParent && sortOrder !== originalNode.length) {
+          return [
+            {
+              id: addedNode.items[0],
+              parentId: key === "root" ? undefined : key,
+              sortOrder: newNode.length - 1,
+              type: "move" as TreeOperationType,
+              simulatedMove: true,
+            },
+            {
+              id: addedNode.items[0],
+              parentId: key === "root" ? undefined : key,
+              sortOrder:
+                sortOrder - newNode.length < 0
+                  ? sortOrder - newNode.length + 1
+                  : sortOrder - newNode.length - 1,
+              type: "move" as TreeOperationType,
+            },
+          ];
+        }
+
+        return {
+          id: addedNode.items[0],
+          parentId: key === "root" ? undefined : key,
+          sortOrder,
+          type: "move" as TreeOperationType,
+        };
+      }
+    }
+
+    return undefined;
+  });
+
+  return diff.filter(d => !!d);
+}
+
+function treeToMap(tree: MenuTreeItem[], parent: string): Record<string, string[]> {
+  const childrenList = tree.map(node => node.id);
+  const childrenMaps = tree.map(node => ({
+    id: node.id,
+    mappedNodes: treeToMap(node.children as MenuTreeItem[], node.id.toString()),
+  }));
+
+  return {
+    [parent]: childrenList,
+    ...childrenMaps.reduce(
+      (acc, childMap) => ({
+        ...acc,
+        ...childMap.mappedNodes,
+      }),
+      {},
+    ),
+  };
+}
---
+++ src/structures/components/MenuListPage/MenuListPage.tsx
@@ -0,0 +1,99 @@
+import { TopNav } from "@dashboard/components/AppLayout/TopNav";
+import { ButtonGroupWithDropdown } from "@dashboard/components/ButtonGroupWithDropdown";
+import { ListPageLayout } from "@dashboard/components/Layouts";
+import { extensionMountPoints } from "@dashboard/extensions/extensionMountPoints";
+import {
+  getExtensionItemsForOverviewCreate,
+  getExtensionsItemsForMenuOverviewActions,
+} from "@dashboard/extensions/getExtensionsItems";
+import { useExtensions } from "@dashboard/extensions/hooks/useExtensions";
+import { MenuFragment } from "@dashboard/graphql";
+import useNavigator from "@dashboard/hooks/useNavigator";
+import { sectionNames } from "@dashboard/intl";
+import { menuListUrl, MenuListUrlSortField } from "@dashboard/structures/urls";
+import { ListActions, PageListProps, SortPage } from "@dashboard/types";
+import { Box, Button, ChevronRightIcon, Text } from "@saleor/macaw-ui-next";
+import React from "react";
+import { FormattedMessage, useIntl } from "react-intl";
+
+import MenuList from "../MenuList";
+
+export interface MenuListPageProps
+  extends PageListProps,
+    ListActions,
+    SortPage<MenuListUrlSortField> {
+  menus: MenuFragment[];
+  onDelete: (id: string) => void;
+  selectedMenuIds: string[];
+}
+
+const MenuListPage: React.FC<MenuListPageProps> = ({ selectedMenuIds, ...listProps }) => {
+  const intl = useIntl();
+  const navigate = useNavigator();
+
+  const handleCreateMenu = () => {
+    navigate(menuListUrl({ action: "add" }));
+  };
+
+  const { MENU_OVERVIEW_CREATE, MENU_OVERVIEW_MORE_ACTIONS } = useExtensions(
+    extensionMountPoints.MENU_LIST,
+  );
+
+  const extensionMenuItems = getExtensionsItemsForMenuOverviewActions(
+    MENU_OVERVIEW_MORE_ACTIONS,
+    selectedMenuIds,
+  );
+  const extensionCreateButtonItems = getExtensionItemsForOverviewCreate(MENU_OVERVIEW_CREATE);
+
+  return (
+    <ListPageLayout>
+      <TopNav
+        title={intl.formatMessage(sectionNames.structures)}
+        isAlignToRight={false}
+        withoutBorder
+      >
+        <Box __flex={1} display="flex" justifyContent="space-between" alignItems="center">
+          <Box display="flex">
+            <Box marginX={5} display="flex" alignItems="center">
+              <ChevronRightIcon />
+            </Box>
+            <Text size={6}>
+              <FormattedMessage
+                id="vDp2tH"
+                defaultMessage="All structures"
+                description="all structures"
+              />
+            </Text>
+          </Box>
+          <Box display="flex" alignItems="center" gap={2}>
+            {extensionMenuItems.length > 0 && <TopNav.Menu items={extensionMenuItems} />}
+            {extensionCreateButtonItems.length > 0 ? (
+              <ButtonGroupWithDropdown
+                options={extensionCreateButtonItems}
+                onClick={handleCreateMenu}
+                data-test-id="add-menu"
+              >
+                <FormattedMessage
+                  id="0dCGBW"
+                  defaultMessage="Create structure"
+                  description="button"
+                />
+              </ButtonGroupWithDropdown>
+            ) : (
+              <Button onClick={handleCreateMenu} variant="primary" data-test-id="add-menu">
+                <FormattedMessage
+                  id="0dCGBW"
+                  defaultMessage="Create structure"
+                  description="button"
+                />
+              </Button>
+            )}
+          </Box>
+        </Box>
+      </TopNav>
+      <MenuList {...listProps} />
+    </ListPageLayout>
+  );
+};
+
+export default MenuListPage;
