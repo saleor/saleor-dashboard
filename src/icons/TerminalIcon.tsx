5788687d8942cea5171911854b08fe590c6f7589
Patryk Andrzejewski <vox3r69@gmail.com>
2025-07-10 13:25:29 +0200

    Fixes for global search (#5714)
    * Table fixes
    
    * Table fixes

--- src/icons/TerminalIcon.tsx
+++ src/icons/TerminalIcon.tsx
@@ -4,8 +4,6 @@ export const TerminalIcon = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
--- src/search/resultsTable/ResultsTable.tsx
+++ src/search/resultsTable/ResultsTable.tsx
@@ -1,11 +1,17 @@
 import { GridTable } from "@dashboard/components/GridTable";
 import { GlobalSearchQuery } from "@dashboard/graphql";
-import { Text } from "@saleor/macaw-ui-next";
+import { Box, Text } from "@saleor/macaw-ui-next";
 import React from "react";
 import { FormattedMessage } from "react-intl";
 
+import { CategoryItem } from "./CategoryItem";
+import { CollectionItem } from "./CollectionItem";
+import { ModelItem } from "./ModelItem";
+import { ModelTypeItem } from "./ModelTypeItem";
+import { OrderItem } from "./OrderItem";
 import { prepareResults } from "./prepareResults";
-import { ResultItem } from "./ResultItem";
+import { ProductItem } from "./ProductItem";
+import { VariantItem } from "./VariantItem";
 
 interface ResultsTableProps {
   data: GlobalSearchQuery;
@@ -14,7 +20,7 @@ interface ResultsTableProps {
 export const ResultsTable = ({ data }: ResultsTableProps) => {
   const results = prepareResults(data);
 
-  if (results.length === 0) {
+  if (results.empty) {
     return (
       <Text textAlign="center" paddingTop={32} color="default2" fontSize={4} fontWeight="medium">
         <FormattedMessage id="hX5PAb" defaultMessage="No results found" />
@@ -23,17 +29,109 @@ export const ResultsTable = ({ data }: ResultsTableProps) => {
   }
 
   return (
-    <GridTable>
-      <GridTable.Colgroup>
-        <GridTable.Col __width="105px" />
-        <GridTable.Col />
-        <GridTable.Col __width="160px" />
-      </GridTable.Colgroup>
-      <GridTable.Body>
-        {results.map(result => (
-          <ResultItem key={result.node.id} result={result} />
-        ))}
-      </GridTable.Body>
-    </GridTable>
+    <Box>
+      {results.orders.length > 0 && (
+        <GridTable __marginBottom="-1px">
+          <GridTable.Colgroup>
+            <GridTable.Col __width="105px" />
+            <GridTable.Col __width="1%" />
+            <GridTable.Col __width="1%" />
+            <GridTable.Col __width="1%" />
+            <GridTable.Col __width="1%" />
+            <GridTable.Col />
+            <GridTable.Col __width="160px" />
+          </GridTable.Colgroup>
+          <GridTable.Body>
+            {results.orders.map(result => (
+              <OrderItem key={result.id} node={result} />
+            ))}
+          </GridTable.Body>
+        </GridTable>
+      )}
+      {results.categories.length > 0 && (
+        <GridTable __marginBottom="-1px">
+          <GridTable.Colgroup>
+            <GridTable.Col __width="105px" />
+            <GridTable.Col />
+            <GridTable.Col __width="160px" />
+          </GridTable.Colgroup>
+          <GridTable.Body>
+            {results.categories.map(result => (
+              <CategoryItem key={result.id} node={result} />
+            ))}
+          </GridTable.Body>
+        </GridTable>
+      )}
+      {results.collections.length > 0 && (
+        <GridTable __marginBottom="-1px">
+          <GridTable.Colgroup>
+            <GridTable.Col __width="105px" />
+            <GridTable.Col />
+            <GridTable.Col __width="160px" />
+          </GridTable.Colgroup>
+          <GridTable.Body>
+            {results.collections.map(result => (
+              <CollectionItem key={result.id} node={result} />
+            ))}
+          </GridTable.Body>
+        </GridTable>
+      )}
+      {results.products.length > 0 && (
+        <GridTable __marginBottom="-1px">
+          <GridTable.Colgroup>
+            <GridTable.Col __width="105px" />
+            <GridTable.Col />
+            <GridTable.Col __width="160px" />
+          </GridTable.Colgroup>
+          <GridTable.Body>
+            {results.products.map(result => (
+              <ProductItem key={result.id} node={result} />
+            ))}
+          </GridTable.Body>
+        </GridTable>
+      )}
+      {results.productVariants.length > 0 && (
+        <GridTable __marginBottom="-1px">
+          <GridTable.Colgroup>
+            <GridTable.Col __width="105px" />
+            <GridTable.Col />
+            <GridTable.Col __width="160px" />
+          </GridTable.Colgroup>
+          <GridTable.Body>
+            {results.productVariants.map(result => (
+              <VariantItem key={result.id} node={result} />
+            ))}
+          </GridTable.Body>
+        </GridTable>
+      )}
+      {results.models.length > 0 && (
+        <GridTable __marginBottom="-1px">
+          <GridTable.Colgroup>
+            <GridTable.Col __width="105px" />
+            <GridTable.Col />
+            <GridTable.Col __width="160px" />
+          </GridTable.Colgroup>
+          <GridTable.Body>
+            {results.models.map(result => (
+              <ModelItem key={result.id} node={result} />
+            ))}
+          </GridTable.Body>
+        </GridTable>
+      )}
+      {results.modelTypes.length > 0 && (
+        <GridTable __marginBottom="-1px">
+          <GridTable.Colgroup>
+            <GridTable.Col __width="105px" />
+            <GridTable.Col />
+            <GridTable.Col __width="160px" />
+          </GridTable.Colgroup>
+          <GridTable.Body>
+            {results.modelTypes.map(result => (
+              <ModelTypeItem key={result.id} node={result} />
+            ))}
+          </GridTable.Body>
+        </GridTable>
+      )}
+    </Box>
   );
 };
