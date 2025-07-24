e5d04c78656269e51961dcaa3826609cb2285ba5
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2025-07-24 17:03:04 +0200

    Remove placeholders

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,7 +8,6 @@ const config: StorybookConfig = {
   "addons": [
     "@chromatic-com/storybook",
     "@storybook/addon-docs",
-    "@storybook/addon-onboarding",
     "@storybook/addon-a11y",
   ],
   "framework": {
