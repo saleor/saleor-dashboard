a62304fc38bec1f46cae45364b5a90e3670af429
andrzejewsky <vox3r69@gmail.com>
2025-07-28 10:04:50 +0200

    Fix linters

--- groupBy.d.ts
+++ groupBy.d.ts
@@ -1,10 +1,10 @@
 /*
-  This file is a workaround to fix the type error in the `groupBy` function.
-  It is a workaround because the `groupBy` function is not available in the
-  `ObjectConstructor` interface Typescript < 5.4.
+  This file is a workaround to fix the type error when using the `groupBy` method.
+  The `groupBy` method is not available in the `ObjectConstructor` interface in Typescript < 5.4.
 
   TODO: Remove this file when we upgrade to Typescript >= 5.4.
 */
+
 
 interface ObjectConstructor {
   groupBy<K extends PropertyKey, T>(
