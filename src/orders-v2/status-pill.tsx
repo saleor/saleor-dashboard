2409cb9625d98b1acefb21055fb5112ca31b9f74
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-08-04 13:55:20 +0200

    Add base order v2 page struct (#5758)

---
+++ src/orders-v2/status-pill.tsx
@@ -0,0 +1,39 @@
+import { PillStatusType } from "@dashboard/misc";
+import { Box, Text } from "@saleor/macaw-ui-next";
+import React from "react";
+
+const getFillColorsBasedOnStatus = (status: PillStatusType) => {
+  switch (status) {
+    case "success":
+      return {
+        __backgroundColor: "oklch(94% 0.0898 164deg)",
+        __borderColor: "oklch(79% 0.204 153deg)",
+      };
+  }
+};
+
+// TODO: move to MacawUI
+export const StatusPill = ({
+  status,
+  children,
+}: {
+  status: PillStatusType;
+  children: React.ReactNode;
+}) => {
+  return (
+    <Box
+      paddingX={3}
+      paddingY={1}
+      borderRadius={6}
+      borderWidth={1}
+      borderStyle="solid"
+      data-test-id="status-info"
+      data-macaw-ui-candidate
+      {...getFillColorsBasedOnStatus(status)}
+    >
+      <Text color="default1" fontWeight="medium" size={2}>
+        {children}
+      </Text>
+    </Box>
+  );
+};
