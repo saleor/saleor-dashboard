c25beaeb9a721fc72308419fbd1d4a41fd7fb4e1
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-08-06 11:54:32 +0200

    fixes after cr

--- src/orders-v2/underline-link.tsx
+++ src/orders-v2/underline-link.tsx
@@ -6,15 +6,22 @@ interface UnderlineLinkProps extends LinkProps {
   textProps?: Omit<TextProps, "children">;
 }
 
-// TODO: move to MacawUI
 export const UnderlineLink = ({ children, textProps, ...props }: UnderlineLinkProps) => (
   <Link {...props}>
     <UnderlineText {...textProps}>{children}</UnderlineText>
   </Link>
 );
 
+// TODO: move to MacawUI
 export const UnderlineText = ({ children, ...props }: TextProps) => (
-  <Text as="span" textDecoration="underline" fontWeight="medium" size={2} {...props}>
+  <Text
+    as="span"
+    textDecoration="underline"
+    fontWeight="medium"
+    size={2}
+    data-macaw-ui-candidate
+    {...props}
+  >
     {children}
   </Text>
 );
