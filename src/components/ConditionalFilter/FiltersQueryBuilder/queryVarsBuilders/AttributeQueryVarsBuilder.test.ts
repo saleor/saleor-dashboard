165ec2c5a62adee72387125e531882b80c0067d9
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2025-08-07 18:22:00 +0200

    test: fix test

--- src/components/ConditionalFilter/FiltersQueryBuilder/queryVarsBuilders/AttributeQueryVarsBuilder.test.ts
+++ src/components/ConditionalFilter/FiltersQueryBuilder/queryVarsBuilders/AttributeQueryVarsBuilder.test.ts
@@ -176,7 +176,7 @@ describe("AttributeQueryVarsBuilder", () => {
       );
       const selected = ConditionSelected.fromConditionItemAndValue(baseConditionItem, {
         label: pageLabel,
-        value: "page-1",
+        value: "UGFnZTox", // Global ID (base64 encoded "Page:1")
         slug: "page-1",
       });
       const condition = new Condition(
@@ -189,17 +189,17 @@ describe("AttributeQueryVarsBuilder", () => {
       const result = def.updateWhereQueryVariables({}, element);
 
       // Assert
-      expect(result).toEqual({ 
-        attributes: [{ 
-          slug: attributeSlug, 
-          value: { 
-            reference: { 
-              referencedIds: { 
-                containsAny: ["page-1"] 
-              } 
-            } 
-          } 
-        }] 
+      expect(result).toEqual({
+        attributes: [{
+          slug: attributeSlug,
+          value: {
+            reference: {
+              referencedIds: {
+                containsAny: ["UGFnZTox"]
+              }
+            }
+          }
+        }]
       });
     });
 
