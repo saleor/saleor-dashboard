e8f1d33bb3f58d37be80cfc06c82942b665484cc
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-08-26 10:56:24 +0200

    last touches

--- src/orders-v2/order-fulfillments/order-fulfillment-status-icon.tsx
+++ src/orders-v2/order-fulfillments/order-fulfillment-status-icon.tsx
@@ -5,11 +5,11 @@ import React from "react";
 export const OrderFulfillmentStatusIcon = ({ status }: { status: FulfillmentStatus }) => {
   switch (status) {
     case FulfillmentStatus.FULFILLED:
-      return <PackageIcon color="#02DD77" size={17} />;
+      return <PackageIcon color="oklch(78.7% 0.203 153deg)" size={17} />;
     case FulfillmentStatus.REFUNDED:
-      return <Banknote color="#1C7EFF" size={17} />;
+      return <Banknote color="oklch(61.4% 0.21 258deg)" size={17} />;
     case FulfillmentStatus.RETURNED:
-      return <PackageIcon color="#4395FF" size={17} />;
+      return <PackageIcon color="oklch(67.1% 0.176 256deg)" size={17} />;
     case FulfillmentStatus.REPLACED:
       return <ReplaceIcon size={17} />;
     default:
