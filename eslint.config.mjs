043999d068161001df7cf0a945144ff76136d92d
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2025-09-04 12:04:23 +0200

    Modify rule to error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -160,7 +160,15 @@ export default tseslint.config(
       "no-restricted-imports": [
         "error",
         {
-          paths: ["lodash", "classnames"],
+          paths: [
+            "lodash",
+            "classnames",
+            {
+              name: "react",
+              importNames: ["default", "React"],
+              message: "Import directly the needed functions, e.g. 'import {useState} from \"react\"'",
+            }
+          ],
         },
       ],
     },
@@ -227,11 +235,6 @@ export default tseslint.config(
             {
               name: "moment-timezone",
               message: "Use react-intl formatDate instead of moment-timezone.",
-            },
-            {
-              name: "react",
-              importNames: ["default", "React"],
-              message: "Import directly the needed functions, e.g. 'import {useState} from \"react\"'",
             }
           ],
         },
