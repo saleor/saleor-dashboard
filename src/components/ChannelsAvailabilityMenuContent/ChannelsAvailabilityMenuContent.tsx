c699c38b57d94e97e55a7f8ee6d730a371c0a354
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2025-09-11 18:14:06 +0200

    Add missing code from plugins

--- src/components/ChannelsAvailabilityMenuContent/ChannelsAvailabilityMenuContent.tsx
+++ src/components/ChannelsAvailabilityMenuContent/ChannelsAvailabilityMenuContent.tsx
@@ -1,7 +1,7 @@
 // @ts-strict-ignore
 import HorizontalSpacer from "@dashboard/components/HorizontalSpacer";
+import ScrollableContent from "@dashboard/components/ScrollableContent";
 import { CollectionFragment } from "@dashboard/graphql";
-import ScrollableContent from "@dashboard/plugins/components/PluginsList/PluginAvailabilityStatusPopup/ScrollableContent";
 import { PillColor } from "@saleor/macaw-ui";
 import { Text } from "@saleor/macaw-ui-next";
 import { MessageDescriptor, useIntl } from "react-intl";
--- src/extensions/utils.tsx
+++ src/extensions/utils.tsx
@@ -1,5 +1,5 @@
 import { gql } from "@apollo/client";
-import { AppErrorCode, AppErrorFragment } from "@dashboard/graphql";
+import { AppErrorCode, AppErrorFragment, WebhookEventTypeAsyncEnum, WebhookFragment } from "@dashboard/graphql";
 import errorTracker from "@dashboard/services/errorTracking";
 import { IntlShape } from "react-intl";
 
@@ -134,3 +134,17 @@ const isEvent = ({ name }: { name: string }) => name === "Event";
 
 export const buildEventsMap = (elements: IntrospectionNode[]) =>
   elements.filter(({ interfaces }) => (interfaces || []).some(isEvent));
+
+export function isUnnamed(webhook: WebhookFragment | undefined): boolean {
+  return !webhook?.name;
+}
+
+export const filterSelectedAsyncEvents = (asyncEvents: WebhookEventTypeAsyncEnum[]) => {
+  const anyEvent = asyncEvents.find(event => event === WebhookEventTypeAsyncEnum.ANY_EVENTS);
+
+  if (anyEvent) {
+    return [anyEvent];
+  }
+
+  return asyncEvents;
+};
