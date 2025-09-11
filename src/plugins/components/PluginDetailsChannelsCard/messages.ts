4c87d2111fb05f1685699782d96e2057c1e03be8
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2025-09-11 18:13:18 +0200

    Remove plugins

--- src/plugins/components/PluginDetailsChannelsCard/messages.ts
+++
@@ -1,9 +0,0 @@
-import { defineMessages } from "react-intl";
-
-export const pluginDetailsChannelsCardMessages = defineMessages({
-  noChannelsSubtitle: {
-    id: "Aeq79M",
-    defaultMessage: "Plugin’s settings are set common across all channels",
-    description: "PluginDetailsChannelsCard no channels subtitle",
-  },
-});
