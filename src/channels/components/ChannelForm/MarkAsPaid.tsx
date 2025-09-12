3df3c9f5719a9f7ada12ea89c52c32800450beab
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-09-12 14:18:09 +0200

    Migrate links to Saleor docs to one file (#5846)
    * Migrate links to Saleor docs to one file
    
    * Add changeset
    
    * prettier this
    
    * prettier this again

--- src/channels/components/ChannelForm/MarkAsPaid.tsx
+++ src/channels/components/ChannelForm/MarkAsPaid.tsx
@@ -1,4 +1,5 @@
 import Link from "@dashboard/components/Link";
+import { PAYMENT_APPS_DOCS_URL, PAYMENT_PLUGINS_DOCS_URL } from "@dashboard/links";
 import { Box, Checkbox, Text } from "@saleor/macaw-ui-next";
 import { FormattedMessage } from "react-intl";
 
@@ -32,11 +33,7 @@ export const MarkAsPaid = ({ hasError, isChecked, onCheckedChange, disabled }: M
           id="Qb2XN5"
           values={{
             link: (
-              <Link
-                href="https://docs.saleor.io/developer/payments/payment-apps"
-                target="_blank"
-                rel="noopener noreferer"
-              >
+              <Link href={PAYMENT_APPS_DOCS_URL} target="_blank" rel="noopener noreferer">
                 <FormattedMessage defaultMessage="Transaction" id="1+ROfp" />
               </Link>
             ),
@@ -49,11 +46,7 @@ export const MarkAsPaid = ({ hasError, isChecked, onCheckedChange, disabled }: M
           id="eR2vV/"
           values={{
             link: (
-              <Link
-                href="https://docs.saleor.io/docs/3.x/developer/payments#payment-plugin"
-                target="_blank"
-                rel="noopener noreferer"
-              >
+              <Link href={PAYMENT_PLUGINS_DOCS_URL} target="_blank" rel="noopener noreferer">
                 <FormattedMessage defaultMessage="Payment" id="NmK6zy" />
               </Link>
             ),
