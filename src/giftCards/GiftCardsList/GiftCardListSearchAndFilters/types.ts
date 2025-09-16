30976f0d3701ed329a2bb231ec4ae60112fe8ee6
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2025-09-16 18:20:03 +0200

    Remove unused enums

--- src/giftCards/GiftCardsList/GiftCardListSearchAndFilters/types.ts
+++ src/giftCards/GiftCardsList/GiftCardListSearchAndFilters/types.ts
@@ -1,36 +1,3 @@
 import { FetchMoreProps, Search, SearchProps } from "@dashboard/types";
 
-enum GiftCardListUrlFiltersEnum {
-  currency = "currency",
-  initialBalanceAmountFrom = "initialBalanceAmountFrom",
-  initialBalanceAmountTo = "initialBalanceAmountTo",
-  currentBalanceAmountFrom = "currentBalanceAmountFrom",
-  currentBalanceAmountTo = "currentBalanceAmountTo",
-  status = "status",
-}
-
-enum GiftCardListUrlFiltersWithMultipleValuesEnum {
-  tag = "tag",
-  product = "product",
-  usedBy = "usedBy",
-}
-
-enum GiftCardListFilterKeys {
-  currency = "currency",
-  balance = "balance",
-  initialBalance = "initialBalance",
-  currentBalance = "currentBalance",
-  initialBalanceAmount = "initialBalanceAmount",
-  currentBalanceAmount = "currentBalanceAmount",
-  tag = "tag",
-  product = "product",
-  usedBy = "usedBy",
-  status = "status",
-}
-
 export type SearchWithFetchMoreProps = FetchMoreProps & Search & SearchProps;
-
-enum GiftCardStatusFilterEnum {
-  enabled = "enabled",
-  disabled = "disabled",
-}
