fae878398fcbc4c4eb5b51c4bc1e57b411b5c3c2
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2025-09-16 19:32:36 +0200

    Run lint-staged on files in root directory

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,7 +2,8 @@
  * @type {import('lint-staged').Configuration}
  */
 const config = {
-  "./{src,playwright,scripts}/**/*.{js,jsx,ts,tsx}": ["eslint --cache --fix", "prettier --write"],
+  "./{src,playwright,scripts}/**/*.{js,jsx,ts,tsx,mjs,cjs}": ["eslint --cache --fix", "prettier --write"],
+  "./*.{js,jsx,ts,tsx,mjs,cjs}": ["eslint --cache --fix", "prettier --write"],
   "*.{json,css,md,yml,yaml}": ["prettier --write"],
   "package.json": "sort-package-json",
 };
