d4d61acda1f6f50f4ee167469f72dadd4672b48c
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2025-09-18 18:15:21 +0200

    Refactor into separate components, add new links to user orders

---
+++ src/orders/components/OrderCustomer/CustomerEditForm.tsx
@@ -0,0 +1,101 @@
+import { Combobox } from "@dashboard/components/Combobox";
+import Form from "@dashboard/components/Form";
+import { SearchCustomersQuery } from "@dashboard/graphql";
+import { FetchMoreProps, RelayToFlat } from "@dashboard/types";
+import createSingleAutocompleteSelectHandler from "@dashboard/utils/handlers/singleAutocompleteSelectChangeHandler";
+import React from "react";
+import { useIntl } from "react-intl";
+
+import { CustomerEditData } from "./OrderCustomer";
+
+interface CustomerEditFormProps extends Partial<FetchMoreProps> {
+  user: any;
+  userEmail: string | null;
+  users?: RelayToFlat<SearchCustomersQuery["search"]>;
+  fetchUsers?: (query: string) => void;
+  onCustomerEdit?: (data: CustomerEditData) => void;
+  loading?: boolean;
+  toggleEditMode: () => void;
+  setUserDisplayName: (name: string) => void;
+  userDisplayName: string;
+}
+
+export const CustomerEditForm: React.FC<CustomerEditFormProps> = ({
+  user,
+  userEmail,
+  users,
+  fetchUsers,
+  onCustomerEdit,
+  onFetchMore,
+  hasMore,
+  loading,
+  toggleEditMode,
+  setUserDisplayName,
+  userDisplayName,
+}) => {
+  const intl = useIntl();
+
+  return (
+    <Form confirmLeave initial={{ query: "" }}>
+      {({ change, data }) => {
+        const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+          change(event);
+
+          const value = event.target.value;
+
+          if (!value) {
+            return;
+          }
+
+          onCustomerEdit?.({
+            prevUser: user?.id,
+            prevUserEmail: userEmail ?? undefined,
+            [value.includes("@") ? "userEmail" : "user"]: value,
+          });
+          toggleEditMode();
+        };
+
+        const userChoices = (users || []).map(user => ({
+          label: user.email,
+          value: user.id,
+        }));
+
+        const handleUserChange = (event: any) => {
+          const handler = createSingleAutocompleteSelectHandler(
+            handleChange,
+            setUserDisplayName,
+            userChoices,
+          );
+
+          handler(event);
+        };
+
+        return (
+          <Combobox
+            data-test-id="select-customer"
+            allowCustomValues={true}
+            label={intl.formatMessage({
+              id: "hkSkNx",
+              defaultMessage: "Search Customers",
+            })}
+            options={userChoices}
+            fetchMore={{
+              onFetchMore: onFetchMore || (() => {}),
+              hasMore: hasMore || false,
+              loading: loading || false,
+            }}
+            fetchOptions={fetchUsers || (() => {})}
+            name="query"
+            value={{
+              label: userDisplayName,
+              value: data.query,
+            }}
+            onChange={handleUserChange}
+          />
+        );
+      }}
+    </Form>
+  );
+};
+
+CustomerEditForm.displayName = "CustomerEditForm";
---
+++ src/orders/components/OrderCustomer/CustomerSection.tsx
@@ -0,0 +1,135 @@
+import Link from "@dashboard/components/Link";
+import RequirePermissions from "@dashboard/components/RequirePermissions";
+import { customerUrl } from "@dashboard/customers/urls";
+import { OrderDetailsFragment, PermissionEnum, SearchCustomersQuery } from "@dashboard/graphql";
+import { maybe } from "@dashboard/misc";
+import { orderListUrlWithCustomerEmail, orderListUrlWithCustomerId } from "@dashboard/orders/urls";
+import { FetchMoreProps, RelayToFlat } from "@dashboard/types";
+import { Skeleton, Text } from "@saleor/macaw-ui-next";
+import React from "react";
+import { FormattedMessage } from "react-intl";
+
+import { CustomerEditForm } from "./CustomerEditForm";
+import { CustomerEditData } from "./OrderCustomer";
+
+const AnonymousCustomer = () => (
+  <Text>
+    <FormattedMessage id="Qovenh" defaultMessage="Anonymous user" />
+  </Text>
+);
+
+interface CustomerWithEmailProps {
+  userEmail: string;
+  className?: string;
+}
+
+const CustomerWithEmail = ({ userEmail, className }: CustomerWithEmailProps) => (
+  <>
+    <Text className={className}>{userEmail}</Text>
+    <div>
+      <Link underline={false} href={orderListUrlWithCustomerEmail(userEmail)}>
+        <FormattedMessage id="J4NBVR" defaultMessage="View Orders" description="link" />
+      </Link>
+    </div>
+  </>
+);
+
+interface RegisteredCustomerProps {
+  user: {
+    id: string;
+    email: string;
+  };
+  className?: string;
+  onProfileView: () => void;
+}
+
+const RegisteredCustomer = ({ user, className, onProfileView }: RegisteredCustomerProps) => (
+  <>
+    <Text className={className} data-test-id="customer-email">
+      {user.email}
+    </Text>
+    <RequirePermissions requiredPermissions={[PermissionEnum.MANAGE_USERS]}>
+      <div>
+        <Link underline={false} href={customerUrl(user.id)} onClick={onProfileView}>
+          <FormattedMessage id="VCzrEZ" defaultMessage="View Profile" description="link" />
+        </Link>
+      </div>
+    </RequirePermissions>
+    <div>
+      <Link underline={false} href={orderListUrlWithCustomerId(user.id)}>
+        <FormattedMessage id="J4NBVR" defaultMessage="View Orders" description="link" />
+      </Link>
+    </div>
+  </>
+);
+
+export interface CustomerSectionProps extends Partial<FetchMoreProps> {
+  order: OrderDetailsFragment;
+  users?: RelayToFlat<SearchCustomersQuery["search"]>;
+  loading?: boolean;
+  canEditCustomer: boolean;
+  fetchUsers?: (query: string) => void;
+  onCustomerEdit?: (data: CustomerEditData) => void;
+  onProfileView: () => void;
+  isInEditMode: boolean;
+  toggleEditMode: () => void;
+  setUserDisplayName: (name: string) => void;
+  userDisplayName: string;
+  userEmailClassName?: string;
+}
+
+export const CustomerSection: React.FC<CustomerSectionProps> = ({
+  order,
+  users,
+  loading,
+  canEditCustomer,
+  fetchUsers,
+  hasMore: hasMoreUsers,
+  onCustomerEdit,
+  onFetchMore: onFetchMoreUsers,
+  onProfileView,
+  isInEditMode,
+  toggleEditMode,
+  setUserDisplayName,
+  userDisplayName,
+  userEmailClassName,
+}) => {
+  const user = maybe(() => order.user);
+  const userEmail = maybe(() => order.userEmail);
+
+  if (user === undefined) {
+    return <Skeleton />;
+  }
+
+  if (isInEditMode && canEditCustomer) {
+    return (
+      <CustomerEditForm
+        user={user}
+        userEmail={userEmail ?? null}
+        users={users}
+        fetchUsers={fetchUsers}
+        onCustomerEdit={onCustomerEdit}
+        onFetchMore={onFetchMoreUsers}
+        hasMore={hasMoreUsers}
+        loading={loading}
+        toggleEditMode={toggleEditMode}
+        setUserDisplayName={setUserDisplayName}
+        userDisplayName={userDisplayName}
+      />
+    );
+  }
+
+  if (user === null) {
+    if (userEmail === null) {
+      return <AnonymousCustomer />;
+    }
+
+    return <CustomerWithEmail userEmail={userEmail} className={userEmailClassName} />;
+  }
+
+  return (
+    <RegisteredCustomer user={user} className={userEmailClassName} onProfileView={onProfileView} />
+  );
+};
+
+CustomerSection.displayName = "CustomerSection";
