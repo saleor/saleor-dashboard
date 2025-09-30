b880bc7ee1fdded658a7b78f36096c6802ff6741
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2025-09-30 10:56:42 +0200

    refactor

--- .featureFlags/generated.tsx
+++ .featureFlags/generated.tsx
@@ -1,5 +1,16 @@
 // @ts-nocheck
 
 
+const new_order_ui = () => (<><p>Enable brand-new UI for the Order Details page.</p>
+</>)
 
-export const AVAILABLE_FLAGS = [] as const;
+export const AVAILABLE_FLAGS = [{
+  name: "new_order_ui",
+  displayName: "New Order UI",
+  component: new_order_ui,
+  visible: false,
+  content: {
+    enabled: false,
+    payload: "default",
+  }
+}] as const;
