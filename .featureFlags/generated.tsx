8bf9fca77cea5a2d00a81b157228aacc25e66e51
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2025-09-30 10:37:15 +0200

    wip

--- .featureFlags/generated.tsx
+++ .featureFlags/generated.tsx
@@ -1,28 +1,5 @@
 // @ts-nocheck
 
 
-const new_filters = () => (<><p>Experience the new look and enhanced abilities of new filtering mechanism.
-Combine any criteria you want, and quickly browse their values.
-New filters have been added to the following pages:</p>
-<ul>
-<li>Collection list</li>
-<li>Customers list</li>
-<li>Vouchers list</li>
-<li>Draft orders list</li>
-<li>Gift cards list</li>
-<li>Content list</li>
-<li>Product types list</li>
-<li>Staff members list</li>
-</ul>
-</>)
 
-export const AVAILABLE_FLAGS = [{
-  name: "new_filters",
-  displayName: "New filtering",
-  component: new_filters,
-  visible: true,
-  content: {
-    enabled: true,
-    payload: "default",
-  }
-}] as const;
+export const AVAILABLE_FLAGS = [] as const;
