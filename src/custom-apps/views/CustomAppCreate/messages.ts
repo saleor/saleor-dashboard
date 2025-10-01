a01d3baedd4840caa7e46e69c516519781cf8537
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2025-10-01 09:46:13 +0200

    Remove unused plugin list components and filters (#5915)
    * remove plugin filters
    
    * remove bunch of files

--- src/custom-apps/views/CustomAppCreate/messages.ts
+++
@@ -1,9 +0,0 @@
-import { defineMessages } from "react-intl";
-
-export const messages = defineMessages({
-  createApp: {
-    id: "agZQkB",
-    defaultMessage: "Create App",
-    description: "window title",
-  },
-});
