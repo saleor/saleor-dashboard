3ea5353cd491474d9a9defaef604c4ad338675cd
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2025-10-02 15:45:06 +0200

    mock

---
+++ __mocks__/react-intl.ts
@@ -0,0 +1,44 @@
+import { createElement, Fragment } from "react";
+
+const useIntl = jest.fn(() => ({
+  formatMessage: jest.fn(x => x.defaultMessage),
+  formatDate: jest.fn(x => x),
+  formatTime: jest.fn(x => x),
+  formatNumber: jest.fn(x => x),
+}));
+
+const defineMessages = jest.fn(x => x);
+const defineMessage = jest.fn(x => x);
+
+const FormattedMessage = ({ defaultMessage }: { defaultMessage: string }) =>
+  createElement(Fragment, null, defaultMessage);
+
+const IntlProvider = ({ children }: { children: React.ReactNode }) =>
+  createElement(Fragment, null, children);
+
+const createIntl = jest.fn(() => ({
+  formatMessage: jest.fn(x => x.defaultMessage),
+  formatDate: jest.fn(x => x),
+  formatTime: jest.fn(x => x),
+  formatNumber: jest.fn(x => x),
+}));
+
+const FormattedDate = ({ value }: { value: any }) => createElement(Fragment, null, value);
+
+export {
+  useIntl,
+  defineMessages,
+  defineMessage,
+  FormattedMessage,
+  IntlProvider,
+  createIntl,
+  FormattedDate,
+};
+
+// Export types for TypeScript
+export type {
+  IntlShape,
+  MessageDescriptor,
+  FormattedMessageProps,
+  ReactIntlErrorCode,
+} from "react-intl";
