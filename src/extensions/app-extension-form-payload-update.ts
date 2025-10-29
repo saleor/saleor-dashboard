5b825a59ba59748f7ef4dd1299d4f7017ce88581
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2025-10-29 14:30:27 +0100

    refactor hooks

---
+++ src/extensions/app-extension-form-payload-update.ts
@@ -0,0 +1,52 @@
+import { AllFormPayloadUpdatePayloads } from "@saleor/app-sdk/app-bridge";
+import { atom, useAtom } from "jotai";
+
+const extensionFormResponseState = atom<AllFormPayloadUpdatePayloads[]>([]);
+
+type FramesByFormType = Record<
+  AllFormPayloadUpdatePayloads["form"],
+  AllFormPayloadUpdatePayloads[]
+>;
+
+// todo test
+export const extensionFormResponseByFormAtom = atom(
+  get => {
+    const states = get(extensionFormResponseState);
+
+    return states.reduce<Record<string, AllFormPayloadUpdatePayloads[]>>((acc, state) => {
+      const formKey = state.form;
+
+      if (!acc[formKey]) {
+        acc[formKey] = [];
+      }
+
+      acc[formKey].push(state);
+
+      return acc;
+    }, {} as FramesByFormType);
+  },
+  (get, set, newState: AllFormPayloadUpdatePayloads) => {
+    const currentStates = get(extensionFormResponseState);
+    // Check if this exact state already exists
+    const exists = currentStates.some(
+      state => state.form === newState.form && JSON.stringify(state) === JSON.stringify(newState),
+    );
+
+    if (!exists) {
+      set(extensionFormResponseState, [...currentStates, newState]);
+    }
+  },
+);
+
+export const useExtensionFormPayloadUpdate = () => {
+  const [extensionResponseFrames, setExtensionResponseState] = useAtom(extensionFormResponseState);
+  const [framesByFormType] = useAtom(extensionFormResponseByFormAtom);
+
+  return {
+    allFrames: extensionResponseFrames,
+    attachFormResponseFrame(response: AllFormPayloadUpdatePayloads) {
+      setExtensionResponseState(prev => [...prev, response]);
+    },
+    framesByFormType,
+  };
+};
--- src/extensions/components/AppExtensionContext/AppExtensionContextProvider.tsx
+++ src/extensions/components/AppExtensionContext/AppExtensionContextProvider.tsx
@@ -1,12 +1,11 @@
 import { APP_VERSION } from "@dashboard/config";
+import { useExtensionFormPayloadUpdate } from "@dashboard/extensions/app-extension-form-payload-update";
 import { ExtensionsUrls } from "@dashboard/extensions/urls";
 import { AppDialog } from "@dashboard/extensions/views/ViewManifestExtension/components/AppDialog/AppDialog";
 import { AppFrame } from "@dashboard/extensions/views/ViewManifestExtension/components/AppFrame/AppFrame";
 import { AppExtensionTargetEnum } from "@dashboard/graphql";
 import useNavigator from "@dashboard/hooks/useNavigator";
 import useShop from "@dashboard/hooks/useShop";
-import { AllFormPayloadUpdatePayloads } from "@saleor/app-sdk/app-bridge";
-import { atom, useAtom } from "jotai";
 import { PropsWithChildren } from "react";
 
 import { AppExtensionActiveParams, useAppExtensionPopup } from "../../app-extension-popup-state";
@@ -42,45 +41,11 @@ export const AppExtensionPopupProvider = ({ children }: PropsWithChildren) => {
   );
 };
 
-// todo this must go to frame-level, to support widgets too
-const extensionFormResponseState = atom<AllFormPayloadUpdatePayloads[]>([]);
-
-// todo test
-const extensionFormResponseByFormAtom = atom(
-  get => {
-    const states = get(extensionFormResponseState);
-
-    return states.reduce<Record<string, AllFormPayloadUpdatePayloads[]>>((acc, state) => {
-      const formKey = state.form;
-
-      if (!acc[formKey]) {
-        acc[formKey] = [];
-      }
-
-      acc[formKey].push(state);
-
-      return acc;
-    }, {});
-  },
-  (get, set, newState: AllFormPayloadUpdatePayloads) => {
-    const currentStates = get(extensionFormResponseState);
-    // Check if this exact state already exists
-    const exists = currentStates.some(
-      state => state.form === newState.form && JSON.stringify(state) === JSON.stringify(newState),
-    );
-
-    if (!exists) {
-      set(extensionFormResponseState, [...currentStates, newState]);
-    }
-  },
-);
-
 // todo extract modal from non-modal
 export const useActiveAppExtension = () => {
   const { state, setActive, setInactive, attachFormState } = useAppExtensionPopup();
   const navigate = useNavigator();
-  const [extensionResponseFrames, setExtensionResponseState] = useAtom(extensionFormResponseState);
-  const [byFormFrames] = useAtom(extensionFormResponseByFormAtom);
+  const { framesByFormType, attachFormResponseFrame } = useExtensionFormPayloadUpdate();
 
   const activate = (appData: AppExtensionActiveParams) => {
     if (appData.target === AppExtensionTargetEnum.POPUP) {
@@ -98,10 +63,7 @@ export const useActiveAppExtension = () => {
     activate,
     deactivate,
     attachFormState,
-    extensionResponseFrames,
-    attachFormResponseFrame(response: AllFormPayloadUpdatePayloads) {
-      setExtensionResponseState(prev => [...prev, response]);
-    },
-    byFormFrames,
+    attachFormResponseFrame,
+    framesByFormType,
   };
 };
