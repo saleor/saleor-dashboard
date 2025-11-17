d8837a65c29f9773dfdd16f2fb093321e663e8b7
Krzysztof Żuraw <9116238+krzysztofzuraw@users.noreply.github.com>
2025-11-17 09:00:57 +0100

    Change header icons to black color
    Update sorting arrows and grip indicator from dynamic theme colors to black (#000000) for better visibility and consistency.

--- src/components/Datagrid/headerIcons.ts
+++ src/components/Datagrid/headerIcons.ts
@@ -2,21 +2,21 @@ import { SpriteMap } from "@glideapps/glide-data-grid";
 
 export const headerIcons: SpriteMap = {
   arrowUp:
-    p => `<svg width="20" height="20"  viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    _p => `<svg width="20" height="20"  viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path
-          fill="${p.bgColor}"
+          fill="#000000"
           d="M8.067 14h7.865a.5.5 0 0 0 .385-.82l-3.933-4.72a.5.5 0 0 0-.768 0l-3.933 4.72a.5.5 0 0 0 .384.82Z"
         />
        </svg>`,
   arrowDown:
-    p => `<svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    _p => `<svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
-      fill="${p.bgColor}"
+      fill="#000000"
       d="M15.932 10H8.067a.5.5 0 0 0-.384.82l3.933 4.72a.5.5 0 0 0 .768 0l3.933-4.72a.5.5 0 0 0-.385-.82Z"
       />
     </svg>`,
   gripVertical:
-    p => `<svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    _p => `<svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <circle cx="9" cy="5" r="1.5" fill="#000000" />
       <circle cx="9" cy="12" r="1.5" fill="#000000" />
       <circle cx="9" cy="19" r="1.5" fill="#000000" />
