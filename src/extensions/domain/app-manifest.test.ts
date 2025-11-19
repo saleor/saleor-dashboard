ad55f6c5618171efc83f0a7a90991b079d9ab4cd
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2025-11-19 11:54:16 +0100

    fix test

--- src/extensions/domain/app-manifest.test.ts
+++ src/extensions/domain/app-manifest.test.ts
@@ -1,4 +1,4 @@
-import { AppManifestFragment, PermissionEnum } from "@dashboard/graphql";
+import { PermissionEnum } from "@dashboard/graphql";
 
 import { appManifestSchema } from "./app-manifest";
 
@@ -20,8 +20,8 @@ describe("App Manifest Schema", () => {
           {
             label: "My Extension",
             url: "https://example.com/extension",
-            mountName: "PRODUCT_OVERVIEW_CREATE",
-            targetName: "POPUP",
+            mount: "PRODUCT_OVERVIEW_CREATE",
+            target: "POPUP",
             permissions: [
               {
                 code: PermissionEnum.MANAGE_PRODUCTS,
@@ -50,7 +50,7 @@ describe("App Manifest Schema", () => {
             default: "",
           },
         },
-      } satisfies AppManifestFragment;
+      };
 
       // Act
       const result = appManifestSchema.safeParse(validData);
