0b242e4caad7011cc1d146e8a7a19a6e73a12bde
Mirek Mencel <mirek@mirumee.com>
2025-12-01 21:11:39 +0100

    Update order status and payment status pills (#6148)
    * Update order status and payment status pills
    
    * Introduce `attention` status
    * Fix color and statuses across the board
    
    * Refactor Order summary card to use Pills
    
    * Add changeset
    
    * Fix compatibility with MacauUI
    
    * Fix tests
    
    * De-gray the nautral to avoid association with fully inactive
    
    * Nudges: colors, paddings
    
    * Fix test so it checks weather the status gets the assigned color instead of checking the exact color
    
    * Fix linting

--- src/orders/components/OrderSummary/OrderChargeStatusBadge.tsx
+++ src/orders/components/OrderSummary/OrderChargeStatusBadge.tsx
@@ -1,6 +1,5 @@
+import { Pill } from "@dashboard/components/Pill";
 import { OrderChargeStatusEnum } from "@dashboard/graphql";
-import { getStatusColor } from "@dashboard/misc";
-import { Box, Text, useTheme } from "@saleor/macaw-ui-next";
 import { BanknoteIcon } from "lucide-react";
 import { useIntl } from "react-intl";
 
@@ -10,95 +9,41 @@ interface OrderChargeStatusBadgeProps {
 
 export const OrderChargeStatusBadge = ({ status }: OrderChargeStatusBadgeProps) => {
   const intl = useIntl();
-  const { theme: currentTheme } = useTheme();
-  const isDark = currentTheme === "defaultDark";
-
-  const successColors = getStatusColor({ status: "success", currentTheme });
-  const warningColors = getStatusColor({ status: "warning", currentTheme });
-  const transparentColors = {
-    background: "transparent",
-    border: isDark ? "hsla(0, 0%, 100%, 0.15)" : "hsla(0, 0%, 0%, 0.15)",
-    text: isDark ? "hsla(0, 0%, 100%, 0.7)" : "hsla(0, 0%, 0%, 0.7)",
-  };
 
   switch (status) {
     case OrderChargeStatusEnum.FULL:
       return (
-        <Box
-          display="flex"
-          gap={2}
-          alignItems="center"
-          __backgroundColor={successColors.base}
-          __borderColor={successColors.border}
-          __color={successColors.text}
-          borderStyle="solid"
-          borderWidth={1}
-          paddingX={2}
-          paddingY={1}
-          __maxWidth="max-content"
-          __borderRadius={500}
-        >
-          <BanknoteIcon size={16} />
-          <Text size={2} fontWeight="medium">
-            {intl.formatMessage({
-              defaultMessage: "Fully charged",
-              id: "Ynjq+C",
-            })}
-          </Text>
-        </Box>
+        <Pill
+          color="success"
+          icon={<BanknoteIcon size={16} />}
+          label={intl.formatMessage({
+            defaultMessage: "Fully charged",
+            id: "Ynjq+C",
+          })}
+        />
       );
 
     case OrderChargeStatusEnum.OVERCHARGED:
       return (
-        <Box
-          display="flex"
-          gap={2}
-          alignItems="center"
-          __backgroundColor={warningColors.base}
-          __borderColor={warningColors.border}
-          __color={warningColors.text}
-          borderStyle="solid"
-          borderWidth={1}
-          paddingX={2}
-          paddingY={1}
-          __maxWidth="max-content"
-          __borderRadius={500}
-        >
-          <BanknoteIcon size={16} />
-          <Text size={2} fontWeight="medium">
-            {intl.formatMessage({
-              defaultMessage: "Overcharged",
-              id: "8Cjxdt",
-            })}
-          </Text>
-        </Box>
+        <Pill
+          color="attention"
+          label={intl.formatMessage({
+            defaultMessage: "Overcharged",
+            id: "8Cjxdt",
+          })}
+        />
       );
 
     case OrderChargeStatusEnum.PARTIAL:
     case OrderChargeStatusEnum.NONE:
       return (
-        <Box
-          display="flex"
-          gap={2}
-          alignItems="center"
-          __backgroundColor={transparentColors.background}
-          __borderColor={transparentColors.border}
-          __color={transparentColors.text}
-          borderStyle="solid"
-          borderWidth={1}
-          paddingX={2}
-          paddingY={1}
-          __maxWidth="max-content"
-          __borderRadius={500}
-        >
-          <BanknoteIcon size={16} />
-          <Text size={2} fontWeight="medium">
-            {intl.formatMessage({
-              defaultMessage: "Not fully charged",
-              id: "X9tptP",
-            })}
-          </Text>
-        </Box>
+        <Pill
+          color="neutral"
+          label={intl.formatMessage({
+            defaultMessage: "Not fully charged",
+            id: "X9tptP",
+          })}
+        />
       );
 
     default:
