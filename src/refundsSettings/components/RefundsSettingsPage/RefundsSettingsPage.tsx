f694832bb8fc28ca8bf5b76e7684b6910978de58
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2025-12-03 16:29:42 +0100

    Reapply "Extract suspense in refund settings (#6143)" (#6159)
    This reverts commit ee0c63b144e6ef199a9eb5558761e4ed3c51adab.

--- src/refundsSettings/components/RefundsSettingsPage/RefundsSettingsPage.tsx
+++ src/refundsSettings/components/RefundsSettingsPage/RefundsSettingsPage.tsx
@@ -1,3 +1,4 @@
+import { useSuspenseQuery } from "@apollo/client/react/hooks";
 import { TopNav } from "@dashboard/components/AppLayout";
 import { DetailPageLayout } from "@dashboard/components/Layouts";
 import Link from "@dashboard/components/Link";
@@ -5,23 +6,25 @@ import PageSectionHeader from "@dashboard/components/PageSectionHeader";
 import { Savebar } from "@dashboard/components/Savebar";
 import { configurationMenuUrl } from "@dashboard/configuration";
 import {
+  ModelTypesQuery,
+  RefundSettingsQuery,
   useModelsOfTypeQuery,
-  useModelTypesQuery,
   useRefundReasonReferenceClearMutation,
-  useRefundSettingsQuery,
   useRefundSettingsUpdateMutation,
 } from "@dashboard/graphql";
 import useNavigator from "@dashboard/hooks/useNavigator";
 import useNotifier from "@dashboard/hooks/useNotifier";
 import { pageCreateUrl } from "@dashboard/modeling/urls";
-import { pageTypeAddUrl, pageTypeUrl } from "@dashboard/modelTypes/urls";
-import { refundsSettingsPageMessages } from "@dashboard/refundsSettings/components/RefundsSettingsPage/messages";
+import { pageTypeAddUrl } from "@dashboard/modelTypes/urls";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Box, Combobox, Skeleton, Text } from "@saleor/macaw-ui-next";
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { useIntl } from "react-intl";
 import { z } from "zod";
+
+import { modelTypes, refundsSettings } from "../../queries";
+import { refundsSettingsPageMessages } from "./messages";
 
 const formSchema = z.object({
   refundReasonReferenceType: z.string(),
@@ -29,16 +32,14 @@ const formSchema = z.object({
 
 type FormSchema = z.infer<typeof formSchema>;
 
-export const RefundsSettingsPage = () => {
-  const navigate = useNavigator();
-  const notify = useNotifier();
+interface ComboboxWithDataProps {
+  value: string;
+  onChange: (value: string | null) => void;
+}
+
+const ComboboxWithData = ({ value, onChange }: ComboboxWithDataProps) => {
   const intl = useIntl();
-
-  const { loading: settingsLoading, data: refundSettingsData } = useRefundSettingsQuery();
-
-  const refundRefModelTypeId = refundSettingsData?.refundSettings.reasonReferenceType?.id ?? null;
-
-  const { loading: modelTypesLoading, data: modelsList } = useModelTypesQuery();
+  const { data: modelsList } = useSuspenseQuery<ModelTypesQuery>(modelTypes);
 
   // TODO: Missing pagination, will fail if more than 100 types
   const modelTypesOptions = modelsList?.pageTypes?.edges
@@ -59,6 +60,18 @@ export const RefundsSettingsPage = () => {
       }),
     },
   ].concat(modelTypesOptions ?? []);
+
+  return <Combobox options={modelTypesOptionsWithEmptyValue} value={value} onChange={onChange} />;
+};
+
+const RefundsSettingsForm = () => {
+  const navigate = useNavigator();
+  const notify = useNotifier();
+  const intl = useIntl();
+
+  const { data: refundSettingsData } = useSuspenseQuery<RefundSettingsQuery>(refundsSettings);
+
+  const refundRefModelTypeId = refundSettingsData?.refundSettings.reasonReferenceType?.id ?? null;
 
   const { handleSubmit, getValues, setValue, watch } = useForm<FormSchema>({
     resolver: zodResolver(formSchema),
@@ -91,6 +104,7 @@ export const RefundsSettingsPage = () => {
       });
     },
   });
+
   const [clearReferenceType] = useRefundReasonReferenceClearMutation({
     onCompleted() {
       notify({
@@ -109,17 +123,11 @@ export const RefundsSettingsPage = () => {
 
   watch("refundReasonReferenceType");
 
-  const selectedTypeLabel = modelsList?.pageTypes?.edges.find(
-    edge => edge.node.id === currentRefundReasonReferenceType,
-  )?.node.name;
-
   const exampleModels = exampleModelData?.pages?.edges.map(edge => edge.node) ?? [];
-
-  const anythingIsLoading = settingsLoading || modelTypesLoading;
 
   useEffect(() => {
     setValue("refundReasonReferenceType", refundRefModelTypeId ?? "");
-  }, [refundRefModelTypeId]);
+  }, [refundRefModelTypeId, setValue]);
 
   const onSubmit = (values: FormSchema) => {
     if (values.refundReasonReferenceType) {
@@ -138,88 +146,89 @@ export const RefundsSettingsPage = () => {
   };
 
   return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)} id="refund-reason-settings-form">
+        <Box marginTop={6} __maxWidth="700px">
+          <Text fontWeight="medium" display="block" marginBottom={2}>
+            {intl.formatMessage(refundsSettingsPageMessages.selectLabel)}
+          </Text>
+          <Suspense fallback={<Skeleton height={8} />}>
+            <ComboboxWithData
+              value={currentRefundReasonReferenceType}
+              onChange={value => setValue("refundReasonReferenceType", value ?? "")}
+            />
+          </Suspense>
+          <Box marginTop={2}>
+            <Text color="default2">
+              {intl.formatMessage(refundsSettingsPageMessages.selectHelper)}{" "}
+            </Text>
+            <Link target={"_blank"} href={pageTypeAddUrl}>
+              <Text color="inherit">
+                {intl.formatMessage(refundsSettingsPageMessages.createModelTypeLink)}
+              </Text>
+            </Link>
+          </Box>
+        </Box>
+
+        <Savebar>
+          <Savebar.Spacer />
+          <Savebar.CancelButton onClick={() => navigate(configurationMenuUrl)} />
+          <Savebar.ConfirmButton
+            form="refund-reason-settings-form"
+            transitionState="default"
+            type="submit"
+          />
+        </Savebar>
+      </form>
+      {!!currentRefundReasonReferenceType && (
+        <Box marginTop={6}>
+          <Text fontWeight="medium" display="block" marginBottom={2}>
+            {intl.formatMessage(refundsSettingsPageMessages.previewTitle)}{" "}
+          </Text>
+          {exampleModels.length > 0 && (
+            <Box marginTop={4}>
+              <Box marginTop={6} display="flex" flexDirection="column">
+                {exampleModels.map(model => (
+                  <Text disabled key={model.id}>
+                    - {model.title}
+                  </Text>
+                ))}
+              </Box>
+            </Box>
+          )}
+          {exampleModels.length === 0 && (
+            <Text>
+              {intl.formatMessage(refundsSettingsPageMessages.emptyModels)}{" "}
+              <Link href={pageCreateUrl({ "page-type-id": currentRefundReasonReferenceType })}>
+                {intl.formatMessage(refundsSettingsPageMessages.createModelLink)}
+              </Link>
+            </Text>
+          )}
+        </Box>
+      )}
+    </>
+  );
+};
+
+export const RefundsSettingsPage = () => {
+  const intl = useIntl();
+
+  return (
     <DetailPageLayout gridTemplateColumns={1}>
       <TopNav
         href={configurationMenuUrl}
         title={intl.formatMessage(refundsSettingsPageMessages.pageTitle)}
       />
       <DetailPageLayout.Content>
-        <form onSubmit={handleSubmit(onSubmit)} id="refund-reason-settings-form">
-          <Box display="grid" __gridTemplateColumns="1fr 2fr 1fr" gap={6} paddingX={6}>
-            <PageSectionHeader
-              title={intl.formatMessage(refundsSettingsPageMessages.explainerTitle)}
-              description={intl.formatMessage(refundsSettingsPageMessages.explainerContent)}
-            />
-            <Box marginTop={6} __maxWidth="700px">
-              {anythingIsLoading ? (
-                <Skeleton />
-              ) : (
-                <>
-                  <Text fontWeight="medium" display="block" marginBottom={2}>
-                    {intl.formatMessage(refundsSettingsPageMessages.selectLabel)}
-                  </Text>
-                  <Combobox
-                    options={modelTypesOptionsWithEmptyValue}
-                    value={currentRefundReasonReferenceType}
-                    onChange={value => setValue("refundReasonReferenceType", value as string)}
-                  />
-                  <Box marginTop={2}>
-                    <Text color="default2">
-                      {intl.formatMessage(refundsSettingsPageMessages.selectHelper)}{" "}
-                    </Text>
-                    <Link target={"_blank"} href={pageTypeAddUrl}>
-                      <Text color="inherit">
-                        {intl.formatMessage(refundsSettingsPageMessages.createModelTypeLink)}
-                      </Text>
-                    </Link>
-                  </Box>
-                </>
-              )}
-            </Box>
-            {!!currentRefundReasonReferenceType && (
-              <Box marginTop={6}>
-                <Text fontWeight="medium" display="block" marginBottom={2}>
-                  {intl.formatMessage(refundsSettingsPageMessages.previewTitle)}{" "}
-                  <Link href={pageTypeUrl(currentRefundReasonReferenceType)}>
-                    {selectedTypeLabel}
-                  </Link>
-                </Text>
-                {exampleModels.length > 0 && (
-                  <Box marginTop={4}>
-                    <Box marginTop={6} display="flex" flexDirection="column">
-                      {exampleModels.map(model => (
-                        <Text disabled key={model.id}>
-                          - {model.title}
-                        </Text>
-                      ))}
-                    </Box>
-                  </Box>
-                )}
-                {exampleModels.length === 0 && (
-                  <Text>
-                    {intl.formatMessage(refundsSettingsPageMessages.emptyModels)}{" "}
-                    <Link
-                      href={pageCreateUrl({ "page-type-id": currentRefundReasonReferenceType })}
-                    >
-                      {intl.formatMessage(refundsSettingsPageMessages.createModelLink)}
-                    </Link>
-                  </Text>
-                )}
-              </Box>
-            )}
-          </Box>
-
-          <Savebar>
-            <Savebar.Spacer />
-            <Savebar.CancelButton onClick={() => navigate(configurationMenuUrl)} />
-            <Savebar.ConfirmButton
-              form="refund-reason-settings-form"
-              transitionState={anythingIsLoading ? "loading" : "default"}
-              disabled={anythingIsLoading}
-              type="submit"
-            />
-          </Savebar>
-        </form>
+        <Box display="grid" __gridTemplateColumns="1fr 2fr 1fr" gap={6} paddingX={6}>
+          <PageSectionHeader
+            title={intl.formatMessage(refundsSettingsPageMessages.explainerTitle)}
+            description={intl.formatMessage(refundsSettingsPageMessages.explainerContent)}
+          />
+          <Suspense>
+            <RefundsSettingsForm />
+          </Suspense>
+        </Box>
       </DetailPageLayout.Content>
     </DetailPageLayout>
   );
