438b1daf4de2d1235704bfaf5d533d35f81138db
Mirek Mencel <mirek@mirumee.com>
2025-12-15 09:22:08 -0100

    Refactor

--- src/orders/components/OrderCardTitle/StatusIndicator.tsx
+++ src/orders/components/OrderCardTitle/StatusIndicator.tsx
@@ -42,14 +42,57 @@ const IconCircle = ({ children, backgroundColor, borderColor }: IconCircleProps)
     alignItems="center"
     justifyContent="center"
     borderRadius="100%"
+    borderStyle="solid"
+    borderWidth={1}
     __width={CIRCLE_SIZE}
     __height={CIRCLE_SIZE}
     __backgroundColor={backgroundColor}
-    __border={`1px solid ${borderColor}`}
+    __borderColor={borderColor}
   >
     {children}
   </Box>
 );
+
+interface GetIconParams {
+  status: CardTitleStatus;
+  textColor: string;
+  currentTheme: "defaultLight" | "defaultDark";
+}
+
+const getIcon = ({ status, textColor, currentTheme }: GetIconParams): JSX.Element | null => {
+  const strokeWidth = 1.5;
+
+  switch (status) {
+    case FulfillmentStatus.FULFILLED:
+      return <PackageIcon color={textColor} size={ICON_SIZE} strokeWidth={strokeWidth} />;
+    case FulfillmentStatus.REFUNDED:
+      return <RefundedIcon size={ICON_SIZE} color={textColor} strokeWidth={strokeWidth} />;
+    case FulfillmentStatus.RETURNED:
+      return <ReturnedIcon size={ICON_SIZE} color={textColor} strokeWidth={strokeWidth} />;
+    case FulfillmentStatus.CANCELED:
+      return <EraserIcon color={textColor} size={ICON_SIZE} strokeWidth={strokeWidth} />;
+    case FulfillmentStatus.REPLACED:
+      return <ReplaceIcon color={textColor} size={ICON_SIZE} strokeWidth={strokeWidth} />;
+    case FulfillmentStatus.WAITING_FOR_APPROVAL:
+      return <SignatureIcon color={textColor} size={ICON_SIZE} strokeWidth={strokeWidth} />;
+    case FulfillmentStatus.REFUNDED_AND_RETURNED: {
+      const infoColors = getStatusColor({ status: "info", currentTheme });
+
+      return (
+        <Box display="flex" gap={1}>
+          <IconCircle backgroundColor={infoColors.base} borderColor={infoColors.border}>
+            <RefundedIcon size={ICON_SIZE} color={infoColors.text} strokeWidth={strokeWidth} />
+          </IconCircle>
+          <IconCircle backgroundColor={infoColors.base} borderColor={infoColors.border}>
+            <ReplaceIcon size={ICON_SIZE} color={infoColors.text} strokeWidth={strokeWidth} />
+          </IconCircle>
+        </Box>
+      );
+    }
+    default:
+      return null;
+  }
+};
 
 interface StatusIndicatorProps {
   status?: CardTitleStatus;
@@ -73,48 +116,15 @@ export const StatusIndicator = ({ status }: StatusIndicatorProps): JSX.Element |
   const ariaLabel = intl.formatMessage(messageForStatus);
   const { base, border, text } = getStatusColor({ status: pillType, currentTheme });
 
-  const getIcon = (): JSX.Element | null => {
-    const strokeWidth = 1.5;
-
-    switch (status) {
-      case FulfillmentStatus.FULFILLED:
-        return <PackageIcon color={text} size={ICON_SIZE} strokeWidth={strokeWidth} />;
-      case FulfillmentStatus.REFUNDED:
-        return <RefundedIcon size={ICON_SIZE} color={text} strokeWidth={strokeWidth} />;
-      case FulfillmentStatus.RETURNED:
-        return <ReturnedIcon size={ICON_SIZE} color={text} strokeWidth={strokeWidth} />;
-      case FulfillmentStatus.CANCELED:
-        return <EraserIcon color={text} size={ICON_SIZE} strokeWidth={strokeWidth} />;
-      case FulfillmentStatus.REPLACED:
-        return <ReplaceIcon color={text} size={ICON_SIZE} strokeWidth={strokeWidth} />;
-      case FulfillmentStatus.WAITING_FOR_APPROVAL:
-        return <SignatureIcon color={text} size={ICON_SIZE} strokeWidth={strokeWidth} />;
-      case FulfillmentStatus.REFUNDED_AND_RETURNED: {
-        const infoColors = getStatusColor({ status: "info", currentTheme });
-
-        return (
-          <Box display="flex" gap={1}>
-            <IconCircle backgroundColor={infoColors.base} borderColor={infoColors.border}>
-              <RefundedIcon size={ICON_SIZE} color={infoColors.text} strokeWidth={strokeWidth} />
-            </IconCircle>
-            <IconCircle backgroundColor={infoColors.base} borderColor={infoColors.border}>
-              <ReplaceIcon size={ICON_SIZE} color={infoColors.text} strokeWidth={strokeWidth} />
-            </IconCircle>
-          </Box>
-        );
-      }
-      default:
-        return null;
-    }
-  };
+  const icon = getIcon({ status, textColor: text, currentTheme });
 
   return (
     <Box marginLeft={2} display="flex" placeItems="center" aria-label={ariaLabel}>
       {status === FulfillmentStatus.REFUNDED_AND_RETURNED ? (
-        getIcon()
+        icon
       ) : (
         <IconCircle backgroundColor={base} borderColor={border}>
-          {getIcon()}
+          {icon}
         </IconCircle>
       )}
     </Box>
