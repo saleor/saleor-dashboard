f3089138710de48b61ac7fdfc6b1d78bdcdfffa1
Mirek Mencel <mirek@mirumee.com>
2025-12-21 21:06:04 -0100

    Fix i18n and consilidate common messages

--- src/hooks/useNotifier/useNotifier.tsx
+++ src/hooks/useNotifier/useNotifier.tsx
@@ -1,10 +1,14 @@
 import { INotification, INotificationCallback } from "@dashboard/components/notifications";
 import { DEFAULT_NOTIFICATION_SHOW_TIME } from "@dashboard/config";
+import { commonMessages } from "@dashboard/intl";
+import { useIntl } from "react-intl";
 import { toast } from "sonner";
 
 export type UseNotifierResult = INotificationCallback;
 
 function useNotifier(): UseNotifierResult {
+  const intl = useIntl();
+
   const notify = (options: INotification) => {
     const duration =
       options.status === "error" ? Infinity : (options.autohide ?? DEFAULT_NOTIFICATION_SHOW_TIME);
@@ -25,17 +29,17 @@ function useNotifier(): UseNotifierResult {
 
     switch (options.status) {
       case "success":
-        toast.success(options.title || "Success", toastOptions);
+        toast.success(options.title || intl.formatMessage(commonMessages.success), toastOptions);
         break;
       case "error":
-        toast.error(options.title || "Error", toastOptions);
+        toast.error(options.title || intl.formatMessage(commonMessages.error), toastOptions);
         break;
       case "warning":
-        toast.warning(options.title || "Warning", toastOptions);
+        toast.warning(options.title || intl.formatMessage(commonMessages.warning), toastOptions);
         break;
       case "info":
       default:
-        toast.info(options.title || "Info", toastOptions);
+        toast.info(options.title || intl.formatMessage(commonMessages.info), toastOptions);
         break;
     }
   };
