39768bf3c6bd9157ab709bbf57c4d851b1223f60
Mirek Mencel <mirek@mirumee.com>
2025-12-25 14:30:46 +0100

    Clean up the debug panel for toast notifications (moved to separate PR)

--- src/components/notifications/NotificationProvider.tsx
+++ src/components/notifications/NotificationProvider.tsx
@@ -2,7 +2,6 @@ import { PropsWithChildren, useMemo } from "react";
 import { toast, Toaster } from "sonner";
 
 import { INotificationContext, NotificationContext } from ".";
-import { ToastDebug } from "./ToastDebug";
 
 const NotificationProvider = ({ children }: PropsWithChildren) => {
   const context = useMemo<INotificationContext>(
@@ -22,7 +21,6 @@ const NotificationProvider = ({ children }: PropsWithChildren) => {
     <NotificationContext.Provider value={context}>
       {children}
       <Toaster position="top-right" expand={false} gap={8} visibleToasts={5} />
-      <ToastDebug />
     </NotificationContext.Provider>
   );
 };
