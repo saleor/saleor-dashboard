085f2f0631063b45132338a166ff8a5c89ffd7e6
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2026-02-11 18:51:44 +0100

    Add stories for new modals
    This reverts commit d5726544b90da0e4a57ba8043213054c76ff0637.

---
+++ src/components/AssignModelDialog/AssignModelDialog.stories.tsx
@@ -0,0 +1,68 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { PageFactory } from "@storybookUtils/AssignDialogShared/factories";
+import { withMockedFilters } from "@storybookUtils/AssignDialogShared/storyDecorators";
+import { ComponentProps } from "react";
+import { fn } from "storybook/test";
+
+import AssignModelDialog from "./AssignModelDialog";
+
+type Props = ComponentProps<typeof AssignModelDialog>;
+
+const meta: Meta<typeof AssignModelDialog> = {
+  title: "Components/Dialogs/AssignModelDialog",
+  component: AssignModelDialog,
+  decorators: [withMockedFilters],
+  loaders: [async () => ({ pages: await PageFactory.buildList(8) })],
+  render: (args: Props, { loaded }: { loaded: { pages: Props["pages"] } }) => (
+    <AssignModelDialog {...args} pages={args.pages ?? loaded.pages} />
+  ),
+  argTypes: {
+    confirmButtonState: {
+      control: "inline-radio",
+      options: ["default", "loading", "success", "error"],
+    },
+    onClose: { table: { disable: true } },
+    onFetchMore: { table: { disable: true } },
+    onSubmit: { table: { disable: true } },
+    onFilterChange: { table: { disable: true } },
+    pages: { table: { disable: true } },
+    excludedFilters: { table: { disable: true } },
+    initialConstraints: { table: { disable: true } },
+    labels: { table: { disable: true } },
+  },
+  args: {
+    open: true,
+    loading: false,
+    hasMore: false,
+    confirmButtonState: "default",
+    onClose: fn(),
+    onFetchMore: fn(),
+    onSubmit: fn(),
+    onFilterChange: fn(),
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof AssignModelDialog>;
+
+export const Default: Story = {};
+
+export const SingleSelection: Story = {
+  args: { selectionMode: "single" },
+};
+
+export const Loading: Story = {
+  args: { loading: true, pages: [] },
+};
+
+export const Empty: Story = {
+  args: { pages: [] },
+};
+
+export const WithLockedPageType: Story = {
+  args: {
+    initialConstraints: {
+      pageTypes: [{ id: "page-type-1", name: "Blog Post" }],
+    },
+  },
+};
