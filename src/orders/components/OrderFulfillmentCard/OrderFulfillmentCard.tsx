747dff0af8f343e34954fb8b64b10e69b4ad2233
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2026-02-13 11:59:00 +0100

    format fulfillment reason

--- src/orders/components/OrderFulfillmentCard/OrderFulfillmentCard.tsx
+++ src/orders/components/OrderFulfillmentCard/OrderFulfillmentCard.tsx
@@ -144,22 +144,15 @@ export const OrderFulfillmentCard = (props: OrderFulfillmentCardProps) => {
       />
       {(fulfillment.reason || fulfillment.reasonReference) && (
         <Box paddingX={6} paddingY={4} backgroundColor="default2">
-          {fulfillment.reasonReference && (
-            <Box marginBottom={fulfillment.reason ? 1 : 0}>
-              <Text size={2} color="default2">
-                Reason type:
-              </Text>{" "}
-              <Text size={2}>{fulfillment.reasonReference.title}</Text>
-            </Box>
-          )}
-          {fulfillment.reason && (
-            <Box>
-              <Text size={2} color="default2">
-                Reason:
-              </Text>{" "}
-              <Text size={2}>{fulfillment.reason}</Text>
-            </Box>
-          )}
+          <Text size={2}>
+            {fulfillment.reasonReference && (
+              <Text as="span" size={2} fontWeight="bold">
+                {fulfillment.reasonReference.title}
+                {fulfillment.reason ? ": " : ""}
+              </Text>
+            )}
+            {fulfillment.reason}
+          </Text>
         </Box>
       )}
       <DashboardCard.Content paddingX={0}>
