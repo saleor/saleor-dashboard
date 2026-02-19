5c6dc74467d63a588a176b9ae3d3411b33fbbc9c
IKarbowiak <iga.karbowiak@saleor.io>
2026-02-19 14:02:05 +0100

    Add useLegacyUpdateWebhookEmission to shop settings section

--- src/fragments/shop.ts
+++ src/fragments/shop.ts
@@ -56,5 +56,6 @@ export const shopFragment = gql`
     reserveStockDurationAuthenticatedUser
     limitQuantityPerCheckout
     enableAccountConfirmationByEmail
+    useLegacyUpdateWebhookEmission
   }
 `;
