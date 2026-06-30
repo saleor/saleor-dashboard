c0c25dab2faced9eaed9ac979d48cb8dcec2955a
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2026-06-30 12:53:56 +0200

    feat: open in GraphiQL on voucher detail page

--- src/discounts/queries.ts
+++ src/discounts/queries.ts
@@ -242,3 +242,12 @@ export const PromotionDetailsQuery = /* GraphQL */ `
     }
   }
 `;
+
+export const voucherGraphiQLQuery = `query VoucherDetails($id: ID!) {
+  voucher(id: $id) {
+    id
+    name
+    type
+    discountValueType
+  }
+}`;
