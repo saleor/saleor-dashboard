859fe5a3a4822bf957df5148db338b2c6497a2bb
Mirek Mencel <mirek@saleor.io>
2026-07-05 19:43:35 +0200

    Migrate order confirmation dialogs and relabel transaction void action

---
+++ src/orders/components/OrderPaymentVoidDialog/messages.ts
@@ -0,0 +1,14 @@
+import { defineMessages } from "react-intl";
+
+export const orderPaymentVoidDialogMessages = defineMessages({
+  title: {
+    id: "KszPFx",
+    defaultMessage: "Void Payment",
+    description: "dialog header",
+  },
+  description: {
+    id: "MqF/Kg",
+    defaultMessage: "Are you sure you want to void this payment?",
+    description: "dialog content",
+  },
+});
