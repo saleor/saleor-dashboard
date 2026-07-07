8dea65f0c610332e4b2d626caca3933c4faae057
Mirek Mencel <mirek@saleor.io>
2026-07-07 19:15:40 +0200

    Fix splitSubmitErrors typing for product save error handling

--- src/products/utils/splitSubmitErrors.ts
+++ src/products/utils/splitSubmitErrors.ts
@@ -1,31 +1,58 @@
 import {
+  type AttributeErrorFragment,
+  type MetadataErrorFragment,
   type ProductChannelListingErrorFragment,
-  type ProductErrorFragment,
+  type ProductErrorWithAttributesFragment,
+  type UploadErrorFragment,
 } from "@dashboard/graphql";
+import getAttributeErrorMessage from "@dashboard/utils/errors/attribute";
 import getProductErrorMessage from "@dashboard/utils/errors/product";
 import { type IntlShape } from "react-intl";
 
-export type ProductSubmitError = ProductErrorFragment | ProductChannelListingErrorFragment;
+export type ProductSubmitError =
+  | ProductErrorWithAttributesFragment
+  | ProductChannelListingErrorFragment;
 
-type SplittableSubmitError = {
+type ProductSplittableSubmitError = {
   __typename?: string;
 };
 
 const isProductChannelListingError = (
-  error: SplittableSubmitError,
+  error: ProductSplittableSubmitError,
 ): error is ProductChannelListingErrorFragment => error.__typename === "ProductChannelListingError";
 
-const isInlineProductSubmitError = (error: SplittableSubmitError): boolean =>
-  isProductChannelListingError(error) || error.__typename === "DatagridError";
+const isProductError = (
+  error: ProductSplittableSubmitError,
+): error is ProductErrorWithAttributesFragment => error.__typename === "ProductError";
 
-export const splitProductSubmitErrors = <T extends SplittableSubmitError>(errors: T[]) => {
-  const productErrors: T[] = [];
+const isAttributeError = (error: ProductSplittableSubmitError): error is AttributeErrorFragment =>
+  error.__typename === "AttributeError";
+
+const isUploadError = (error: ProductSplittableSubmitError): error is UploadErrorFragment =>
+  error.__typename === "UploadError";
+
+const isMetadataError = (error: ProductSplittableSubmitError): error is MetadataErrorFragment =>
+  error.__typename === "MetadataError";
+
+const isDatagridSubmitError = (error: ProductSplittableSubmitError): boolean =>
+  error.__typename === "DatagridError";
+
+const isInlineProductSubmitError = (error: ProductSplittableSubmitError): boolean =>
+  isProductChannelListingError(error) || isDatagridSubmitError(error);
+
+export const splitProductSubmitErrors = (
+  errors: ProductSplittableSubmitError[],
+): {
+  productErrors: ProductErrorWithAttributesFragment[];
+  channelsErrors: ProductChannelListingErrorFragment[];
+} => {
+  const productErrors: ProductErrorWithAttributesFragment[] = [];
   const channelsErrors: ProductChannelListingErrorFragment[] = [];
 
   errors.forEach(error => {
     if (isProductChannelListingError(error)) {
       channelsErrors.push(error);
-    } else if (!isInlineProductSubmitError(error)) {
+    } else if (isProductError(error)) {
       productErrors.push(error);
     }
   });
@@ -33,13 +60,32 @@ export const splitProductSubmitErrors = <T extends SplittableSubmitError>(errors
   return { productErrors, channelsErrors };
 };
 
+const getNotificationMessageForSubmitError = (
+  error: ProductSplittableSubmitError,
+  intl: IntlShape,
+): string | undefined => {
+  if (isProductError(error) || isProductChannelListingError(error)) {
+    return getProductErrorMessage(error, intl);
+  }
+
+  if (isAttributeError(error)) {
+    return getAttributeErrorMessage(error, intl);
+  }
+
+  if (isUploadError(error) || isMetadataError(error)) {
+    return error.message ?? undefined;
+  }
+
+  return undefined;
+};
+
 export const getProductSubmitErrorNotificationMessages = (
-  errors: SplittableSubmitError[],
+  errors: ProductSplittableSubmitError[],
   intl: IntlShape,
 ): string[] => {
   const messages = errors
     .filter(error => !isInlineProductSubmitError(error))
-    .map(error => getProductErrorMessage(error, intl))
+    .map(error => getNotificationMessageForSubmitError(error, intl))
     .filter((message): message is string => Boolean(message));
 
   return [...new Set(messages)];
